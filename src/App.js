0a48d8251a101fd2085965c1c2bdcdc8817e175f
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-07-17 18:40:03 +0300

    prepared

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import './App.css';
-
-import users from './api/users';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Static list of todos</h1>
-
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
     </div>
   );
 }
