e5c000264100616d7c89aedc6c6ca184b5c33ec5
Nastia Krat <nastyakpi1995@gmail.com>
2019-07-18 19:47:39 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,16 +1,120 @@
 import React from 'react';
 import './App.css';
-import TodoList from './TodoList';
-import todos from './api/todos';
-// import users from './api/users';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Static list of todos</h1>
-      <TodoList currentTodos={todos} />
-    </div>
-  );
+import todos from './api/todos';
+import users from './api/users';
+import TodoList from './TodoList';
+
+class App extends React.Component {
+    state = { todos };
+
+  addTodo = (todo) => {
+    this.setState(prevState => ({
+      todos: [
+        ...prevState.todos,
+        {
+          userName: todo.userName,
+          title: todo.title,
+          id: prevState.todos.length + 1,
+          completed: false,
+        },
+      ],
+    }));
+  }
+
+  render() {
+    // const { todos } = this.state;
+
+    return (
+      <div className="App">
+        <NewTodo
+          users={users}
+          onSubmit={this.addTodo}
+        />
+        <h1>Static list of todos</h1>
+        <p>
+          <span>Users: </span>
+          {users.length}
+        </p>
+        <TodoList todos={this.state.todos} />
+      </div>
+    );
+  }
 }
+
+class NewTodo extends React.Component {
+  state = {
+    todo: {
+      title: '',
+    },
+  }
+
+  handleFieldChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState(prevState => ({
+      todo: {
+        ...prevState.todo,
+        [name]: value,
+      },
+    }));
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const { todo } = this.state;
+    const { onSubmit } = this.props;
+
+    onSubmit(todo);
+    this.setState(prevState => ({
+      todo: {
+        ...prevState.todo,
+        userName: '',
+        title: '',
+      },
+    }));
+  }
+
+  render() {
+    const { todo } = this.state;
+
+    return (
+      <fieldset>
+        <legend>Add TODOs</legend>
+        <form
+          className="form"
+          onSubmit={this.handleFormSubmit}
+        >
+          <label
+            className="new-todo"
+            htmlFor="new-todo-title"
+          >
+            <span>Please, enter a title </span>
+            <input
+              id="new-todo-title"
+              placeholder="Title"
+              value={todo.title}
+              name="title"
+              type="text"
+              onChange={this.handleFieldChange}
+            />
+          </label>
+          <button
+            className="btn-add"
+            type="submit"
+          >
+            Add
+          </button>
+        </form>
+      </fieldset>
+    );
+  }
+}
+
+NewTodo.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,23 +1,35 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import './App.css';
-import PropTypes from 'prop-types';
-import User from './User';
 
-const TodoList = ({ currentTodos }) => (
-  <table>
-    {currentTodos.map(todo => (
-      <User user={todo} key={todo.id} />
-    ))}
+const TodoList = ({ todos }) => (
+  <table className="todo-list">
+    <thead>
+      <tr className="todo-list__row">
+        <th>Id</th>
+        <th>Name</th>
+        <th>Title</th>
+        <th>Completed</th>
+      </tr>
+    </thead>
+    <tbody>
+      {todos.map(todo => (
+        <tr
+          key={todo.id}
+          className="todo-list__row"
+        >
+          <td>{todo.id}</td>
+          <td>{todo.userName}</td>
+          <td>{todo.title}</td>
+          <td>{todo.completed || 'x'}</td>
+        </tr>
+      ))}
+    </tbody>
   </table>
 );
 
 TodoList.propTypes = {
-  currentTodos: PropTypes.arrayOf({
-    todo: PropTypes.shape({
-      userPrepare: PropTypes.object.isRequired,
-    }).isRequired,
-    i: PropTypes.number.isRequired,
-  }).isRequired,
+  todos: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default TodoList;
