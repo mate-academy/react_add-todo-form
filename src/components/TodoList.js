a806f61034ffbf21b0f18cf23b1d87dd78de3c41
Ira <kalmykova_ira@ukr.net>
2019-07-18 12:36:55 +0300

    Solution 1-6

---
+++ src/components/TodoList.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import '../App.css';
+
+const TodoList = ({ todos }) => (
+  <table className="todo-list">
+    <thead>
+      <tr className="todo-list__row">
+        <th>Id</th>
+        <th>Name</th>
+        <th>Title</th>
+      </tr>
+    </thead>
+    <tbody>
+      {todos.map(todo => (
+        <tr
+          key={todo.id}
+          className="todo-list__row"
+        >
+          <td>{todo.id}</td>
+          <td>{todo.userName}</td>
+          <td>{todo.title}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default TodoList;
