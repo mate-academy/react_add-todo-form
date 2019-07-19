701962ca8b95773a66aa095a051128757fc72053
Serhii <davydko.serhiy@gmail.com>
2019-07-19 11:55:26 +0300

    add other files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
 import React from 'react';
+import TodoList from './TodoList'
+import Modal from './Modal'
 import './App.css';
 
 import users from './api/users';
-import todos from './api.todos';
+import todos from './api/todos';
 
 class App extends React.Component {
   state = {
-    todos: [],
+    todosData: [],
+    usersData: [],
+    isOpen: false,
+    idCounter: 0,
+
+    valuesMap: {
+      task: '',
+      person: '',
+    },
+    errorsMap: {
+      task: '',
+      person: '',
+    },
+
+  }
+
+  componentDidMount() {
+    this.setState({
+      usersData: users,
+      idCounter: Math.max(...todos.map(item => item.id), 0),
+      todosData: todos.map(todo => ({
+          ...todo,
+        user: users.find(user => (user.id === todo.userId)),
+      })),
+    })
+  }
+
+  submitForm = (event) => {
+    console.log(this.state.todosData);
+    event.preventDefault();
+    console.log(event.target);
+
+    const errorsMap = {};
+
+    this.setState(prevState => {
+      if (!prevState.valuesMap.task) {
+        errorsMap.task = 'Please, write what shoult be done'
+      }
+
+     if (!prevState.valuesMap.person) {
+        errorsMap.person = 'Chose executor from the list'
+      }
+
+     if (Object.keys(errorsMap).length > 0) {
+        return { errorsMap };
+      }
+
+      return {todosData: [...prevState.todosData, {
+        id: prevState.idCounter +1,
+        title: prevState.valuesMap.task,
+        completed: false,
+        user: JSON.parse(prevState.valuesMap.person),
+      }],
+      valuesMap: {},
+      }
+
+    })
+  }
+
+  handleFieldChange = (event) => {
+    const {name, value} = event.target;
+    this.setState(prevState =>({
+      valuesMap: {...prevState.valuesMap, [name]:value, },
+      errorsMap: {},
+    }))
+  }
+
+  toggleModal = () => {
+    this.setState( {
+      isOpen: true,
+    });
+  }
+
+  toggleModalClose = () => {
+    this.setState({
+      isOpen: false,
+      errorsMap: {},
+    })
   }
 
   render() {
+
     return (
       <div className="App">
-        <h1>Static list of todos</h1>
+        <header>
+        <h1>List of todos</h1>
+        <button className ="btn" type="button" onClick={this.toggleModal}> Add new item</button>
+        </header>
+        {this.state.isOpen &&  < Modal
+          show={this.state.isOpen}
+          toggleModalClose ={this.toggleModalClose}
+          usersData={this.state.usersData}
+          submitForm= {this.submitForm}
+          errorsMap = {this.state.errorsMap}
+          handleFieldChange = {this.handleFieldChange} />}
 
-        <p>
-          <span>Users: </span>
-          {users.length}
-        </p>
-      </div>
+          <TodoList todo={this.state.todosData} />
+
+    </div>
     );
 
   }
---
+++ src/Modal.js
@@ -0,0 +1,95 @@
+import React from 'react';
+
+class Modal extends React.Component {
+
+  componentDidMount() {
+    this.nameInput.focus();
+  }
+
+  render() {
+    const { toggleModalClose, usersData, submitForm, handleFieldChange, errorsMap } = this.props
+
+    const backdropStyle = {
+      position: 'fixed',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: 'rgba(0,0,0,0.3)',
+      padding: 50
+    };
+
+    const modalStyle = {
+      backgroundColor: '#fff',
+      borderRadius: 5,
+      maxWidth: 600,
+      minHeight: 45,
+      margin: '0 auto',
+      padding: 30
+    };
+
+    return (
+      <div className="backdrop" style={backdropStyle} >
+        <div className="modal" style={modalStyle}>
+
+          <button className="btn btn-close" onClick={toggleModalClose}>close</button>
+
+          <form onSubmit={submitForm} className="imput-container" >
+
+            <div className="input-field">
+              {errorsMap.task && (
+                <div className="error" style={{ color: 'red' }}>
+                  {errorsMap.task}
+                </div>
+              )}
+              <input
+                ref={(input) => { this.nameInput = input }}
+                type="search"
+                autocomplete="off"
+                name="task"
+                placeholder="Enter task"
+                onChange={handleFieldChange}
+              />
+
+
+            </div>
+
+            <div className="select-field">
+
+              {errorsMap.person && (
+                <div className="error" style={{ color: 'red' }}>
+                  {errorsMap.person}
+                </div>
+              )}
+
+              <select onChange={handleFieldChange} name="person">
+                <option value=''>Chose executor from list</option>
+                {usersData.map(user => (
+                  <option value={JSON.stringify(user)}>{user.name}</option>
+                ))}
+              </select>
+
+
+
+
+            </div>
+
+            <button
+              type="submit"
+              name="add-item"
+              className="subm btn"
+            >
+              Add task
+            </button>
+
+          </form>
+
+        </div>
+      </div>
+    )
+
+  }
+
+}
+
+export default Modal
