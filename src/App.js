b2fe83e8056b3f71993322fb7e7dd2b604d42930
Kamran <djkamry@gmail.com>
2019-07-23 16:07:01 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
 import React from 'react';
 import './App.css';
 
+import NewTodos from './NewTodos';
 import users from './api/users';
+import todos from './api/todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Static list of todos</h1>
+class App extends React.Component {
+  state = {
+    todos: todos.map(todo => ({
+      ...todo,
+      user: users.find(u => u.id === todo.userId),
+    })),
+  }
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+  handleFormSubmit = (currentTitle, currentUserId) => {
+    this.setState(state => ({
+      todos: [
+        ...state.todos,
+        {
+          userId: +currentUserId,
+          user: users.find(u => u.name === currentUserId),
+          id: state.todos.length + 1,
+          title: currentTitle,
+          completed: false,
+        },
+      ],
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="add-todo-form">
+          <NewTodos
+            onFormSubmit={this.handleFormSubmit}
+          />
+        </div>
+        <div className="users-info">
+          {this.state.todos.map(todo => (
+
+            <div className="single-user">
+              <div>
+                ID:
+                {todo.id}
+              </div>
+              <div>
+                Title:
+                {todo.title}
+              </div>
+              <div>
+                Name:
+                {todo.user.name}
+              </div>
+              <div>
+                Email:
+                {todo.user.email}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/NewTodos.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TodoList from './TodoList';
+
+import users from './api/users';
+
+class NewTodos extends React.Component {
+  state= {
+    currentValue: '',
+    currentUser: '',
+    errorUser: false,
+    errorValue: false,
+  };
+
+  handleSubmit = (event) => {
+    const {
+      currentUser, currentValue,
+    } = this.state;
+
+    event.preventDefault();
+    if (currentUser && currentValue) {
+      this.props.onFormSubmit(currentValue, currentUser);
+      this.setState({
+        currentUser: '',
+        currentValue: '',
+        errorUser: false,
+        errorValue: false,
+      });
+    } else if (!currentUser && !currentValue) {
+      this.setState({
+        errorUser: true,
+        errorValue: true,
+      });
+    } else if (!currentUser || currentValue) {
+      this.setState({
+        errorUser: true,
+        errorValue: false,
+      });
+    } else if (currentUser || !currentValue) {
+      this.setState({
+        errorUser: false,
+        errorValue: true,
+      });
+    }
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      currentValue: event.target.value,
+      errorValue: false,
+    });
+  };
+
+  selectChange = (event) => {
+    this.setState({
+      currentUser: event.target.value,
+      errorUser: false,
+    });
+  };
+
+  render() {
+    const {
+      currentUser, currentValue, errorUser, errorValue,
+    } = this.state;
+
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <p>JUST DO IT!</p>
+          <div className={errorValue && 'no-title__error'}>
+            <input
+              name="title"
+              placeholder="Title"
+              type="text"
+              onChange={this.handleChange}
+              value={currentValue}
+            />
+
+          </div>
+          <div className={errorUser && 'no-selected-user__error'}>
+            <select
+              name="users"
+              onChange={this.selectChange}
+              value={currentUser}
+            >
+              <option
+                value=""
+              >
+              ---Choose user---
+              </option>
+              {users.map(person => (
+                <option
+                  key={person.id}
+                  value={person.name}
+                >
+                  {person.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button type="submit">Save</button>
+        </form>
+
+        <TodoList currentUser={currentUser} currentValue={currentValue} />
+      </>
+    );
+  }
+}
+
+NewTodos.propTypes = {
+  onFormSubmit: PropTypes.func.isRequired,
+};
+
+export default NewTodos;
