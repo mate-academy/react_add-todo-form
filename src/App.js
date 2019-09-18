5ed42648750aebf0f6c902f3352bd8d6cf6b65eb
VladyslavSak <vladyslav.sak@gmail.com>
2019-09-18 19:59:22 +0300

    Start coding

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './App.css';
 
+import { getTodosWithUsers } from './api/data';
 import users from './api/users';
+import todos from './api/todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Static list of todos</h1>
+import TodoList from './components/TodoList/TodoList';
+import NewTodos from './components/NewTodos/NewTodos';
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+const preparedTodos = getTodosWithUsers(todos, users);
+
+const App = () => (
+  <>
+    <h1 className="todos__title">Todos for different users</h1>
+
+    <div className="todo-content">
+      <TodoList todos={preparedTodos} />
+      <NewTodos />
     </div>
-  );
-}
+  </>
+);
 
 export default App;
---
+++ src/components/NewTodos/NewTodos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NewTodos.css';
+
+const NewTodos = () => (
+  <div className="todos__form">
+    <form>
+      {/* <select></select> */}
+    </form>
+  </div>
+);
+
+export default NewTodos;
