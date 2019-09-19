8ed0dd9481cf7ef1ec793704b0cfb5c3b9e9b354
Alex Andrutskii <alex.andrutskii@gmail.com>
2019-09-19 17:17:07 +0300

    Changes according to comments

--- src/App.js
+++ src/App.js
@@ -13,62 +13,6 @@ class App extends React.Component {
   state = {
     usersList: [...users],
     todosList: [...todos],
-    selectedUser: 0,
-    inputTodoValue: '',
-    inputTodoError: false,
-    selectUserError: false,
-  }
-
-  addTodo = (event) => {
-    event.preventDefault();
-    const { inputTodoValue, selectedUser, todosList } = this.state;
-
-    if (inputTodoValue && selectedUser !== 0) {
-      this.setState({
-        todosList: [
-          ...todosList,
-          {
-            userId: Number(selectedUser),
-            id: todosList.length + 1,
-            title: inputTodoValue,
-            completed: false,
-          },
-        ],
-        selectedUser: 0,
-        inputTodoValue: '',
-      });
-    } else {
-      this.setState({
-        selectUserError: selectedUser === 0,
-        inputTodoError: !inputTodoValue,
-      });
-    }
-  }
-
-  onChangeSelectOfUser = ({ target: { value } }) => {
-    if (value !== 0) {
-      this.setState({
-        selectedUser: Number(value),
-        selectUserError: false,
-      });
-    } else {
-      this.setState({
-        selectedUser: Number(value),
-      });
-    }
-  }
-
-  onChangeInputOfTodo = ({ target: { value } }) => {
-    if (value !== '') {
-      this.setState({
-        inputTodoValue: value,
-        inputTodoError: false,
-      });
-    } else {
-      this.setState({
-        inputTodoValue: value,
-      });
-    }
   }
 
   getTodosWithUsers = (todosList, usersList) => (
@@ -78,14 +22,24 @@ class App extends React.Component {
     }))
   )
 
+  updatedTodosList = (newTodo) => {
+    const { todosList } = this.state;
+
+    this.setState({
+      todosList: [
+        ...todosList,
+        {
+          id: todosList.length + 1,
+          ...newTodo,
+        },
+      ],
+    });
+  }
+
   render() {
     const {
       usersList,
       todosList,
-      selectedUser,
-      inputTodoValue,
-      inputTodoError,
-      selectUserError,
     } = this.state;
     const preparedTodos = this.getTodosWithUsers(todosList, usersList);
 
@@ -94,13 +48,8 @@ class App extends React.Component {
         <Header usersCount={usersList.length} todosCount={todosList.length} />
         <NewTodo
           usersList={usersList}
-          selectedUser={selectedUser}
           addTodo={this.addTodo}
-          inputTodoValue={inputTodoValue}
-          onChangeSelectOfUser={this.onChangeSelectOfUser}
-          onChangeInputOfTodo={this.onChangeInputOfTodo}
-          inputTodoError={inputTodoError}
-          selectUserError={selectUserError}
+          updatedTodosList={this.updatedTodosList}
         />
         <TodoList preparedTodos={preparedTodos} />
       </>
--- src/components/NewTodo/NewTodo.js
+++ src/components/NewTodo/NewTodo.js
@@ -1,91 +1,150 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const NewTodo = ({
-  usersList,
-  selectedUser,
-  addTodo,
-  inputTodoValue,
-  onChangeSelectOfUser,
-  onChangeInputOfTodo,
-  inputTodoError,
-  selectUserError,
-}) => {
-  const inputTodoClass = classNames(
-    'form-control',
-    {
-      'is-invalid': inputTodoError,
-    }
-  );
-  const selectUserClass = classNames(
-    'form-control',
-    {
-      'is-invalid': selectUserError,
-    }
-  );
+class NewTodo extends React.Component {
+  state = {
+    selectedUser: 0,
+    inputTodoValue: '',
+    inputTodoError: false,
+    selectUserError: false,
+  }
 
-  return (
-    <div className="container mb-5">
-      <div className="row">
-        <form className="col-lg-6 mx-auto" onSubmit={addTodo}>
-          <div className="form-group row">
-            <label
-              className="col-sm-3 col-form-label"
-              htmlFor="todo-text"
-            >
-              Todo text:
-            </label>
-            <div className="col-sm-9">
-              <input
-                type="text"
-                value={inputTodoValue}
-                onChange={onChangeInputOfTodo}
-                id="todo-text"
-                className={inputTodoClass}
-              />
-              <div className="invalid-feedback">
-                Please enter the title
+  onChangeSelectOfUser = ({ target: { value } }) => {
+    if (value !== 0) {
+      this.setState({
+        selectedUser: Number(value),
+        selectUserError: false,
+      });
+    } else {
+      this.setState({
+        selectedUser: Number(value),
+      });
+    }
+  }
+
+  onChangeInputOfTodo = ({ target: { value } }) => {
+    if (value !== '') {
+      this.setState({
+        inputTodoValue: value,
+        inputTodoError: false,
+      });
+    } else {
+      this.setState({
+        inputTodoValue: value,
+      });
+    }
+  }
+
+  addTodo = (event) => {
+    event.preventDefault();
+    const { inputTodoValue, selectedUser } = this.state;
+    const { updatedTodosList } = this.props;
+
+    if (inputTodoValue && selectedUser !== 0) {
+      const newTodo = {
+        userId: Number(selectedUser),
+        title: inputTodoValue,
+        completed: false,
+      };
+      this.setState({
+        selectedUser: 0,
+        inputTodoValue: '',
+      });
+      updatedTodosList(newTodo);
+    } else {
+      this.setState({
+        selectUserError: selectedUser === 0,
+        inputTodoError: !inputTodoValue,
+      });
+    }
+  }
+
+  render() {
+    const {
+      usersList,
+    } = this.props;
+    const {
+      selectedUser,
+      inputTodoValue,
+      inputTodoError,
+      selectUserError,
+    } = this.state;
+    const inputTodoClass = classNames(
+      'form-control',
+      {
+        'is-invalid': inputTodoError,
+      }
+    );
+    const selectUserClass = classNames(
+      'form-control',
+      {
+        'is-invalid': selectUserError,
+      }
+    );
+
+    return (
+      <div className="container mb-5">
+        <div className="row">
+          <form className="col-lg-6 mx-auto" onSubmit={this.addTodo}>
+            <div className="form-group row">
+              <label
+                className="col-sm-3 col-form-label"
+                htmlFor="todo-text"
+              >
+                Todo text:
+              </label>
+              <div className="col-sm-9">
+                <input
+                  type="text"
+                  value={inputTodoValue}
+                  onChange={this.onChangeInputOfTodo}
+                  id="todo-text"
+                  className={inputTodoClass}
+                />
+                <div className="invalid-feedback">
+                  Please enter the title
+                </div>
               </div>
             </div>
-          </div>
-          <div className="form-group row">
-            <label
-              className="col-sm-3 col-form-label"
-              htmlFor="todo-user"
-            >
-              User:
-            </label>
-            <div className="col-sm-9">
-              <select
-                value={selectedUser}
-                onChange={onChangeSelectOfUser}
-                className={selectUserClass}
-                id="todo-user"
+            <div className="form-group row">
+              <label
+                className="col-sm-3 col-form-label"
+                htmlFor="todo-user"
               >
-                <option key="Choose a user" value={0}>Choose a user</option>
-                {usersList.map(user => (
-                  <option key={user.id} value={user.id}>{user.name}</option>
-                ))}
-              </select>
-              <div className="invalid-feedback">
-                Please choose a user
+                User:
+              </label>
+              <div className="col-sm-9">
+                <select
+                  value={selectedUser}
+                  onChange={this.onChangeSelectOfUser}
+                  className={selectUserClass}
+                  id="todo-user"
+                >
+                  <option key="Choose a user" value={0}>Choose a user</option>
+                  {usersList.map(user => (
+                    <option key={user.id} value={user.id}>{user.name}</option>
+                  ))}
+                </select>
+                <div className="invalid-feedback">
+                  Please choose a user
+                </div>
               </div>
             </div>
-          </div>
-          <div className="form-group row justify-content-end">
-            <div className="col-sm-9 text-right">
-              <button
-                type="submit"
-                className="btn btn-primary btn-block"
-              >
-                Add
-              </button>
+            <div className="form-group row justify-content-end">
+              <div className="col-sm-9 text-right">
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-block"
+                >
+                  Add
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default NewTodo;
