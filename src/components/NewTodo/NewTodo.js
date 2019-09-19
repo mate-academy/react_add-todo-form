e19a7107c3aa4f25dcab32d129a4cf0454dfe272
alexDemche <alexeyydemchenko@gmail.com>
2019-09-19 13:06:22 +0300

    remove unnecessary comments in NewTodo.js

--- src/components/NewTodo/NewTodo.js
+++ src/components/NewTodo/NewTodo.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import users from '../../api/users';
 
 export default class NewTodo extends Component {
   state = {
