fe74115cd78b46e787d399a305c134ac488d914d
Volodsher <volodsher@gmail.com>
2019-09-23 10:13:16 +0300

    20%

--- src/App.js
+++ src/App.js
@@ -2,18 +2,45 @@ import React from 'react';
 import './App.css';
 
 import users from './api/users';
+import todos from './api/todos';
+import { getTodosWithUsers } from './getTodos';
+import TodoList from './components/TodoList/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Static list of todos</h1>
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    const preparedTodos = getTodosWithUsers(todos, users);
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+    return (
+      <div className="App">
+        <h1>Static list of todos</h1>
+        <p>
+          <span>Users: </span>
+          {users.length}
+        </p>
+        <input type="text" />
+        <TodoList todos={preparedTodos} />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// import React from 'react';
+// import './App.css';
+
+// import todos from './api/todos';
+// import users from './api/users';
+// import TodoList from './components/TodoList/TodoList';
+// import { getTodosWithUsers } from './getTodos';
+
+// function App() {
+//   const preparedTodos = getTodosWithUsers(todos, users);
+
+//   return (
+//     <TodoList todos={preparedTodos} />
+//   );
+// }
+
+// export default App;
