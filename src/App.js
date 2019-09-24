f5f62897fc3344da7a9025f61d855023ee18e27a
Volodsher <volodsher@gmail.com>
2019-09-24 16:34:03 +0300

    40%

--- src/App.js
+++ src/App.js
@@ -3,44 +3,107 @@ import './App.css';
 
 import users from './api/users';
 import todos from './api/todos';
-import { getTodosWithUsers } from './getTodos';
+// import { getTodosWithUsers } from './getTodos';
 import TodoList from './components/TodoList/TodoList';
+// import { tsConstructorType } from '@babel/types';
 
-// eslint-disable-next-line react/prefer-stateless-function
+function getTodosWithUsers(todosArr, usersArr) {
+  return todosArr.map((todo) => {
+    const todoUser = usersArr.find(user => user.id === todo.userId);
+
+    return {
+      ...todo,
+      user: todoUser,
+    };
+  });
+}
+
 class App extends React.Component {
+  state = {
+    newTodos: [...todos],
+    newUsers: [...users],
+    text: '',
+    selectedOption: '',
+  }
+
+  handleSelect = (event) => {
+    this.setState({
+      selectedOption: event.target.value,
+    });
+    //
+  }
+
+  addTodo = (todo) => {
+    this.setState(prevState => ({
+      newTodos: [todo, ...prevState.newTodos],
+    }));
+  }
+
+  // addTodo = todo => {
+  //   this.setState({
+  //     todos: [...this.state.todos, todo],
+  //   })
+  // }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  // handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   this.onSubmit({
+  //     userId:
+  //     text: this.state.text,
+  //     complete: false,
+  //   });
+  // }
+
   render() {
-    const preparedTodos = getTodosWithUsers(todos, users);
+    const preparedTodos = getTodosWithUsers(
+      this.state.newTodos, this.state.newUsers
+    );
 
     return (
       <div className="App">
-        <h1>Static list of todos</h1>
         <p>
           <span>Users: </span>
-          {users.length}
+          {this.state.newUsers.length}
         </p>
-        <input type="text" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.handleChange}
+            placeholder="add a new task"
+          />
+          <select
+            className="select"
+            name="good-length"
+            id="selectedLength"
+            onChange={this.handleSelect}
+            value={this.state.selectedOption}
+          >
+            {this.state.newUsers.map(user => (
+              <option value={user.id}>{user.name}</option>))
+            }
+          </select>
+
+          <button
+            type="submit"
+            onClick={this.addTodo}
+          >
+            AddTodo
+          </button>
+        </form>
+
         <TodoList todos={preparedTodos} />
+        {/* {JSON.stringify(this.state.newTodos)} */}
       </div>
     );
   }
 }
 
 export default App;
-
-// import React from 'react';
-// import './App.css';
-
-// import todos from './api/todos';
-// import users from './api/users';
-// import TodoList from './components/TodoList/TodoList';
-// import { getTodosWithUsers } from './getTodos';
-
-// function App() {
-//   const preparedTodos = getTodosWithUsers(todos, users);
-
-//   return (
-//     <TodoList todos={preparedTodos} />
-//   );
-// }
-
-// export default App;
