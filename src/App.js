4c242f22f09416abc8ec7c0ea62e74de4a84fcc2
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2019-10-31 10:26:51 +0200

    sol

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import './App.css';
 
-import users from './api/users';
+import usersApi from './api/users';
+import todosApi from './api/todos';
+import TodoList from './components/todoList/TodoList';
+import AddTodo from './components/addTodo/AddTodo';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Static list of todos</h1>
+class App extends React.Component {
+  state = {
+    todosData: this.getTodosData(todosApi, usersApi),
+  };
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+  getTodosData(todos, users) {
+    return todos.map(todo => ({
+      ...todo,
+      user: users.find(user => user.id === todo.userId),
+    }));
+  }
+
+  addTodo = (todoTitle, userName) => {
+    this.setState((prevState) => {
+      return ({
+        ...prevState,
+        todosData: [...prevState.todosData, {
+          id: prevState.todosData.length + 1,
+          title: todoTitle,
+          completed: false,
+          user: usersApi.find(user => user.name === userName),
+        }],
+      });
+    });
+  };
+
+  render() {
+    const passingDataForTodoList = this.state.todosData;
+
+    return (
+      <>
+        <AddTodo users={usersApi} addTodoFunction={this.addTodo} />
+        <TodoList todosData={passingDataForTodoList} />
+      </>
+    );
+  }
 }
 
 export default App;
