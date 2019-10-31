089e02872c371df3ec394ca23b0be1674bb8f588
Dmitriymush <powder322@gmail.com>
2019-10-31 04:32:32 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -2,18 +2,89 @@ import React from 'react';
 import './App.css';
 
 import users from './api/users';
+import todos from "./api/todos";
+import TodoList from "./Components/TodoList";
+import NewToDo from "./Components/NewToDo";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Static list of todos</h1>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+    this.state = {
+      todolist: [...todos],
+      users: [...users],
+      currentUser: 0,
+      currentValue: '',
+      isCurrentUserValid: null,
+      isValidToDo: null,
+    };
+  }
+
+  selectUser = (event) => {
+    const userIndex = event.target.value;
+    this.setState( (prevState) => {
+      if (+userIndex === 0) {
+        return {
+          ...prevState,
+          currentUser: +userIndex,
+          isCurrentUserValid: false,
+        };
+      }
+      return {
+        ...prevState,
+        currentUser: userIndex,
+        isCurrentUserValid: true,
+      };
+    });
+  };
+
+  changeInput = (event) => {
+    const value = event.target.value;
+    this.setState((PrevState) => ({
+      ...PrevState,
+      currentValue: value,
+      isValidToDo: null,
+    }));
+  };
+
+  addItem = (event) => {
+    event.preventDefault();
+    if (!this.state.isCurrentUserValid) return;
+    if (!this.state.currentValue.match(/\w/g)) {
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          isValidToDo: false,
+          currentValue: '',
+        };
+      });
+      return;
+    }
+
+    const newToDo = {
+      userId: +this.state.currentUser,
+      title: this.state.currentValue,
+      completed: this.state.currentValue === 'todo nothing' || this.state.currentValue === 'nothing todo' || this.state.currentValue === 'nothing',
+      id: Math.max(...this.state.todolist.map(todo => todo.id)) + 1,
+    };
+
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        todolist: [...prevState.todolist, newToDo],
+        currentValue: '',
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <NewToDo users={this.state.users} selectUser={this.selectUser} changeInput={this.changeInput} value={this.state.currentValue} valid={this.state.isCurrentUserValid} addItem={this.addItem}/>
+        <TodoList users={this.state.users} todos={this.state.todolist} />
+      </div>
+    );
+  }
 }
 
 export default App;
