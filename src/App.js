e80ea248d8b80df5e5d5cd5dffaeceb53610c84c
Anna Kazantseva <annetastara@gmail.com>
2019-11-06 12:47:37 +0200

    Fixed props and state

--- src/App.js
+++ src/App.js
@@ -13,16 +13,67 @@ function usersListTodo(todosArr, usersArr) {
 
 const listOfUsers = usersListTodo(todos, users);
 
-function App() {
-  return (
-    <div className="todo_list">
-      <h1>LIST OF TODOS</h1>
-      <span>ToDo: </span>
-      <Form
-        listOfUsers={listOfUsers}
-      />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    usersSelected: users,
+    selectUser: '',
+    taskUser: '',
+    error: '',
+    name: listOfUsers.map(item => item.user.name),
+    task: listOfUsers.map(item => item.title),
+
+  };
+
+  addToDo = (event) => {
+    this.setState({
+      taskUser: event.target.value,
+    });
+  };
+
+  addUser = (event) => {
+    this.setState({
+      selectUser: event.target.value,
+    });
+  };
+
+  buttonClick = (event) => {
+    event.preventDefault();
+    if (this.state.selectUser.length > 3 && this.state.taskUser.length > 0) {
+      this.setState(prevState => ({
+        name: [...prevState.name, prevState.selectUser],
+        task: [...prevState.task, prevState.taskUser],
+        selectUser: '',
+        taskUser: '',
+        error: '',
+      }));
+    } else {
+      this.setState({
+        error: 'type task and choose person',
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="todo_list">
+        <h1>LIST OF TODOS</h1>
+        <span>ToDo: </span>
+        <Form
+          state={this.state}
+          listOfUsers={listOfUsers}
+          usersSelected={this.state.usersSelected}
+          selectUser={this.state.selectUser}
+          taskUser={this.state.taskUser}
+          error={this.state.error}
+          name={this.state.name}
+          task={this.state.task}
+          buttonClick={this.buttonClick}
+          addUser={this.addUser}
+          addToDo={this.addToDo}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
+
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,70 +2,42 @@ import React from 'react';
 import users from '../../api/users';
 import Todolist from '../Todolist/Todolist';
 
-class Form extends React.Component {
-  state = {
-    usersSelected: users,
-    selectUser: '',
-    taskUser: '',
-    error: '',
-    name: this.props.listOfUsers.map(item => item.user.name),
-    task: this.props.listOfUsers.map(item => item.title),
 
-  };
+function Form(props) {
+  const {state,
+    listOfUsers,
+    usersSelected,
+    selectUser,
+    taskUser,
+    error,
+    name,
+    task,
+    buttonClick,
+    addUser,
+    addToDo} = props;
 
-  addToDo = (event) => {
-    this.setState({
-      taskUser: event.target.value,
-    });
-  };
-
-  addUser = (event) => {
-    this.setState({
-      selectUser: event.target.value,
-    });
-  };
-
-  buttonClick = (event) => {
-    event.preventDefault();
-    if (this.state.selectUser.length > 3 && this.state.taskUser.length > 0) {
-      this.setState(prevState => ({
-        name: [...prevState.name, prevState.selectUser],
-        task: [...prevState.task, prevState.taskUser],
-        selectUser: '',
-        taskUser: '',
-        error: '',
-      }));
-      } else {
-      this.setState({
-        error: 'type task and choose person',
-      });
-    }
-  };
-
-  render() {
     return (
-      <form onSubmit={this.buttonClick} className="ui form">
-        <input className="field" type="text" onChange={this.addToDo} value={this.state.taskUser} className="input"/>
-        <span>{this.state.error}</span>
+      <form onSubmit={buttonClick} className="ui form">
+        <input className="field" type="text" onChange={addToDo} value={taskUser} className="input"/>
+        <span>{error}</span>
         <p>
           <span>Users:</span>
           <select
-            onChange={this.addUser}
-            value={this.state.selectUser}
+            onChange={addUser}
+            value={selectUser}
             role="listbox" aria-expanded="false" className="ui fluid selection dropdown" tabIndex="0">
             <option>Choose a person</option>
-            {this.state.usersSelected.map(item => <option>{item.name}</option>)}
+            {usersSelected.map(item => <option>{item.name}</option>)}
           </select>
-          <span>{this.state.error}</span>
+          <span>{error}</span>
           <button type="submit" className="ui primary button">
             Add
           </button>
         </p>
-        <Todolist state={this.state} />
+        <Todolist state={state} />
       </form>
 
     );
-  }
 }
 
 export default Form;
