b155a0791e7f7740ed802ac1ad3376157841d2a3
helmax-y <max_rad@ukr.net>
2019-12-11 00:24:37 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import './App.css';
-
+import './App.scss';
+import todos from './api/todos';
 import users from './api/users';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Static list of todos</h1>
+class App extends React.Component {
+  state = { todos: [...todos] }
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+  addNew = (userId, title, completed) => {
+    this.setState(state => ({
+      todos: [
+        ...state.todos,
+        {
+          userId,
+          id: state.todos.length + 1,
+          title,
+          completed,
+        },
+      ],
+    }));
+  }
+
+  render() {
+    return (
+      <>
+        <h1>List of todos</h1>
+        <article className="todos">
+          <TodoList todos={this.state.todos} />
+          <NewTodo users={users} addNew={this.addNew} />
+        </article>
+      </>
+    );
+  }
 }
 
 export default App;
