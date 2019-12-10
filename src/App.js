7ed84d494b8e12a1abac655304bd220bd3c0b1b9
Alexandra Boyko <Alexboyko04@gmail.com>
2019-12-10 23:07:20 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-
+import todos from './api/todos';
 import users from './api/users';
+import NewTodo from './api/NewTodo';
 
 function App() {
   return (
     <div className="App">
       <h1>Static list of todos</h1>
-
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+      <NewTodo
+        todos={todos}
+        users={users}
+      />
     </div>
   );
 }
