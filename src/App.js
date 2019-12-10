ba72a8aeafc37e67bb6b3b92c24ee791fca4da19
Denys <denys.cheporniuk@gmail.com>
2019-12-10 18:55:02 +0200

    fix form

--- src/App.js
+++ src/App.js
@@ -10,63 +10,29 @@ class App extends React.Component {
   state = {
     users: [...users],
     todoList: [...todos],
-    newText: '',
-    newUser: 0,
-    error: '',
   }
 
-  maxTextLen = 25;
-
-  handleNewText = (event) => {
-    if (event.target.value.length >= this.maxTextLen) {
-      this.setState({ newText: event.target.value.slice(0, this.maxTextLen) });
-    } else {
-      this.setState({ newText: event.target.value });
-    }
-  }
-
-  handleNewUser = (event) => {
-    this.setState({ newUser: event.target.value });
-  }
-
-  setNewTodo = (event) => {
-    event.preventDefault();
-
-    if (!this.state.newText) {
-      this.setState({ error: 'Please enter the title' });
-    } else if (!this.state.newUser) {
-      this.setState({ error: 'Please choose a user' });
-    } else {
-      this.setState(state => ({
-        todoList: [...state.todoList, {
-          title: state.newText,
-          id: state.todoList.length + 1,
-          userId: +state.newUser,
-        }],
-        newText: '',
-        error: '',
-      }));
-    }
-  }
+  addTodo = todo => (
+    this.setState(state => ({
+      todoList: [...state.todoList, {
+        ...todo,
+        id: state.todoList.length + 1,
+      }],
+    }))
+  )
 
   render() {
     return (
       <div className="App">
         <h1>Static list of todos</h1>
-        <form onSubmit={this.setNewTodo}>
-          <TodoList
-            todoList={this.state.todoList}
-            users={this.state.users}
-          />
-          <NewTodo
-            users={this.state.users}
-            newText={this.state.newText}
-            error={this.state.error}
-            newUser={this.state.newUser}
-            handleText={this.handleNewText}
-            setUser={this.handleNewUser}
-          />
-        </form>
+        <TodoList
+          todoList={this.state.todoList}
+          users={this.state.users}
+        />
+        <NewTodo
+          addTodo={this.addTodo}
+          users={this.state.users}
+        />
       </div>
     );
   }
--- src/NewTodo.js
+++ src/NewTodo.js
@@ -1,51 +1,88 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const NewTodo = ({ users, newText, handleText, setUser, error }) => (
-  <>
-    <input
-      type="text"
-      value={newText}
-      onChange={handleText}
-      className="todo__input"
-      placeholder="New TODO"
-    />
-    <span className="todo__text">
-      Select User
-    </span>
-    <select
-      onChange={setUser}
-      className="todo__users"
-    >
-      <option value="">
-        {''}
-      </option>
-      {users.map(user => (
-        <option
-          key={user.name}
-          value={user.id}
+class NewTodo extends React.Component {
+  state = {
+    newText: '',
+    newUser: 0,
+    error: '',
+  };
+
+  maxTextLen = 25;
+
+  handleNewText = (event) => {
+    if (event.target.value.length >= this.maxTextLen) {
+      this.setState({ newText: event.target.value.slice(0, this.maxTextLen) });
+    } else {
+      this.setState({ newText: event.target.value });
+    }
+  }
+
+  handleNewUser = (event) => {
+    this.setState({ newUser: event.target.value });
+  }
+
+  validateForm = (event) => {
+    event.preventDefault();
+
+    if (!this.state.newText) {
+      this.setState({ error: 'Please enter the title' });
+    } else if (!this.state.newUser) {
+      this.setState({ error: 'Please choose a user' });
+    } else {
+      this.props.addTodo({
+        title: this.state.newText,
+        userId: +this.state.newUser,
+      });
+      this.setState({ newText: '' });
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.validateForm}>
+        <input
+          type="text"
+          value={this.state.newText}
+          onChange={this.handleNewText}
+          className="todo__input"
+          placeholder="New TODO"
+        />
+        <span className="todo__text">
+          Select User
+        </span>
+        <select
+          onChange={this.handleNewUser}
+          className="todo__users"
         >
-          {user.name}
-        </option>
-      ))}
-    </select>
-    <span className="todo__error">
-      {error}
-    </span>
-    <input
-      type="submit"
-      value="Add"
-      className="todo__button"
-    />
-  </>
-);
+          <option value="">
+            {''}
+          </option>
+          {this.props.users.map(user => (
+            <option
+              key={user.name}
+              value={user.id}
+            >
+              {user.name}
+            </option>
+          ))}
+        </select>
+        <span className="todo__error">
+          {this.state.error}
+        </span>
+        <input
+          type="submit"
+          value="Add"
+          className="todo__button"
+        />
+      </form>
+    );
+  }
+}
 
 NewTodo.propTypes = {
   users: PropTypes.arrayOf.isRequired,
-  newText: PropTypes.string.isRequired,
-  handleText: PropTypes.func.isRequired,
-  setUser: PropTypes.func.isRequired,
-  error: PropTypes.string.isRequired,
+  addTodo: PropTypes.func.isRequired,
 };
 
 export default NewTodo;
