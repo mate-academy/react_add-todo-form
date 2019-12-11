ae539b49b20e4fc6ceeec0f20b87655fb414507a
bandura <banduraweb@ukr.net>
2019-12-12 01:37:06 +0200

    solution

---
+++ src/Components/TodoList.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import UserList from './UsersList';
+
+export default class TodoList extends Component {
+  getUserById(id) {
+    return this.props.users.find(item => item.id === id);
+  }
+
+  render() {
+    const { items: lists } = this.props;
+    const alllists = lists.map(list => (
+      <div className="row" key={list.title}>
+        <div className="col s12 m5">
+          <div className="card blue-grey darken-1 wrap-img">
+            <img
+              className="img"
+              src="img/5a991c82be987_thumb900.png"
+              alt=""
+            />
+            <div className="card-content white-text">
+              <span className="card-title">Todos Card</span>
+              <p>
+USER ID:
+                {list.userId}
+              </p>
+              <p>
+                <i className="fas fa-tasks" />
+                {' '}
+                {list.title}
+              </p>
+              <p>
+                <i className="fas fa-sync-alt" />
+                {' '}
+                {list.completed === false ? 'not completed' : 'completed'}
+              </p>
+              <UserList usersList={this.getUserById(list.userId)} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+    ));
+
+    return (
+      <div>
+        {alllists}
+      </div>
+    );
+  }
+}
+
+TodoList.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.object).isRequired,
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
---
+++ src/api/listOfTodosCards.js
@@ -0,0 +1,12 @@
+// import todos from './todos';
+// import users from './users';
+//
+//  const listOfTodosCards = todos.map(item=> {
+//    return {
+//      ...item,
+//      user: users.find(user => user.id === item.userId),
+//    }
+//  });
+//
+//
+// export default listOfTodosCards;
