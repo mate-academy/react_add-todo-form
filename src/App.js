8586a90d9a89a9b3b358b9d486a551e035b1abdd
Vvxforce <vvxforce@gmail.com>
2019-12-12 13:33:28 +0200

    solutionnn

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import React from 'react';
-import PropTypes from 'prop-types';
 import users from './api/users';
 import todos from './api/todos';
+import NewTodo from './addNewTodo';
+import TodoList from './TodoList';
 
 const todosWithUsers = todos.map(todo => ({
   ...todo,
@@ -37,117 +38,5 @@ class App extends React.Component {
     );
   }
 }
-
-class NewTodo extends React.Component {
-  state = {
-    inputValue: '',
-    selectValue: 0,
-    inputError: false,
-    selectError: false,
-  };
-
-  handleInputValue = (element) => {
-    this.setState({
-      inputValue: element.target.value,
-      inputError: false,
-    });
-  };
-
-  handleSelectValue = (element) => {
-    this.setState({
-      selectValue: element.target.value,
-      selectError: false,
-    });
-  };
-
-  handleFormSubmit = (element) => {
-    element.preventDefault();
-
-    const { inputValue, selectValue } = this.state;
-
-    if (!inputValue || !selectValue) {
-      this.setState({
-        inputError: !inputValue,
-        selectError: !selectValue,
-      });
-
-      return;
-    }
-
-    this.props.addNewTodo(inputValue, selectValue);
-
-    this.setState({
-      inputValue: '',
-      selectValue: 0,
-    });
-  };
-
-  render() {
-    const { inputValue, selectValue, inputError, selectError } = this.state;
-
-    return (
-      <form onSubmit={this.handleFormSubmit}>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={this.handleInputValue}
-          placeholder="write title"
-        />
-
-        <div className="error">{inputError && 'need title'}</div>
-        <div>
-          <select
-            value={selectValue}
-            onChange={this.handleSelectValue}
-          >
-            <option value="0">
-            Choose a user
-            </option>
-            {users.map(user => (
-              <option key={user.id}>{user.name}</option>
-            ))}
-          </select>
-          <div className="error">{selectError && 'need User'}</div>
-        </div>
-        <button type="submit">
-          Add
-        </button>
-      </form>
-    );
-  }
-}
-
-class TodoList extends React.PureComponent {
-  render() {
-    const { addTodos } = this.props;
-
-    return (
-      <div>
-        <ul>
-          {addTodos.map(text => (
-            <li
-              key={text.id}
-            >
-              Title:
-              {text.title}
-              <p>
-              User:
-                {text.user.name}
-              </p>
-              <hr />
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
-
-TodoList.propTypes = {
-  addTodos: PropTypes.arrayOf(PropTypes.shape({
-  })).isRequired,
-};
-
-NewTodo.propTypes = { addNewTodo: PropTypes.func.isRequired };
 
 export default App;
