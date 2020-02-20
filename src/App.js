d03fb8490c4ffb9d84e2771f0fb3b36ba36b4bb4
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2020-02-20 16:50:52 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { NewTodo } from './NewTodo';
+import { TodoList } from './TodoList';
 import './App.css';
 
 import users from './api/users';
+import todos from './api/todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const todoWithUsers = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId),
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+const uuid = require('uuid/v1');
+
+class App extends Component {
+  state = {
+    todos: [...todoWithUsers],
+  }
+
+  addNewTodo = (title, userName) => {
+    this.setState(prevState => ({
+      todos: [
+        ...prevState.todos,
+        {
+          title,
+          id: uuid(),
+          user: { name: userName },
+        },
+      ],
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Add Todo Form</h1>
+        <NewTodo users={users} addNewTodo={this.addNewTodo} />
+        <div className="todo">
+          <TodoList todos={this.state.todos} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
