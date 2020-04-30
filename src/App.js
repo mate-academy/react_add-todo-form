30b3031fdbb3d67faa92ac2d144a2cd639d811cc
Oleg <fasfas7971@gmail.com>
2020-04-30 15:22:21 +0300

    Fix comm

--- src/App.js
+++ src/App.js
@@ -12,110 +12,37 @@ class App extends React.Component {
       ...todo,
       executant: users.find(user => user.id === todo.userId),
     })),
-
-    currentValue: '',
-    currentChooseUserValue: '',
-    currentStatus: '',
-    hiddenHint: false,
   }
 
   changeStatusComplete = (todoId) => {
-    const whoWillBeChange = this.state.todos
-      .find(todo => todo.id === todoId);
-
-    whoWillBeChange.completed = !whoWillBeChange.completed;
-    const prevTodos = this.state.todos.slice(0, todoId - 1);
-    const nextTodos = this.state.todos.slice(todoId);
-    const newTodosList = [];
-
-    if (prevTodos.length >= 1) {
-      newTodosList.push(...prevTodos);
-    }
-
-    newTodosList.push(whoWillBeChange);
-
-    if (nextTodos.length >= 1) {
-      newTodosList.push(...nextTodos);
-    }
+    const { todos } = this.state;
 
     this.setState(() => ({
-      todos: newTodosList,
+      todos: todos.map(todo => ({
+        ...todo,
+        completed: todo.id === todoId
+          ? !todo.completed
+          : todo.completed,
+      })),
     }));
   }
 
-  newTask = (event) => {
-    this.setState({ currentValue: event.target.value });
-  }
-
-  chooseUserName = (event) => {
-    const selectUser = users
-      .find(user => user.id === +event.target.value);
-
-    this.setState({ currentChooseUserValue: selectUser.id });
-  }
-
-  chooseStatusTodo = (event) => {
-    if (event.target.value === 'true') {
-      this.setState({ currentStatus: true });
-    } else {
-      this.setState({ currentStatus: false });
-    }
-  }
-
-  addNewTodo = () => {
-    const {
-      currentValue, currentChooseUserValue,
-      currentStatus, todos,
-    } = this.state;
-    const idForTask = todos.length + 1;
-    let newCreateTask;
-
-    if ((typeof currentValue === 'string'
-      && currentValue.length >= 4)
-      && (typeof currentChooseUserValue === 'number')
-      && (typeof currentStatus === 'boolean')) {
-      newCreateTask = {
-        userId: currentChooseUserValue,
-        id: idForTask,
-        title: currentValue,
-        completed: currentStatus,
-        executant: users
-          .find(user => user.id === currentChooseUserValue),
-      };
-    } else {
-      this.setState(state => ({
-        hiddenHint: true,
-      }));
-    }
-
-    if (newCreateTask) {
-      this.setState(state => ({
-        todos: [...state.todos, newCreateTask],
-        currentValue: '',
-        currentChooseUserValue: '',
-        currentStatus: '',
-        hiddenHint: false,
-      }));
-    }
+  changeTodo = (todo) => {
+    this.setState(() => ({ todos: todo }));
   }
 
   render() {
-    const { todos,
-      currentValue, currentChooseUserValue,
-      currentStatus, hiddenHint } = this.state;
+    const { todos, hiddenHint } = this.state;
 
     return (
       <div className="App todo">
         <TodoForm
-          currentValue={currentValue}
-          newTask={this.newTask}
-          currentChooseUserValue={currentChooseUserValue}
+          hiddenHint={hiddenHint}
           chooseUserName={this.chooseUserName}
           users={users}
-          currentStatus={currentStatus}
-          chooseStatusTodo={this.chooseStatusTodo}
-          hiddenHint={hiddenHint}
           addNewTodo={this.addNewTodo}
+          todos={todos}
+          changeTodo={this.changeTodo}
         />
         <TodoList
           todos={todos}
--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,85 +1,173 @@
 import React from 'react';
-import PropTypes, { object } from 'prop-types';
 
-const TodoForm = ({
-  currentValue, newTask, currentChooseUserValue, chooseUserName,
-  users, currentStatus, chooseStatusTodo, hiddenHint, addNewTodo,
-}) => (
-  <form
-    className="todo__form form"
-    onSubmit={e => e.preventDefault()}
-  >
-    <div className="form__wrap">
-      <label htmlFor="newTask">Write a new task: </label>
-      <input
-        type="text"
-        id="newTask"
-        value={currentValue}
-        onChange={e => newTask(e)}
-      />
-    </div>
-    <div className="form__wrap">
-      <label htmlFor="chooseUser">Choose a user: </label>
-      <select
-        id="chooseUser"
-        value={currentChooseUserValue}
-        onChange={e => chooseUserName(e)}
+class TodoForm extends React.Component {
+  state = {
+    currentValue: '',
+    currentChooseUserValue: '',
+    currentStatus: '',
+    errorInput: false,
+    errorSelect: false,
+  }
+
+  newTask = (event) => {
+    const { errorInput, currentValue } = this.state;
+
+    this.setState({ currentValue: event.target.value });
+
+    if (errorInput && currentValue.length >= 3) {
+      this.setState({ errorInput: false });
+    }
+  }
+
+  chooseStatusTodo = (event) => {
+    if (event.target.value === 'true') {
+      this.setState({ currentStatus: true });
+    } else {
+      this.setState({ currentStatus: false });
+    }
+  }
+
+  chooseUserName = (event) => {
+    const selectUser = this.props.users
+      .find(user => user.id === +event.target.value);
+
+    this.setState({ currentChooseUserValue: selectUser.id });
+  }
+
+  addNewTodo = () => {
+    const {
+      currentValue, currentChooseUserValue,
+      currentStatus,
+    } = this.state;
+    const { todos, users, changeTodo } = this.props;
+    const idForTask = todos[todos.length - 1].id + 1;
+    let newCreateTask;
+
+    if ((typeof currentValue === 'string'
+      && currentValue.length >= 4)
+      && (typeof currentChooseUserValue === 'number')
+      && (typeof currentStatus === 'boolean')) {
+      newCreateTask = {
+        userId: currentChooseUserValue,
+        id: idForTask,
+        title: currentValue,
+        completed: currentStatus,
+        executant: users
+          .find(user => user.id === currentChooseUserValue),
+      };
+    } else {
+      this.setState(() => ({
+        hiddenHint: true,
+      }));
+    }
+
+    if (newCreateTask) {
+      this.setState(() => ({
+        currentValue: '',
+        currentChooseUserValue: '',
+        currentStatus: '',
+        hiddenHint: false,
+      }));
+      changeTodo([...todos, newCreateTask]);
+    }
+
+    if (!newCreateTask && (typeof currentValue !== 'string'
+      || currentValue.length <= 3)) {
+      this.setState(() => ({ errorInput: true }));
+    }
+
+    if (!newCreateTask && (typeof currentChooseUserValue !== 'number'
+      || (typeof currentStatus !== 'boolean'))) {
+      this.setState(() => ({ errorSelect: true }));
+    }
+  }
+
+  checkSelectError = () => {
+    const { currentChooseUserValue, currentStatus, errorSelect } = this.state;
+
+    if (errorSelect && currentChooseUserValue !== ''
+      && currentStatus !== '') {
+      this.setState(() => ({ errorSelect: false }));
+    }
+  }
+
+  render() {
+    const { users } = this.props;
+    const {
+      currentValue, currentChooseUserValue, currentStatus, errorSelect, errorInput,
+    } = this.state;
+
+    this.checkSelectError();
+
+    return (
+      <form
+        className="todo__form form"
+        onSubmit={e => e.preventDefault()}
       >
-        <option value="" hidden>
-          Choose here
-        </option>
-        {users.map(user => (
-          <option
-            value={user.id}
-            key={user.id}
+        <div className="form__wrap">
+          <label htmlFor="newTask">Write a new task: </label>
+          <input
+            type="text"
+            id="newTask"
+            value={currentValue}
+            onChange={this.newTask}
+          />
+        </div>
+        <div className="form__wrap">
+          <label htmlFor="chooseUser">Choose a user: </label>
+          <select
+            id="chooseUser"
+            value={currentChooseUserValue}
+            onChange={this.chooseUserName}
           >
-            {user.name}
-          </option>
-        ))}
-      </select>
-    </div>
-    <div className="form__wrap">
-      <label htmlFor="chooseStatus">Select status for new todo: </label>
-      <select
-        id="chooseStatus"
-        value={`${currentStatus}`}
-        onChange={e => chooseStatusTodo(e)}
-      >
-        <option value="" hidden>Choose here</option>
-        <option value="true">Done</option>
-        <option value="false">In process</option>
-      </select>
-    </div>
-    <button
-      type="button"
-      onClick={() => addNewTodo()}
-    >
-      Add todo
-    </button>
-    {hiddenHint
-      ? (
-        <p className="form__hint">
-          Please, write 4 or more symbols
-          {' '}
-          <br />
-          {' '}
-          and choose all parameters!
-        </p>
-      )
-      : <p />}
-  </form>
-);
-
-TodoForm.propTypes = {
-  currentValue: PropTypes.string.isRequired,
-  currentChooseUserValue: PropTypes.number.isRequired,
-  chooseUserName: PropTypes.func.isRequired,
-  users: PropTypes.arrayOf(object).isRequired,
-  hiddenHint: PropTypes.bool.isRequired,
-  newTask: PropTypes.func.isRequired,
-  currentStatus: PropTypes.bool.isRequired,
-  chooseStatusTodo: PropTypes.func.isRequired,
-  addNewTodo: PropTypes.func.isRequired,
-};
+            <option value="" hidden>
+              Choose here
+            </option>
+            {users.map(user => (
+              <option
+                value={user.id}
+                key={user.id}
+              >
+                {user.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form__wrap">
+          <label htmlFor="chooseStatus">Select status for new todo: </label>
+          <select
+            id="chooseStatus"
+            value={`${currentStatus}`}
+            onChange={this.chooseStatusTodo}
+          >
+            <option value="" hidden>Choose here</option>
+            <option value="true">Done</option>
+            <option value="false">In process</option>
+          </select>
+        </div>
+        <button
+          type="button"
+          onClick={this.addNewTodo}
+        >
+          Add todo
+        </button>
+        {errorInput
+          ? (
+            <p className="form__hint">
+              Please, write 4 or more symbols
+            </p>
+          )
+          : <p />}
+        {errorSelect
+          ? (
+            <p className="form__hint2">
+              Please, select both field
+            </p>
+          )
+          : <p />}
+      </form>
+    );
+  }
+}
 
 export default TodoForm;
