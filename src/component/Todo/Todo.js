7d74c7d5b02197ce2e2c81c2e3833bca22856687
Oly Timoshenko <o.b.timoshenko@gmail.com>
2020-05-04 17:43:16 +0300

    fixed

--- src/component/Todo/Todo.js
+++ src/component/Todo/Todo.js
@@ -31,8 +31,8 @@ Todo.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   completed: PropTypes.bool.isRequired,
-  user: PropTypes.objectOf(PropTypes.shape({
+  user: PropTypes.shape({
     name: PropTypes.string,
     id: PropTypes.number,
-  })).isRequired,
+  }).isRequired,
 };
