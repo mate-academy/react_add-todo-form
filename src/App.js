3352f77498b715470afff06ae28c4e0f34e888a4
Yuriy Panasiuk <yur.panasiuk@gmail.com>
2020-05-04 07:20:35 +0300

    result v-0.1

--- src/App.js
+++ src/App.js
@@ -2,18 +2,52 @@ import React from 'react';
 import './App.css';
 
 import users from './api/users';
+import todoss from './api/todos';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const preparedTodos = todoss.map((todo) => {
+  const user = users.find(curUser => curUser.id === todo.userId);
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+  return {
+    ...todo,
+    name: user.name,
+  };
+});
+
+const lastId = (preparedTodos[preparedTodos.length - 1].id + 1);
+
+class App extends React.Component {
+  state = {
+    todos: [...preparedTodos],
+  }
+
+  addTodos = (newTodo) => {
+    this.setState(prev => (
+      { todos: [...prev.todos, newTodo] }
+    ));
+  }
+
+  render() {
+    const { todos } = this.state;
+
+    return (
+      <>
+        <div className="form__container">
+          <NewTodo
+            users={users}
+            addTodos={this.addTodos}
+            todos={todos}
+            lastId={lastId}
+          />
+        </div>
+        <div className="todo__container">
+          <TodoList todos={todos} />
+        </div>
+
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/NewTodo.js
@@ -0,0 +1,196 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './NewTodo.css';
+
+class NewTodo extends React.Component {
+  state = {
+    title: '',
+    name: '',
+    completed: '',
+    userId: 0,
+    id: this.props.lastId,
+
+    checkTitle: false,
+    checkUser: false,
+    checkCompleated: false,
+  }
+
+  addTodoText = (event) => {
+    this.setState(
+      {
+        title: event.target.value,
+        checkTitle: false,
+      },
+    );
+  }
+
+  addUser = (id) => {
+    const { users } = this.props;
+
+    this.setState(
+      {
+        userId: +id,
+        name: users.find(user => user.id === +id).name,
+        checkUser: false,
+      },
+    );
+  }
+
+  statusTodo = (event) => {
+    if (event.target.value === 'true') {
+      this.setState({
+        completed: true,
+        checkCompleated: false,
+      });
+    }
+
+    if (event.target.value === 'false') {
+      this.setState({
+        completed: false,
+        checkCompleated: false,
+      });
+    }
+  }
+
+  reset = () => {
+    this.setState(prev => (
+      {
+        title: '',
+        name: '',
+        completed: '',
+        userId: 0,
+        id: prev.id + 1,
+
+        checkTitle: false,
+        checkUser: false,
+        checkCompleated: false,
+      }
+    ));
+  }
+
+  sendTodo = (event) => {
+    const { name, title, userId, id, completed } = this.state;
+
+    event.preventDefault();
+    if (title.length === 0) {
+      this.setState({
+        checkTitle: true,
+      });
+
+      return;
+    }
+
+    if (userId === 0) {
+      this.setState({
+        checkUser: true,
+      });
+
+      return;
+    }
+
+    if (completed.length === 0) {
+      this.setState({
+        checkCompleated: true,
+      });
+
+      return;
+    }
+
+    this.props.addTodos({
+      name,
+      title,
+      userId,
+      id,
+      completed,
+    });
+    this.reset();
+  }
+
+  render() {
+    const { users } = this.props;
+    const { title,
+      checkTitle,
+      userId,
+      checkUser,
+      completed,
+      checkCompleated } = this.state;
+
+    return (
+      <>
+        <form onSubmit={this.sendTodo}>
+          <label htmlFor="inputText">
+            New todo:
+            <input
+              className={checkTitle
+                ? 'form__error'
+                : ''}
+              value={title}
+              onChange={this.addTodoText}
+              id="inputText"
+              type="text"
+            />
+            {checkTitle
+              ? <p className="form__error--message">Please enter the title</p>
+              : <p className="form__error--message" />}
+          </label>
+          <label htmlFor="choseUser">
+            Chose user:
+            <select
+              className={checkUser
+                ? 'form__error'
+                : ''}
+              value={userId}
+              id="choseUser"
+              onChange={e => this.addUser(e.target.value)}
+            >
+              <option value={0} hidden>
+                Choose a user
+              </option>
+              {users.map(user => (
+                <option
+                  key={user.id}
+                  value={user.id}
+                >
+                  {user.name}
+                </option>
+              ))}
+            </select>
+            {checkUser
+              ? <p className="form__error--message">Please choose user</p>
+              : <p className="form__error--message" />}
+          </label>
+          <label htmlFor="selectStatus">
+            Status of todo:
+            <select
+              className={checkCompleated
+                ? 'form__error'
+                : ''}
+              value={completed}
+              id="selectStatus"
+              onChange={this.statusTodo}
+            >
+              <option value="" hidden>Chose status</option>
+              <option value>done</option>
+              <option value={false}>in process</option>
+            </select>
+            {checkCompleated
+              ? <p className="form__error--message">Please choose status</p>
+              : <p className="form__error--message" />}
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </>
+    );
+  }
+}
+
+export default NewTodo;
+
+NewTodo.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.shape({
+    user: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  })).isRequired,
+  lastId: PropTypes.number.isRequired,
+  addTodos: PropTypes.func.isRequired,
+};
