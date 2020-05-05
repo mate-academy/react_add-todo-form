62b10f405472292f9734cee2798bfed33b2ef28e
Nina <pinkangelssugar@gmail.com>
2020-05-05 18:11:48 +0300

    Nina

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.css';
-
-import users from './api/users';
+import NewTodo from './components/NewTodo/NewTodo';
 
 function App() {
   return (
     <div className="App">
       <h1>Add todo form</h1>
+      <NewTodo />
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
     </div>
   );
 }
---
+++ src/components/NewTodo/NewTodo.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import todos from '../../api/todos';
+import users from '../../api/users';
+import TaskList from '../TaskList/TaskList';
+import './NewTodo.css';
+
+function getUserById(userId) {
+  return users.find(user => user.id === userId);
+}
+
+export const preparedTasks = todos.map(task => ({
+  ...task,
+  user: getUserById(task.userId),
+}));
+
+class NewTodo extends React.Component {
+state = {
+  tasks: preparedTasks,
+  newTaskTitle: '',
+  selectedUserId: 0,
+  hasTitleError: false,
+  hasUserError: false,
+};
+
+handleTitleChange = (event) => {
+  this.setState({
+    hasTitleError: false,
+    newTaskTitle: event.target.value,
+  });
+};
+
+handleUserChange = (event) => {
+  this.setState({
+    hasUserError: false,
+    selectedUserId: +event.target.value,
+  });
+};
+
+handleFormSubmit = (event) => {
+  event.preventDefault();
+
+  const { newTaskTitle, selectedUserId } = this.state;
+
+  if (!newTaskTitle || !selectedUserId) {
+    this.setState({
+      hasTitleError: !newTaskTitle,
+      hasUserError: !selectedUserId,
+    });
+
+    return;
+  }
+
+  this.setState((state) => {
+    const newTask = {
+      id: +new Date(),
+      title: state.newTaskTitle,
+      userId: state.selectedUserId,
+      user: getUserById(state.selectedUserId),
+    };
+
+    return {
+      tasks: [...state.tasks, newTask],
+      newTaskTitle: '',
+      selectedUserId: 0,
+    };
+  });
+};
+
+render() {
+  const {
+    tasks,
+    newTaskTitle,
+    selectedUserId,
+    hasTitleError,
+    hasUserError,
+  } = this.state;
+
+  return (
+    <>
+      <form onSubmit={this.handleFormSubmit}>
+        <label htmlFor="task">
+          Enter task:
+          {' '}
+        </label>
+        <textarea
+          id="task"
+          rows="5"
+          cols="45"
+          placeholder="For example: Go to jogging"
+          value={newTaskTitle}
+          onChange={this.handleTitleChange}
+        />
+
+        {hasTitleError && (
+          <>
+            <br />
+            <span className="error">Please enter a title</span>
+          </>
+        )}
+        <br />
+        <label>
+          <select
+            value={selectedUserId}
+            onChange={this.handleUserChange}
+          >
+            <option value="0" hidden>Please select a user</option>
+
+            {users.map(({ id, name }) => (
+              <option key={id} value={id}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </label>
+        {hasUserError && (
+
+          <span className="error">Please select a user</span>
+
+        )}
+        <br />
+        <button type="submit">
+          Add
+        </button>
+      </form>
+
+      <TaskList tasks={tasks} />
+    </>
+  );
+}
+}
+
+export default NewTodo;
