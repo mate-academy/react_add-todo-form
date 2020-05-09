b9886b127bcad9bbca9a1f8a7b68ac9a4396435d
AnnieMarkina <anniemurkina@gmail.com>
2020-05-09 16:26:39 +0300

    fixed NewTodo component

--- src/App.js
+++ src/App.js
@@ -15,22 +15,50 @@ class App extends React.Component {
     })),
   };
 
+  handleCompleteChange = (id) => {
+    this.setState(state => ({
+      todos: state.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+
+        return todo;
+      }),
+    }));
+  };
+
+  handleAddTodo = (todo) => {
+    const { todosList } = this.state;
+    console.log(todo);
+    this.setState({
+      todosList: [todo, ...todosList],
+    });
+  }
+
   render() {
     const { todosList } = this.state;
 
     return (
       <div className="App">
-        <h1 className="App__heading">
-          todos
-        </h1>
+        <h1 className="App__heading">todos</h1>
 
-        <NewTodo users={users} todos={todosList} />
+        <NewTodo
+          users={users}
+          todos={todosList}
+          addTodo={this.handleAddTodo}
+        />
+
+        <p>
+          <span>Todos: {todosList.length}</span>
+        </p>
 
         <TodoList
           todos={todosList}
           setStatus={this.handleCompleteChange}
         />
-
       </div>
     );
   }
--- src/Components/NewTodo/NewTodo.js
+++ src/Components/NewTodo/NewTodo.js
@@ -6,84 +6,83 @@ import './NewTodo.css';
 
 class NewTodo extends React.Component {
   state = {
-    todos: this.props.todos.map(todo => ({
-      ...todo,
-      user: this.props.users.find(user => user.id === todo.userId),
-    })),
-    newTodo: '',
-    userId: 0,
+    userId: '',
+    id: null,
+    title: '',
+    completed: false,
+    user: null,
     isUserSelected: true,
     isTodoInputed: true,
   };
 
-  handleSelect = (event) => {
+  handleSelect = (id) => {
+    const { users } = this.props;
+    console.log(this.state);
+
     this.setState({
-      userId: this.props.users.find(user => (
-        user.name === event.target.value)).id,
+      userId: +id,
+      user: users.find(user => (user.id === +id)),
       isUserSelected: true,
     });
   }
 
   handleAddInput = (event) => {
     event.preventDefault();
+    console.log(this.state);
+
     this.setState({
-      newTodo: event.target.value.trim(),
+      title: event.target.value.trim(),
+      isTodoInputed: true,
+    });
+  }
+
+  handleClearState = () => {
+    this.setState({
+      userId: '',
+      id: null,
+      title: '',
+      completed: false,
+      user: null,
+      isUserSelected: true,
       isTodoInputed: true,
     });
   }
 
   handleSubmit = (event) => {
-    const { userId, newTodo } = this.state;
+    const { userId, title, completed, user } = this.state;
+    const { todos, addTodo } = this.props;
 
+    console.log(this.state);
     event.preventDefault();
 
-    if (userId === 0) {
+    if (!userId) {
       this.setState({ isUserSelected: false });
     }
 
-    if (newTodo === '') {
+    if (!title) {
       this.setState({ isTodoInputed: false });
     }
 
-    if (userId && newTodo) {
-      this.setState(state => ({
-        todos: [{
-          userId: state.userId,
-          id: state.todos.length + 1,
-          title: state.newTodo,
-          completed: false,
-          user: this.props.users.find(user => user.id === state.userId),
-        },
-        ...state.todos],
-        newTodo: '',
-        userId: 0,
-        isUserSelected: true,
-        isTodoInputed: true,
-      }));
+    if (userId && title) {
+      addTodo({
+        userId,
+        id: todos.length + 1,
+        title,
+        completed,
+        user,
+      });
     }
+
+    this.handleClearState();
   }
 
-  handleCompleteChange = (id) => {
-    this.setState(state => ({
-      todos: state.todos.map((todo) => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            completed: !todo.completed,
-          };
-        }
-
-        return todo;
-      }),
-    }));
-  };
-
   render() {
-    const { todos,
-      newTodo,
+    const {
+      title,
       userId,
       isUserSelected,
-      isTodoInputed } = this.state;
+      isTodoInputed,
+    } = this.state;
 
     const { users } = this.props;
 
@@ -93,11 +92,19 @@ class NewTodo extends React.Component {
           <form className="todo__form" onSubmit={this.handleSubmit}>
 
             <div className="todo__select">
-              <select value={userId} onChange={this.handleSelect}>
-                <option value="0" disabled>choose user</option>
+              <select
+                value={userId}
+                onChange={e => this.handleSelect(e.target.value)}
+              >
+                <option
+                  value=""
+                  hidden
+                >
+                  choose user
+                </option>
 
-                {users.map(({ id, name }) => (
-                  <option key={id} value={name}>{name}</option>
+                {users.map(user => (
+                  <option key={user.id} value={user.id}>{user.name}</option>
                 ))}
               </select>
 
@@ -113,7 +120,7 @@ class NewTodo extends React.Component {
               <input
                 type="text"
                 placeholder="add your new todo!"
-                value={newTodo.trim()}
+                value={title.trim()}
                 onChange={this.handleAddInput}
               />
               <span className={classNames('todo__add-todo', {
@@ -132,22 +139,12 @@ class NewTodo extends React.Component {
 
           </form>
         </div>
-
-        <p>
-          <span>Todos: {todos.length}</span>
-        </p>
       </>
     );
   }
 }
 
 NewTodo.propTypes = {
-  todos: PropTypes.arrayOf(PropTypes.shape({
-    userId: PropTypes.number.isRequired,
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    completed: PropTypes.bool.isRequired,
-  })).isRequired,
   users: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
