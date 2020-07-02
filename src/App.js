f540ed2af53c5e82f79974038269aa30ef386659
bran4u <s.oselskiy@gmail.com>
2020-07-02 10:07:12 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 import React from 'react';
 import './App.css';
+import { TodoForm } from './Component/TodoForm';
 
+import todos from './api/todos';
 import users from './api/users';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
-
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+function getTodosWithUsers(allTodos, allUsers) {
+  return allTodos.map(oneTodo => ({
+    ...oneTodo,
+    user: allUsers.find(person => person.id === oneTodo.userId),
+  }));
 }
 
-export default App;
+export class App extends React.Component {
+  state = {
+    allTodos: getTodosWithUsers(todos, users),
+  }
+
+  addTodo = newTodo => (
+    this.setState(prevState => ({
+      allTodos: [...prevState.allTodos, {
+        ...newTodo,
+        id: prevState.allTodos.length + 1,
+      }],
+    }))
+  );
+
+  render() {
+    return (
+      <section>
+        <div className="App">
+          <TodoForm addTodo={this.addTodo} />
+        </div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th className="table__heading">ID</th>
+              <th className="table__heading">Title</th>
+              <th className="table__heading">User</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.allTodos.map(todo => (
+              <tr>
+                <td className="table__item">{todo.id}</td>
+                <td className="table__item">{todo.title}</td>
+                <td className="table__item">{todo.user.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    );
+  }
+}
---
+++ src/Component/TodoForm.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import users from '../api/users';
+
+export class TodoForm extends React.Component {
+  state = {
+    newTitle: '',
+    newUserId: 0,
+    titleError: false,
+    userError: false,
+  };
+
+  handleChange = ({ target: { value } }) => {
+    this.setState(prevState => ({
+      newTitle: value,
+      titleError: false,
+    }));
+  };
+
+  handleSelect = ({ target: { value } }) => {
+    this.setState(prevState => ({
+      newUserId: value,
+      userError: false,
+    }));
+  };
+
+  saveHandler = (event) => {
+    event.preventDefault();
+
+    if (!this.state.newTitle) {
+      this.setState(prevState => ({ titleError: true }));
+    }
+
+    if (this.state.newUserId === 0) {
+      this.setState(prevState => ({ userError: true }));
+    }
+
+    if (this.state.newTitle && this.state.newUserId !== 0) {
+      this.props.addTodo({
+        title: this.state.newTitle,
+        userId: +this.state.userId,
+        completed: false,
+        user: users.find(user => user.id === +this.state.newUserId),
+      });
+
+      this.setState(prevState => ({
+        newTitle: '',
+        newUserId: 0,
+      }));
+    }
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.saveHandler}>
+        <label>
+          Title:
+          <input
+            type="text"
+            name="text"
+            value={this.state.newTitle}
+            onChange={this.handleChange}
+            placeholder="Write todo"
+          />
+        </label>
+        {this.state.titleError && (
+          <div className="todos__error">Please, enter todos text</div>
+        )}
+        <label>
+          User:
+          <select
+            className="todos__select"
+            value={this.state.newUserId}
+            onChange={event => this.handleSelect(event)}
+          >
+            <option value={0}>Choose User</option>
+            {users.map(user => (
+              <option
+                key={user.id}
+                value={user.id}
+              >
+                {user.name}
+              </option>
+            ))}
+            {this.state.userError && (
+              <div>Please, choose a user</div>
+            )}
+          </select>
+        </label>
+        <button
+          className="todos__button"
+          type="submit"
+        >
+          add Todo
+        </button>
+      </form>
+    );
+  }
+}
+
+TodoForm.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+};
