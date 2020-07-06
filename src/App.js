c5b54d4336c2dffb44f09c324a88b6905730d205
Stanislav <stasusss07@gmail.com>
2020-07-06 17:10:48 +0300

    modif readme & app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,94 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import users from './api/users';
+import todos from './api/todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+import { TodoList } from './components/TodoList/TodoList';
+import { Select } from './components/Select/Select';
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+const userName = users.map(user => ({
+  name: user.name,
+  id: user.id,
+}));
+
+const todoList = [...todos];
+
+class App extends Component {
+  state = {
+    todo: [],
+    name: [],
+    userId: 0,
+    todoTextError: false,
+    userError: false,
+  }
+
+  change = (event) => {
+    this.setState({
+      todo: event.target.value,
+      todoTextError: false,
+    });
+  }
+
+  options = (event) => {
+    this.setState({
+      userId: event.target.value,
+      userError: false,
+    });
+  }
+
+  click = () => {
+    if (this.state.todo.length < 1 || this.state.todo.trim().length < 1) {
+      this.setState(prevState => ({ todoTextError: true }));
+    }
+
+    if (this.state.userId === 0) {
+      this.setState(prevState => ({ userError: true }));
+    }
+
+    if (this.state.todo.length >= 1 && this.state.userId !== 0) {
+      todoList.push({
+        title: this.state.todo,
+        name: this.state.name,
+        userId: this.state.userId,
+        completed: false,
+      });
+      this.setState(prevState => ({ todo: '' }));
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Add todo form</h1>
+        <form>
+          <input
+            type="text"
+            className="input"
+            placeholder="Write your text"
+            onChange={this.change}
+            value={this.state.todo}
+          />
+          {this.state.todoTextError && <div>Enter text</div>}
+          <Select
+            options={this.options}
+            userName={userName}
+          />
+          <button
+            type="button"
+            onClick={this.click}
+          >
+            ADD
+          </button>
+          {this.state.userError && <div>Choose a user</div>}
+          <TodoList
+            toggle={this.toggle}
+            todoList={todoList}
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
