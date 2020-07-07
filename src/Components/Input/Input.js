1309c60893b3c103688bce19b6b79c5076b54d52
solaryasha <solaryasha@gmail.com>
2020-07-07 19:41:48 +0300

    Change input class component to functional

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class Input extends Component {
-  state = {
-  }
-
-  render() {
-    const { value, onChange, name } = this.props;
-
-    return (
-      <label>
-        <input
-          type="text"
-          name={name}
-          placeholder="What do you want to do ?"
-          onChange={({ target }) => onChange(target.name, target.value)}
-          value={value}
-        />
-      </label>
-    );
-  }
-}
+export const Input = ({ value, onChange, name }) => (
+  <label>
+    <input
+      type="text"
+      name={name}
+      placeholder="What do you want to do ?"
+      onChange={({ target }) => onChange(target.name, target.value)}
+      value={value}
+    />
+  </label>
+);
 
 Input.defaultProps = {
   value: '',
