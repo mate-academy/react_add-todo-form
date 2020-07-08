aab75465fdc63d5637235ef73015f6f4a2a331ae
RammsfanHTW <64034634+RammsfanHTW@users.noreply.github.com>
2020-07-08 06:17:41 +0300

    Solution w/o styles

--- src/App.js
+++ src/App.js
@@ -2,18 +2,54 @@ import React from 'react';
 import './App.css';
 
 import users from './api/users';
+import todos from './api/todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+import { TodoForm } from './Components/TodoForm';
+import { TodoList } from './Components/TodoList';
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+const setTodos = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId),
+}));
+
+const setUsers = users.map(user => ({
+  id: user.id,
+  name: user.name,
+}));
+
+class App extends React.Component {
+  state = {
+    todoList: [...setTodos],
+  }
+
+  addNewTodo = (task, userId) => {
+    this.setState(prevState => ({
+      todoList: [
+        ...prevState.todoList,
+        {
+          id: prevState.todoList.length + 1,
+          user: setUsers.find(user => user.id === userId),
+          title: task,
+        },
+      ],
+    }));
+  }
+
+  render() {
+    const { todoList } = this.state;
+
+    return (
+      <div className="app">
+        <h1 className="app__title">Add todo App</h1>
+        <TodoForm
+          users={setUsers}
+          addNewTodo={this.addNewTodo}
+          todos={todoList}
+        />
+        <TodoList todos={todoList} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/TodoForm.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export class TodoForm extends React.Component {
+  state = {
+    task: '',
+    selectedUserId: 0,
+    inputIsValid: false,
+    selectIsValid: false,
+  }
+
+  handleInput = (value) => {
+    this.setState({
+      task: value.replace(/\s/g, ''),
+      inputIsValid: false,
+    });
+  }
+
+  handleSelect = (value) => {
+    this.setState({
+      selectedUserId: +value,
+      selectIsValid: false,
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { task, selectedUserId } = this.state;
+    const { addNewTodo } = this.props;
+
+    if (!task) {
+      return this.setState({
+        inputIsValid: true,
+      });
+    }
+
+    if (selectedUserId === 0) {
+      return this.setState({
+        selectIsValid: true,
+      });
+    }
+
+    addNewTodo(task, selectedUserId);
+
+    return this.setState({
+      task: '',
+      selectedUserId: 0,
+    });
+  }
+
+  render() {
+    const { selectedUserId, task, inputIsValid, selectIsValid } = this.state;
+    const { users } = this.props;
+
+    return (
+      <form className="form" onSubmit={this.handleSubmit}>
+        {
+          inputIsValid && (
+            <div>
+              Invalid input
+            </div>
+          )
+        }
+        <input
+          type="text"
+          name="todo"
+          placeholder="Input task"
+          onChange={event => this.handleInput(event.target.value)}
+          value={task}
+        />
+        {
+          selectIsValid && (
+            <div>
+              No user selected
+            </div>
+          )
+        }
+        <select
+          onChange={event => this.handleSelect(event.target.value)}
+          value={selectedUserId}
+        >
+          <option value={0}>Select user</option>
+          {users.map(user => (
+            <option
+              key={user.id}
+              value={user.id}
+            >
+              {user.name}
+            </option>
+          ))}
+        </select>
+        <button
+          type="submit"
+        >
+          Add
+        </button>
+      </form>
+    );
+  }
+}
+
+TodoForm.propTypes = {
+  addNewTodo: PropTypes.func.isRequired,
+  users: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+  })).isRequired,
+};
