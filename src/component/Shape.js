db422447e0826b1e85264a6ed0a4c619f523a0d7
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-07-14 09:52:58 +0300

    solution

--- src/component/Shape.js
+++ src/component/Shape.js
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types';
 
 export const TodoListShape = PropTypes.shape({
   toggle: PropTypes.func.isRequired,
-  todoList: PropTypes.arrayOf(PropTypes.string).isRequired,
+  todoList: PropTypes.arrayOf(PropTypes.object).isRequired,
 });
 
 export const SelectShape = PropTypes.shape({
-  userName: PropTypes.arrayOf(PropTypes.string).isRequired,
+  userName: PropTypes.arrayOf(PropTypes.object).isRequired,
   options: PropTypes.func.isRequired,
 });
