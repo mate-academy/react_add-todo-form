129dce647316d96775e7c3af8bff97ec30d439a0
Vitalii Arseniuk <arsenyuk.v.v@gmail.com>
2020-09-05 19:57:27 +0300

    added status toggle

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,19 +3,40 @@ import PropTypes from 'prop-types';
 import './Todo.css';
 import { User } from '../User/User';
 
-export const Todo = ({ title, completed, user }) => {
-  const status = `${completed ? 'done' : 'undone'}`;
+export class Todo extends React.Component{
+  state = {
+    status: false,
+  }
 
-  return (
-    <>
-      <User {...user} />
-      <p>{title}</p>
-      <span className={status}>
-        {status.slice(0, 1).toUpperCase() + status.slice(1)}
-      </span>
-    </>
-  );
-};
+  render() {
+    const { title, id, user } = this.props;
+    const { status } = this.state;
+
+    return (
+      <>
+        <User {...user} />
+        <p>{title}</p>
+        <label
+          htmlFor={`status-check-${id}`}
+          className={status ? 'done' : 'undone'}
+        >
+          {status ? 'Done' : 'Undone'}
+        </label>
+        <input
+          type="checkbox"
+          id={`status-check-${id}`}
+          className="status-check"
+          checked={status}
+          onChange={(event) => {
+            this.setState({
+              status: event.target.checked,
+            });
+          }}
+        />
+      </>
+    );
+  }
+}
 
 Todo.propTypes = {
   title: PropTypes.string.isRequired,
