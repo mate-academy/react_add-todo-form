0daa99195f77b08e4dcda786272a3dd99e386344
Roman <romikkus@gmail.com>
2020-09-07 14:17:14 +0300

    removed the ability to add an empty title with blank space

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
    const { name, value } = event.target;
 
    this.setState({
-     [name]: value,
+     [name]: value.trimLeft(),
      userError: false,
      titleError: false,
    });
