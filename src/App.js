b2d37d092f1452ae2fc7c503f698b86aba83f1d5
DmitryArhipenko <42931799+DmitryArhipenko@users.noreply.github.com>
2020-10-29 19:11:34 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
+import { TodoList } from './components/TodoList';
+
 import users from './api/users';
+import todos from './api/todos';
+import { Form } from './components/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+export class App extends Component {
+  state = {
+    listOfTodos: todos,
+  }
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+  addnewTodo = (newTodo) => {
+    this.setState(state => ({
+      listOfTodos: [...state.listOfTodos, newTodo],
+    }));
+  }
+
+  render() {
+    const {
+      listOfTodos,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <div
+          className="general-info"
+        >
+          <h3
+            className="header"
+          >
+            General Info
+          </h3>
+          <p
+            className="field"
+          >
+            <span>People got </span>
+            {listOfTodos.length}
+            <span> todos</span>
+          </p>
+
+          <p
+            className="field"
+          >
+            <span>Amount of users is </span>
+            {users.length}
+          </p>
+        </div>
+
+        <Form
+          addnewTodo={this.addnewTodo}
+          users={users}
+          todos={todos}
+        />
+        <TodoList todos={listOfTodos.map(todo => ({
+          ...todo,
+          user: users.find(user => user.id === todo.userId),
+        }))}
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,185 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+import PropTypes from 'prop-types';
+
+export class Form extends Component {
+  state = {
+    todoTitle: '',
+    userId: '',
+    titleError: '',
+    userError: '',
+    tooLongError: '',
+    counter: 0,
+  }
+
+  validate = () => {
+    this.setState((state) => {
+      if (state.userId) {
+        this.setState({
+          userError: '',
+        });
+      }
+
+      if (state.todoTitle) {
+        this.setState({
+          titleError: '',
+        });
+      }
+
+      if (state.todoTitle.length <= 50) {
+        this.setState({
+          tooLongError: '',
+        });
+      }
+    });
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+    this.validate();
+  };
+
+  submitionEvent = (event) => {
+    event.preventDefault();
+
+    const {
+      todoTitle,
+      userId,
+      counter,
+    } = this.state;
+
+    const {
+      addnewTodo,
+      users,
+    } = this.props;
+
+    if (!userId) {
+      this.setState({
+        userError: 'Please choose a user!',
+      });
+    } else if (!todoTitle) {
+      this.setState({
+        titleError: 'Please enter the title!',
+      });
+    } else if (todoTitle.length > 50) {
+      this.setState({
+        tooLongError:
+        'Input less then 50 symbols!',
+      });
+    } else {
+      this.setState({
+        counter: counter + 1,
+      });
+      const newTodo = {
+        userId: users.find(user => user.name === userId).id,
+        id: counter,
+        title: todoTitle,
+        completed: false,
+      };
+
+      addnewTodo(newTodo);
+      this.setState({
+        todoTitle: '',
+        userId: '',
+        titleError: '',
+        userError: '',
+        tooLongError: '',
+      });
+    }
+  }
+
+  render() {
+    const {
+      todoTitle,
+      userId,
+      titleError,
+      userError,
+      tooLongError,
+    } = this.state;
+    const { users } = this.props;
+
+    return (
+      <div className="Form">
+        <h3
+          className="header"
+        >
+          Input form
+        </h3>
+        <form onSubmit={this.submitionEvent}>
+          <label
+            className="field"
+            htmlFor="userId"
+          >
+            Select user who has to complete the task
+          </label>
+          <br />
+          <select
+            className="field"
+            name="userId"
+            value={userId}
+            onChange={this.handleChange}
+          >
+            <option value="">
+              Select
+            </option>
+            {users.map(user => (
+              <option
+                key={user.id}
+                value={user.name}
+              >
+                {user.name}
+              </option>
+            ))}
+          </select>
+          <p>
+            <p className="field">
+              <label htmlFor="todoTitle">
+                Name a todo
+              </label>
+              <br />
+              <input
+                type="text"
+                name="todoTitle"
+                label="todo name"
+                value={todoTitle}
+                onChange={this.handleChange}
+              />
+
+            </p>
+          </p>
+          <div>
+            <span className="field error">
+              {titleError}
+              {tooLongError}
+              {userError}
+            </span>
+          </div>
+          <p
+            className="button"
+          >
+            <button type="submit">Add</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
+}
+
+Form.propTypes = {
+  addnewTodo: PropTypes.func.isRequired,
+  users: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ),
+};
+
+Form.defaultProps = {
+  users: [],
+  todos: [],
+};
