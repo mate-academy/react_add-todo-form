bc3eb29836818fa7d5e4c11a057ed1767c6d36dc
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-11-02 12:36:18 +0200

    add error message

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,65 +7,109 @@ export class Form extends React.Component {
     task: '',
     userName: '',
     user: '',
+    userError: false,
+    taskError: false,
   };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { task, userName, user } = this.state;
+    const { addUser } = this.props;
+
+    if (!task || !userName) {
+      this.setState({
+        userError: true,
+        taskError: true,
+      });
+
+      return;
+    }
+
+    addUser(user, task);
+    this.setState({
+      task: '',
+      userName: '',
+      user: '',
+      userError: false,
+      taskError: false,
+    });
+  }
+
+  handleSelect = (event) => {
+    const { users } = this.props;
+
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+    this.setState(prevState => ({
+      user: users.find(userToFind => (
+        userToFind.name === prevState.userName
+      )),
+    }));
+  }
+
   render() {
-    const { users, addUser } = this.props;
-    const { userName, task, user } = this.state;
+    const { users } = this.props;
+    const { userName, task, userError, taskError } = this.state;
 
     return (
       <form
+        name="newUser"
         className="ui focus input"
-        onSubmit={(event) => {
-          event.preventDefault();
-
-          if (!task || !userName) {
-            return;
-          }
-
-          addUser(user, task);
-          this.setState({
-            task: '',
-            userName: '',
-            user: '',
-          });
-        }}
+        onSubmit={this.handleSubmit}
       >
-        <input
-          type="text"
-          name="task"
-          value={task}
-          placeholder="Add your task"
-          className="ui input"
-          onChange={(event) => {
-            this.setState({
-              task: event.target.value,
-            });
-          }}
-        />
-        <select
-          name="userName"
-          id="userName"
-          value={userName}
-          className="ui placeholder"
-          onChange={(event) => {
-            this.setState({
-              [event.target.name]: event.target.value,
-            });
-            this.setState(prevState => ({
-              user: users.find(userToFind => (
-                userToFind.name === prevState.userName
-              )),
-            }));
-          }}
-        >
-          <option value="">Choose user</option>
-          {users.map(person => (
-            <option key={person.id}>
-              {person.name}
-            </option>
-          ))}
-        </select>
+        {
+          taskError
+            ? <input placeholder="Error" />
+            : (
+              <input
+                type="text"
+                name="task"
+                value={task}
+                placeholder="Add your task"
+                className="ui input"
+                onChange={(event) => {
+                  this.setState({
+                    task: event.target.value,
+                  });
+                }}
+              />
+            )
+        }
+        {
+          userError
+            ? (
+              <select
+                className="select"
+                name="userName"
+                id="userName"
+              >
+                <option value="">Choose user</option>
+                {users.map(person => (
+                  <option key={person.id}>
+                    {person.name}
+                  </option>
+                ))}
+              </select>
+            )
+            : (
+              <select
+                name="userName"
+                id="userName"
+                value={userName}
+                className="ui placeholder"
+                onChange={this.handleSelect}
+              >
+                <option value="">Choose user</option>
+                {users.map(person => (
+                  <option key={person.id}>
+                    {person.name}
+                  </option>
+                ))}
+              </select>
+            )
+        }
+
         <button
           type="submit"
           className="ui button"
