92e11be68f4f9baf8c6a7e3f1537c0e70074316b
Lancaelot <Lancaelot@gmail.com>
2020-11-02 17:43:21 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import './App.css';
 
 import users from './api/users';
+import todos from './api/todos';
+import TodoList from './components/TodoList';
+import { AddTodoForm } from './components/AddTodoForm';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const preparedTodos = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId),
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+class App extends PureComponent {
+  state = {
+    todos: preparedTodos,
+  }
+
+  addTodo = (title, user) => {
+    this.setState((state) => {
+      const newTodo = {
+        userId: user.id,
+        id: state.todos.length + 1,
+        title,
+        completed: false,
+        user,
+      };
+
+      return { todos: [...state.todos, newTodo] };
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 style={{ textAlign: 'center' }}>LIST OF TODOS</h1>
+        <AddTodoForm
+          users={users}
+          addTodo={this.addTodo}
+        />
+        <table className="ui celled table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Title</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TodoList
+              todosList={this.state.todos}
+            />
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
+
+App.propTypes = {};
 
 export default App;
---
+++ src/components/AddTodoForm.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import './AddTodoForm.css'
+export class AddTodoForm extends React.PureComponent {
+
+  state = {
+    title: '',
+    userName: '',
+    taskTitleError: false,
+    userNameError: false,
+  }
+
+  handleChange = (event) => {
+    const { name, value, type } = event.target;
+
+    this.setState({
+      [name]: type === 'text'
+        ? value.replace(/\d|[&\\#,+()$~%.'":*?<>{}!]/g, '')
+        : value,
+      [`${name}Error`]: false,
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { title, userName } = this.state;
+    const { addTodo, users } = this.props;
+
+    if (!title) {
+      this.setState({ taskTitleError: true });
+    }
+
+
+    if (userName === 'Choose a user' || !userName) {
+      this.setState({ userNameError: true });
+    }
+
+    if (title.trim() && userName) {
+      const newUser = users.find(user => user.name === userName);
+
+      this.setState({
+        title: '',
+        userName: '',
+      });
+      addTodo(title, newUser);
+    }
+  }
+  render() {
+    const { users } = this.props;
+    const {
+      title,
+      userName,
+      taskTitleError,
+      userNameError,
+    } = this.state;
+
+    return (
+      <form
+        className="ui form"
+        onSubmit={this.handleSubmit}
+      >
+        <div className="three fields">
+          <div className="field">
+          <label>Choose User</label>
+          <select
+            className="ui fluid dropdown"
+            name="userName"
+            id="userName"
+            value={userName}
+            onChange={this.handleChange}>
+            <option>
+              Choose a user
+            </option>
+            {users.map(user => (
+              <option value={user.name} key={user.id}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+          {
+            userNameError
+              ? <div className="ui red message">Please choose a user</div>
+              : ''
+          }
+          </div>
+          <div className="field">
+            <label>Enter Title</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Task title"
+              value={title}
+              onChange={this.handleChange}
+            />
+              {
+                taskTitleError
+                  ? <div className="ui red message">Please enter the title</div>
+                  : ''
+              }
+          </div>
+        </div>
+        <button className="ui button primary" type="submit">Add Task</button>
+      </form>
+    )
+  };
+}
+
+export default AddTodoForm;
+
+
