2e594fc3af38484821beaf9a7fec6814e2b54778
OnlyRaydek <bayramsaid00@gmail.com>
2020-11-02 21:02:28 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
 import React from 'react';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
 
+import todosFromServer from './api/todos';
 import users from './api/users';
+import { TodoList } from './components/todolist/TodoList';
+import { TodoForm } from './components/todoform/TodoForm';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const prepearedTodos = todosFromServer.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId),
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+class App extends React.PureComponent {
+  state = {
+    todos: prepearedTodos,
+  }
+
+  addTodo = (title, user) => {
+    this.setState((prevState) => {
+      const newTodo = {
+        userId: user.id,
+        id: prevState.todos.length + 1,
+        title,
+        completed: false,
+        user,
+      };
+
+      return { todos: [...prevState.todos, newTodo] };
+    });
+  }
+
+  render() {
+    const { todos } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Add todo form</h1>
+
+        <p>
+          <span>Users: </span>
+          {users.length}
+        </p>
+        <div className="App__wrapper">
+          <TodoForm
+            users={users}
+            addTodo={this.addTodo}
+          />
+
+          <TodoList
+            todos={todos}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/todoform/TodoForm.js
@@ -0,0 +1,148 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { UserShape } from '../shapes/UserShape';
+
+export class TodoForm extends React.PureComponent {
+  state = {
+    title: '',
+    userName: '',
+    titleError: false,
+    userNameError: false,
+  }
+
+  handleChange = (event) => {
+    const {
+      name, value, type,
+    } = event.target;
+
+    this.setState({
+      [name]: type === 'text'
+        ? value.replace(/[^\w]+/, '')
+        : value,
+      [`${name}Error`]: false,
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { title, userName } = this.state;
+    const { addTodo, users } = this.props;
+
+    if (!title.trim()) {
+      this.setState({ titleError: true });
+    }
+
+    if (userName === 'Choose a user' || !userName) {
+      this.setState({ userNameError: true });
+    }
+
+    if (title.trim() && userName) {
+      const newUser = users.find(user => user.name === userName);
+
+      this.setState({
+        title: '',
+        userName: '',
+      });
+
+      addTodo(title, newUser);
+    }
+  }
+
+  render() {
+    const { users } = this.props;
+    const {
+      title,
+      userName,
+      titleError,
+      userNameError,
+    } = this.state;
+
+    return (
+      <form
+        className="ui form"
+        onSubmit={this.handleSubmit}
+      >
+
+        <input
+          name="title"
+          id="title"
+          placeholder="Task"
+          type="text"
+          value={title}
+          onChange={this.handleChange}
+          className="ui input"
+        />
+
+        {
+          titleError
+            ? (
+              <span className="ui red pointing basic label">
+                Please enter the title
+              </span>
+            )
+            : (
+              <label
+                className="ui pointing label"
+                htmlFor="title"
+              >
+                Enter the title
+              </label>
+            )
+        }
+
+        <div className="ui divider" />
+
+        <select
+          name="userName"
+          id="userName"
+          value={userName}
+          onChange={this.handleChange}
+          className="ui compact selection dropdown"
+        >
+          <option>Choose a user</option>
+          {users.map(user => (
+            <option
+              key={user.id}
+              value={user.name}
+            >
+              {user.name}
+            </option>
+          ))}
+        </select>
+
+        {
+          userNameError
+            ? (
+              <span className="ui red pointing basic label">
+                Please choose a user
+              </span>
+            )
+            : (
+              <label
+                className="ui pointing label"
+                htmlFor="userName"
+              >
+                Choose a user
+              </label>
+            )
+        }
+
+        <div className="ui divider" />
+
+        <button
+          type="submit"
+          className="ui button"
+          onClick={this.handleSubmit}
+        >
+          Add
+        </button>
+      </form>
+    );
+  }
+}
+
+TodoForm.propTypes = {
+  users: PropTypes.arrayOf(UserShape).isRequired,
+  addTodo: PropTypes.func.isRequired,
+};
