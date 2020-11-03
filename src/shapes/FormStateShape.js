0545184f82b59841b7e7b034d735f817dc48cc59
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-11-03 11:15:43 +0200

    fix

--- src/shapes/FormStateShape.js
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const StateShape = PropTypes.shape({
-  userName: PropTypes.string.isRequired,
-  userNameError: PropTypes.bool.isRequired,
-  title: PropTypes.string.isRequired,
-  titleError: PropTypes.bool.isRequired,
-}).isRequired;
