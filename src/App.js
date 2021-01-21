4e6ecaaa1ba999325d7300616339e5e08083035d
Olesia <olesia.tk@gmail.com>
2021-01-21 15:14:06 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,141 @@
 import React from 'react';
 import './App.css';
+import classNames from 'classnames';
+import { TodoList } from './components/TodoList';
 
 import users from './api/users';
+import todos from './api/todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+function getUserById(userId) {
+  return users.find(user => user.id === userId);
+}
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+const todosWithUser = todos.map(todo => ({
+  ...todo,
+  user: getUserById(todo.userId),
+}));
+
+class App extends React.Component {
+  state = {
+    todosNew: todosWithUser,
+    todoTitle: '',
+    todoUserId: 0,
+    errorNoTitle: false,
+    errorNoUser: false,
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Add todo form</h1>
+        <p>
+          <span>Users: </span>
+          {users.length}
+        </p>
+        <form
+          action="/api/todolist"
+          method="Post"
+          onSubmit={(event) => {
+            event.preventDefault();
+
+            this.setState((prevState) => {
+              if (prevState.todoTitle !== '' && prevState.todoUserId !== 0) {
+                const newTodo = {
+                  id: Math.random(),
+                  title: prevState.todoTitle,
+                  userId: prevState.todoUserId,
+                  user: getUserById(prevState.todoUserId),
+                };
+
+                return ({
+                  todosNew: [...prevState.todosNew, newTodo],
+                  todoTitle: '',
+                  todoUserId: 0,
+                });
+              }
+
+              if (prevState.todoTitle === '' && prevState.todoUserId === 0) {
+                return ({
+                  errorNoTitle: true,
+                  errorNoUser: true,
+                });
+              }
+
+              if (prevState.todoTitle === '') {
+                return ({ errorNoTitle: true });
+              }
+
+              if (prevState.todoUserId === 0) {
+                return ({ errorNoUser: true });
+              }
+
+              return prevState;
+            });
+          }}
+        >
+          <div className="form__field">
+            <label htmlFor="new-todo-title">Todo title </label>
+            <input
+              type="text"
+              name="todoTitle"
+              value={this.state.todoTitle}
+              onChange={(event) => {
+                this.setState({
+                  todoTitle: event.target.value,
+                  errorNoTitle: false,
+                });
+              }}
+              id="new-todo-title"
+              placeholder="Todo title"
+            />
+            <div
+              className={classNames(
+                'error-msg',
+                { hidden: !this.state.errorNoTitle },
+              )}
+            >
+              Please enter the title
+            </div>
+          </div>
+
+          <div className="form__field">
+            <label htmlFor="new-todo-user">Todo user </label>
+            <select
+              name="todoUser"
+              id="new-todo-user"
+              value={this.state.todoUserId}
+              onChange={(event) => {
+                this.setState({
+                  todoUserId: +event.target.value,
+                  errorNoUser: false,
+                });
+              }}
+            >
+              <option>Please choose a user</option>
+              {users.map(user => (
+                <option value={user.id} key={user.id}>
+                  {user.name}
+                </option>
+              ))}
+            </select>
+            <div
+              className={classNames(
+                'error-msg',
+                { hidden: !this.state.errorNoUser },
+              )}
+            >
+              Please choose a user
+            </div>
+          </div>
+
+          <button type="submit">
+            Add
+          </button>
+        </form>
+        <TodoList todos={this.state.todosNew} />
+      </div>
+    );
+  }
 }
 
 export default App;
