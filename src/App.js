1306954445c4220f226540998b632fc28b5ffc2b
MarchD <dubchmar@gmail.com>
2021-01-21 16:11:55 +0200

    added solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,131 @@
 import React from 'react';
+import { TodoList } from './components/TodoList';
 import './App.css';
+import todosFromApi from './api/todos';
+import usersFromApi from './api/users';
 
-import users from './api/users';
+function getUserById(UserId) {
+  return usersFromApi.find(user => user.id === UserId);
+}
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const preparedTodos = todosFromApi.map(todo => ({
+  ...todo,
+  user: getUserById(todo.userId),
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: preparedTodos,
+    users: usersFromApi,
+    taskTitle: '',
+    userId: 0,
+    errorUser: false,
+    errorTitle: false,
+  }
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+    const correctValue = name === 'userId' ? +value : value;
+
+    this.setState({ [name]: correctValue });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { userId, taskTitle } = this.state;
+
+    this.setState({
+      errorUser: false,
+      errorTitle: false,
+    });
+
+    if (userId === 0) {
+      return this.setState({ errorUser: true });
+    }
+
+    if (taskTitle.trim().length < 1) {
+      return this.setState({ errorTitle: true });
+    }
+
+    return this.setState((state) => {
+      const newTask = {
+        id: Math.random(),
+        title: taskTitle,
+        completed: false,
+        user: getUserById(userId),
+      };
+
+      return ({
+        todos: [newTask, ...state.todos],
+        users: state.users,
+        taskTitle: '',
+        userId: 0,
+      });
+    });
+  }
+
+  render() {
+    const { todos,
+      users,
+      taskTitle,
+      userId,
+      errorUser,
+      errorTitle } = this.state;
+
+    return (
+      <div className="App">
+        <h1>TODO</h1>
+        <form
+          action="/api/goods"
+          method="POST"
+          className="input-group"
+          onSubmit={this.handleSubmit}
+        >
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <label htmlFor="task" className="input-group-text">
+                Title of the new task
+              </label>
+            </div>
+            <input
+              type="text"
+              name="taskTitle"
+              className="form-control"
+              placeholder="Type your task..."
+              value={taskTitle}
+              onChange={this.handleChange}
+            />
+
+            <select
+              name="userId"
+              className="form-control"
+              value={userId}
+              onChange={this.handleChange}
+            >
+              <option value="0">Choose a user</option>
+
+              {users.map(user => (
+                <option value={user.id} key={user.id}>{user.name}</option>
+              ))}
+            </select>
+            <div className="input-group-append">
+              <button type="submit" className="btn btn-outline-secondary">
+                Add a new task
+              </button>
+            </div>
+
+          </div>
+        </form>
+
+        { errorUser
+        && <div className="alert alert-danger">Please choose a user!</div>}
+        { errorTitle
+        && <div className="alert alert-danger">Please enter the title!</div>}
+
+        <TodoList todos={todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { User } from '../User';
+import './Todo.css';
+
+export const Todo = ({ task }) => (
+  <div className="card bg-light mb-3 todo">
+    <p className="h4">{task.title}</p>
+    {task.completed
+      ? <p className="completed">Done</p>
+      : <p className="uncompleted">In process</p>}
+    <User user={task.user} />
+  </div>
+);
+
+Todo.propTypes = {
+  task: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    user: PropTypes.object.isRequired,
+    completed: PropTypes.bool.isRequired,
+  }).isRequired,
+};
