182f3f2fe47c72128a68d4df5bc826a834b151ca
Andrii Badekha <badekha@gmail.com>
2021-02-08 14:25:17 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React from 'react';
+import { TodoList } from './components/TodoList';
+import { AddTodoForm } from './components/AddTodoForm';
 import './App.css';
 
 import users from './api/users';
+import todos from './api/todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+class App extends React.Component {
+  state = {
+    todoss: todos,
+  }
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+  addTodo = (todo) => {
+    this.setState(state => ({
+      todoss: [
+        ...state.todoss,
+        todo,
+      ],
+    }));
+  };
+
+  render() {
+    const { todoss } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Add todo form</h1>
+        <AddTodoForm
+          users={users}
+          newTodoId={todoss.length + 1}
+          addTodo={this.addTodo}
+        />
+        <TodoList todos={todoss} users={users} />
+      </div>
+    );
+  }
 }
 
 export default App;
