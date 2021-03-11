27c78636c661f62b9dd910ddef06740fb91b6b1e
Bohdan Dobak <chudolipton@gmail.com>
2021-03-11 12:12:10 +0200

    made form which add todo to todolist, made component form using semantic ui , in form made few methods for form items and check validity for todo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import React from 'react';
 import './App.css';
-
 import users from './api/users';
+import todos from './api/todos';
+import { TodoList } from './components/TodoList';
+import { FormTodo } from './components/FormTodo';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const preparedTodos = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId),
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+function getUserId(item) {
+  return users.find(user => user.id === item);
+}
+
+class App extends React.Component {
+  state = {
+    todosData: preparedTodos,
+  }
+
+  handleTodo = (title, userId) => {
+    this.setState((prevState) => {
+      const newTodo = {
+        userId,
+        title,
+        completed: false,
+        id: prevState.todosData.length + 1,
+        user: getUserId(userId),
+      };
+
+      return ({
+        todosData: [...prevState.todosData, newTodo],
+      });
+    });
+  }
+
+  render() {
+    const { todosData } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Add todo form</h1>
+        <FormTodo
+          handleTodo={this.handleTodo}
+          users={users}
+        />
+        <TodoList todos={todosData} />
+      </div>
+    );
+  }
 }
 
 export default App;
