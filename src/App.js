de0d9087026a656f3ca328c86a755f689e6630f8
Nazarii <nazarrishchuk@gmail.com>
2021-03-15 12:16:50 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -6,46 +6,12 @@ import { Form } from './api/components/Form';
 import { TodoList } from './api/components/TodoList';
 
 export class App extends React.Component {
-  state ={
+  state = {
     todoList: [],
-    title: '',
-    userName: '',
-    classValidTitle: '',
-    classValidUser: '',
   }
 
-  userNameHandler = (e) => {
-    const { name, value } = e.target;
-
-    this.setState({
-      [name]: value, classValidTitle: '', classValidUser: '',
-    });
-  }
-
-  addUserHandler = (e) => {
-    e.preventDefault();
-
-    if (!this.state.userName) {
-      this.setState({ classValidUser: 'showUser' });
-
-      return;
-    }
-
-    if (!this.state.title) {
-      this.setState({ classValidTitle: 'showTitle' });
-
-      return;
-    }
-
-    this.setState(state => ({ todoList: [...state.todoList, {
-      title: state.title,
-      name: state.userName,
-      id: state.todoList.length + 1,
-    }] }));
-
-    this.setState({
-      title: '', userName: '',
-    });
+  updateTodoList = (todos) => {
+    this.setState(({ todoList: [...todos] }));
   }
 
   render() {
@@ -53,12 +19,7 @@ export class App extends React.Component {
       <div className="App">
         <Form
           users={users}
-          userNameHandler={this.userNameHandler}
-          addUserHandler={this.addUserHandler}
-          valueTitle={this.state.title}
-          valueUser={this.state.userName}
-          classValidTitle={this.state.classValidTitle}
-          classValidUser={this.state.classValidUser}
+          updateTodoList={this.updateTodoList}
         />
         <TodoList todoList={this.state.todoList} />
       </div>
--- src/api/components/Form.jsx
+++ src/api/components/Form.jsx
@@ -2,47 +2,116 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Form.css';
 
-export const Form = ({
-  users,
-  userNameHandler,
-  addUserHandler,
-  valueTitle,
-  valueUser,
-  classValidTitle,
-  classValidUser,
-}) => (
-  <div className="form-style-6">
-    <form onSubmit={addUserHandler}>
-      <h1>Add Todo form</h1>
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-      <select name="userName" onChange={userNameHandler} value={valueUser}>
-        <option name="userName">Please select User...</option>
-        {users.map(el => <option key={el.id}>{el.name}</option>)}
-      </select>
-      <p className={`validation ${classValidUser}`}>Please choose a user</p>
-      <input
-        onChange={userNameHandler}
-        name="title"
-        type="text"
-        placeholder="Write a title..."
-        maxLength={64}
-        value={valueTitle}
-      />
-      <p className={`validation ${classValidTitle}`}>Please enter the title</p>
-      <input type="submit" value="Add" />
-    </form>
-  </div>
-);
+export class Form extends React.Component {
+  state = {
+    title: '',
+    userName: '',
+    classValidTitle: '',
+    classValidUser: '',
+    todoList: [],
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevState.todoList !== this.state.todoList) {
+      this.props.updateTodoList(this.state.todoList);
+    }
+  }
+
+  userNameHandler = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value,
+      classValidUser: '',
+      classValidTitle: '',
+    });
+  }
+
+  addUserToList = (e) => {
+    e.preventDefault();
+
+    if (!this.state.userName) {
+      this.setState({ classValidUser: 'showUser' });
+
+      return;
+    }
+
+    if (!this.state.title) {
+      this.setState({ classValidTitle: 'showTitle' });
+
+      return;
+    }
+
+    this.setState(state => ({
+      todoList: [
+        ...state.todoList,
+        {
+          title: state.title,
+          name: state.userName,
+          id: state.todoList.length + 1,
+        },
+      ],
+      title: '',
+      userName: '',
+    }));
+  }
+
+  render() {
+    const { users } = this.props;
+    const {
+      title,
+      userName,
+      classValidTitle,
+      classValidUser,
+    } = this.state;
+
+    return (
+      <div className="form-style-6">
+        <form
+          onSubmit={this.addUserToList}
+        >
+          <h1>Add Todo form</h1>
+          <p>
+            <span>Users: </span>
+            {users.length}
+          </p>
+          <select
+            name="userName"
+            onChange={this.userNameHandler}
+            value={userName}
+          >
+            <option name="userName">Please select User...</option>
+            {users.map(el => <option key={el.id}>{el.name}</option>)}
+          </select>
+          <p
+            className={`validation ${classValidUser}`}
+          >
+            Please choose a user
+          </p>
+          <input
+            onChange={this.userNameHandler}
+            name="title"
+            type="text"
+            placeholder="Write a title..."
+            maxLength={64}
+            value={title}
+          />
+          <p
+            className={`validation ${classValidTitle}`}
+          >
+            Please enter the title
+          </p>
+          <input
+            type="submit"
+            value="Add"
+          />
+        </form>
+      </div>
+    );
+  }
+}
 
 Form.propTypes = {
   users: PropTypes.arrayOf(PropTypes.object).isRequired,
-  userNameHandler: PropTypes.func.isRequired,
-  addUserHandler: PropTypes.func.isRequired,
-  valueTitle: PropTypes.string.isRequired,
-  valueUser: PropTypes.string.isRequired,
-  classValidTitle: PropTypes.string.isRequired,
-  classValidUser: PropTypes.string.isRequired,
+  updateTodoList: PropTypes.func.isRequired,
 };
