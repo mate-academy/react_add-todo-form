a7fc227eaee368acbcb3da91c697fb8402b59ed4
Artem Topolskyi <topolskyiav@gmail.com>
2021-03-16 16:38:05 +0200

    replaced almost all logic into TodoForm

--- src/App.js
+++ src/App.js
@@ -1,82 +1,33 @@
 import React from 'react';
 import { TodoList } from './components/TodoList';
 import { TodoForm } from './components/TodoForm';
-import { ErrorMessage } from './components/ErrorMessage';
 import './App.css';
 
 import users from './api/users';
 import initialTodos from './api/todos';
 
-const todos = initialTodos.map(todo => ({
-  ...todo,
-  user: findUser(todo.userId),
-}));
-
 function findUser(id) {
   return users.find(user => user.id === id);
 }
 
+const preparedTodos = initialTodos.map(todo => ({
+  ...todo,
+  user: findUser(todo.userId),
+}));
+
 class App extends React.Component {
   state = {
-    title: '',
-    isTitleCorrect: true,
-    userId: '',
-    isUserIdCorrect: true,
+    todos: preparedTodos,
   };
 
-  handleChange = (event) => {
-    let { value } = event.target;
-    const { name } = event.target;
-
-    if (name === 'title') {
-      value = value.replace(/[^A-Za-z0-9 ]/g, '');
-    }
-
-    this.setState({
-      [name]: value,
-      [`is${name[0].toUpperCase() + name.slice(1)}Correct`]: true, // :)
-    });
-  }
-
-  formSubmitHandler = (event) => {
-    event.preventDefault();
-
-    const { title, userId } = this.state;
-
-    let isDataCorrect = true;
-
-    if (title.trim().length === 0) {
-      this.setState({ isTitleCorrect: false });
-      isDataCorrect = false;
-    }
-
-    if (userId.length === 0) {
-      this.setState({ isUserIdCorrect: false });
-      isDataCorrect = false;
-    }
-
-    if (isDataCorrect) {
-      todos.push({
-        userId: Number(userId),
-        id: Math.max(...todos.map(todo => todo.id)) + 1,
-        title,
-        completed: false,
-        user: findUser(Number(userId)),
-      });
-
-      this.clearForm();
-    }
-  }
-
-  clearForm = () => {
-    this.setState({
-      title: '',
-      userId: '',
-    });
+  addTodoToList = (newTodo) => {
+    this.setState(prevState => ({
+      todos: [...prevState.todos, newTodo],
+    }));
   }
 
   render() {
-    const { title, userId, isTitleCorrect, isUserIdCorrect } = this.state;
+    const { todos } = this.state;
 
     return (
       <div className="App">
@@ -85,29 +36,13 @@ class App extends React.Component {
           Add todo form
         </h1>
 
-        { !isTitleCorrect && (
-          <ErrorMessage
-            title="Wrong title"
-            message="Please enter the title."
-          />
-        )}
-
-        { !isUserIdCorrect && (
-          <ErrorMessage
-            title="Wrong user"
-            message="Please choose a user."
-          />
-        )}
-
         <TodoForm
-          title={title}
-          userId={userId}
           users={users}
-          handleChange={this.handleChange}
-          formSubmitHandler={this.formSubmitHandler}
+          addTodo={this.addTodoToList}
+          newTodoId={Math.max(...todos.map(todo => todo.id)) + 1}
         />
 
-        <TodoList todos={todos} />
+        <TodoList todos={this.state.todos} />
       </div>
     );
   }
--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,71 +1,148 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { UserType } from '../../types';
+import { ErrorMessage } from '../ErrorMessage';
 
 import './TodoForm.css';
 
-export const TodoForm = ({
-  title,
-  userId,
-  users,
-  handleChange,
-  formSubmitHandler,
-}) => (
-  <form>
-    <label htmlFor="title" className="label">
-      <span className="label__text">Title:</span>
-      <input
-        type="text"
-        name="title"
-        id="title"
-        placeholder="Your todo"
-        value={title}
-        onChange={handleChange}
-        className="input"
-      />
-    </label>
+export class TodoForm extends React.Component {
+  state = {
+    title: '',
+    isTitleCorrect: true,
+    userId: '',
+    isUserIdCorrect: true,
+  };
 
-    <br />
+  findUser = id => this.props.users.find(user => user.id === id);
 
-    <label htmlFor="user" className="label">
-      <span className="label__text">User:</span>
-      <select
-        name="userId"
-        id="user"
-        value={userId}
-        onChange={handleChange}
-        className="input"
-      >
-        <option
-          value=""
-        >
-          Choose user
-        </option>
-        {users.map(user => (
-          <option
-            value={user.id}
-            key={user.id}
+  validateForm = (event) => {
+    event.preventDefault();
+
+    const { title, userId } = this.state;
+    const { addTodo, newTodoId } = this.props;
+
+    let isDataCorrect = true;
+
+    if (title.trim().length === 0) {
+      this.setState({ isTitleCorrect: false });
+      isDataCorrect = false;
+    }
+
+    if (userId.length === 0) {
+      this.setState({ isUserIdCorrect: false });
+      isDataCorrect = false;
+    }
+
+    if (isDataCorrect) {
+      addTodo({
+        userId: Number(userId),
+        id: newTodoId,
+        title,
+        completed: false,
+        user: this.findUser(Number(userId)),
+      });
+
+      this.clearForm();
+    }
+  }
+
+  clearForm = () => {
+    this.setState({
+      title: '',
+      userId: '',
+    });
+  }
+
+  handleChange = (event) => {
+    let { value } = event.target;
+    const { name } = event.target;
+
+    if (name === 'title') {
+      value = value.replace(/[^A-Za-z0-9 ]/g, '');
+    }
+
+    this.setState({
+      [name]: value,
+      [`is${name[0].toUpperCase() + name.slice(1)}Correct`]: true,
+    });
+  }
+
+  render() {
+    const { users } = this.props;
+    const { title, isTitleCorrect, userId, isUserIdCorrect } = this.state;
+
+    return (
+      <>
+        { !isTitleCorrect && (
+          <ErrorMessage
+            title="Wrong title"
+            message="Please enter the title."
+          />
+        )}
+
+        { !isUserIdCorrect && (
+          <ErrorMessage
+            title="Wrong user"
+            message="Please choose a user."
+          />
+        )}
+
+        <form>
+          <label htmlFor="title" className="label">
+            <span className="label__text">Title:</span>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              placeholder="Your todo"
+              value={title}
+              onChange={this.handleChange}
+              className="input"
+            />
+          </label>
+
+          <br />
+
+          <label htmlFor="user" className="label">
+            <span className="label__text">User:</span>
+            <select
+              name="userId"
+              id="user"
+              value={userId}
+              onChange={this.handleChange}
+              className="input"
+            >
+              <option
+                value=""
+              >
+                Choose user
+              </option>
+              {users.map(user => (
+                <option
+                  value={user.id}
+                  key={user.id}
+                >
+                  {user.name}
+                </option>
+              ))}
+            </select>
+          </label>
+
+          <button
+            type="submit"
+            onClick={this.validateForm}
+            className="button"
           >
-            {user.name}
-          </option>
-        ))}
-      </select>
-    </label>
-
-    <button
-      type="submit"
-      onClick={formSubmitHandler}
-      className="button"
-    >
-      Add todo!
-    </button>
-  </form>
-);
+            Add todo!
+          </button>
+        </form>
+      </>
+    );
+  }
+}
 
 TodoForm.propTypes = {
-  title: PropTypes.string.isRequired,
-  userId: PropTypes.string.isRequired,
+  newTodoId: PropTypes.number.isRequired,
   users: PropTypes.arrayOf(UserType).isRequired,
-  handleChange: PropTypes.func.isRequired,
-  formSubmitHandler: PropTypes.func.isRequired,
+  addTodo: PropTypes.func.isRequired,
 };
