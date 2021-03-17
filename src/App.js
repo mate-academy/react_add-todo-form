b158b28cd94e777bf3ab57192e9b420c2f776256
Misha Lysak <myhaillysak@gmail.com>
2021-03-17 16:35:03 +0200

    asdgofvnc;kandsoigjas;dx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Form } from './component/Form';
+import { TodoList } from './component/TodoList';
 import './App.css';
 
 import users from './api/users';
@@ -10,11 +11,88 @@ const todoFromServer = todos.map(todo => ({
   user: users.find(user => user.id === todo.userId),
 }));
 
-export const App = () => (
-  <div className="App">
-    <h1>Add todo form</h1>
-    <Form todos={todoFromServer} users={users} />
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    todos: todoFromServer,
+    selectedUserId: 0,
+    title: '',
+    isTitleWritten: false,
+    isUserWritten: false,
+  }
+
+  changeTitleHandler = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+      isTitleWritten: false,
+    });
+  }
+
+  selectUserHandler = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: +value,
+      isUserWritten: false,
+    });
+  }
+
+  addTodo = (event) => {
+    const { title, selectedUserId, isTitleWritten, isUserWritten } = this.state;
+
+    event.preventDefault();
+
+    if (!title || selectedUserId === 0) {
+      this.setState({
+        isTitleWritten: !isTitleWritten,
+        isUserWritten: isUserWritten !== 0,
+      });
+
+      return;
+    }
+
+    this.setState((state) => {
+      const newTodo = {
+        id: state.todos.length + 1,
+        title,
+        completed: false,
+        userId: selectedUserId,
+        user: users.find(user => user.id === selectedUserId),
+      };
+
+      return ({
+        selectedUserId: 0,
+        title: '',
+        todos: [...state.todos, newTodo],
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Add todo form</h1>
+        <Form
+          users={users}
+          addTodo={this.addTodo}
+          changeTitleHandler={this.changeTitleHandler}
+          selectUserHandler={this.selectUserHandler}
+          selectedUserId={this.state.selectedUserId}
+          title={this.state.title}
+          isTitleWritten={this.state.isTitleWritten}
+          isUserWritten={this.state.isUserWritten}
+        />
+        <TodoList todos={this.state.todos} />
+        {this.state.isTitleWritten && (
+          <div className="isTitleWritten">Dude, write some title</div>
+        )}
+        {this.state.isUserWritten && (
+          <div className="isUserWritten">Mate, u didnt choose user</div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/component/Form/Form.jsx
+++ src/component/Form/Form.jsx
@@ -1,109 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TodoList } from '../TodoList';
 import { Select } from '../Select';
 
-export class Form extends React.Component {
-  state = {
-    selectedUserId: 0,
-    title: '',
-    isTitleWritten: false,
-    isUserWritten: false,
-    todos: this.props.todos,
-  }
+export const Form = (props) => {
+  const {
+    selectedUserId,
+    title,
+    changeTitleHandler,
+    selectUserHandler,
+    addTodo,
+    users,
+  } = props;
 
-  changeTitleHandler = (event) => {
-    const { name, value } = event.target;
-
-    this.setState({
-      [name]: value,
-      isTitleWritten: false,
-    });
-  }
-
-  selectUserHandler = (event) => {
-    const { name, value } = event.target;
-
-    this.setState({
-      [name]: +value,
-      isUserWritten: false,
-    });
-  }
-
-  addTodo = (event) => {
-    const { title, selectedUserId, isTitleWritten, isUserWritten } = this.state;
-
-    event.preventDefault();
-
-    if (!title || selectedUserId === 0) {
-      this.setState({
-        isTitleWritten: !isTitleWritten,
-        isUserWritten: isUserWritten !== 0,
-      });
-
-      return;
-    }
-
-    this.setState((state) => {
-      const newTodo = {
-        id: state.todos.length + 1,
-        title,
-        completed: false,
-        userId: selectedUserId,
-        user: this.props.users.find(user => user.id === selectedUserId),
-      };
-
-      return ({
-        selectedUserId: 0,
-        title: '',
-        todos: [...state.todos, newTodo],
-      });
-    });
-  };
-
-  render() {
-    const {
-      selectedUserId,
-      title,
-      isTitleWritten,
-      isUserWritten,
-    } = this.state;
-
-    return (
-      <>
-        <form method="GET">
-          <label>
-            <input
-              type="text"
-              name="title"
-              placeholder="Please enter the title"
-              value={title}
-              onChange={this.changeTitleHandler}
-            />
-          </label>
-          <Select
-            selectUserHandler={this.selectUserHandler}
-            selectedUserId={selectedUserId}
-            users={this.props.users}
+  return (
+    <>
+      <form method="GET">
+        <label>
+          <input
+            type="text"
+            name="title"
+            placeholder="Please enter the title"
+            value={title}
+            onChange={changeTitleHandler}
           />
-          <button
-            type="submit"
-            onClick={this.addTodo}
-          >
-            Add
-          </button>
-        </form>
-        <TodoList todos={this.state.todos} />
-        {isTitleWritten && (
-          <div className="isTitleWritten">Dude, write some title</div>
-        )}
-        {isUserWritten && (
-          <div className="isUserWritten">Mate, u didnt choose user</div>
-        )}
-      </>
-    );
-  }
-}
+        </label>
+        <Select
+          selectUserHandler={selectUserHandler}
+          selectedUserId={selectedUserId}
+          users={users}
+        />
+        <button
+          type="submit"
+          onClick={addTodo}
+        >
+          Add
+        </button>
+      </form>
+    </>
+  );
+};
 
 Form.propTypes = {
   users: PropTypes.arrayOf(
@@ -112,16 +47,9 @@ Form.propTypes = {
       id: PropTypes.number.isRequired,
     }).isRequired,
   ).isRequired,
-  todos: PropTypes.arrayOf(
-    PropTypes.shape({
-      userId: PropTypes.number.isRequired,
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      completed: PropTypes.bool.isRequired,
-      user: PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        name: PropTypes.string.isRequired,
-      }).isRequired,
-    }).isRequired,
-  ).isRequired,
+  selectedUserId: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  changeTitleHandler: PropTypes.func.isRequired,
+  selectUserHandler: PropTypes.func.isRequired,
+  addTodo: PropTypes.func.isRequired,
 };
