94bb2748583a09e7bd08a7934f823e21f4a6048b
Misha Lysak <myhaillysak@gmail.com>
2021-03-22 15:25:23 +0200

    make maybe perfect this task and will put him in my cv))

--- src/App.js
+++ src/App.js
@@ -14,59 +14,10 @@ const todoFromServer = todos.map(todo => ({
 export class App extends React.Component {
   state = {
     todos: todoFromServer,
-    selectedUserId: 0,
-    title: '',
-    isTitleWritten: false,
-    isUserWritten: false,
   }
 
-  changeTitleHandler = (event) => {
-    const { name, value } = event.target;
-
-    this.setState({
-      [name]: value,
-      isTitleWritten: false,
-    });
-  }
-
-  selectUserHandler = (event) => {
-    const { name, value } = event.target;
-
-    this.setState({
-      [name]: +value,
-      isUserWritten: false,
-    });
-  }
-
-  addTodo = (event) => {
-    const { title, selectedUserId, isTitleWritten, isUserWritten } = this.state;
-
-    event.preventDefault();
-
-    if (!title || selectedUserId === 0) {
-      this.setState({
-        isTitleWritten: !isTitleWritten,
-        isUserWritten: isUserWritten !== 0,
-      });
-
-      return;
-    }
-
-    this.setState((state) => {
-      const newTodo = {
-        id: state.todos.length + 1,
-        title,
-        completed: false,
-        userId: selectedUserId,
-        user: users.find(user => user.id === selectedUserId),
-      };
-
-      return ({
-        selectedUserId: 0,
-        title: '',
-        todos: [...state.todos, newTodo],
-      });
-    });
+  addTodo = (newTodo) => {
+    this.setState(prevState => ({ todos: [...prevState.todos, newTodo] }));
   };
 
   render() {
@@ -76,20 +27,8 @@ export class App extends React.Component {
         <Form
           users={users}
           addTodo={this.addTodo}
-          changeTitleHandler={this.changeTitleHandler}
-          selectUserHandler={this.selectUserHandler}
-          selectedUserId={this.state.selectedUserId}
-          title={this.state.title}
-          isTitleWritten={this.state.isTitleWritten}
-          isUserWritten={this.state.isUserWritten}
         />
         <TodoList todos={this.state.todos} />
-        {this.state.isTitleWritten && (
-          <div className="isTitleWritten">Dude, write some title</div>
-        )}
-        {this.state.isUserWritten && (
-          <div className="isUserWritten">Mate, u didnt choose user</div>
-        )}
       </div>
     );
   }
--- src/component/Form/Form.jsx
+++ src/component/Form/Form.jsx
@@ -2,41 +2,113 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { SelectUser } from '../SelectUser';
 
-export const Form = (props) => {
-  const {
-    selectedUserId,
-    title,
-    changeTitleHandler,
-    selectUserHandler,
-    addTodo,
-    users,
-  } = props;
+export class Form extends React.Component {
+  state = {
+    selectedUserId: 0,
+    title: '',
+    isTitleWritten: false,
+    isUserWritten: false,
+  };
 
-  return (
-    <form method="GET">
-      <label>
-        <input
-          type="text"
-          name="title"
-          placeholder="Please enter the title"
-          value={title}
-          onChange={changeTitleHandler}
-        />
-      </label>
-      <SelectUser
-        selectUserHandler={selectUserHandler}
-        selectedUserId={selectedUserId}
-        users={users}
-      />
-      <button
-        type="submit"
-        onClick={addTodo}
-      >
-        Add
-      </button>
-    </form>
-  );
-};
+  changeTitleHandler = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+      isTitleWritten: false,
+    });
+  }
+
+  selectUserHandler = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: +value,
+      isUserWritten: false,
+    });
+  }
+
+  addTodoInForm = (e) => {
+    e.preventDefault();
+
+    const {
+      title,
+      selectedUserId,
+      isTitleWritten,
+      isUserWritten,
+    } = this.state;
+
+    if (!title || selectedUserId === 0) {
+      this.setState({
+        isTitleWritten: !isTitleWritten,
+        isUserWritten: isUserWritten !== 0,
+      });
+
+      return;
+    }
+
+    const newTodo = {
+      id: this.props.users.length + 1,
+      title,
+      completed: false,
+      userId: selectedUserId,
+      user: this.props.users.find(user => user.id === selectedUserId),
+    };
+
+    this.props.addTodo(newTodo);
+
+    this.setState({
+      selectedUserId: 0,
+      title: '',
+    });
+  }
+
+  render() {
+    const {
+      title,
+      selectedUserId,
+      isTitleWritten,
+      isUserWritten,
+    } = this.state;
+
+    const {
+      users,
+    } = this.props;
+
+    return (
+      <>
+        <form method="GET">
+          <label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Please enter the title"
+              value={title}
+              onChange={this.changeTitleHandler}
+            />
+          </label>
+          <SelectUser
+            selectUserHandler={this.selectUserHandler}
+            selectedUserId={selectedUserId}
+            users={users}
+          />
+          <button
+            type="submit"
+            onClick={this.addTodoInForm}
+          >
+            Add
+          </button>
+        </form>
+        {isTitleWritten && (
+          <div className="isTitleWritten">Dude, write some title</div>
+        )}
+        {isUserWritten && (
+          <div className="isUserWritten">Mate, u didnt choose user</div>
+        )}
+      </>
+    );
+  }
+}
 
 Form.propTypes = {
   users: PropTypes.arrayOf(
