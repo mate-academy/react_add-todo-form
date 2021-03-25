3df8f5a40901d45ac15ec6be38a008784ded689d
anastasiia <anastasiia.aliinyk@gmail.com>
2021-03-25 23:44:18 +0200

    add styles

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TodoList } from './TodoList';
+import { TodoList } from '../TodoList';
+import './Form.css';
 
 export class Form extends React.Component {
   state = {
