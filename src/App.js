0555c36845d452cc365d45a6b73026c495f16115
Nikita Puhovskii <cheliojul@gmail.com>
2021-03-26 09:47:52 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React from 'react';
+import todosFromServer from './api/todos';
+import users from './api/users';
+import { TodoList } from './components/TodoList';
+import { TodoForm } from './components/TodoForm';
+
 import './App.css';
 
-import users from './api/users';
+const preparedTodos = todosFromServer.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId),
+}));
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+class App extends React.Component {
+  state = {
+    todos: preparedTodos,
+  };
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+  addTodoToList = (newTodo) => {
+    this.setState(prevState => ({
+      todos: [...prevState.todos, newTodo],
+    }));
+  }
+
+  render() {
+    const { todos } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Todo Form
+        </h1>
+
+        <TodoForm
+          users={users}
+          addTodo={this.addTodoToList}
+          newTodoId={Math.max(...todos.map(todo => todo.id)) + 1}
+        />
+
+        <TodoList todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
