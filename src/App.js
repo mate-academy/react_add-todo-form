27a900cb8526e24593236258872bc693ba13975d
Vitalii Skrynkovych <vitaliiskrynkovych@gmail.com>
2021-03-29 19:27:34 +0300

    added state to form component

--- src/App.js
+++ src/App.js
@@ -5,94 +5,24 @@ import './App.css';
 import users from './api/users';
 import todos from './api/todos';
 
-const { uuid } = require('uuidv4');
-
 class App extends React.Component {
   state = {
     todos,
-    selectedUserName: 'Choose User',
-    title: '',
-    shouldCreateTitleError: false,
-    shouldCreateUserError: false,
   }
 
-  handleSelectChange = (event) => {
-    this.setState({
-      selectedUserName: event.target.value,
-    });
-
-    if (event.target.value !== 'Choose User') {
-      this.setState({
-        shouldCreateUserError: false,
-      });
-    }
-  }
-
-  handleInputChange = (event) => {
-    this.setState({
-      title: event.target.value,
-    });
-    if (event.target.value !== '') {
-      this.setState({
-        shouldCreateTitleError: false,
-      });
-    }
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    if (this.state.title === '') {
-      this.setState({
-        shouldCreateTitleError: true,
-      });
-    }
-
-    if (this.state.selectedUserName === 'Choose User') {
-      this.setState({
-        shouldCreateUserError: true,
-      });
-    }
-
-    if (this.state.selectedUserName === 'Choose User'
-      || this.state.title === '') {
-      return;
-    }
-
-    const createdTodo = {
-      userId: users.find(user => user.name === this.state.selectedUserName).id,
-      id: uuid(),
-      title: this.state.title,
-      completed: false,
-    };
-
-    this.setState(prevState => ({
-      todos: [...prevState.todos, createdTodo],
-      selectedUserName: 'Choose User',
-      title: '',
+  addTodo = (todo) => {
+    this.setState(prevsState => ({
+      todos: [...prevsState.todos, todo],
     }));
-  }
+  };
 
   render() {
     return (
       <div className="App">
         <h1>Add todo form</h1>
-        {this.state.shouldCreateTitleError && (
-          <p className="error">
-            Warning: Please enter the title!!!
-          </p>
-        )}
-        {this.state.shouldCreateUserError && (
-          <p className="error">
-            Warning: Please choose a user
-          </p>
-        )}
         <Form
           users={users}
-          onSubmit={this.handleSubmit}
-          title={this.state.title}
-          onInputChange={this.handleInputChange}
-          onSelectChange={this.handleSelectChange}
-          selectedUserName={this.state.selectedUserName}
+          addTodo={this.addTodo}
         />
         <ul className="todolist">
           {this.state.todos.map(todo => (
--- src/Form.js
+++ src/Form.js
@@ -1,40 +1,117 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Form = ({
-  users,
-  onSubmit,
-  title,
-  onInputChange,
-  onSelectChange,
-  selectedUserName,
-}) => (
-  <form onSubmit={onSubmit}>
-    <input
-      type="text"
-      name="title"
-      placeholder="Title"
-      value={title}
-      onChange={onInputChange}
-    />
-    <select
-      value={selectedUserName}
-      onChange={onSelectChange}
-    >
-      <option>
-        Choose User
-      </option>
-      {users.map(user => (
-        <option key={user.id}>{user.name}</option>
-      ))}
-    </select>
-    <button
-      type="submit"
-    >
-      Add
-    </button>
-  </form>
-);
+const { uuid } = require('uuidv4');
+
+export class Form extends React.Component {
+  state = {
+    selectedUserName: 'Choose User',
+    title: '',
+    shouldCreateTitleError: false,
+    shouldCreateUserError: false,
+  }
+
+  handleSelectChange = (event) => {
+    this.setState({
+      selectedUserName: event.target.value,
+    });
+
+    if (event.target.value !== 'Choose User') {
+      this.setState({
+        shouldCreateUserError: false,
+      });
+    }
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      title: event.target.value,
+    });
+    if (event.target.value !== '') {
+      this.setState({
+        shouldCreateTitleError: false,
+      });
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.title === '') {
+      this.setState({
+        shouldCreateTitleError: true,
+      });
+    }
+
+    if (this.state.selectedUserName === 'Choose User') {
+      this.setState({
+        shouldCreateUserError: true,
+      });
+    }
+
+    if (this.state.selectedUserName === 'Choose User'
+      || this.state.title === '') {
+      return;
+    }
+
+    const createdTodo = {
+      userId: this.props.users.find(
+        user => user.name === this.state.selectedUserName,
+      ).id,
+      id: uuid(),
+      title: this.state.title,
+      completed: false,
+    };
+
+    this.props.addTodo(createdTodo);
+
+    this.setState({
+      selectedUserName: 'Choose User',
+      title: '',
+    });
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.shouldCreateTitleError && (
+          <p className="error">
+            Warning: Please enter the title!!!
+          </p>
+        )}
+        {this.state.shouldCreateUserError && (
+          <p className="error">
+            Warning: Please choose a user
+          </p>
+        )}
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={this.state.title}
+            onChange={this.handleInputChange}
+          />
+          <select
+            value={this.state.selectedUserName}
+            onChange={this.handleSelectChange}
+          >
+            <option>
+              Choose User
+            </option>
+            {this.props.users.map(user => (
+              <option key={user.id}>{user.name}</option>
+            ))}
+          </select>
+          <button
+            type="submit"
+          >
+            Add
+          </button>
+        </form>
+      </>
+    );
+  }
+}
 
 Form.propTypes = {
   users: PropTypes.arrayOf(
@@ -43,10 +120,5 @@ Form.propTypes = {
       name: PropTypes.string.isRequired,
     }).isRequired,
   ).isRequired,
-
-  onSubmit: PropTypes.func.isRequired,
-  title: PropTypes.string.isRequired,
-  onInputChange: PropTypes.func.isRequired,
-  onSelectChange: PropTypes.func.isRequired,
-  selectedUserName: PropTypes.string.isRequired,
+  addTodo: PropTypes.func.isRequired,
 };
