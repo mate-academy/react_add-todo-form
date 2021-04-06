fd079bac102fd9af620c84fe405d39c2cfaa58f3
Oleg Bezriadin <muhachov86@gmail.com>
2021-04-06 08:23:33 +0300

    App.js => item renamed to todo

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import { Form } from './components/Form/Form';
 import todos from './api/todos';
 import users from './api/users';
 
-const preparedTodos = todos.map(item => ({
-  ...item,
-  user: users.find(human => human.id === item.userId),
+const preparedTodos = todos.map(todo => ({
+  ...todo,
+  user: users.find(human => human.id === todo.userId),
 }));
 
 export class App extends Component {
