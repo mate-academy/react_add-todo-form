4310d5679c455b66f88317a82f6a26ee75d39806
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-04-08 20:10:02 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -2,18 +2,45 @@ import React from 'react';
 import './App.css';
 
 import users from './api/users';
+import todos from './api/todos';
+import { Form } from './components/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const todoList = todos.map(todo => ({
+  ...todo,
+  user: users.find(person => person.id === todo.userId),
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+export class App extends React.PureComponent {
+  state = {
+    list: todoList,
+  }
+
+  addNewUser = (userName, newTitle) => {
+    this.setState(prev => ({
+      list: [
+        ...prev.list,
+        {
+          id: +new Date(),
+          title: newTitle,
+          completed: false,
+          user: {
+            name: userName,
+          },
+        },
+      ],
+    }));
+  }
+
+  render() {
+    const { list } = this.state;
+
+    return (
+      <>
+        <Form
+          list={list}
+          addNewUser={this.addNewUser}
+        />
+      </>
+    );
+  }
 }
-
-export default App;
