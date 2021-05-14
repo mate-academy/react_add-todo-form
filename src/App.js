714e932e4c4bb040958f545bcb51644481fd7c82
Yevgeniy Podobreev <podobr_2018@ukr.net>
2021-05-14 23:36:54 +0300

    Fixes after code review

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import initTodos from './api/todos';
 import initUsers from './api/users';
+import NewTodoForm from './NewTodoForm';
 
 const preparedTodos = initTodos.map(todo => (
   {
@@ -16,76 +17,27 @@ const preparedTodos = initTodos.map(todo => (
 class App extends React.Component {
   state = {
     todos: preparedTodos,
-    user: '',
-    title: '',
-    hasErrors: {
-      userError: false,
-      titleError: false,
-    },
   }
 
-  handleChange = (event) => {
-    const { name, value } = event.target;
+  addTodo = (title, selectedUser) => {
+    this.setState((state) => {
+      const newTodo = {
+        title,
+        userId: initUsers
+          .find(person => person.name === selectedUser).id,
+        user: initUsers
+          .find(person => person.name === selectedUser),
+        id: state.todos.length + 1,
+      };
 
-    this.setState(state => ({
-      [name]: value,
-      hasErrors: {
-        ...state.hasErrors,
-        [name]: false,
-      },
-    }));
-  }
-
-  submitTodo = (event) => {
-    event.preventDefault();
-
-    const { user, title } = this.state;
-
-    if (title && user) {
-      this.setState((state) => {
-        const newTodo = {
-          title: state.title,
-          userId: initUsers
-            .find(person => person.name === state.user).id,
-          user: initUsers
-            .find(person => person.name === state.user),
-          id: state.todos.length + 1,
-        };
-
-        return ({
-          todos: [...state.todos, newTodo],
-          user: '',
-          title: '',
-          hasErrors: {
-            userError: false,
-            titleError: false,
-          },
-        });
+      return ({
+        todos: [...state.todos, newTodo],
       });
-    }
-
-    if (!user) {
-      this.setState(state => ({
-        hasErrors: {
-          ...state.hasErrors,
-          userError: true,
-        },
-      }));
-    }
-
-    if (!title) {
-      this.setState(state => ({
-        hasErrors: {
-          ...state.hasErrors,
-          titleError: true,
-        },
-      }));
-    }
+    });
   }
 
   render() {
-    const { todos, user, title } = this.state;
-    const { titleError, userError } = this.state.hasErrors;
+    const { todos } = this.state;
 
     return (
       <div className="App">
@@ -96,58 +48,10 @@ class App extends React.Component {
           {todos.length}
         </p>
 
-        <form onSubmit={this.submitTodo}>
-          <div>
-            <label htmlFor="title">
-              Enter title
-              <input
-                type="text"
-                name="title"
-                placeholder="Title"
-                value={title}
-                onChange={this.handleChange}
-              />
-            </label>
-            {titleError && (
-              <div>
-                Please enter the title
-              </div>
-            )}
-          </div>
-          <div>
-            <label htmlFor="user">
-              Select User
-              <select
-                type="text"
-                name="user"
-                value={user}
-                onChange={this.handleChange}
-              >
-                <option value="">
-                  Choose a user
-                </option>
-                {initUsers.map(person => (
-                  <option
-                    value={person.name}
-                    key={person.id}
-                  >
-                    {person.name}
-                  </option>
-                ))}
-              </select>
-            </label>
-            {userError && (
-              <div>
-                Please select a user
-              </div>
-            )}
-          </div>
-          <button
-            type="submit"
-          >
-            click
-          </button>
-        </form>
+        <NewTodoForm
+          addTodo={this.addTodo}
+          initUsers={initUsers}
+        />
 
         <TodoList todos={todos} />
       </div>
---
+++ src/NewTodoForm.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class NewTodoForm extends React.Component {
+  state = {
+    user: '',
+    title: '',
+    hasErrors: {
+      userError: false,
+      titleError: false,
+    },
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState(state => ({
+      [name]: value,
+      hasErrors: {
+        ...state.hasErrors,
+        [name]: false,
+      },
+    }));
+  }
+
+  submitTodo = (event) => {
+    event.preventDefault();
+
+    const { user, title } = this.state;
+
+    if (title && user) {
+      this.props.addTodo(title, user);
+      this.setState({
+        user: '',
+        title: '',
+        hasErrors: {
+          userError: false,
+          titleError: false,
+        },
+      });
+    }
+
+    if (!user) {
+      this.setState(state => ({
+        hasErrors: {
+          ...state.hasErrors,
+          userError: true,
+        },
+      }));
+    }
+
+    if (!title) {
+      this.setState(state => ({
+        hasErrors: {
+          ...state.hasErrors,
+          titleError: true,
+        },
+      }));
+    }
+  }
+
+  render() {
+    const { user, title } = this.state;
+    const { titleError, userError } = this.state.hasErrors;
+    const { initUsers } = this.props;
+
+    return (
+      <form onSubmit={this.submitTodo}>
+        <div>
+          <label htmlFor="title">
+            Enter title
+            <input
+              type="text"
+              name="title"
+              placeholder="Title"
+              value={title}
+              onChange={this.handleChange}
+            />
+          </label>
+          {titleError && (
+            <div>
+              Please enter the title
+            </div>
+          )}
+        </div>
+        <div>
+          <label htmlFor="user">
+            Select User
+            <select
+              type="text"
+              name="user"
+              value={user}
+              onChange={this.handleChange}
+            >
+              <option value="">
+                Choose a user
+              </option>
+              {initUsers.map(person => (
+                <option
+                  value={person.name}
+                  key={person.id}
+                >
+                  {person.name}
+                </option>
+              ))}
+            </select>
+          </label>
+          {userError && (
+            <div>
+              Please select a user
+            </div>
+          )}
+        </div>
+        <button
+          type="submit"
+        >
+          click
+        </button>
+      </form>
+    );
+  }
+}
+
+NewTodoForm.propTypes = {
+  initUsers: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+  })).isRequired,
+  addTodo: PropTypes.func.isRequired,
+};
+export default NewTodoForm;
