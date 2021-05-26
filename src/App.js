88d2dc61848d1d3be2053e8743be66631994c451
Ruslana Podshyvalova <lanataylorpersonal@gmail.com>
2021-05-27 00:48:39 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,96 @@
 import React from 'react';
 import './App.css';
-
+import todos from './api/todos';
 import users from './api/users';
+import { TodoList } from './components/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+class App extends React.Component {
+  state = {
+    user: '',
+    title: '',
+    preparedTodos: [],
+  }
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+  componentDidMount() {
+    const data = todos.map(todo => ({
+      ...todo,
+      user: users.find(user => user.id === todo.userId),
+    }));
+
+    this.setState({ preparedTodos: data });
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { user, title, preparedTodos } = this.state;
+    const userId = Number(user.split('*').shift());
+
+    const newTodo = {
+      userId,
+      id: preparedTodos.length + 1,
+      title,
+      completed: false,
+      user: users.find(person => person.id === userId),
+    };
+
+    this.setState({ preparedTodos: [
+      newTodo,
+      ...preparedTodos,
+    ] });
+
+    this.setState({
+      user: '',
+      title: '',
+    });
+  }
+
+  render() {
+    const { user, title, preparedTodos } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Add a new task</h1>
+        <form
+          onSubmit={this.handleSubmit}
+          className="form"
+        >
+          <select
+            name="user"
+            value={user}
+            onChange={this.handleChange}
+          >
+            <option value="">
+              Select user
+            </option>
+            {users.map(({ id, name }) => (
+              <option key={id} value={`${id}*${name}`}>{name}</option>
+            ))}
+          </select>
+          <p hidden>Please choose a user</p>
+          <label htmlFor="title">Input todo title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={title}
+            onChange={this.handleChange}
+          />
+          <p hidden>Please enter the title</p>
+          <button type="submit">Create a task</button>
+        </form>
+
+        <TodoList list={preparedTodos} />
+      </div>
+    );
+  }
 }
 
 export default App;
