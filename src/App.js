a391451fd5f31b1383162e8de10a763bd26e4ca1
Nazar Matsevych <nazar6192111@gmail.com>
2021-05-27 19:16:49 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import './App.css';
+import AddTodoForm from './components/AddTodoForm/AddTodoForm';
+import TodoList from './components/TodoList/TodoList';
 
 import users from './api/users';
+import todos from './api/todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const getUserById = userId => users.find(user => user.id === userId);
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+const preparedTodos = todos.map(todo => ({
+  ...todo,
+  user: getUserById(todo.userId),
+}));
+
+class App extends React.Component {
+  state = {
+    todos: preparedTodos,
+  };
+
+  addTodo = (todoName, userId) => {
+    const newTodo = {
+      id: Math.random(),
+      title: todoName,
+      userId,
+      completed: false,
+      user: getUserById(userId),
+    };
+
+    this.setState(state => ({
+      todos: [...state.todos, newTodo],
+    }));
+  }
+
+  render() {
+    // eslint-disable-next-line
+    const { todos } = this.state;
+
+    return (
+      <div className="App">
+        <AddTodoForm onAdd={this.addTodo} />
+        <TodoList todos={todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
