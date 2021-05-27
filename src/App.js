eb3051a0854df508ff3e6feb92ce67fa8cc23a2d
Pavel Kuzmenko <pashokkuzmenko@gmail.com>
2021-05-27 13:54:51 +0300

    Solution v0.1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,140 @@
 import React from 'react';
 import './App.css';
+import { TodoList } from './components/TodoList';
 
+import todos from './api/todos';
 import users from './api/users';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const preparedTodos = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId),
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: preparedTodos,
+    user: '',
+    title: '',
+    isCorrectTitle: false,
+    isCorrectUserName: false,
+  }
+
+  addTodo = () => {
+    this.setState(state => ({
+      todos: [...state.todos, {
+        userId: users.find(user => user.name === state.user).id,
+        id: state.todos[state.todos.length - 1].id + 1,
+        title: state.title,
+        completed: false,
+        user: users.find(user => user.name === state.user),
+      }],
+    }));
+  }
+
+  hendlerFormChange = (event) => {
+    const { user, title } = this.state;
+
+    event.preventDefault();
+    this.setState(state => ({
+      isCorrectTitle: !title,
+      isCorrectUserName: !user,
+    }));
+
+    if (!user) {
+      return;
+    }
+
+    if (!title) {
+      return;
+    }
+
+    this.addTodo();
+    this.setState({
+      user: '',
+      title: '',
+    });
+  }
+
+  render() {
+    const {
+      isCorrectTitle,
+      isCorrectUserName,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Add todo form</h1>
+
+        <p>
+          <span>Users: </span>
+          {users.length}
+        </p>
+        <form
+          className="form"
+          onSubmit={this.hendlerFormChange}
+        >
+          <label
+            htmlFor="title"
+          >
+            Title
+          </label>
+          <input
+            value={this.state.title}
+            id="title"
+            onChange={(event) => {
+              this.setState({
+                title: event.target.value,
+                isCorrectTitle: (event.target.value.length < 3),
+              });
+            }}
+          />
+          {isCorrectTitle && (
+            <span className="error">
+              please enter a title longer than 3 characters
+            </span>
+          )}
+          <label
+            htmlFor="users"
+          >
+            User
+          </label>
+          <select
+            id="users"
+            value={this.state.user}
+            onChange={(event) => {
+              this.setState({
+                user: event.target.value,
+                isCorrectUserName: !event.target.value,
+              });
+            }}
+          >
+            <option
+              disabled
+              value=""
+            >
+              Select user
+            </option>
+            {users.map(({ id, name }) => (
+              <option key={id} value={name}>
+                {name}
+              </option>
+            ))}
+          </select>
+          {isCorrectUserName && (
+            <span className="error">
+              please selected user
+            </span>
+          )}
+          <button
+            type="submit"
+          >
+            add
+          </button>
+        </form>
+        <TodoList todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/User/User.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './User.scss';
+
+export function User({ name }) {
+  return (
+    <p className="user">
+      {name}
+    </p>
+  );
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+};
+
+User.defaultProps = {
+  name: 'df',
+};
