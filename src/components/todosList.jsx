a71ef64b77fc05abe3369c3bcc6174bb8ecedbff
Taras Kozak <kozaktarasofficial@gmail.com>
2021-05-30 18:59:14 +0300

    v1.04

---
+++ src/components/todosList.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './todosList.css';
+
+import PropTypes from 'prop-types';
+
+export const TodosList = ({ todos }) => (
+  <ul className="App__list">
+    {todos.map(todo => (
+      <li key={todo.id} className="App__item">
+        <h3>{todo.user}</h3>
+
+        <span>
+          {`Task: `}
+          <strong className="App__card-title">{todo.title}</strong>
+        </span>
+
+        <p>
+          {`Status: `}
+          {todo.completed
+            ? <strong className="App__card-completed">Completed</strong>
+            : (
+              <strong className="App__card-notCompleted">
+                Not completed
+              </strong>
+            )
+          }
+        </p>
+      </li>
+    ))}
+  </ul>
+);
+
+TodosList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      user: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+};
