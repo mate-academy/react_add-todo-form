3f4456cbaab38c48c3a1feb1cd432884e7122379
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-19 12:14:02 +0300

    Implemented task

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import React from 'react';
+import { TodoList } from './components/TodoList/TodoList';
+import { AddTodo } from './components/AddTodo/AddTodo';
 import './App.css';
 
+import todos from './api/todos';
 import users from './api/users';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const preparedTodos = todos.map((todo) => {
+  const preparedTodo = { ...todo };
+  const todoUser = users.find(user => user.id === todo.userId);
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+  preparedTodo.user = { ...todoUser };
+
+  return preparedTodo;
+});
+
+class App extends React.Component {
+  state = {
+    todos: preparedTodos,
+  }
+
+  addTodo(todo) {
+    this.setState((state) => {
+      const newTodos = [...state.todos];
+
+      newTodos.push(todo);
+
+      return {
+        todos: newTodos,
+      };
+    });
+  }
+
+  render() {
+    const visibleTodos = [...this.state.todos];
+
+    return (
+      <div className="App">
+        <h1>Add todo form</h1>
+
+        <TodoList preparedTodos={visibleTodos} />
+        <AddTodo
+          users={users}
+          todos={visibleTodos}
+          app={this}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/AddTodo/AddTodo.js
@@ -0,0 +1,179 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { UserType, PreparedTodoType } from '../../types';
+import './AddTodo.css';
+
+export class AddTodo extends React.Component {
+  state = {
+    title: '',
+    userId: '',
+    validUser: true,
+    validTitle: true,
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    let flag;
+
+    switch (name) {
+      case 'title': {
+        flag = 'validTitle';
+        break;
+      }
+
+      case 'userId': {
+        flag = 'validUser';
+        break;
+      }
+
+      default: {
+        break;
+      }
+    }
+
+    this.setState({
+      [name]: value,
+      [flag]: true,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { title, userId } = this.state;
+
+    if (!this.checkInputs(title, userId)) {
+      return;
+    }
+
+    const todoId = this.props.todos.reduce((id, todo) => (
+      id > todo.id ? id : todo.id
+    ), Number.NEGATIVE_INFINITY) + 1;
+    const user = this.props.users.find(todoUser => todoUser.id === +userId);
+    const todo = {
+      userId: +userId,
+      id: todoId,
+      title,
+      completed: false,
+      user,
+    };
+
+    this.props.app.addTodo(todo);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({
+      title: '',
+      userId: '',
+      validUser: true,
+      validTitle: true,
+    });
+  }
+
+  checkInputs = (title, userId) => {
+    let check = true;
+
+    if (title === '') {
+      this.setState({
+        validTitle: false,
+      });
+
+      check = false;
+    }
+
+    if (userId === '') {
+      this.setState({
+        validUser: false,
+      });
+
+      check = false;
+    }
+
+    return check;
+  };
+
+  render() {
+    return (
+      <form
+        className="addTodoForm"
+        onSubmit={this.handleSubmit}
+      >
+        <label
+          htmlFor="title"
+          className="addTodoForm__label"
+        >
+          Title(
+          30 chars maximum
+          ):
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={this.state.title}
+            placeholder="Title"
+            maxLength={30}
+            onChange={this.handleChange}
+          />
+          {(this.state.validTitle
+            ? ''
+            : (
+              <span
+                className="addTodoForm__label-message"
+              >
+                Please enter the title
+              </span>
+            )
+            )}
+        </label>
+        <label
+          htmlFor="userId"
+          className="addTodoForm__label"
+        >
+          User:
+          <select
+            name="userId"
+            id="userId"
+            value={this.state.userId}
+            onChange={this.handleChange}
+          >
+            <>
+              <option value="">
+                Choose a user
+              </option>
+              {this.props.users.map(user => (
+                <option value={user.id} key={user.id}>
+                  {user.name}
+                </option>
+              ))}
+            </>
+          </select>
+          {(this.state.validUser
+            ? ''
+            : (
+              <span
+                className="addTodoForm__label-message"
+              >
+                Please choose a user
+              </span>
+            )
+            )}
+        </label>
+        <button
+          type="submit"
+          className="addTodoForm__submit"
+        >
+          Add
+        </button>
+      </form>
+    );
+  }
+}
+
+AddTodo.propTypes = {
+  users: PropTypes.arrayOf(UserType).isRequired,
+  todos: PropTypes.arrayOf(PreparedTodoType).isRequired,
+  app: PropTypes.shape({
+    addTodo: PropTypes.func.isRequired,
+  }).isRequired,
+};
