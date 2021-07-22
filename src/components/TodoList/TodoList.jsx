c68ec620948494a63814a3fc16de3640c4a7c74b
Viktor Dzhyranov <v.dzhyranov@gmail.com>
2021-07-22 12:52:13 +0300

    fix

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './TodoList.css';
 import PropTypes from 'prop-types';
 
-const TodoList = ({todos}) => (
+const TodoList = ({ todos }) => (
   <div className="todosList">
     {todos.map(todo => (
       <div className="todo" key={todo.userName + todo.id}>
         <h2>{todo.userName}</h2>
-        <span>{todo.title.replace(/[^\w\s]/g, '')}</span>
+        <span>{todo.title.replace(/[^a-z\u0400-\u04FF]/gi, '')}</span>
         <p>{todo.completed ? 'copmleted' : 'in the process...'}</p>
       </div>
     ))}
