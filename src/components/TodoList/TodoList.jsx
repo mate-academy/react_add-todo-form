5205db908e11c9fe46a0ed893863b41019b6f965
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-07-28 14:43:34 +0300

    feat: completed task todo-form

---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Todo } from '../Todo/Todo';
+// console.log(this.props)
+export const TodoList = ({ todos }) => (
+  <ul>
+    {todos.map(task => (
+      <li key={task.id}>
+        <Todo {...task} />
+      </li>
+    ))}
+  </ul>
+);
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      completed: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
