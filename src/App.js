598691c3318c1b6ee9dec7b2ae6e74edd6d1e011
Sergiy Fartushniak <fartux97@gmail.com>
2021-07-29 18:21:57 +0300

    todo form done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,99 @@
 import React from 'react';
+import { TodoList } from './components/TodoList/TodoList';
 import './App.css';
 
 import users from './api/users';
+import todos from './api/todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const todosWithUsers = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId),
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+export class App extends React.Component {
+  state = {
+    preparedTodos: todosWithUsers,
+    createdTitle: '',
+    userId: 0,
+    isTitleValid: false,
+    isUserValid: false,
+  }
+
+  addTodo = (todo) => {
+    this.setState(state => ({
+      preparedTodos: [...state.preparedTodos, todo],
+    }));
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const {
+      preparedTodos,
+      createdTitle,
+      userId,
+    } = this.state;
+
+    this.setState({
+      isTitleValid: !createdTitle,
+      isUserValid: !userId,
+    });
+
+    if (!createdTitle || !userId) {
+      return;
+    }
+
+    const todo = {
+      id: preparedTodos.length + 1,
+      userId,
+      title: createdTitle,
+      completed: false,
+      user: users.find(user => user.id === userId),
+    };
+
+    this.addTodo(todo);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Add todo form</h1>
+
+        <TodoList todos={this.state.preparedTodos} />
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <input
+              type="text"
+              placeholder="Title"
+              value={this.state.createdTitle}
+              onChange={(event) => {
+                this.setState({ createdTitle: event.target.value });
+              }}
+            />
+            {this.state.isTitleValid
+              && <span className="error"> Please enter the title.</span>}
+          </p>
+          <p>
+            <select
+              required
+              value={this.state.userId}
+              onChange={(event) => {
+                this.setState({ userId: Number(event.target.value) });
+              }}
+            >
+              <option value="0">Choose a user</option>
+              {users.map(user => (
+                <option key={user.id} value={user.id}>
+                  {user.name}
+                </option>
+              ))}
+            </select>
+            {this.state.isUserValid
+              && <span className="error"> Please choose a user.</span>}
+          </p>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Todo } from './Todo';
+
+export const TodoList = ({ todos }) => (
+  <ul className="list">
+    {todos.map(todo => (
+      <li key={todo.id}>
+        <Todo
+          title={todo.title}
+          completed={todo.completed}
+          user={todo.user}
+        />
+      </li>
+    ))}
+  </ul>
+);
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+    user: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }).isRequired,
+  })).isRequired,
+};
