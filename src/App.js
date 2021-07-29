083aa2c87efca89cbfc0f0ce06c0301f799c9625
VadymMakevytss <vadymlat@mail.ru>
2021-07-29 18:39:24 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card } from 'react-bootstrap';
 
+import TodoList from './components/TodoList/TodoList';
+import TodoForm from './components/TodoForm/TodoForm';
 import users from './api/users';
+import todos from './api/todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const prepearedTodos = todos.map(todo => ({
+  id: todo.id,
+  title: todo.title,
+  name: users.find(user => user.id === todo.id).name,
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+export default class App extends Component {
+  state={
+    todoList: [...prepearedTodos],
+    userList: [...users],
+  }
+
+  addUser = (name, title) => {
+    this.setState((prevState) => {
+      const newUser = {
+        id: prevState.todoList.length + 1,
+        title,
+        name,
+      };
+
+      return {
+        todoList: [...prevState.todoList, newUser],
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Card>
+          <Card.Body>
+            <TodoList todoList={this.state.todoList} />
+            <TodoForm
+              userList={this.state.userList}
+              onAdd={this.addUser}
+            />
+          </Card.Body>
+        </Card>
+
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/TodoForm/TodoForm.jsx
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+import './TodoForm.css';
+import { Form, Button } from 'react-bootstrap';
+
+import ErrorModal from '../ErrorModal/ErrorModal';
+
+export default class TodoForm extends Component {
+  state={
+    userList: this.props.userList,
+    userName: '',
+    textareaInput: '',
+    setError: '',
+  }
+
+  handleUserName = (event) => {
+    this.setState({
+      userName: event.target.value,
+      setError: null,
+    });
+  }
+
+  handleTitle = (event) => {
+    this.setState({
+      textareaInput: event.target.value,
+      setError: null,
+    });
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (this.state.userName.length === 0
+      || this.state.textareaInput.trim().length === 0) {
+      this.setState({
+        setError: 'Please fill up all filds',
+      });
+
+      return;
+    }
+
+    this.props.onAdd(this.state.userName, this.state.textareaInput);
+    this.setState({
+      userName: '',
+      textareaInput: '',
+    });
+  }
+
+  render() {
+    const userListWithKey = this.state.userList.map(user => ({
+      key: uuidv4(),
+      ...user,
+    }));
+
+    return (
+      <div>
+        <h2>Select a user</h2>
+        <Form
+          onSubmit={this.handleFormSubmit}
+          className="form todo__form"
+        >
+          <Form.Group
+            className="mb-6 todo__form-group"
+            controlId="exampleForm.ControlTextarea1"
+          >
+            <Form.Label>Print title</Form.Label>
+            <Form.Control
+              value={this.state.textareaInput}
+              name="textarea"
+              onChange={this.handleTitle}
+              as="textarea"
+              rows={3}
+              style={{
+                height: '100px',
+                width: '300px',
+              }}
+            />
+            {
+              this.state.setError
+              && <ErrorModal error={this.state.setError} />
+            }
+          </Form.Group>
+
+          <Form.Group
+            className="mb-3 todo__form-group"
+            controlId="formBasicPassword"
+          >
+            <Form.Select
+              onChange={this.handleUserName}
+              value={this.state.userName}
+              name="select"
+              style={{
+                width: '300px',
+              }}
+            >
+              <option value="0">Choose a user</option>
+              {userListWithKey.map(({ key, name }) => (
+                <option
+                  value={name}
+                  key={key}
+                >
+                  {name}
+                </option>
+              ))}
+            </Form.Select>
+            {
+              this.state.setError
+              && <ErrorModal error={this.state.setError} />
+            }
+          </Form.Group>
+          <Form.Group className="mb-3 todo__form-group">
+            <Button
+              className="todo-form__submit"
+              variant="primary"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Form.Group>
+        </Form>
+      </div>
+    );
+  }
+}
+
+TodoForm.propTypes = {
+  userList: PropTypes.func.isRequired,
+  onAdd: PropTypes.func.isRequired,
+};
