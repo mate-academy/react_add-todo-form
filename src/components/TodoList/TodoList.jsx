d9232fff91032850dd9d24a183b067655c705cb0
Ruslan Vyvdyuk <84183046+Vivdyuk@users.noreply.github.com>
2021-07-30 11:45:20 +0300

    proptypes edited

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -42,28 +42,7 @@ TodoList.propTypes = {
       title: PropTypes.string.isRequired,
       completed: PropTypes.bool.isRequired,
       user: PropTypes.shape({
-        id: PropTypes.number.isRequired,
         name: PropTypes.string.isRequired,
-        username: PropTypes.string.isRequired,
-        email: PropTypes.string.isRequired,
-        phone: PropTypes.string.isRequired,
-        website: PropTypes.string.isRequired,
-        address: PropTypes.shape({
-          street: PropTypes.string.isRequired,
-          suite: PropTypes.string.isRequired,
-          city: PropTypes.string.isRequired,
-          zipcode: PropTypes.string.isRequired,
-          geo: PropTypes.shape({
-            lat: PropTypes.string.isRequired,
-            lng: PropTypes.string.isRequired,
-          }).isRequired,
-        }).isRequired,
-        company: PropTypes.shape({
-          name: PropTypes.string.isRequired,
-          catchPhrase: PropTypes.string.isRequired,
-          bs: PropTypes.string.isRequired,
-        }).isRequired,
-
       }).isRequired,
     }).isRequired,
   ).isRequired,
