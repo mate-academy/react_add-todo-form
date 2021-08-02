705cbea6cd80de1bdcea33d009f1c4d3df684197
tkachukione <tkachukione@gmail.com>
2021-08-02 03:20:25 +0300

    first try

--- src/App.js
+++ src/App.js
@@ -1,19 +1,129 @@
 import React from 'react';
 import './App.css';
-
 import users from './api/users';
+import todosServer from './api/todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const todosList = todosServer.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId),
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+export class App extends React.Component {
+  state = {
+    todos: todosList,
+    title: '',
+    userId: '',
+    isTitleCorrect: false,
+    isUserChosen: false,
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  addTodoContent = (event) => {
+    if (!this.state.title) {
+      this.setState({
+        isTitleCorrect: true,
+      });
+    }
+
+    if (!this.state.userId) {
+      this.setState({
+        isUserChosen: true,
+      });
+    }
+
+    if (this.state.title && this.state.userId) {
+      const newTodos = {
+        title: this.state.title,
+        id: this.state.todos.length + 1,
+        userId: this.state.userId,
+      };
+
+      this.setState(state => ({
+        todos: [...state.todos, newTodos],
+      }));
+
+      this.setState({
+        title: '',
+        userId: '',
+        isTitleCorrect: false,
+        isUserChosen: false,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            this.addTodoContent();
+          }}
+        >
+          <label>
+            Write title for to do list
+            <input
+              className="input"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+            {this.state.isTitleCorrect && (
+              <span className="error">
+                write correct title
+              </span>
+            )}
+          </label>
+          <label className="header">
+            Choose user
+            <select
+              className="input"
+              name="userId"
+              value={this.state.userId}
+              onChange={this.handleChange}
+            >
+              <option>
+                Select user
+              </option>
+              {users.map(({ id, name }) => (
+                <option value={id} key={id}>
+                  {name}
+                </option>
+              ))}
+            </select>
+            {this.state.isUserChosen && (
+              <span className="error">
+                choose a user
+              </span>
+            )}
+          </label>
+          <button
+            type="submit"
+          >
+            Add
+          </button>
+        </form>
+        <ul>
+          {this.state.todos.map(todo => (
+            <li key={todo.id}>
+              <p>
+                {`Title: ${todo.title}`}
+              </p>
+              <p>
+                {`Todo Id: ${todo.id}`}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
-
 export default App;
