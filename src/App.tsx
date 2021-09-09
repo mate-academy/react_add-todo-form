fd13bd16f55935229e4979c37f5c09469cd49305
Alex Hlotov <alexhlotov67@gmail.com>
2021-09-09 19:01:19 +0300

    add new component TodoForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form } from 'react-bootstrap';
 
 import users from './api/users';
 import todos from './api/todos';
 import { TodoList } from './components/TodoList';
 import { Todo } from './types/Todo';
+import { TodoForm } from './components/TodoForm';
 
 const preparedTodos: Todo[] = todos.map(todo => {
   const user = users.find(person => person.id === todo.userId) || null;
@@ -15,157 +14,29 @@ const preparedTodos: Todo[] = todos.map(todo => {
 });
 
 type State = {
-  id: number;
-  todoTitle: string;
   currentTodos: Todo[];
-  userId: string;
-  isUserChoosed: boolean;
-  isTodoInclude: boolean;
 };
 
 class App extends React.Component<{}, State> {
   state: State = {
-    id: 1,
-    todoTitle: '',
     currentTodos: preparedTodos,
-    userId: '0',
-    isUserChoosed: false,
-    isTodoInclude: false,
   };
 
-  handleChangeUser = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const { value } = event.target;
-
-    this.setState({
-      userId: value,
-      isUserChoosed: false,
-    });
-  };
-
-  handleChangeTask = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-
-    this.setState({
-      todoTitle: value,
-      isTodoInclude: false,
-    });
-  };
-
-  addTodo = () => {
-    const { id, userId, todoTitle } = this.state;
-
-    if (todoTitle.trim().length !== 0) {
-      this.setState({ isTodoInclude: false });
-    } else {
-      this.setState({ isTodoInclude: true });
-
-      return;
-    }
-
-    if (+userId !== 0) {
-      this.setState({ isUserChoosed: false });
-    } else {
-      this.setState({ isUserChoosed: true });
-
-      return;
-    }
-
-    const todo: Todo = {
-      user: users.find(user => user.id === +userId) || null,
-      userId: +userId,
-      id,
-      title: todoTitle.toLocaleUpperCase(),
-      completed: false,
-    };
-
+  setNewTodo = (todo: Todo) => {
     this.setState(state => (
-      {
-        id: state.id + 1,
-        todoTitle: '',
-        currentTodos: [...state.currentTodos, todo],
-        userId: '',
-        isUserChoosed: false,
-        isTodoInclude: false,
-      }
+      { currentTodos: [...state.currentTodos, todo] }
     ));
   };
 
   render() {
-    const {
-      currentTodos,
-      userId,
-      todoTitle,
-      isTodoInclude,
-      isUserChoosed,
-    } = this.state;
+    const { currentTodos } = this.state;
 
     return (
       <div className="App">
         <h1>
           List of todos
         </h1>
-
-        <Form
-          className="form mb-3"
-          onSubmit={(event) => {
-            event.preventDefault();
-            this.addTodo();
-          }}
-        >
-          <Form.Label
-            htmlFor="task"
-            className="form__label"
-          >
-            <Form.Control
-              className="form__todo_input"
-              type="text"
-              id="task"
-              placeholder="Write your task"
-              value={todoTitle}
-              onChange={this.handleChangeTask}
-            />
-          </Form.Label>
-
-          {isTodoInclude && (
-            <p className="form__alert">
-              Please, enter todo title
-            </p>
-          )}
-
-          <Form.Label
-            htmlFor="selectedUser"
-            className="form__label"
-          >
-            <Form.Select
-              name="selectedUser"
-              id="selectedUser"
-              value={userId}
-              onChange={this.handleChangeUser}
-            >
-              <option value={0}>
-                Choose user
-              </option>
-              {users.map(user => (
-                <option value={user.id} key={user.id}>
-                  {user.name}
-                </option>
-              ))}
-            </Form.Select>
-          </Form.Label>
-
-          {isUserChoosed && (
-            <p className="form__alert">
-              Please, choose user
-            </p>
-          )}
-
-          <Button
-            variant="primary"
-            type="submit"
-          >
-            Add Todo
-          </Button>
-        </Form>
+        <TodoForm users={users} setNewTodo={this.setNewTodo} />
         <TodoList todos={currentTodos} />
       </div>
     );
---
+++ src/components/TodoForm/TodoForm.tsx
@@ -0,0 +1,162 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from 'react-bootstrap';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+
+type State = {
+  id: number;
+  todoTitle: string;
+  userId: string;
+  isUserChoosed: boolean;
+  isTodoInclude: boolean;
+};
+
+type Props = {
+  users: User[];
+  setNewTodo: (todo: Todo) => void;
+};
+
+export class TodoForm extends React.Component<Props, State> {
+  state: State = {
+    id: 1,
+    todoTitle: '',
+    userId: '0',
+    isUserChoosed: false,
+    isTodoInclude: false,
+  };
+
+  handleChangeUser = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.target;
+
+    this.setState({
+      userId: value,
+      isUserChoosed: false,
+    });
+  };
+
+  handleChangeTask = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    this.setState({
+      todoTitle: value,
+      isTodoInclude: false,
+    });
+  };
+
+  addTodo = () => {
+    const { id, userId, todoTitle } = this.state;
+    const { users } = this.props;
+
+    if (todoTitle.trim().length !== 0) {
+      this.setState({ isTodoInclude: false });
+    } else {
+      this.setState({ isTodoInclude: true });
+
+      return;
+    }
+
+    if (+userId !== 0) {
+      this.setState({ isUserChoosed: false });
+    } else {
+      this.setState({ isUserChoosed: true });
+
+      return;
+    }
+
+    const todo: Todo = {
+      user: users.find(user => user.id === +userId) || null,
+      userId: +userId,
+      id,
+      title: todoTitle.toLocaleUpperCase(),
+      completed: false,
+    };
+
+    this.props.setNewTodo(todo);
+
+    this.setState(state => (
+      {
+        id: state.id + 1,
+        todoTitle: '',
+        userId: '',
+        isUserChoosed: false,
+        isTodoInclude: false,
+      }
+    ));
+  };
+
+  render() {
+    const {
+      userId,
+      todoTitle,
+      isTodoInclude,
+      isUserChoosed,
+    } = this.state;
+
+    const { users } = this.props;
+
+    return (
+      <Form
+        className="form mb-3"
+        onSubmit={(event) => {
+          event.preventDefault();
+          this.addTodo();
+        }}
+      >
+        <Form.Label
+          htmlFor="task"
+          className="form__label"
+        >
+          <Form.Control
+            className="form__todo_input"
+            type="text"
+            id="task"
+            placeholder="Write your task"
+            value={todoTitle}
+            onChange={this.handleChangeTask}
+          />
+        </Form.Label>
+
+        {isTodoInclude && (
+          <p className="form__alert">
+            Please, enter todo title
+          </p>
+        )}
+
+        <Form.Label
+          htmlFor="selectedUser"
+          className="form__label"
+        >
+          <Form.Select
+            name="selectedUser"
+            id="selectedUser"
+            value={userId}
+            onChange={this.handleChangeUser}
+          >
+            <option value={0}>
+              Choose user
+            </option>
+            {users.map(user => (
+              <option value={user.id} key={user.id}>
+                {user.name}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Label>
+
+        {isUserChoosed && (
+          <p className="form__alert">
+            Please, choose user
+          </p>
+        )}
+
+        <Button
+          variant="primary"
+          type="submit"
+        >
+          Add Todo
+        </Button>
+      </Form>
+    );
+  }
+}
