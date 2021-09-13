caf620c420f5a112714e2da6e1f014a390c999fa
Dmitriy Pleshakov <dmitriypleshakov1@gmail.com>
2021-09-13 22:55:27 +0300

    React_add-todo-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { TodoList } from './components/TodoList/TodoList';
 import './App.css';
 
 import users from './api/users';
+import todos from './api/todos';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const preparedTodos = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId) || null,
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
-};
+interface State {
+  todos: Todo[];
+}
+
+class App extends React.Component<{}, State> {
+  state = {
+    todos: preparedTodos,
+  };
+
+  addTodo = (todo: Todo) => {
+    this.setState(currentState => ({
+      todos: [...currentState.todos, todo],
+    }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Add todo form</h1>
+        <TodoList
+          todos={this.state.todos}
+          addTodo={this.addTodo}
+          users={users}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="react-scripts" />
+
+interface Todo {
+  userId: number | string;
+  id: number | string;
+  title: string;
+  user: User | null;
+}
+
+interface User {
+  id: number;
+  name: string;
+}
