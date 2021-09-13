c156fd6ad21e696039f15dad26b2698826ac8dad
Yevhenii Sviderskyi <evgen1sider@gmail.com>
2021-09-13 17:46:55 +0300

    add todo form

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,36 @@
 /// <reference types="react-scripts" />
+
+interface Todo {
+  userId: number;
+  uuid: string;
+  id?: number;
+  title: string;
+  completed: boolean;
+  user: User | null;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+}
