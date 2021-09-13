5f5cd4825a57cfcbb0dd456d1b4e7bd98fb15bdb
Oleksandr Morozov <oleksandr.morozov@protonmail.com>
2021-09-13 17:50:30 +0300

    commit for mentor review

--- src/TodoList/TodoList.tsx
+++ src/TodoList/TodoList.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import classNames from 'classnames';
 
 interface Props {
   todos: Todo[];
@@ -11,35 +12,69 @@ interface Props {
 interface State {
   title: string;
   name: string;
+  isTitle: boolean;
+  isName: boolean;
 }
 
 export class TodoList extends React.Component<Props, State> {
   state: State = {
     title: '',
     name: '',
+    isTitle: false,
+    isName: false,
   };
 
   handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = event.target;
 
-    this.setState({
-      [name]: value,
-    } as Pick<State, keyof State>);
+    if (name === 'title') {
+      this.setState({
+        [name]: value,
+        isTitle: true,
+      });
+    }
+
+    if (name === 'name') {
+      this.setState({
+        [name]: value,
+        isName: true,
+      });
+    }
   };
 
   clearState = () => {
-    this.setState({ title: '', name: '' });
+    this.setState({
+      title: '',
+      name: '',
+      isTitle: false,
+      isName: false,
+    });
   };
 
   handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
 
     const { users, todos } = this.props;
-    const { name, title } = this.state;
+    const {
+      name, title,
+    } = this.state;
+
+    if (title !== '') {
+      console.log('123');
+      this.setState({ isTitle: true });
+    }
+
+    if (name !== '') {
+      this.setState({ isName: true });
+    }
 
     const maxID = Math.max.apply(null, todos.map(item => item.id));
     const relevantUser = users.find(user => user.name === name) || null;
     const relevantUserID = relevantUser ? relevantUser.id : null;
+
+    if (!title || !name) {
+      return;
+    }
 
     const todo = {
       title,
@@ -55,7 +90,12 @@ export class TodoList extends React.Component<Props, State> {
 
   render() {
     const { todos, users } = this.props;
-    const { title, name } = this.state;
+    const {
+      title, name, isName, isTitle,
+    } = this.state;
+
+    const classNameForTitle = isTitle ? 'd-none' : 'd-block';
+    const classNameForName = isName ? 'd-none' : 'd-block';
 
     return (
       <div>
@@ -65,17 +105,19 @@ export class TodoList extends React.Component<Props, State> {
         >
 
           <input
-            required
             type="text"
+            id="textInput"
             name="title"
             placeholder="Add a todo"
             value={title}
             onChange={this.handleChange}
             className="w-50 mb-2 py-2 border rounded"
           />
+          {isTitle && (
+            <p className={classNameForTitle}>*Please enter the title</p>
+          )}
 
           <select
-            required
             name="name"
             value={name}
             onChange={this.handleChange}
@@ -93,6 +135,9 @@ export class TodoList extends React.Component<Props, State> {
               </option>
             ))}
           </select>
+          {isName && (
+            <p className={classNameForName}>*Choose a user</p>
+          )}
 
           <button
             type="submit"
@@ -102,7 +147,7 @@ export class TodoList extends React.Component<Props, State> {
           </button>
         </form>
 
-        <ul className="w-100 mx-auto row align-items-center">
+        <ul className="mx-auto row align-items-center">
           {todos.map(todo => (
             <li
               key={todo.id}
