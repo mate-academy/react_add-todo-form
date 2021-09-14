6f7bfe0e20dada41472f7e71f844260881cda284
Polina Razumna <poliandiy@gmail.com>
2021-09-14 15:39:18 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,47 @@ import React from 'react';
 import './App.css';
 
 import users from './api/users';
+import todosFromServer from './api/todos';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+import { TodoList } from './TodoList';
+import { Form } from './Form';
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+type State = {
+  todos: Todo[];
 };
+
+const preparedTodos: Todo[] = todosFromServer.map(todo => {
+  return {
+    ...todo,
+    user: users.find(user => user.id === todo.userId) || null,
+  };
+});
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    todos: preparedTodos,
+  };
+
+  addTodo = (todo: Todo) => {
+    this.setState((currentState) => ({
+      todos: [...currentState.todos, todo],
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+
+    return (
+      <div className="App">
+        <Form
+          users={users}
+          todos={todos}
+          addTodo={this.addTodo}
+        />
+        <TodoList todos={todos} />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/Form.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import './Form.scss';
+
+type State = {
+  userId: number,
+  title: string,
+  isSelected: boolean,
+  hasTitle: boolean,
+};
+
+type Props = {
+  todos: Todo[],
+  users: User[],
+  addTodo: (todo: Todo) => void,
+};
+
+export class Form extends React.Component<Props, State> {
+  state: State = {
+    userId: 0,
+    title: '',
+    isSelected: true,
+    hasTitle: true,
+  };
+
+  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { userId, title } = this.state;
+    const { users, todos, addTodo } = this.props;
+
+    if (userId === 0) {
+      this.setState({
+        isSelected: false,
+      });
+    }
+
+    if (title.length > 0) {
+      this.setState({
+        hasTitle: false,
+      });
+    }
+
+    this.setState(() => {
+      const newTodo = {
+        user: users.find(user => user.id === userId) || null,
+        userId,
+        id: todos.length + 1,
+        title,
+        completed: false,
+      };
+
+      addTodo(newTodo);
+
+      return ({
+        userId: 0,
+        title: '',
+      });
+    });
+  };
+
+  selectUser = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState({
+      userId: +event.target.value,
+      isSelected: true,
+    });
+  };
+
+  addTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      title: event.target.value,
+      hasTitle: true,
+    });
+  };
+
+  render() {
+    const { users } = this.props;
+    const {
+      userId, isSelected, title, hasTitle,
+    } = this.state;
+
+    return (
+      <form
+        action="#"
+        onSubmit={this.handleSubmit}
+        className="form"
+      >
+        <select
+          onChange={this.selectUser}
+          value={userId}
+          className="select"
+        >
+          <option value="">Choose a user</option>
+          {users.map(user => (
+            <option value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+        {!isSelected
+          && <div className="error">Please choose a user</div>}
+        <input
+          type="text"
+          onChange={this.addTitle}
+          value={title}
+          placeholder="Enter the title"
+        />
+        {!hasTitle
+          && <div className="error">Please enter the title</div>}
+        <button
+          type="submit"
+          className="button"
+        >
+          Add
+        </button>
+      </form>
+    );
+  }
+}
