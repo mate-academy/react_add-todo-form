2e27787d8f98db0bbe57acec5aea92f48726577a
Clon <clonvhs@gmail.com>
2021-09-15 21:27:04 +0300

    Fix current task count display

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { AddTaskForm } from './components/AddTaskForm';
 
 type State = {
   users: Users[];
-  todos: Todos[];
   preparedToDos: PreparedToDo[];
   todoMaxId:number;
 };
@@ -18,7 +17,6 @@ type Props = {
 class App extends React.Component<Props, State> {
   state:State = {
     users,
-    todos,
     todoMaxId: Math.max(...todos.map(todo => todo.id)),
     preparedToDos: todos.map(
       todo => {
@@ -66,7 +64,7 @@ class App extends React.Component<Props, State> {
           Current count of tasks:
           {' '}
           <span className="Todo__CountNumber">
-            {this.state.todos.length}
+            {this.state.preparedToDos.length}
           </span>
         </h2>
         <p className="Todo__description">To add new task: 1)write task name 2)chose a user</p>
