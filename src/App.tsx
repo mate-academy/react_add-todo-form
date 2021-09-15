27d6220e1e8828c3d45c2a4a7dc60da35434419f
Yevhen Pecherskyi <gekape4erskii@gmail.com>
2021-09-15 10:47:41 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import './App.css';
-
+import { TodoList } from './TodoList';
+import todos from './api/todos';
 import users from './api/users';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const todosWithUser: Todos[] = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId) || null,
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
-};
+interface State {
+  todoList: Todos[];
+  userList : Users[];
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    todoList: [...todosWithUser],
+    userList: users,
+  };
+
+  addTodo = (todo: Todos) => {
+    this.setState((currentState: State) => {
+      return {
+        todoList: [...currentState.todoList, todo],
+      };
+    });
+  };
+
+  render() {
+    const { userList, todoList } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Add todo form</h1>
+        Todos:
+        <TodoList
+          todos={todoList}
+          addTodo={this.addTodo}
+          users={userList}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
