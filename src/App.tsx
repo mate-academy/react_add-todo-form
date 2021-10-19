690b1868d67033b261b35d31fd2543e0eb4a9ad7
Igor Igor <you@example.com>
2021-10-19 15:15:21 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,121 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import classNames from 'classnames';
 
-import users from './api/users';
+import usersFromApi from './api/users';
+import todosFromApi from './api/todos';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+interface User {
+  id: number,
+  name: string,
+}
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
-};
+interface Todo {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+  user: User,
+}
+
+const preparedTodos: Todo[] = todosFromApi.map(todo => {
+  const findedUser = usersFromApi.find(user => todo.userId === user.id);
+
+  return {
+    ...todo,
+    user: findedUser,
+  } as Todo;
+});
+
+interface State {
+  todos: Todo[]
+  userId: number,
+  title: string
+}
+
+class App extends React.Component<{}, State> {
+  state = {
+    todos: [...preparedTodos],
+    userId: 0,
+    title: '',
+  };
+
+  setTodo: React.FormEventHandler = (event): void => {
+    const { userId, title, todos } = this.state;
+    const newTodo = {
+      userId,
+      id: todos.length,
+      title,
+      completed: false,
+      user: usersFromApi.find(user => user.id === this.state.userId),
+    } as Todo;
+
+    this.setState({
+      todos: [...todos, newTodo],
+      userId: 0,
+      title: '',
+    });
+
+    event.preventDefault();
+  };
+
+  render() {
+    const { todos, userId, title } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Add todo form</h1>
+
+        <form onSubmit={this.setTodo}>
+          <input
+            type="text"
+            value={title}
+            onChange={(event) => {
+              this.setState({ title: event.target.value });
+            }}
+          />
+          <select
+            name="user"
+            value={userId}
+            onChange={(event) => {
+              this.setState({ userId: +event.target.value });
+            }}
+            required
+          >
+            <option value="">Choose a user</option>
+            {usersFromApi.map(user => {
+              return (
+                <option key={user.id} value={user.id}>{user.name}</option>
+              );
+            })}
+          </select>
+          <button type="submit">Add</button>
+        </form>
+        <ul className="todo-list">
+          {todos.map(todo => {
+            return (
+              <li
+                key={todo.id}
+                className={classNames(
+                  'todo-list__item',
+                  {
+                    'todo-list__item--done': todo.completed,
+                  },
+                )}
+              >
+                <p>
+                  {`Name: ${todo.user.name}`}
+                  {' '}
+                </p>
+                <p>{todo.title}</p>
+                <p>{todo.completed ? 'Done' : 'In progress'}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
