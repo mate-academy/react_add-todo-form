830dc566823f767684c586dbfc3d81aaa2e131c1
Fiann(Larin) <dimiklarin420@gmail.com>
2021-10-26 23:41:30 +0300

    solution

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import users from "../api/users";
+import { Todo } from "../types/Todo";
+
+import classNames from 'classnames';
+
+type Props = {
+  todos: Todo[],
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <ul className="App__list">
+    {todos.map(todo => {
+      const newUser = users.find(user => user.id === todo.userId);
+
+      return (
+        <li
+          key={todo.id}
+          className={classNames({'list__item': true, 'list__item--done': todo.completed})}
+        >
+          {newUser?.name}
+          <br />
+          {todo.title}
+          <br />
+          {todo.completed ? 'Done' : 'Not done'}
+        </li>
+      );
+    })}
+  </ul>
+);
