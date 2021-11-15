c57d177aeef7f48b7934b8f3ac368706845f1a58
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2021-11-15 22:29:20 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,22 @@
 import React from 'react';
 import './App.scss';
 
-import users from './api/users';
 import todos from './api/todos';
 import { Todo } from './types/TodoType';
 import { TodosList } from './components/TodosList';
+import { NewTodoForm } from './components/NewTodoForm';
 
 interface State {
-  title: string;
-  userId: number;
   todosList: Todo[];
-  isInvalidTitle: boolean;
-  isInvalidUser: boolean;
 }
 
 class App extends React.Component<{}, State> {
   state = {
-    title: '',
-    userId: 0,
     todosList: [...todos],
-    isInvalidTitle: false,
-    isInvalidUser: false,
   };
 
-  handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      title: event.target.value,
-      isInvalidTitle: false,
-    });
-  };
-
-  handleUserChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    this.setState({
-      userId: Number(event.target.value),
-      isInvalidUser: false,
-    });
-  };
-
-  addTodo = () => {
-    const { title, userId, todosList } = this.state;
+  addTodo = (title: string, userId: number) => {
+    const { todosList } = this.state;
 
     const newTodo = {
       id: todosList.length + 1,
@@ -55,95 +33,13 @@ class App extends React.Component<{}, State> {
     });
   };
 
-  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const { title, userId } = this.state;
-
-    if (!title || userId === 0) {
-      if (!title) {
-        this.setState({
-          isInvalidTitle: true,
-        });
-      }
-
-      if (userId === 0) {
-        this.setState({
-          isInvalidUser: true,
-        });
-      }
-
-      return;
-    }
-
-    this.addTodo();
-
-    this.setState({
-      title: '',
-      userId: 0,
-    });
-  };
-
   render() {
-    const {
-      title, userId, todosList, isInvalidTitle, isInvalidUser,
-    } = this.state;
+    const { todosList } = this.state;
 
     return (
       <div className="App">
         <h1>Add todo form</h1>
-
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="title" className="App__label">
-            Task
-            <input
-              type="text"
-              name="title"
-              id="title"
-              value={title}
-              placeholder="Task to add"
-              onChange={this.handleTitleChange}
-            />
-          </label>
-          {
-            isInvalidTitle && (
-              'Please enter the title'
-            )
-          }
-
-          <label htmlFor="userId" className="App__label">
-            User:
-            <select
-              name="users"
-              id="userId"
-              value={userId}
-              onChange={this.handleUserChange}
-            >
-              <option value="0" disabled>Choose a user</option>
-
-              {users.map(user => (
-                <option
-                  value={user.id}
-                  key={user.id}
-                >
-                  {user.name}
-                </option>
-              ))}
-            </select>
-          </label>
-          {
-            isInvalidUser && (
-              'Please choose a user'
-            )
-          }
-
-          <button
-            type="submit"
-          >
-            Add task
-          </button>
-        </form>
-
+        <NewTodoForm addTodo={this.addTodo} />
         <TodosList todosList={todosList} />
       </div>
     );
--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -8,7 +8,7 @@ export const TodosList: React.FC<Props> = ({ todosList }) => (
   <ul className="App__list">
     {todosList.map((todo: Todo) => (
       <li className="App__item">
-        {`Task №:${todo.id}, Task:${todo.title}, For userId:${todo.userId}`}
+        {`Task №:${todo.id}, Task: ${todo.title}, For userId:${todo.userId}`}
       </li>
     ))}
   </ul>
