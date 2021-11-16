778921d0e8c1d8ddc81ef0488ac44f016de0e392
Alina Kostiuk <alina.hariaha@gmail.com>
2021-11-16 17:14:25 +0200

    Create a Todo form.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,125 @@
 import React from 'react';
-import './App.css';
-
+import './App.scss';
+import { TodoList } from './components';
+import todos from './api/todos';
 import users from './api/users';
+import { Todo } from './components/types';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+type Props = {};
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
-};
+interface State {
+  allTodos: Todo[],
+  selectedUser: number,
+  titleTodo: string,
+  hasTodoError: boolean,
+  hasUserError: boolean,
+}
+
+class App extends React.Component<Props, State> {
+  state: State = {
+    allTodos: [...todos],
+    selectedUser: 0,
+    titleTodo: '',
+    hasTodoError: false,
+    hasUserError: false,
+  };
+
+  addTodo = () => {
+    this.setState(state => {
+      const { allTodos, selectedUser, titleTodo } = state;
+
+      const newTodo = {
+        userId: selectedUser,
+        id: allTodos.length + 1,
+        title: titleTodo,
+        completed: false,
+        user: users.find(user => user.id === selectedUser),
+      };
+
+      return { allTodos: [...allTodos, newTodo] };
+    });
+  };
+
+  changeTodoHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ titleTodo: event.target.value });
+    this.checkError();
+  };
+
+  changeUserHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState({ selectedUser: +event.target.value });
+    this.checkError();
+  };
+
+  checkError = () => {
+    this.setState((state) => {
+      return {
+        hasTodoError: !state.titleTodo,
+        hasUserError: !state.selectedUser,
+      };
+    });
+  };
+
+  submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.checkError();
+    const { titleTodo, selectedUser } = this.state;
+
+    if (titleTodo && selectedUser) {
+      this.addTodo();
+
+      this.setState({
+        selectedUser: 0,
+        titleTodo: '',
+      });
+    }
+  };
+
+  render() {
+    const {
+      titleTodo, allTodos, selectedUser, hasTodoError, hasUserError,
+    } = this.state;
+
+    return (
+      <div className="app">
+        <h1>Add todo form</h1>
+        <form
+          action=""
+          method="get"
+          onSubmit={this.submitHandler}
+        >
+          <label htmlFor="titleTodo" className="app__input">
+            <input
+              type="text"
+              name="todo"
+              id="titleTodo"
+              value={titleTodo}
+              onChange={this.changeTodoHandler}
+              placeholder="todo"
+            />
+            {hasTodoError && <p className="app__error">Add todo please</p>}
+          </label>
+          <div className="app__select">
+            <select
+              name="user"
+              id=""
+              value={selectedUser}
+              onChange={this.changeUserHandler}
+            >
+              <option value="0" disabled>Choose a user</option>
+              {users.map(({ id, name }) => (
+                <option value={id} key={id}>{name}</option>
+              ))}
+            </select>
+            {hasUserError && <p className="app__error">Chose a user please</p>}
+          </div>
+          <button type="submit" className="app__button">Add</button>
+        </form>
+        <p>
+          <TodoList todos={allTodos} />
+        </p>
+      </div>
+    );
+  }
+}
 
 export default App;
