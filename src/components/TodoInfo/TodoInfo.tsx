a65933527c35a875d421fab638d80339ab241103
Lera Permiakova <vv.permiakova@gmail.com>
2021-12-14 19:17:33 +0200

    add todo form

---
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+import { TodoWithUser } from '../../types/TodoWithUser';
+
+import './TodoInfo.scss';
+
+export const TodoInfo: React.FC<TodoWithUser> = ({ title, completed, user }) => {
+  const completedStatus = completed ? 'Completed' : 'Not completed';
+
+  return (
+    <>
+      <h2 className="todo__title">{title}</h2>
+      <p className={classNames('todo__completed', {
+        'todo__completed--true': completed,
+      })}
+      >
+        {completedStatus}
+      </p>
+      {user && (
+        <div className="todo__user user">
+          {user && (
+            <UserInfo {...user} />
+          )}
+        </div>
+      )}
+    </>
+  );
+};
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+import { TodoWithUser } from '../../types/TodoWithUser';
+import './TodoList.scss';
+
+type Prop = {
+  todos: TodoWithUser[],
+};
+
+export const TodoList: React.FC<Prop> = ({ todos }) => (
+  <ul className="todos">
+    {todos.map((todo) => (
+      <li className="todos__todo todo" key={todo.id}>
+        <TodoInfo {...todo} />
+      </li>
+    ))}
+  </ul>
+);
