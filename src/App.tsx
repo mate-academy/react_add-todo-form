f2b9aecd9b0b3b2472166d62e064dbedb8712bb2
Valeriia Dlugash <lera.dlugash@gmail.com>
2021-12-16 20:46:09 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unused-state */
 import React from 'react';
-import './App.css';
+import './App.scss';
 import todos from './api/todos';
 import users from './api/users';
 
@@ -9,8 +9,8 @@ import { Todo } from './types';
 
 const preparedTodos: Todo[] = todos.map((todo) => {
   return {
-    ...todo, // old properties
-    user: users.find(user => user.id === todo.userId) || null, // + user or null
+    ...todo,
+    user: users.find(user => user.id === todo.userId) || null,
   };
 });
 
@@ -18,8 +18,6 @@ interface State {
   todos: Todo[];
   newTodoTitle: string;
   userId: number;
-  // isUserSelected: boolean;
-  // isNewTodoProvided: boolean;
 }
 
 export class App extends React.Component<{}, State> {
@@ -27,52 +25,29 @@ export class App extends React.Component<{}, State> {
     todos: [...preparedTodos],
     newTodoTitle: '',
     userId: 1,
-    // isUserSelected: false,
-    // isNewTodoProvided: false,
   };
 
   handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // event.preventDefault();
+    event.preventDefault();
+
     const text = event.target.value;
 
     this.setState({
       newTodoTitle: text,
     });
-
-    // eslint-disable-next-line no-console
-    console.log(this.state.newTodoTitle);
-
-    // if (text) {
-    //   this.setState({
-    //     isNewTodoProvided: true,
-    //   });
-    // }
   };
 
   handleUserChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    // event.preventDefault();
+    event.preventDefault();
+
     const userId = +event.target.value;
 
     this.setState({
       userId,
     });
-
-    // if (text) {
-    //   this.setState({
-    //     isUserSelected: true,
-    //   });
-    // }
   };
 
   handleSubmit = () => {
-    // event.preventDefault();
-
-    // const currentUser = users.find(user => this.state.user === user.name) || null;
-
-    // if (currentUser) {
-    //   const newTodoID = this.state.todos; // ????
-    // }
-
     const { userId, newTodoTitle } = this.state;
 
     if (!newTodoTitle) {
@@ -99,20 +74,21 @@ export class App extends React.Component<{}, State> {
       newTodoTitle: '',
       userId: 1,
     }));
-
-    // eslint-disable-next-line no-console
-    console.log(this.state);
   };
 
   render() {
     return (
       <div className="App">
-        <>
-          <h1>
-            Add todo form
-          </h1>
-          <form name="newTodo">
+        <div className="App__form">
+          <form
+            name="newTodo"
+            className="App__form--form"
+            onSubmit={event => {
+              event.preventDefault();
+            }}
+          >
             <input
+              placeholder="Enter your task"
               type="text"
               name="newTodoTitle"
               onChange={this.handleTitleChange}
@@ -120,30 +96,26 @@ export class App extends React.Component<{}, State> {
             />
           </form>
           <select
+            className="App__form--select"
             name=""
             onChange={this.handleUserChange}
           >
             {users.map(({ id, name }) => (
               <option key={id} value={id}>{name}</option>
             ))}
-            {/* <option
-              value=""
-            >
-              Choose user
-            </option> */}
           </select>
           <button
+            className="App__form--button"
             type="submit"
-            // onSubmit={this.handleSubmit}
             onClick={this.handleSubmit}
           >
             Add
           </button>
-          <p>
-            <span>Users: </span>
-            <TodoList preparedTodos={this.state.todos} />
-          </p>
-        </>
+        </div>
+        <p className="todos__container">
+          <span className="todos__title">Latest Todos</span>
+          <TodoList preparedTodos={this.state.todos} />
+        </p>
       </div>
     );
   }
