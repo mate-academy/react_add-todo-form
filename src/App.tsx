1b59bdecb1769c81c163dcc5ffa664952ad79c2a
Serhii Dvorets <dvorets.sergey@gmail.com>
2022-01-24 22:34:32 +0200

    3

--- src/App.tsx
+++ src/App.tsx
@@ -71,82 +71,107 @@ class App extends React.Component<{}, State> {
   render(): React.ReactNode {
     return (
       <div className="App">
-        <h1 className="main__title">create new todo!</h1>
-        <form action="/" className="form">
-          <div
-            className="error"
-            style={{
-              visibility: !this.state.errorTitle ? 'hidden' : 'visible',
-            }}
-          >
-            Please enter the title!
-          </div>
-          <div
-            className="error error__user"
-            style={{
-              visibility: !this.state.errorUser ? 'hidden' : 'visible',
-            }}
-          >
-            Please choose a user!
-          </div>
-          <div className="input__container">
-            title:
-            <input
-              className="input__field"
-              type="text"
-              name="title"
-              value={this.state.title}
-              onChange={(event) => {
-                this.setState({
-                  title: this.validationOfTitle(event.target.value),
-                  errorTitle: false,
-                });
+        <div className="form__container">
+          <h1 className="main__title">create new todo!</h1>
+          <form action="/" className="form">
+            <div
+              className="error"
+              style={{
+                visibility: !this.state.errorTitle ? 'hidden' : 'visible',
               }}
-            />
-          </div>
-          <label
-            htmlFor="select-user"
-            className="input__lable"
-          >
+            >
+              Please enter the title!
+            </div>
+            <div
+              className="error error__user"
+              style={{
+                visibility: !this.state.errorUser ? 'hidden' : 'visible',
+              }}
+            >
+              Please choose a user!
+            </div>
             <div className="input__container">
-              select user:
-              <select
+              title:
+              <input
                 className="input__field"
-                name="user"
-                id="select-user"
-                value={this.state.userId}
+                type="text"
+                name="title"
+                value={this.state.title}
                 onChange={(event) => {
                   this.setState({
-                    userId: +event.target.value,
-                    errorUser: false,
+                    title: this.validationOfTitle(event.target.value),
+                    errorTitle: false,
                   });
                 }}
-              >
-                <option value={0}>
-                  Choose a user
-                </option>
-                {users.map(user => {
-                  return (
-                    <>
-                      <option value={user.id}>
-                        {user.name}
-                      </option>
-                    </>
-                  );
-                })}
-              </select>
+              />
             </div>
-          </label>
-          <div className="input__container button__container">
-            <button
-              type="button"
-              className="submit__button input__field"
-              onClick={this.addTodo}
+            <label
+              htmlFor="select-user"
+              className="input__lable"
             >
-              Add
-            </button>
-          </div>
-        </form>
+              <div className="input__container">
+                select user:
+                <select
+                  className="input__field"
+                  name="user"
+                  id="select-user"
+                  value={this.state.userId}
+                  onChange={(event) => {
+                    this.setState({
+                      userId: +event.target.value,
+                      errorUser: false,
+                    });
+                  }}
+                >
+                  <option value={0}>
+                    Choose a user
+                  </option>
+                  {users.map(user => {
+                    return (
+                      <>
+                        <option value={user.id}>
+                          {user.name}
+                        </option>
+                      </>
+                    );
+                  })}
+                </select>
+              </div>
+            </label>
+            <div className="input__container button__container">
+              <button
+                type="button"
+                className="submit__button input__field"
+                onClick={this.addTodo}
+              >
+                Add
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="todos form">
+          <ol className="todo__list">
+            {this.state.todosFromServer.map(todo => (
+              <li
+                key={todo.id}
+                className="todo__cell"
+              >
+                <div className="todo__title">
+                  What to do:
+                </div>
+                <div className="todo__content">
+                  {todo.title}
+                </div>
+                <div className="todo__title">
+                  Who will do it:
+                </div>
+                <div className="todo__content">
+                  {users.find(user => user.id === todo.userId)?.name}
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
       </div>
     );
   }
