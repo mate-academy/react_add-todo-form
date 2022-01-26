3ea6ab99df1428c4bb82bb20c212013618b90938
Oleksandr Leshchenko <bulochkin72@gmail.com>
2022-01-26 14:52:21 +0200

    some minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ interface State {
   myUsers: User[],
   title: string,
   userId: string,
+  titleError: boolean,
+  userError: boolean,
 }
 
 type Event = React.ChangeEvent<HTMLInputElement | HTMLSelectElement>;
@@ -26,6 +28,8 @@ class App extends React.Component<{}, State> {
     })),
     title: '',
     userId: '',
+    titleError: false,
+    userError: false,
   };
 
   change = (event: Event) => {
@@ -33,62 +37,83 @@ class App extends React.Component<{}, State> {
 
     this.setState((prev: State) => ({
       ...prev,
+      titleError: false,
+      userError: false,
       [name]: value,
     }));
   };
 
+  handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const { title, userId } = this.state;
+
+    if (title === '') {
+      this.setState({ titleError: true });
+    }
+
+    if (userId === '') {
+      this.setState({ userError: true });
+    }
+  };
+
   updateTodos = () => {
     this.setState((prev: State): State | undefined => {
-      if (prev.title !== '' || prev.userId !== '') {
-        prev.myTodos.push({
-          userId: +prev.userId,
-          id: prev.myTodos.length + 1,
-          title: prev.title,
-          completed: false,
-          user: prev.myUsers.find(person => person.id === +prev.userId) || null,
-        });
-
-        return {
-          ...prev,
-          title: '',
-          userId: '',
-        };
+      if (prev.title === '' || prev.userId === '') {
+        return;
       }
 
-      return undefined;
+      prev.myTodos.push({
+        userId: +prev.userId,
+        id: prev.myTodos.length + 1,
+        title: prev.title,
+        completed: false,
+        user: prev.myUsers.find(person => person.id === +prev.userId) || null,
+      });
+
+      // eslint-disable-next-line
+      return {
+        ...prev,
+        title: '',
+        userId: '',
+      };
     });
   };
 
   render() {
+    const { titleError, userError } = this.state;
+
     return (
       <div className="App">
         <h1>Add todo form</h1>
-        <form onSubmit={(event) => event.preventDefault()}>
-          <input
-            type="text"
-            name="title"
-            value={this.state.title}
-            onChange={this.change}
-            required
-            // onInvalid={}  question
-            // onInput={}
-          />
-          <select
-            name="userId"
-            value={this.state.userId}
-            onChange={this.change}
-            required
-          >
-            <option value="">Choose a user</option>
-            {this.state.myUsers.map(user => (
-              <option key={user.id} value={user.id}>
-                {user.name}
-                {user.username}
-                {' '}
-                {user.email}
-              </option>
-            ))}
-          </select>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
+              type="text"
+              name="title"
+              value={this.state.title}
+              onChange={this.change}
+            />
+            {titleError && <span>Please enter the title</span>}
+          </div>
+          <div>
+            <select
+              name="userId"
+              value={this.state.userId}
+              onChange={this.change}
+            >
+              <option value="">Choose a user</option>
+              {this.state.myUsers.map(user => (
+                <option key={user.id} value={user.id}>
+                  {user.name}
+                  {user.username}
+                  {' '}
+                  {user.email}
+                </option>
+              ))}
+            </select>
+            {userError && <span>Please choose a user</span>}
+          </div>
           <button
             type="submit"
             onClick={this.updateTodos}
