bbd49635b66d5530171e190d1ae48158e08375a4
Bohdan Maksymiuk <bohdan.max@ukr.net>
2022-01-26 21:08:32 +0200

    Solution without styles and regExp

---
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -0,0 +1,34 @@
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({
+  todo,
+}) => (
+  <>
+    <h2 className="todo__title">
+      {todo.title}
+    </h2>
+
+    <span className={classNames(
+      'todo__status',
+      { 'todo__status--completed': todo.completed },
+    )}
+    >
+      Status:
+      {todo.completed
+        ? ' Completed'
+        : ' Not completed'}
+    </span>
+
+    {
+      todo.user
+        ? <UserInfo user={todo.user} />
+        : <h4>Responsible person has not been chosen</h4>
+    }
+  </>
+);
