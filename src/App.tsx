fbd6610e59a2d6cbe6a058ecb606f5e7ce17c0fd
Volodymyr Oleksiienko <oleksienkovladimir50@gmail.com>
2022-01-26 19:30:08 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,153 @@
 import React from 'react';
 import './App.css';
-
+import { TodoList } from './components/TodoList/TodoList';
+import todosFromServer from './api/todos';
 import users from './api/users';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+const newTodos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId) || null,
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+type Props = {};
+
+type State = {
+  todos: Todo[],
+  newTitle: string,
+  selectedId: string,
+  hasTitleError: boolean,
+  hasUserSelectedError: boolean,
 };
+
+class App extends React.PureComponent<Props, State> {
+  state: State = {
+    todos: [...newTodos],
+    newTitle: '',
+    selectedId: '0',
+    hasTitleError: false,
+    hasUserSelectedError: false,
+  };
+
+  handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    this.setState((state) => ({
+      ...state,
+      newTitle: value,
+      hasTitleError: false,
+    }));
+
+    //   this.setState((state) => {
+    //     const code = value.charCodeAt(value.length - 1);
+
+    //     if ((code === 32)
+    //       || (code >= 48 && code <= 57)
+    //       || (code >= 65 && code <= 90)
+    //       || (code >= 97 && code <= 122)
+    //       || (code >= 1040 && code <= 1103)
+    //       || (code === 1025 || code === 1105)
+    //     ) {
+    //       return {
+    //         newTitle: event.target.value,
+    //       };
+    //     }
+
+    //     if (state.newTitle.length > 1) {
+    //       return {
+    //         newTitle: state.newTitle,
+    //       };
+    //     }
+
+    //     return {
+    //       newTitle: '',
+    //     };
+    //   });
+  };
+
+  chooseUser = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState(() => ({
+      selectedId: event.target.value,
+      hasUserSelectedError: false,
+    }));
+  };
+
+  handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { newTitle, selectedId } = this.state;
+
+    const notSelected = selectedId === '0';
+
+    if (!newTitle || notSelected) {
+      this.setState({
+        hasTitleError: !newTitle,
+        hasUserSelectedError: notSelected,
+      });
+
+      return;
+    }
+
+    const newTodo = {
+      title: this.state.newTitle,
+      id: this.state.todos.length + 1,
+      userId: users[+this.state.selectedId - 1].id,
+      completed: true,
+      user: users.find(user => +this.state.selectedId === user.id) || null,
+    };
+
+    this.setState((state) => ({
+      todos: [
+        ...state.todos,
+        newTodo,
+      ],
+      newTitle: '',
+      selectedId: '0',
+    }));
+  };
+
+  render() {
+    const {
+      todos,
+      newTitle,
+      selectedId,
+      hasTitleError,
+      hasUserSelectedError,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Add todo form</h1>
+        <p>
+          <span>Users: </span>
+          {users.length}
+        </p>
+
+        <TodoList todoList={todos} />
+
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={newTitle} onChange={this.handleInput} />
+
+          {hasTitleError && (
+            <span>Please type a title</span>
+          )}
+
+          <select value={selectedId} onChange={this.chooseUser}>
+            <option value="0">Choose user</option>
+
+            {users.map(user => (
+              <option value={user.id}>{user.name}</option>
+            ))}
+          </select>
+
+          {hasUserSelectedError && (
+            <span>Please choose an user</span>
+          )}
+
+          <button type="submit"> Add User</button>
+        </form>
+      </div>
+
+    );
+  }
+}
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="react-scripts" />
+
+interface User {
+  id: numder,
+  name: string,
+  username: string,
+  email: string,
+}
+
+interface Todo {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+  user: User | null,
+}
