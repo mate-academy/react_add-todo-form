c6bc92450c39cd40cc2bdaf6d9fce18855861c10
Ernesto Fernandez <ernesto.fernandez.1980@gmail.com>
2022-01-27 21:00:17 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,118 @@
 import React from 'react';
-import './App.css';
+import { PreparedTodo } from './types/PreparedTodo';
 
+import todos from './api/todos';
 import users from './api/users';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+import './App.css';
+import { TodoList } from './components/TodoList/TodoList';
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+const preparedTodos: PreparedTodo[] = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId) || null,
+}));
+
+type State = {
+  query: string,
+  stateTodos: PreparedTodo[],
+  currentUserId: number,
 };
+
+class App extends React.PureComponent<{}, State> {
+  state: State = {
+    query: '',
+    stateTodos: [...preparedTodos],
+    currentUserId: 0,
+  };
+
+  queryChangeHandler = (inputValue: string) => {
+    this.setState({
+      query: inputValue,
+    });
+  };
+
+  selectUserHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.target;
+
+    this.setState({
+      currentUserId: +value,
+    });
+  };
+
+  formSubmitHandler = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { query, currentUserId, stateTodos } = this.state;
+    const newId = Math.max(...stateTodos.map(todo => todo.id)) + 1;
+
+    const newTodo: PreparedTodo = {
+      userId: currentUserId,
+      id: newId,
+      title: query,
+      completed: false,
+      user: users.find(user => user.id === currentUserId) || null,
+    };
+
+    this.setState({
+      stateTodos: [...stateTodos, newTodo],
+      currentUserId: 0,
+      query: '',
+    });
+  };
+
+  render() {
+    const {
+      stateTodos,
+      query,
+      currentUserId,
+    } = this.state;
+
+    return (
+      <>
+        <form onSubmit={this.formSubmitHandler}>
+          <p>
+            <input
+              type="text"
+              onChange={event => this.queryChangeHandler(event.target.value)}
+              value={query}
+              placeholder="input title todo..."
+            />
+          </p>
+
+          <label htmlFor="someId">
+            <select
+              name="select"
+              id="someId"
+              onChange={event => this.selectUserHandler(event)}
+              value={currentUserId}
+            >
+              <option
+                disabled
+                value={currentUserId}
+              >
+                Choose User
+              </option>
+              {users.map(user => (
+                <option
+                  key={user.id}
+                  value={user.id}
+                >
+                  {user.name}
+                </option>
+              ))}
+            </select>
+          </label>
+
+          <button
+            type="submit"
+          >
+            Add
+          </button>
+        </form>
+
+        <TodoList todos={stateTodos} />
+      </>
+    );
+  }
+}
 
 export default App;
