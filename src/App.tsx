84f819a6f4642250e2c26fbca87cc92948a57b77
Iryna Zolko <koko_irina@ukr.net>
2022-01-28 14:50:50 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
+import todos from './api/todos';
 import users from './api/users';
+import { TodoList } from './components/TodoList/TodoList';
+import { NewTodoForm } from './components/NewTodoForm/NewTodoForm';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+export const preparedTodos: Todo[] = todos.map(todo => ({
+  ...todo,
+  user: users.find((user) => user.id === todo.userId) || null,
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+type Props = {};
+type State = {
+  allTodos: Todo[];
 };
 
-export default App;
+export class App extends React.Component<Props, State> {
+  state: State = {
+    allTodos: [...preparedTodos],
+  };
+
+  addNewTodo = (newTodo: Todo) => {
+    this.setState((prevState) => ({
+      allTodos: [...prevState.allTodos, newTodo],
+    }));
+  };
+
+  render() {
+    const { allTodos } = this.state;
+
+    return (
+      <div className="page">
+        <div className="page-content">
+          <NewTodoForm allTodos={allTodos} addNewTodo={this.addNewTodo} />
+
+          <TodoList todos={allTodos} />
+        </div>
+      </div>
+    );
+  }
+}
