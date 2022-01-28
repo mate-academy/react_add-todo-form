f955c4c066e69809777fb9635e797446f169549a
Yevhenii Korzun <marty.music.official@gmail.com>
2022-01-28 05:18:54 +0200

    Solution v2.0

--- src/App.tsx
+++ src/App.tsx
@@ -4,211 +4,34 @@ import './App.css';
 import usersFromServer from './api/users';
 import todosFromServer from './api/todos';
 import { TodoList } from './components/TodoList';
+import { AddTodoForm } from './components/AddTodoForm';
 
-type State = {
-  title: string,
-  todos: PreparedTodo[],
-  selectedUser: User | null,
-  selectValue: string,
-  isUserSelected: boolean,
-  isTitleEntered: boolean,
-  showTitleFlag: boolean,
-  showUserSelectFlag: boolean,
-  isInputValid: boolean,
-};
-
-const maxInputLength = 20;
-const validSymbols = 'qwertyuiopasdfghjklzxcvbnm ';
 const preparedTodos: PreparedTodo[] = todosFromServer.map(todo => ({
   ...todo,
   user: usersFromServer.find(user => user.id === todo.userId) || null,
 }));
 
+type State = {
+  todos: PreparedTodo[],
+};
+
 export class App extends React.PureComponent<{}, State> {
   state: State = {
-    title: '',
     todos: preparedTodos,
-    selectedUser: null,
-    selectValue: '',
-    isUserSelected: false,
-    isTitleEntered: false,
-    showTitleFlag: false,
-    showUserSelectFlag: false,
-    isInputValid: true,
   };
 
-  titleAddHandler = (text: string) => {
-    const inputValue: string = text.replace(/[^a-zа-яё\s]/gi, '');
-
-    this.setState({ isInputValid: true });
-
-    text.split('').forEach(char => {
-      if (!validSymbols.includes(char.toLowerCase())) {
-        this.setState({ isInputValid: false });
-      }
-    });
-
-    this.setState({
-      title: inputValue,
-      isTitleEntered: text.length !== 0,
-      showTitleFlag: false,
-    });
-  };
-
-  selectUserHandler = (value: string) => {
-    const id = +value;
-    const user = usersFromServer.find(person => person.id === id);
-
-    if (user) {
-      const preparedUser = {
-        id: user?.id,
-        name: user?.name,
-        email: user?.email,
-      };
-
-      this.setState(state => ({
-        selectedUser: preparedUser || state.selectedUser,
-        selectValue: value,
-        isUserSelected: true,
-        showUserSelectFlag: false,
-      }));
-    }
-  };
-
-  addTodoHandler = () => {
-    const {
-      title,
-      todos,
-      selectedUser,
-      isUserSelected,
-      isTitleEntered,
-    } = this.state;
-
-    const todosIdentificators = todos.map(todo => todo.id);
-    const newTodoIdentificator = Math.max(...todosIdentificators) + 1;
-    let titleCheck = isTitleEntered;
-
-    this.setState({
-      showTitleFlag: true,
-      showUserSelectFlag: true,
-      isInputValid: true,
-    });
-
-    this.setState({ title: title.trim() });
-
-    if (title.trim().length === 0) {
-      this.setState({ isTitleEntered: false });
-      titleCheck = false;
-    }
-
-    if (selectedUser && isUserSelected && titleCheck) {
-      const preparedTodo: PreparedTodo = {
-        id: newTodoIdentificator,
-        title,
-        userId: selectedUser?.id,
-        completed: false,
-        user: usersFromServer.find(person => person.id === selectedUser?.id) || null,
-      };
-
-      this.setState(state => ({
-        todos: [...state.todos, preparedTodo],
-      }));
-    }
-  };
-
-  clearForm = () => {
-    this.setState({
-      title: '',
-      selectedUser: null,
-      selectValue: '',
-      isTitleEntered: false,
-      isUserSelected: false,
-      showTitleFlag: false,
-      showUserSelectFlag: false,
-      isInputValid: true,
-    });
-  };
-
-  submit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    this.clearForm();
+  addTodo = (newTodo: PreparedTodo) => {
+    this.setState(state => ({
+      todos: [...state.todos, newTodo],
+    }));
   };
 
   render() {
-    const {
-      title,
-      todos,
-      selectValue,
-      isUserSelected,
-      isTitleEntered,
-      showTitleFlag,
-      showUserSelectFlag,
-      isInputValid,
-    } = this.state;
+    const { todos } = this.state;
 
     return (
       <>
-        <form
-          method="post"
-          onSubmit={event => this.submit(event)}
-        >
-          <p>Title:</p>
-          <input
-            type="text"
-            value={title}
-            onChange={event => this.titleAddHandler(event.target.value)}
-            placeholder="Please enter a title..."
-            required
-            maxLength={maxInputLength}
-          />
-          {!isInputValid && (
-            <span>Use the correct characters: A-z А-я</span>
-          )}
-          {title.length === maxInputLength && (
-            <span>{`Max title length is ${maxInputLength} characters`}</span>
-          )}
-          {(showTitleFlag && !isTitleEntered) && (
-            <span>Please enter a title</span>
-          )}
-          <p>User:</p>
-          <div>
-            <label htmlFor="select">
-              <select
-                name="select"
-                id="select"
-                onChange={event => this.selectUserHandler(event.target.value)}
-                value={selectValue}
-                required
-              >
-                <option
-                  value=""
-                  disabled
-                  selected
-                  hidden
-                >
-                  ---Choose user---
-                </option>
-                {usersFromServer.map(user => (
-                  <option value={user.id}>
-                    {user.name}
-                  </option>
-                ))}
-              </select>
-            </label>
-            {(showUserSelectFlag && !isUserSelected) && (
-              <span>Please choose the user</span>
-            )}
-            <div>
-              <button
-                type="submit"
-                onClick={this.addTodoHandler}
-              >
-                Add
-              </button>
-            </div>
-          </div>
-        </form>
+        <AddTodoForm addTodo={this.addTodo} />
         <TodoList todos={todos} />
       </>
     );
---
+++ src/components/AddTodoForm/AddTodoForm.tsx
@@ -0,0 +1,190 @@
+import React from 'react';
+import './AddTodoForm.scss';
+
+import usersFromServer from '../../api/users';
+import { getUserById } from '../../helpers';
+
+const maxInputLength = 30;
+const inputValidetionExpression = /[^\da-zа-яё\s]/gi;
+
+type Props = {
+  addTodo: (newTodo: PreparedTodo) => void,
+};
+
+type State = {
+  todoTitle: string,
+  selectedUserId: number;
+  isUserSelected: boolean,
+  isTitleEntered: boolean,
+  isInputValid: boolean,
+  isInputLengthCorrect: boolean,
+};
+
+export class AddTodoForm extends React.Component<Props, State> {
+  state: State = {
+    todoTitle: '',
+    selectedUserId: 0,
+    isUserSelected: true,
+    isTitleEntered: true,
+    isInputValid: true,
+    isInputLengthCorrect: true,
+  };
+
+  changeIputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    const inputValue: string = value.replace(inputValidetionExpression, '');
+
+    this.setState({
+      isTitleEntered: true,
+      isInputValid: true,
+      isInputLengthCorrect: true,
+    });
+
+    if (value.length > maxInputLength) {
+      this.setState({ isInputLengthCorrect: false });
+    }
+
+    if (inputValidetionExpression.test(value)) {
+      this.setState({ isInputValid: false });
+    }
+
+    if ((value.length <= maxInputLength) && !(inputValidetionExpression.test(value))) {
+      this.setState({
+        todoTitle: inputValue,
+      });
+    }
+  };
+
+  selectUserHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.target;
+    const id = Number(value);
+
+    this.setState({
+      selectedUserId: id,
+      isUserSelected: true,
+    });
+  };
+
+  formSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const { todoTitle, selectedUserId } = this.state;
+    const title = todoTitle.trim();
+
+    this.setState({
+      isInputValid: true,
+    });
+
+    if (!title.length) {
+      this.setState({
+        todoTitle: '',
+        isTitleEntered: false,
+      });
+    }
+
+    if (!selectedUserId) {
+      this.setState({
+        isUserSelected: false,
+      });
+    }
+
+    if (title.length && selectedUserId) {
+      const newTodoId = Math.max(...usersFromServer.map(user => user.id)) + 1;
+      const newTodo = {
+        userId: selectedUserId,
+        id: newTodoId,
+        title,
+        completed: false,
+        user: getUserById(selectedUserId) || null,
+      };
+
+      this.props.addTodo(newTodo);
+
+      this.clearForm();
+    }
+  };
+
+  clearForm = () => {
+    this.setState({
+      todoTitle: '',
+      selectedUserId: 0,
+      isUserSelected: true,
+      isTitleEntered: true,
+      isInputValid: true,
+      isInputLengthCorrect: true,
+    });
+  };
+
+  render() {
+    const {
+      todoTitle,
+      selectedUserId,
+      isUserSelected,
+      isTitleEntered,
+      isInputValid,
+      isInputLengthCorrect,
+    } = this.state;
+
+    return (
+      <div className="form">
+        <form
+          className="form__content"
+          onSubmit={this.formSubmit}
+        >
+          <p className="form__title">Title:</p>
+          <input
+            className="form__input"
+            type="text"
+            value={todoTitle}
+            onChange={this.changeIputHandler}
+            placeholder="Please input the title..."
+          />
+          <p className="form__warning">
+            {!isInputLengthCorrect && (
+              <span>{`itle max length is ${maxInputLength} characters!`}</span>
+            )}
+            {!isInputValid && (
+              <span>Use only letters, digits or backspaces please!</span>
+            )}
+            {!isTitleEntered && (
+              <span>Please enter a title!</span>
+            )}
+          </p>
+          <p className="form__title form__title--user">User:</p>
+          <label htmlFor="userSelect">
+            <select
+              className="form__input form__input--select"
+              id="userSelect"
+              value={selectedUserId}
+              onChange={this.selectUserHandler}
+            >
+              <option
+                value="0"
+                disabled
+                selected
+                hidden
+              >
+                ---Choose user---
+              </option>
+              {usersFromServer.map(user => (
+                <option value={user.id} key={user.id}>
+                  {user.name}
+                </option>
+              ))}
+            </select>
+          </label>
+          <p className="form__warning">
+            {!isUserSelected && (
+              <span>Please choose the user!</span>
+            )}
+          </p>
+          <div className="form__submit">
+            <button type="submit" className="form__button">
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
