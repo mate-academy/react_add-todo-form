d4d3b1894a633f3ffa47abf7e19f877d43df74dd
Aleksandra Mileant <aleksandramileant1@gmail.com>
2022-01-30 00:18:49 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import './App.css';
 
-import users from './api/users';
+import todos from './api/todos';
+import { TodosList } from './components/TodosList';
+import { getUserById } from './helpers';
+import { AddTodoForm } from './components/AddTodoForm';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+export const todoWithUser: TodoWithUser[] = todos.map((todo) => ({
+  ...todo,
+  user: getUserById(todo.userId) || null,
+}));
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+type Props = {};
+type State = {
+  todo: TodoWithUser[],
 };
 
+export class App extends React.Component<Props, State> {
+  state: State = {
+    todo: [...todoWithUser],
+  };
+
+  addNewTodo = (newTodo: TodoWithUser) => {
+    this.setState((prevState) => ({
+      todo: [...prevState.todo, newTodo],
+    }));
+  };
+
+  render() {
+    const { todo } = this.state;
+
+    return (
+      <div className="App card">
+        <h1 className="title is-3 has-text-primary">Add good form</h1>
+
+        <AddTodoForm todo={todo} addNewTodo={this.addNewTodo} />
+        <TodosList todos={todo} />
+      </div>
+    );
+  }
+}
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="react-scripts" />
+
+interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+}
+
+interface Todo {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+}
+
+interface TodoWithUser extends Todo {
+  user: User | null;
+}
