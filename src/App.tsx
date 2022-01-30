195d729c6fe651060c90fe84156ecf9c801b4cc3
Ernesto Fernandez <ernesto.fernandez.1980@gmail.com>
2022-01-30 02:57:53 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { PreparedTodo } from './types/PreparedTodo';
 import todos from './api/todos';
 import users from './api/users';
 
-import './App.css';
+import './App.scss';
 import { TodoList } from './components/TodoList/TodoList';
 
 const preparedTodos: PreparedTodo[] = todos.map(todo => ({
@@ -79,7 +79,7 @@ class App extends React.PureComponent<{}, State> {
         userId: currentUserId,
         id: newId,
         title: query,
-        completed: false,
+        completed: Boolean(Math.round(Math.random())),
         user: users.find(user => user.id === currentUserId) || null,
       };
 
@@ -104,57 +104,86 @@ class App extends React.PureComponent<{}, State> {
 
     return (
       <>
-        <form onSubmit={this.formSubmitHandler}>
-          <p>
-            <input
-              type="text"
-              onChange={event => this.queryChangeHandler(event.target.value)}
-              value={query}
-              placeholder="input title todo..."
-            />
-            {!isCorrectQuery && (
-              <span> Please enter the title</span>
-            )}
-            {isQueryTooLong && (
-              <span>{` Max query length: ${maxQuery}`}</span>
-            )}
-          </p>
-
-          <label htmlFor="someId">
-            <select
-              name="select"
-              id="someId"
-              onChange={event => this.selectUserHandler(event)}
-              value={currentUserId}
-            >
-              <option
-                disabled
-                value={0}
-              >
-                Choose User
-              </option>
-              {users.map(user => (
-                <option
-                  key={user.id}
-                  value={user.id}
-                >
-                  {user.name}
-                </option>
-              ))}
-            </select>
-          </label>
-
-          <button
-            type="submit"
+        <div className="App">
+          <form
+            className="form"
+            onSubmit={this.formSubmitHandler}
           >
-            Add
-          </button>
-          {!isCorrectSelect && (
-            <span> Choose a user</span>
-          )}
-        </form>
+            <div className="form__container">
+              <input
+                className="form__input"
+                type="text"
+                onChange={event => this.queryChangeHandler(event.target.value)}
+                value={query}
+                placeholder="input title todo..."
+              />
 
-        <TodoList todos={stateTodos} />
+              {!isCorrectQuery && (
+                <span
+                  className="form__alert form__alert--title"
+                >
+                  Please enter the title
+                </span>
+              )}
+
+              {isQueryTooLong && (
+                <span
+                  className="form__alert"
+                >
+                  {` Max query length: ${maxQuery}`}
+                </span>
+              )}
+            </div>
+            <div className="form__container">
+              <label
+                className="form__label"
+                htmlFor="someId"
+              >
+                <select
+                  className="form__select"
+                  name="select"
+                  id="someId"
+                  onChange={event => this.selectUserHandler(event)}
+                  value={currentUserId}
+                >
+                  <option
+                    disabled
+                    value={0}
+                  >
+                    choose user
+                  </option>
+
+                  {users.map(user => (
+                    <option
+                      key={user.id}
+                      value={user.id}
+                    >
+                      {user.name}
+                    </option>
+                  ))}
+                </select>
+              </label>
+
+              {!isCorrectSelect && (
+                <span
+                  className="form__alert"
+                >
+                  Choose a user
+                </span>
+              )}
+            </div>
+            <div className="form__container">
+              <button
+                className="form__button"
+                type="submit"
+              >
+                Add
+              </button>
+            </div>
+          </form>
+
+          <TodoList todos={stateTodos} />
+        </div>
       </>
     );
   }
