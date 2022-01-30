1f85d10b231c647f31f21b872be8323eb0648125
Roman Labonin <roman.labonin@gmail.com>
2022-01-30 17:15:36 +0200

    sol

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="react-scripts" />
+
+interface Todo {
+  user: User | null;
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+/* export interface TodoOfUser extends Todo {
+  user : User | null;
+} */
+
+interface User {
+  id: number,
+  name: string,
+  email: string
+}
