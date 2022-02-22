ddb12c74762d369716bb0cb70a091ed4ce9ee4b4
AndryKK <korbutiaktso@gmail.com>
2022-02-22 22:28:58 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,56 @@
-import React from 'react';
-import './App.css';
-
+import React, { useState } from 'react';
+import './App.scss';
+import preparedTodos from './api/types/components/preparedTodos';
+import { TodoList } from './api/types/components/TodoList/TodoList';
 import users from './api/users';
 
 const App: React.FC = () => {
+  const [prepeadTodosActual, SetPrepeadTodosActual] = useState([...preparedTodos]);
+  const [nameSelected, SetName] = useState('');
+  const [newTodo, SetNewDo] = useState({ title: '', completed: false, user: users.find(u => u.name === nameSelected) || null });
+
   return (
     <div className="App">
-      <h1>Add todo form</h1>
-
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+      <h1 className="App__chapter">Adding new todo</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          SetPrepeadTodosActual([...prepeadTodosActual, newTodo]);
+          SetNewDo({ ...newTodo, title: '' });
+        }}
+        className="App__form"
+      >
+        <input
+          required
+          type="text"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            SetNewDo({ ...newTodo, title: event.target.value });
+          }}
+        />
+        <select
+          required
+          name="name"
+          id="user"
+          onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
+            SetName(event.target.value);
+            SetNewDo({ ...newTodo, user: users.find(u => u.name === event.target.value) || null });
+          }}
+        >
+          <option value="Choose your person">Choose your person</option>
+          {users.map(user => (
+            <option
+              key={user.id}
+              value={user.name}
+            >
+              {user.name}
+            </option>
+          ))}
+        </select>
+        <button type="submit">
+          Add
+        </button>
+      </form>
+      <TodoList todos={prepeadTodosActual} />
     </div>
   );
 };
