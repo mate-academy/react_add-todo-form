98dc26d8968b87212a51b16cd776a0ea5da6ef37
Alex <asultan4ik@gmail.com>
2022-02-23 15:43:57 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,118 @@
+/* eslint-disable import/no-duplicates */
 import React from 'react';
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
 
-import users from './api/users';
+import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+
+const getUsersByID = (userId: number) => (usersFromServer.find(user => user.id === userId));
+
+const preparedTodos = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUsersByID(todo.userId),
+}));
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+  const [title, setTitle] = useState('');
+  const [hasTitleError, setHasTitleError] = useState(false);
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
+  const [userId, setUserId] = useState(0);
+  const [hasUserIdError, sethasUserIdError] = useState(false);
+
+  const [users, setUsers] = useState(preparedTodos);
+
+  const addUsers = (titele: string, usereId: number) => {
+    const newUser = {
+      userId: usereId,
+      id: Date.now(),
+      title: titele,
+      completed: false,
+      user: getUsersByID(usereId),
+    };
+
+    setUsers([...users, newUser]);
+  };
+
+  const onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    setHasTitleError(!title);
+    sethasUserIdError(!userId);
+
+    if (title && userId) {
+      addUsers(title, userId);
+      setTitle('');
+      setUserId(0);
+    }
+  };
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <div className="App">
+        <h1>Add todo form</h1>
+
+        <p>
+          <input
+            type="text"
+            value={title}
+            placeholder="Enter title"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setTitle(event.target.value);
+              setHasTitleError(false);
+            }}
+          />
+          {hasTitleError
+            && <span> Please enter the title </span>}
+        </p>
+
+        <select
+          value={userId}
+          onChange={(event) => {
+            setUserId(+event.target.value);
+            sethasUserIdError(false);
+          }}
+        >
+          <option value="0" disabled>Please choose a user</option>
+
+          {usersFromServer.map(user => (
+            <option
+              value={user.id}
+              key={user.id}
+            >
+              {user.name}
+            </option>
+          ))}
+        </select>
+
+        {hasUserIdError
+          && <span> Please choose a user </span>}
+        <p>
+          <button type="submit">Add</button>
+        </p>
+
+        <div className="todo">
+          <h1>Static list of todos</h1>
+          <ul className="todo__list">
+            {users.map(todo => (
+              <li className="todo__item" key={todo.id}>
+                {todo.user
+                  && (
+                    <>
+                      <p>{` User name: ${todo.user.name}`}</p>
+                      <p>{` User email: ${todo.user.email}`}</p>
+                    </>
+                  )}
+                <p>{` Todo title: ${todo.title}`}</p>
+                <p>{` Todo compelted: ${todo.completed}`}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+    </form>
+
   );
 };
 
