c5544b934e68479b9d30279c9f97ff711fdb0ead
Mykhailo Molnar <mmolnar@atlassian.com>
2022-03-17 18:17:31 +0200

    works

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,130 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
+import todos from './api/todos';
 import users from './api/users';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+import { Todo } from './types/Todo';
+import { TodoList } from './components/TodoList/TodoList';
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+type State = {
+  todos: Todo[];
+  title: string;
+  userId: number;
+  titleError: boolean;
+  userError: boolean;
 };
+
+const preparedTodosFromServer: Todo[] = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId) || null,
+}));
+
+class App extends React.Component<{}, State> {
+  state = {
+    todos: preparedTodosFromServer,
+    title: '',
+    userId: 0,
+    titleError: false,
+    userError: false,
+    maxLength: 25,
+  };
+
+  addTodo = (event: React.FormEvent) => {
+    const {
+      title,
+      userId,
+      maxLength,
+    } = this.state;
+
+    event.preventDefault();
+
+    if (!title.trim()) {
+      this.setState({ titleError: true });
+    }
+
+    if (!userId) {
+      this.setState({ userError: true });
+    }
+
+    if (title.trim() && userId && title.length <= maxLength) {
+      const currentUser = users.find(user => userId === user.id);
+
+      const newTodo = {
+        userId,
+        id: Math.max(...(this.state.todos.map(todo => todo.id))) + 1,
+        title,
+        completed: false,
+        user: currentUser || null,
+      };
+
+      this.setState((state) => ({
+        todos: [...state.todos, newTodo],
+      }));
+
+      this.state.title = '';
+      this.state.userId = 0;
+    }
+  };
+
+  render() {
+    const currentTodos = [...this.state.todos];
+    const {
+      title,
+      userId,
+      titleError,
+      userError,
+      maxLength,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <h2>Add a task</h2>
+        <div className="Form__wrapper">
+          <form
+            onSubmit={this.addTodo}
+            className="Form"
+          >
+            <div className="Form__itemBlock">
+              <input
+                type="text"
+                placeholder="title"
+                value={title}
+                className="Form__item"
+                onChange={(event) => {
+                  this.setState({ title: event.target.value, userError: false });
+                }}
+              />
+              {titleError && <span className="Error">Please enter the title</span>}
+              {title.length > maxLength && <span className="Error">Title length should be no more than 35 characters</span>}
+            </div>
+            <div className="Form__itemBlock">
+              <select
+                value={userId}
+                onChange={(event) => (
+                  this.setState({ userId: +event.target.value, userError: false }))}
+                className="Form__item"
+              >
+                <option key={0} value={0}>Please chose a user</option>
+                {users.map((user, index) => (
+                  <option key={`${user.id}__${index + 1}`} value={user.id}>{user.name}</option>
+                ))}
+              </select>
+              {userError
+              && <span className="Error">Please, select a user</span>}
+            </div>
+            <button
+              type="submit"
+              className="Form__item"
+            >
+              Add
+            </button>
+          </form>
+        </div>
+        <TodoList preparedTodos={currentTodos} />
+      </div>
+    );
+  }
+}
 
 export default App;
