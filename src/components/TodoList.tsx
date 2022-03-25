52b19ba673738b1155294b7081ba2b4467ca34be
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-03-25 13:10:07 +0200

    key was added

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,15 +6,19 @@ import { TodoInfo } from './TodoInfo';
 export const TodoList: React.FC<Props> = ({ todos }) => {
   return (
     <ul className="todoList">
-      {todos.map(todo => (
-        <li className={classNames('todoList__item', {
-          'todoList__item--g': todo.completed,
-          'todoList__item--r': !todo.completed,
-        })}
-        >
-          <TodoInfo todo={todo} />
-        </li>
-      ))}
+      {todos.map(todo => {
+        return (
+          <li
+            key={todo.id}
+            className={classNames('todoList__item', {
+              'todoList__item--g': todo.completed,
+              'todoList__item--r': !todo.completed,
+            })}
+          >
+            <TodoInfo todo={todo} />
+          </li>
+        );
+      })}
     </ul>
   );
 };
