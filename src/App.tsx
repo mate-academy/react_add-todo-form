c44f43da90366ea1d3ac8debf233845e0039e473
Roman Khemych <roman19940912@gmail.com>
2022-04-04 19:25:54 +0300

    React Todo Form version

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,119 @@
-import React from 'react';
-import './App.css';
+import React, { ChangeEvent, useState } from 'react';
+import './App.scss';
 
+import todos from './api/todos';
 import users from './api/users';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Add todo form</h1>
+import { FullTodo, Todo } from './components/types/Todo';
+import { TodoList } from './components/TodoList/TodoList';
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
+function preparedTodos(
+  todosList: Todo[],
+): FullTodo[] {
+  return todosList.map((todo) => ({
+    ...todo,
+    user: users.find(user => user.id === todo.userId) || null,
+  }));
+}
+
+const prepareTodos = preparedTodos(todos);
+
+const App: React.FC = () => {
+  const [title, setTitle] = useState('');
+  const [userId, setUserId] = useState(0);
+  const [todo, addTodo] = useState(prepareTodos);
+
+  const [userError, setUserError] = useState('');
+  const [titleError, setTitleError] = useState('');
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!title) {
+      setTitleError('Please enter the title');
+    }
+
+    if (!userId) {
+      setUserError('Please choose a user');
+    }
+
+    if (userId && title) {
+      addTodo(allTodos => [...allTodos, {
+        userId,
+        title,
+        id: todo.length + 1,
+        completed: false,
+        user: users.find(user => user.id === userId) || null,
+      }]);
+    }
+
+    setUserId(0);
+    setTitle('');
+  };
+
+  const handlerChange = (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    switch (event.target.name) {
+      case 'title':
+        setTitle(event.target.value);
+        setTitleError('');
+        break;
+      case 'user':
+        setUserId(Number(event.target.value));
+        setUserError('');
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <>
+      <h1 className="main">Add Todo Form</h1>
+      <form
+        method="post"
+        action="#"
+        onSubmit={submitHandler}
+        className="form"
+      >
+        <div className="label">
+          <label htmlFor="title">
+            <input
+              type="text"
+              name="title"
+              id="title"
+              placeholder="Write the title"
+              value={title}
+              onChange={handlerChange}
+              className="title"
+            />
+          </label>
+        </div>
+        {titleError && <p className="title_error">{titleError}</p>}
+        <div className="select">
+          <select
+            id="user"
+            name="user"
+            value={userId}
+            onChange={handlerChange}
+            className="user_select"
+          >
+            <option value="">Choose a user</option>
+            {users.map(({ id, name }) => (
+              <option key={id} value={id}>
+                {name}
+              </option>
+            ))}
+          </select>
+          {userError && <p className="user_error">{userError}</p>}
+        </div>
+        <div className="button">
+          <button className="button_add" type="submit">
+            Add the Form
+          </button>
+        </div>
+      </form>
+      <TodoList todos={todo} />
+    </>
   );
 };
 
--- src/api/todos.ts
+++ src/api/todos.ts
@@ -13,6 +13,24 @@ const todos = [
     title: 'quis ut nam facilis et officia qui',
     completed: false,
   },
+  {
+    userId: 1,
+    id: 3,
+    title: 'fugiat veniam minus',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: 'et porro tempora',
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 5,
+    title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+    completed: false,
+  },
 ];
 
 export default todos;
