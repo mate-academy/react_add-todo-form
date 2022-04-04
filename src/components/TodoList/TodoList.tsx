41d12e5ec3eb35caccec0144a17d011832180fcd
Max Shlikhta <maxok.vibe@gmail.com>
2022-04-04 22:19:32 +0300

    Add Todo form

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+import './TodoList.scss';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = React.memo(
+  ({ todos }) => (
+    <div className="todo">
+      <ul className="todo__list">
+        {todos.map((todoItem) => (
+          <li className="todo__item" key={todoItem.id}>
+            <TodoInfo {...todoItem} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  ),
+);
