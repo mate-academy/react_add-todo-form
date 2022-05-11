2649e53911ccf2b67e18c5f898eaa0832b6df2f0
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2022-05-11 17:52:51 +0300

    Create App component to add new Todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,138 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import { TodoList } from './TodoList';
+import { Todo } from './types/Todo';
 
 import users from './api/users';
+import todos from './api/todos';
+
+const preparedTodos: Todo[] = [];
+
+todos.forEach((currentTodo) => {
+  const currentUser = users.find(user => currentTodo.userId === user.id);
+  const toDo = {
+    user: currentUser || null,
+    ...currentTodo,
+  };
+
+  preparedTodos.push(toDo);
+});
 
 const App: React.FC = () => {
+  const [toDoTitle, setTodoTitle] = useState('');
+  const [toDoStatus, setTodoStatus] = useState(false);
+  const [selectUser, setSelectUser] = useState('Choose a user');
+  const [, setNewToDo] = useState({});
+  const [isClicked, setIsClicked] = useState(false);
+
+  const isValidTitle = toDoTitle.length > 0;
+  const isValidChooseInput = selectUser !== 'Choose a user';
+  const isValidInput = isValidTitle && isValidChooseInput;
+
+  const addNewTodo = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+
+    if (isValidInput) {
+      const selectedUser = users.find(user => user.name === selectUser);
+      const maxID = [...preparedTodos].sort((a, b) => b.id - a.id)[0].id;
+
+      const toDo = {
+        user: selectedUser || null,
+        userId: selectedUser?.id,
+        id: maxID,
+        title: toDoTitle.replaceAll(/[^a-zA-Z ЁёА-я ЯЄI яєі 0-9]/g, ''),
+        completed: toDoStatus,
+      };
+
+      preparedTodos.push(toDo);
+
+      setNewToDo(toDo);
+      setTodoTitle('');
+      setTodoStatus(false);
+      setSelectUser('Choose a user');
+      setIsClicked(false);
+    }
+  };
+
+  const changeTodoStatus = (event: string) => {
+    if (event === 'Completed') {
+      setTodoStatus(true);
+    } else {
+      setTodoStatus(false);
+    }
+  };
+
   return (
     <div className="App">
-      <h1>Add todo form</h1>
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+      <div className="form">
+        <form onSubmit={addNewTodo}>
+          <label>
+            <textarea
+              className="input_title"
+              contentEditable
+              value={toDoTitle}
+              placeholder="Title"
+              onChange={(event) => {
+                setTodoTitle(event.target.value);
+              }}
+            />
+            {(!isValidTitle && isClicked)
+            && <p className="error">Please, enter the title!</p>}
+          </label>
+
+          <label>
+            <select
+              onChange={(event) => changeTodoStatus(event.target.value)}
+              value={!toDoStatus ? 'In progress' : 'Completed'}
+            >
+              <option value="Completed">
+                Completed
+              </option>
+              <option value="In progress">
+                In progress
+              </option>
+            </select>
+          </label>
+
+          <label>
+            <select
+              id={selectUser}
+              value={selectUser}
+              onChange={(event) => {
+                setSelectUser(event.target.value);
+              }}
+            >
+              <option>
+                Choose a user
+              </option>
+              {users.map(user => (
+                <option
+                  key={user.id}
+                  value={user.name}
+                >
+                  {user.name}
+                </option>
+              ))}
+            </select>
+          </label>
+
+          {(!isValidChooseInput && isClicked)
+          && <p className="error">Please, choose a user!</p>}
+
+          <button
+            type="submit"
+            // disabled={!isValid}
+            onClick={() => setIsClicked(true)}
+          >
+            Add new todo
+          </button>
+        </form>
+      </div>
+
+      <div className="todo_list">
+        <TodoList todos={preparedTodos} />
+      </div>
     </div>
   );
 };
