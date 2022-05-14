e737f7d7f6b1a00530b620ae3777c89a677e2d3e
MariaBondar <marybondar777@gmail.com>
2022-05-14 19:24:11 +0200

    develop

---
+++ src/components/PreparedTodos.tsx
@@ -0,0 +1,14 @@
+export interface PreparedTodos {
+  userId?: number;
+  id: number;
+  title: string;
+  completed: boolean;
+  user?: User | null;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { UserInfo } from '../UserList/UserInfo';
+import { TodoInfo } from './TodoInfo';
+import { PreparedTodos } from '../PreparedTodos';
+
+type Props = {
+  todos: PreparedTodos[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <ul className="todoList">
+    {todos.map(todo => (
+      <li className="todoList__items" key={todo.id}>
+        <TodoInfo todo={todo} />
+        <UserInfo user={todo.user} />
+      </li>
+    ))}
+  </ul>
+);
