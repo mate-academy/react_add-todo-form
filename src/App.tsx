e027e9690e90b363c5116c56b12be03921176c64
Helen Petrovska <paznachka@gmail.com>
2022-05-17 16:45:07 +0300

    condition for title was added

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
   const addTodo = (event: React.FormEvent) => {
     event.preventDefault();
 
-    if (!title) {
+    if (!title.trim()) {
       setIsErrorTitle(true);
     }
 
