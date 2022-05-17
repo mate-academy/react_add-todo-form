738619e372292229bcdc664bb6556e368a43ea21
yurii837 <gorogotskiy@gmail.com>
2022-05-17 06:23:06 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
+import todos from './api/todos';
 import users from './api/users';
+import { TodoList } from './Components/TodoList/TodoList';
+import { Todo } from './Components/Types/Types';
+
+const findUserById = (userId: number) => {
+  return users.find(user => user.id === userId) || null;
+};
+
+const todosWithUser = todos.map(todo => (
+  { ...todo, user: findUserById(todo.userId) }
+));
 
 const App: React.FC = () => {
+  const [title, setTitle] = useState('');
+  const [titleError, setTitleError] = useState(false);
+  const [userId, setUserId] = useState(0);
+  const [userIdError, setUserIdError] = useState(false);
+  const [toDos, setToDos] = useState(todosWithUser);
+
+  const addNewTodo = () => {
+    if (title === '') {
+      setTitleError(true);
+    }
+
+    if (userId === 0) {
+      setUserIdError(true);
+    }
+
+    if (title !== '' && userId !== 0) {
+      const newTodo = {
+        userId,
+        id: toDos.length + 1,
+        title,
+        completed: false,
+        user: findUserById(userId),
+      };
+
+      setToDos((currentTodos) => [...currentTodos, newTodo]);
+      setUserId(0);
+      setTitle('');
+    }
+  };
+
+  const setNewUser = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setUserId(Number(event.target.value));
+    setUserIdError(false);
+  };
+
   return (
     <div className="App">
-      <h1>Add todo form</h1>
+      <h1>Static list of todos</h1>
+      <div>
+        <TodoList visibleTodos={toDos as Todo[]} />
+      </div>
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+      <form
+        className="form"
+        action="post"
+        onSubmit={(event) => {
+          event.preventDefault();
+          addNewTodo();
+        }}
+      >
+        <div>
+          <input
+            type="text"
+            name="title"
+            placeholder="title"
+            value={title}
+            onChange={event => {
+              setTitle(event.target.value);
+              setTitleError(false);
+            }}
+          />
+          {titleError && (
+            <span className="error">
+              Please enter the title
+            </span>
+          )}
+        </div>
+
+        <div>
+          <select
+            value={userId}
+            onChange={setNewUser}
+          >
+            <option
+              value={0}
+            >
+              Choose a user
+            </option>
+            {users.map(user => (
+              <option
+                value={user.id}
+                key={user.id}
+              >
+                {user.name}
+              </option>
+            ))}
+          </select>
+          {userIdError && (
+            <span className="error">
+              Please choose a user
+            </span>
+          )}
+        </div>
+
+        <button
+          className="button"
+          type="submit"
+        >
+          Add
+        </button>
+
+      </form>
     </div>
   );
 };
