f2e3028011368a917a87597ca68abc9ec6858d83
Denys Cheporniuk <denys.cheporniuk@gmail.com>
2022-06-07 22:34:17 +0300

    fe_mar22 react add form

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="react-scripts" />
+
+export interface Color {
+  id: number;
+  name: string;
+}
+
+export interface GoodWithoutColor {
+  id: number;
+  name: string;
+  colorId: number;
+}
+
+export interface Good extends GoodWithoutColor {
+  color: Color | null;
+}
