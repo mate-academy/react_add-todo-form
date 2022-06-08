70863bd6e13ff967b16bf4057602a51b3f8a89a7
Andrii Yelnikov <whiteyelnikov@gmail.com>
2022-06-09 00:45:42 +0300

    added a checking the input for letters and numbers

--- src/App.tsx
+++ src/App.tsx
@@ -24,9 +24,17 @@ const App: React.FC = () => {
   const [inputedTitle, setInputedTitle] = useState('');
   const [createdId, setCreatedId] = useState(0);
   const [chosenUserId, setChosenUserId] = useState(0);
+  const [titleError, setTitleError] = useState('');
+  const [selectError, setSelectError] = useState('');
 
   const addUser = () => {
-    if (inputedTitle && chosenUserId !== 0) {
+    if (!inputedTitle) {
+      setTitleError('Please enter the title');
+    }
+
+    if (chosenUserId === 0) {
+      setSelectError('Please choose a user');
+    } else {
       setCreatedId(preparedUsers.length + 1);
       const createdUser = {
         title: inputedTitle,
@@ -37,31 +45,48 @@ const App: React.FC = () => {
       };
 
       setPreparedUsers([...preparedUsers, createdUser]);
+      setInputedTitle('');
+    }
+  };
+
+  const checkTheInput = (input: string | number) => {
+    if (typeof (input) === 'string') {
+      if (input.toUpperCase() !== input.toLowerCase()
+    || +input || input === ' ' || input === '') {
+        setInputedTitle(input);
+      }
     }
   };
 
   return (
-    <>
-      <div className="App">
+    <div className="container">
+      <div className="app">
         <h1>Static list of todos</h1>
         <h3>Add a new user</h3>
         <div>
-          <form>
-            <input
-              type="text"
-              placeholder="Title"
-              name="title"
-              value={inputedTitle}
-              onChange={(event) => {
-                setInputedTitle(event.target.value);
-              }}
-            />
+          <form className="form">
+            <div className="form__input">
+              <input
+                type="text"
+                placeholder="Title"
+                className="form__input-the-input"
+                name="title"
+                value={inputedTitle}
+                onChange={(event) => {
+                  checkTheInput(event.target.value);
+                  setTitleError('');
+                }}
+              />
+            </div>
+            <span className="form__input-error">{titleError}</span>
 
             <select
               name="users"
+              className="select-the-user"
               defaultValue={chosenUserId}
               onChange={(event) => {
                 setChosenUserId(+event.target.value);
+                setSelectError('');
               }}
             >
               <option
@@ -75,19 +100,24 @@ const App: React.FC = () => {
                 </option>
               ))}
             </select>
+
+            <span className="form__input-error">{selectError}</span>
             <button
               type="button"
-              onClick={addUser}
+              className="btn"
+              onClick={() => {
+                addUser();
+              }}
             >
               Add
             </button>
           </form>
         </div>
       </div>
-      <div className="container">
+      <div className="todos">
         <TodoList todos={preparedUsers} />
       </div>
-    </>
+    </div>
   );
 };
 
