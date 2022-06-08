ee190d4ce354957e98e8c6bd564786a659949227
Zubov Illia <contactduodiamonds@gmail.com>
2022-06-08 14:40:27 +0300

    something work

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import users from './api/users';
 import todos from './api/todos';
 
-import { User, Todo, PreparedTodo } from './react-app-env';
+import { PreparedTodo, User } from './react-app-env';
 import { TodoList } from './components/TodoList';
 
-const getPreparedTodos = (user: User[], todosInp: Todo[]): PreparedTodo[] => {
-  return todosInp.map((todo: Todo) => ({
-    ...todo,
-    user: user.find((person: User) => (person.id === todo.userId)) || null,
-  }));
+const getUserById = (userId: number): User | null => {
+  return users.find(person => person.id === userId) || null;
 };
 
-const preparedTodos: PreparedTodo[] = getPreparedTodos(users, todos);
+const preparedTodos: PreparedTodo[] = todos.map(todo => ({
+  ...todo,
+  user: getUserById(todo.userId),
+}));
 
 const App: React.FC = () => {
+  const [listPreparedTodos, setListPreparedTodos]
+    = useState([...preparedTodos]);
+  const [title, setTitle] = useState('');
+  const [selectedUserId, setSelectedUserId] = useState(0);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const newObjTodo = {
+      id: Date.now(),
+      title,
+      userId: selectedUserId,
+      user: getUserById(selectedUserId),
+      completed: false,
+    };
+
+    setListPreparedTodos((currentPreparedTodos) => {
+      return [...currentPreparedTodos, newObjTodo];
+    });
+  };
+
   return (
     <div className="App">
-      <h1>Add todo form</h1>
+      <h1>{title}</h1>
 
-      <p>
-        <span>Users: </span>
-        <TodoList prepTodos={preparedTodos} />
-      </p>
+      <form
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter Title here"
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+
+        <select
+          name="users"
+          value={selectedUserId}
+          onChange={(event) => {
+            setSelectedUserId(+event.target.value);
+          }}
+        >
+          <option
+            value="Choose a User"
+            disabled
+          >
+            Choose a User
+          </option>
+
+          {users.map(user => (
+            <option
+              value={user.id}
+              key={user.id}
+            >
+              {user.name}
+            </option>
+          ))}
+        </select>
+
+        <button
+          type="submit"
+        >
+          Add
+        </button>
+      </form>
+
+      <section>
+        <TodoList prepTodos={listPreparedTodos} />
+      </section>
     </div>
   );
 };
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -7,7 +7,12 @@ interface Props {
 
 export const UserInfo: React.FC<Props> = ({ user }) => (
   <>
-    <h2 data-cy="username">{user.name}</h2>
-    <a href={`mailto:${user.email}`} data-cy="email">{user.email}</a>
+    <h2 data-cy="username">
+      {user.name}
+    </h2>
+
+    <a href={`mailto:${user.email}`} data-cy="email">
+      {user.email}
+    </a>
   </>
 );
