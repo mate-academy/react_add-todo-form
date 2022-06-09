345fa400b8846fc5e0a7f0ca9a923311c33e5919
Denys Cheporniuk <denys.cheporniuk@gmail.com>
2022-06-09 21:21:29 +0300

    fe_mar22 react add goods forms done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,126 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import classnames from 'classnames';
 
-import users from './api/users';
+import './App.css';
+import { Color, Good, GoodWithoutColor } from './react-app-env';
+import {GoodsList} from "./components/GoodsList";
+
+const colors: Color[] = [
+  { id: 1, name: 'red' },
+  { id: 2, name: 'green' },
+  { id: 3, name: 'blue' },
+];
+
+const goodsFromServer: GoodWithoutColor[] = [
+  { id: 1, colorId: 1, name: 'Dumplings' },
+  { id: 2, colorId: 2, name: 'Carrot' },
+  { id: 3, colorId: 3, name: 'Eggs' },
+  { id: 4, colorId: 1, name: 'Ice cream' },
+  { id: 5, colorId: 2, name: 'Apple' },
+  { id: 6, colorId: 3, name: 'Bread' },
+  { id: 7, colorId: 1, name: 'Fish' },
+  { id: 8, colorId: 2, name: 'Honey' },
+  { id: 9, colorId: 3, name: 'Jam' },
+  { id: 10, colorId: 1, name: 'Garlic' },
+];
+
+const getColorById = (colorId: number): Color | null => {
+  return colors.find(color => color.id === colorId) || null;
+}
+
+const goodsWithColor: Good[] = goodsFromServer.map(good => ({
+  ...good,
+  color: getColorById(good.colorId),
+}));
+
 
 const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([...goodsWithColor]);
+
+  const [newGoodName, setNewGoodName] = useState('');
+  const [hasNameError, setHasNameError] = useState(false);
+
+  const [selectColorId, setSelectColorId] = useState(0);
+  const [hasColorIdError, setHasColorIdError] = useState(false);
+
+  const addGood = (name: string, colorId: number) => {
+    const newGood = {
+      id: Date.now(),
+      name,
+      colorId,
+      color: getColorById(selectColorId)
+    }
+
+    setGoods((currentGoods) => [...currentGoods, newGood])
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setHasNameError(!newGoodName)
+    setHasColorIdError(!selectColorId);
+
+    if (newGoodName && selectColorId) {
+      addGood(newGoodName, selectColorId);
+      setNewGoodName('')
+      setSelectColorId(0);
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Add todo form</h1>
+      <h1>Form</h1>
+      <GoodsList goods={goods} />
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+
+      <form
+        onSubmit={handleSubmit}
+      >
+        <input
+          className={classnames({ error: hasNameError })}
+          type="text"
+          value={newGoodName}
+          placeholder="test"
+          onChange={(event) => {
+            setNewGoodName(event.target.value);
+            setHasNameError(false);
+          }}
+        />
+
+        {hasNameError && (
+          <span>Name is empty</span>
+        )}
+
+        <select
+          className={classnames({ error: hasColorIdError })}
+          value={selectColorId}
+          onChange={(event) => {
+            setSelectColorId(+event.target.value);
+            setHasColorIdError(false);
+          }}
+        >
+          <option value="0" disabled>Choose a color</option>
+
+          {colors.map(color => (
+            <option
+              key={color.id}
+              value={color.id}
+            >
+              {color.name}
+            </option>
+          ))}
+        </select>
+
+        {hasColorIdError && (
+          <span>Color is empty</span>
+        )}
+
+        <button
+          type="submit"
+        >
+          Add
+        </button>
+      </form>
     </div>
   );
 };
---
+++ src/components/GoodsList/GoodsList.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Good } from '../../react-app-env';
+
+interface Props {
+  goods: Good[]
+}
+
+export const GoodsList: React.FC<Props> = ({ goods }) => (
+  <ul>
+    {goods.map(good => (
+      <li
+        key={good.id}
+        style={{ color: good.color?.name }}
+      >
+        {good.name}
+      </li>
+    ))}
+  </ul>
+)
