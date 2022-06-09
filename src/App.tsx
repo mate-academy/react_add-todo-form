3ed2f20f59b1f8c0cab781e7bc293fed17983756
Anatolii Vlasiuk <vlasyuk_a@ukr.net>
2022-06-09 15:41:11 +0300

    TODODForm was done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,142 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
+import { PrepArray } from './react-app-env';
 import users from './api/users';
+import todos from './api/todos';
+
+const preparedtodos:PrepArray[] = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId || null),
+}));
 
 const App: React.FC = () => {
+  const [todosCurrent, setTodosCurrent] = useState(preparedtodos);
+  const [newtitle, setNewTitle] = useState('');
+  const [newUser, setNewUser] = useState(0);
+  const [completed, setCompleted] = useState(false);
+  const [isUserChoosed, setIsUserChoosed] = useState(false);
+  const [isTitleadded, setIsTitleadded] = useState(false);
+
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!newtitle) {
+      setIsTitleadded(true);
+    }
+
+    if (!newUser) {
+      setIsUserChoosed(true);
+    }
+
+    if (newtitle && newUser) {
+      const newTodo: PrepArray = {
+        userId: +newUser,
+        id: todosCurrent.length + 1,
+        title: newtitle,
+        completed,
+        user: users.find(user => user.id === newUser),
+      };
+
+      setTodosCurrent([...todosCurrent, newTodo]);
+    }
+
+    setNewTitle('');
+    setNewUser(0);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newtitle}
+          placeholder="add new title"
+          onChange={(event) => {
+            setNewTitle(event.target.value);
+            setIsTitleadded(false);
+          }}
+        />
+        {isTitleadded && (
+          <div style={
+            {
+              border: '4px solid red',
+              width: '50vw',
+              margin: '0 auto',
+            }
+          }
+          >
+            Please, add a title!!!
+          </div>
+        )}
+        <select
+          value={newUser}
+          onChange={(event) => {
+            setNewUser(+event.target.value);
+            setIsUserChoosed(false);
+          }}
+        >
+          <option value="0" hidden>Choose a user</option>
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+          {users.map(user => (
+            <option
+              key={user.id}
+              value={user.id}
+            >
+              {user.name}
+            </option>
+          ))}
+        </select>
+        {isUserChoosed && (
+          <div style={
+            {
+              border: '4px solid red',
+              width: '50vw',
+              margin: '0 auto',
+            }
+          }
+          >
+            Please, choose a user!!!
+          </div>
+        )}
+
+        <div>
+          <input
+            type="checkbox"
+            name="completed"
+            checked={completed}
+            onChange={() => setCompleted(!completed)}
+          />
+          <label htmlFor="completed">Check, if completed</label>
+        </div>
+        <button
+          type="submit"
+        >
+          Add
+        </button>
+      </form>
+      {todosCurrent.map(item => (
+        <div className="App__box" key={item.id}>
+          <p className="">
+            <span className="App__span">Name: </span>
+            {item.user?.name}
+          </p>
+          <p className="">
+            <span className="App__span">Title: </span>
+            {item.title}
+          </p>
+          <p className="">
+            <span className="App__span">Id: </span>
+            {item.id}
+          </p>
+          <p className="">
+            <span className="App__span">Compleated: </span>
+            {`${item.completed}`}
+          </p>
+        </div>
+      ))}
+
     </div>
   );
 };
