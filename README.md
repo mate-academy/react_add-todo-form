2afc2f77df6f7a26b4467fc9a9a3c312b7c2290f
Murat Yesimbietov <96179533+murat-yesimbietov@users.noreply.github.com>
2022-06-10 16:38:58 +0300

    Adding cypress tests (#890)
    * [react_add_todo_form]: added components tests
    
    * [react_add_todo_form]: added e2e tests
    
    * [react_add_todo_form]: little changes
    
    * Update cypress/integration/page.spec.js
    
    Co-authored-by: Yaroslav Kostenko <39385053+iBrianWarner@users.noreply.github.com>
    
    * Update page.spec.js
    
    Co-authored-by: Yaroslav Kostenko <39385053+iBrianWarner@users.noreply.github.com>

--- README.md
+++ README.md
@@ -9,8 +9,9 @@ Implement ability to add TODOs to the `TodoList` implemented in [Static list of 
 1. Create `App` component storing `todos` array and displaying it using `TodoList`
 1. Create a form to add new TODOs
     - DON'T create a separate component for the form (later we will learn how to do it)
-    - there should be a text input for the `title`
+    - there should be a text input for the `title` with `data-cy="titleInput"` attribute
     - also add a `<select>` with all the users from `./src/api/users.js`
+    - `<select>` should have `data-cy="userSelect"` attribute
     - add labels and placeholders where they are needed
     - the `TODO` should be added to the list after clicking `Add` button
     - each TODO should have `id`, `title`, `userId` and `completed` (`false` by default)
@@ -18,8 +19,8 @@ Implement ability to add TODOs to the `TodoList` implemented in [Static list of 
 1. Add a validation to the form
     - add an empty option `Choose a user` to the users select
     - before creating a todo check if a `user` was selected, if not show an error message next to the `select` (`Please choose a user`)
-    - if the `title` is empty show an error message next to the `title` field (`Please enter the title`)
-    - errors should appear only after pressing `Add` button
+    - if the `title` is empty, show an error message next to the `title` field (`Please enter the title`)
+    - errors should appear only after clicking `Add` button
     - hide a message immediately after any change of the field with an error
 1. If the form is valid add a todo to the list and clear the form
 1. (* Optional) Allow entering only letters (`ru` and `en`), digits and `spaces` in the `title` field.
--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,7 +1,90 @@
-// WRITE TESTS HERE
+import users from '../../src/api/users';
+import todos from '../../src/api/todos';
+
+const page = {
+  visit() {
+    cy.visit('/');
+  },
+
+  get titleInput() {
+    return cy.getByDataCy('titleInput');
+  },
+
+  get userSelect() {
+    return cy.getByDataCy('userSelect');
+  },
+
+  get selectedOption() {
+    return cy.get('select[data-cy="userSelect"] option:selected');
+  },
+
+  get addButton() {
+    return cy.contains('button', 'Add');
+  }
+};
 
 describe('Page', () => {
-  it('should be visitable', () => {
-    cy.visit('/');
+  beforeEach(() => {
+    page.visit();
+  });
+
+  it('should have "Add todo" form', () => {
+    page.titleInput
+      .should('have.attr', 'placeholder');
+
+    page.selectedOption
+      .should('have.text', 'Choose a user');
+
+    page.addButton
+      .should('exist');
+  });
+
+  it('should show an error if "title" is empty', () => {
+    const { name } = users[5];
+
+    page.titleInput
+      .invoke('val', '');
+
+    page.userSelect
+      .select(name);
+
+    page.addButton
+      .click();
+
+    cy.contains('Please enter the title')
+      .should('exist');
+  });
+
+  it('should show an error if user is not selected', () => {
+    page.titleInput
+      .type('to do something');
+
+    page.addButton
+      .click();
+
+    cy.contains('Please choose a user')
+      .should('exist');
+  });
+
+  it('should add todo to the list', () => {
+    const { name, email } = users[5];
+    const todoTitle = 'to do something';
+
+    page.titleInput
+      .type(todoTitle);
+
+    page.userSelect
+      .select(name);
+
+    page.addButton
+      .click();
+
+    cy.get('.todoList')
+      .should('have.length', todos.length + 1);
+
+    cy.contains('.todoList', todoTitle)
+      .should('contain.text', name)
+      .and('contain.text', email)
+      .and('contain.text', 'Not completed')
   });
 });
