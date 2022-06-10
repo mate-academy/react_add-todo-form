4d56d7e87127ea5b8128baa5ce2d28e971e5e2b3
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-06-10 23:51:39 +0300

    Task Done v1.0

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,36 @@
 /// <reference types="react-scripts" />
+
+export interface Todo {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+}
+
+export interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+  address: {
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    geo: {
+      lat: string,
+      lng: string,
+    },
+  },
+  phone: string,
+  website: string,
+  company: {
+    name: string,
+    catchPhrase: string,
+    bs: string,
+  },
+}
+
+export interface TodoWidthUser extends Todo {
+  user: User | null;
+}
