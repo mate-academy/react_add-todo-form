3fc52d7ac098ef6ffbd384cf154d57be6a1a0956
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2022-06-10 19:17:41 +0300

    solution

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,42 @@
 /// <reference types="react-scripts" />
+
+export interface Todo {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+}
+
+export interface PreparedTodos extends Todo {
+  user: User | null,
+}
+
+export interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+  address: Address,
+  phone: string,
+  website: string,
+  company: Company,
+}
+
+interface Address {
+  street: string,
+  suite: string,
+  city: string,
+  zipcode: string,
+  geo: Geo,
+}
+
+interface Geo {
+  lat: string,
+  lng: string,
+}
+
+interface Company {
+  name: string,
+  catchPhrase: string,
+  bs: string,
+}
