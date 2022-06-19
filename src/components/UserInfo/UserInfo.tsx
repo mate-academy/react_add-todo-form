75f171a2b11abd638137674727148a25a9b1c997
PRO-GRAM-MER <ankitbaage3@gmail.com>
2022-06-19 18:20:53 +0530

    Solution

---
+++ src/components/UserInfo/UserInfo.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  name :string,
+  email :string
+};
+
+export const UserInfo: React.FC<Props> = ({ name, email }) => (
+  <>
+    <p data-cy="username">{name}</p>
+    <p data-cy="email">{email}</p>
+  </>
+);
