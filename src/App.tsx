f323d71f9da78b7e8e49256b45e4a36a4285a022
Karina Prozner <prozner.karina@gmail.com>
2022-06-22 22:46:17 +0200

    here by now

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import todos from './api/todos';
@@ -13,22 +13,125 @@ const preparedTodos: PreparedTodo[] = todos.map(todo => {
   };
 });
 
-const App: React.FC = () => (
-  <>
-    <div className="App">
-      <h1>Static list of todos</h1>
-      <TodoList todos={preparedTodos} />
-    </div>
+const App: React.FC = () => {
+  const [todosList, setTodosList] = useState([...preparedTodos]);
+  const [title, setTitle] = useState('');
+  const [selectUser, setSelectUser] = useState(0);
+  const [completed, setCompleted] = useState(false);
+  const [errorUser, setErrorUser] = useState(false);
+  const [errorTitle, setErrorTitle] = useState(false);
 
-    <div className="App">
-      <h1>Add todo form</h1>
+  const newTodo = () => (
+    [...todosList, {
+      userId: selectUser,
+      id: todosList.length + 1,
+      title,
+      completed,
+      user: users.find(user => user.id === selectUser),
+    }]
+  );
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+  const addTodo = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (title && selectUser) {
+      setTodosList(newTodo);
+      setTitle('');
+      setCompleted(false);
+      setSelectUser(0);
+    }
+
+    setErrorUser(!selectUser);
+    setErrorTitle(!title);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="title">Add todo form</h1>
+      <form
+        action="get"
+        onSubmit={addTodo}
+        className="form"
+      >
+        <div>
+          <select
+            name=""
+            id=""
+            data-cy="userSelect"
+            value={selectUser}
+            onChange={(changeEvent) => {
+              setSelectUser(+changeEvent.target.value);
+              setErrorUser(false);
+            }}
+          >
+            <option
+              value={0}
+              disabled
+            >
+              Choose a user
+            </option>
+            {users.map(user => (
+              <option
+                key={user.id}
+                value={user.id}
+              >
+                {user.name}
+              </option>
+            ))}
+          </select>
+
+          <p>
+            {errorUser && (
+              'Please choose a user'
+            )}
+          </p>
+        </div>
+
+        <div>
+          <input
+            type="text"
+            name="title"
+            data-cy="titleInput"
+            placeholder="Add todo"
+            value={title}
+            onChange={(changeEvent) => {
+              setTitle(changeEvent.target.value);
+              setErrorTitle(false);
+            }}
+
+          />
+          <p>
+            {errorTitle && (
+              'Please enter the title'
+            )}
+          </p>
+        </div>
+
+        <button type="submit">Add todo</button>
+
+      </form>
+
+      <TodoList todos={todosList} />
     </div>
-  </>
-);
+  );
+};
+
+// (
+//   <>
+//     <div className="App">
+//       <h1>Static list of todos</h1>
+//       <TodoList todos={preparedTodos} />
+//     </div>
+
+//     <div className="App">
+//       <h1>Add todo form</h1>
+
+//       <p>
+//         <span>Users: </span>
+//         {users.length}
+//       </p>
+//     </div>
+//   </>
+// );
 
 export default App;
