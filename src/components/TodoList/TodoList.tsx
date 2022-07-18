4b25c292b226d3e4ec932981fae29456fd5fecc5
Yaroslav Baiduk <byaa99@gmail.com>
2022-07-18 15:57:32 +0300

    Compleated

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './todoList.css';
+import TodoInfo from '../TodoInfo/TodoInfo';
+
+type Props = {
+  todos: Todo[],
+};
+
+const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>
+          <TodoInfo todo={todo} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
