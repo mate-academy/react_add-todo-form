ed4a34ee7ec1804e80d171146c1505152aa65d1b
Dmytro Mostovyi <7kozita@gmail.com>
2022-07-20 21:12:24 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,17 @@
-export const TodoInfo = () => {};
+import React from 'react';
+
+type Props = {
+  title: string,
+  completed: boolean,
+};
+
+export const TodoInfo: React.FC<Props> = ({ title, completed }) => (
+  <>
+    <h2 className="list__item-title" data-cy="title">{title}</h2>
+    <p data-cy="status">
+      {completed
+        ? 'Complited'
+        : 'The task is not finished'}
+    </p>
+  </>
+);
