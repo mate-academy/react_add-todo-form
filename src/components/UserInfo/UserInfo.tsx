baa4637105f8828267f10ef127c66af0e6e6dbc3
Ihor Kuchin <vedunigor@gmail.com>
2022-07-20 23:51:12 +0300

    Solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,20 @@
-export const UserInfo = () => {};
+import React from 'react';
+import './UserInfo.scss';
+
+type Props = {
+  user: User
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  return (
+    <div className="UserInfo">
+      <p className="UserInfo__name">
+        {user.name}
+      </p>
+
+      <a className="UserInfo__email" href={`mailto:${user.email}`}>
+        {user.email}
+      </a>
+    </div>
+  );
+};
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,12 @@
-/// <reference types="react-scripts" />
+interface Todo {
+  id: number,
+  title: string,
+  userId: number | null,
+  completed: boolean,
+  user: User | null,
+}
+
+interface User {
+  name: string,
+  email: string,
+}
