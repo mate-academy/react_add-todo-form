1b34ea22fafeef5fc95f4be9b0efd54775fc43a6
Yana Ostapenko <yana.ostapenko@gmail.com>
2022-07-20 21:41:36 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,32 @@
-export const TodoInfo = () => {};
+import { User, UserInfo } from '../UserInfo/UserInfo';
+
+export interface Todo {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+  user?: User | null,
+}
+
+type Props = {
+  todo: Todo,
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const { user, title, completed } = todo;
+
+  return (
+
+    <article
+      data-id={todo.id}
+      className={`TodoInfo${completed ? ' TodoInfo--completed' : ''}`}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+      {user
+        ? <UserInfo user={user} />
+        : 'waiting for user info'}
+    </article>
+  );
+};
