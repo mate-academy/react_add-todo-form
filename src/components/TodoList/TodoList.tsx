becb960dd26830209c6cffe09d3a29c50d2972cc
Luyda Andrikevych <l.andrikevych@gmail.com>
2022-07-21 07:33:16 +0200

    presolution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,30 @@
-export const TodoList = () => {};
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+type Props = {
+  todos: Todo[],
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <article
+          key={todo.id}
+          data-id="1"
+          className={classNames(
+            'TodoInfo',
+            {
+              'TodoInfo--completed': todo.completed,
+            },
+          )}
+        >
+          <TodoInfo todo={todo} />
+          {todo.user && <UserInfo user={todo.user} />}
+        </article>
+      ))}
+    </section>
+  );
+};
