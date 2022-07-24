9887844df916b546c36a6abc637c881d27faf407
Bogdan Pyshchyk <bohdan.pyshchyk@gmail.com>
2022-07-24 21:29:49 +0300

    form 3

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -11,13 +11,8 @@ type Props = {
   user?: User | null;
 };
 
-export const UserInfo: React.FC<Props | null> = ({ user }) => (
-  <>
-    {user && (
-      <>
-        <h2 data-cy="username">{user.name}</h2>
-        <h3 data-cy="email">{user.email}</h3>
-      </>
-    )}
-  </>
-);
+export const UserInfo: React.FC<Props | null> = ({ user }) => {
+  return (
+    <h2 data-cy="username">{user?.name}</h2>
+  );
+};
