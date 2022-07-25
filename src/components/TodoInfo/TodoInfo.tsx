b53506e37fe3019e09c3d8b4e08480980cbb997c
Sasha Pavlenko <380971520240@gmail.com>
2022-07-25 09:55:56 +0300

    Todo form

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,19 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import { PreparedToDos } from '../../types/interfaces';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+type Props = {
+  todo: PreparedToDos,
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <article
+    data-id={todo.id}
+  >
+    <h2 className="TodoInfo__title title is-2 is-spaced">{todo.title}</h2>
+
+    {todo.user && (
+      <UserInfo user={todo.user} />
+    )}
+  </article>
+);
