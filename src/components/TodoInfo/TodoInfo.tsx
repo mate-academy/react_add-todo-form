888db974d5ea0d52808c113febc1e86c89079085
Batick3737 <antonandriy@ukr.net>
2022-07-27 14:05:53 +0300

    write code

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,16 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { Todo } from '../../react-app-env';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <>
+    <h2 className="TodoInfo__title">
+      {todo.title}
+    </h2>
+    <UserInfo user={todo.user} />
+  </>
+);
