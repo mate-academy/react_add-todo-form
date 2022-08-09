5faf6da92276bb427bb44b29354cf52d31bc9997
Ksenia Didukh <k.tsviatko@gmail.com>
2022-08-09 23:34:36 +0300

    Solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,35 @@
-export const TodoList = () => {};
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+import usersFromServer from '../../api/users';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <article
+          data-id={todo.id}
+          className={classNames(
+            'TodoInfo',
+            {
+              'TodoInfo--completed': todo.completed,
+            },
+          )}
+        >
+          <h2 className="TodoInfo__title">
+            {todo.title}
+          </h2>
+
+          <UserInfo user={[...usersFromServer]
+            .find(user => user.id === todo.userId) || null}
+          />
+        </article>
+      ))}
+
+    </section>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,13 @@
-export const UserInfo = () => {};
+import { User } from '../../types/User';
+
+type Props = {
+  user: User | null;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  return user ? (
+    <a className="UserInfo" href={user.email || ''}>
+      {user.name}
+    </a>
+  ) : null;
+};
