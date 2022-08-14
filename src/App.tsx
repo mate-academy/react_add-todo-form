ba46b1376ad1ddccf36c13852e4182262353828e
Aleksandr Yuschenko <105875778+AleksandrYuschenko@users.noreply.github.com>
2022-08-14 18:49:44 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,147 @@
+import { useState } from 'react';
+import classNames from 'classnames';
+import { TodoList } from './components/TodoList/TodoList';
+import { Todo } from './types/Todos';
+import { User } from './types/Users';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+
+function getUser(userid: number | null): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userid);
+
+  return foundUser || null;
+}
+
+const getUserId = (username: string): number | null => {
+  const foundId = usersFromServer.find(user => user.name === username);
+
+  return foundId?.id || null;
+};
+
+const todos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
 
 export const App = () => {
+  const [user, setUser] = useState('');
+  const [title, setTitle] = useState('');
+  const [toDo, setToDo] = useState(todos);
+  const [eror, setEror] = useState(false);
+
+  const newId = [
+    ...toDo.sort((a, b) => a.id - b.id)][todosFromServer.length - 1].id + 1;
+  const newUserId = getUserId(user);
+  const newUser = getUser(newUserId);
+  const createTodo = () => (
+    {
+      id: newId,
+      title,
+      completed: false,
+      userId: newUserId,
+      user: newUser,
+    }
+  );
+
+  const newEl = createTodo();
+  const add = () => {
+    toDo.push(newEl);
+
+    return todos;
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        id="form"
+        action="/api/users"
+        method="Post"
+        onSubmit={(event) => {
+          event.preventDefault();
+
+          if (user !== '') {
+            setTitle('');
+          }
+
+          if (title !== '') {
+            setUser('');
+          }
+
+          if (user !== '' && title !== '') {
+            setToDo(add());
+            setEror(false);
+          }
+        }}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <input
+            id="text"
+            type="text"
+            data-cy="titleInput"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            placeholder="enter todo title"
+            required
+          />
+          {eror && (
+            <span
+            // eslint-disable-next-line quote-props
+              className={classNames('no-error', { 'error': title === '' })}
+            >
+              Please enter a title
+            </span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
+          <select
+            data-cy="userSelect"
+            value={user}
+            onChange={(event => setUser(event.target.value))}
+            required
+          >
+            <option value="0">Choose a user</option>
+            {usersFromServer.map(el => (
+              <option
+                value={el.name}
+                key={el.id}
+              >
+                {el.name}
+              </option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {eror && (
+            <span
+              className={classNames(
+                // eslint-disable-next-line quote-props
+                'no-error', { 'error': user === '0' || user === '' },
+              )}
+            >
+              Please choose a user
+            </span>
+          )}
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button
+          type="submit"
+          data-cy="submitButton"
+          onClick={() => {
+            if (user === '' || title === '') {
+              setEror(true);
+            }
+          }}
+        >
           Add
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
+      <TodoList todos={toDo} />
 
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
     </div>
   );
 };
