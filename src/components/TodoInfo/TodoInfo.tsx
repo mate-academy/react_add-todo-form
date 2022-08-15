d3cd3394032beccfbbce041dc7bd65b55c851ae9
Oleksandr-Pustovit <Pustovit.o2020@gmail.com>
2022-08-15 18:02:41 +0300

    implemented add function for todo list

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,29 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { Todo } from '../../Interfaces/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+interface Props {
+  todo: Todo;
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  return (
+    <article
+      data-id={todo.id}
+      className={classNames(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': todo.completed,
+        },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+
+      {todo.user && (
+        <UserInfo user={todo.user} key={todo.user.id} />
+      )}
+    </article>
+  );
+};
