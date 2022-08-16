3d5b4fb18364b1adaf31af9e99f31daf680e2831
denis-khristyuk <monsieur.apala@gmail.com>
2022-08-16 10:37:38 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,146 @@
 import './App.scss';
-
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import { FormEvent, useState } from 'react';
+import { TodoList } from './components/TodoList/TodoList';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { Todo } from './types';
 
 export const App = () => {
+  const getUser = (userId: number) => {
+    const foundedUser = usersFromServer.find(user => user.id === userId);
+
+    return foundedUser || null;
+  };
+
+  const todosWithUsers = todosFromServer.map((todo) => ({
+    ...todo,
+    user: getUser(todo.userId),
+  }));
+
+  const [title, setTitle] = useState('');
+  const [userId, setUserId] = useState('0');
+  const [todos, setTodos] = useState(todosWithUsers);
+  const [isCompleted, setIsCompleted] = useState(false);
+  const [selectorError, setSelectorError] = useState(false);
+  const [titleError, setTitleError] = useState(false);
+
+  const createTodo = (
+    currentTitle: string,
+    currentUserId: string,
+    currentIsCompleted: boolean,
+  ) => {
+    const createdTodo: Todo = {
+      id: Math.max(...todos.map(el => el.id)) + 1,
+      title: currentTitle,
+      completed: currentIsCompleted,
+      userId: Number(currentUserId),
+      user: usersFromServer.find(person => (
+        person.id === Number(currentUserId))) || null,
+    };
+
+    setTodos([
+      ...todos,
+      createdTodo,
+    ]);
+  };
+
+  const handleSelector = (value: string) => {
+    setUserId(value);
+    setSelectorError(false);
+  };
+
+  const handleTitle = (value: string) => {
+    setTitle(value);
+    if (title === '') {
+      setTitleError(false);
+    }
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (title.trim() === '') {
+      setTitleError(true);
+    }
+
+    if (userId === '0') {
+      setSelectorError(true);
+    }
+
+    if (title.trim() !== '' && userId !== '0') {
+      createTodo(title, userId, isCompleted);
+      setTitle('');
+      setUserId('0');
+      setIsCompleted(false);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={(event) => handleSubmit(event)}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <Input
+            placeholder="Please enter a title"
+            data-cy="titleInput"
+            value={title}
+            onChange={event => handleTitle(event.target.value)}
+          />
+          {titleError && (
+            <span className="error">
+              Please enter a title
+            </span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
-          </select>
+          <Select
+            data-cy="userSelect"
+            value={userId}
+            onChange={event => handleSelector(event.target.value)}
+          >
+            <MenuItem value="0" disabled>Choose a user</MenuItem>
 
-          <span className="error">Please choose a user</span>
+            {usersFromServer.map((person) => (
+              <MenuItem value={person.id} key={person.id}>
+                {person.name}
+              </MenuItem>
+            ))}
+          </Select>
+          {selectorError && (
+            <span className="error">
+              Please choose a user
+            </span>
+          )}
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <div className="field">
+          <label>
+            <input
+              type="checkbox"
+              onClick={() => setIsCompleted(!isCompleted)}
+            />
+            Completed
+          </label>
+        </div>
+        <Button
+          type="submit"
+          data-cy="submitButton"
+          variant="outlined"
+        >
           Add
-        </button>
+        </Button>
       </form>
-
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={todos} />
     </div>
   );
 };
