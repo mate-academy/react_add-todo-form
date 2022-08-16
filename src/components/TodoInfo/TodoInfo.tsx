a77e238a777ede72619f4f2f2081bc3da9aea598
Andrew Bessonov <andrewbessik@gmail.com>
2022-08-16 09:43:45 +0300

    Soluton

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,26 @@
-export const TodoInfo = () => {};
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+type Props = {
+  todo: Todo,
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const { title, completed, user, id } = todo;
+
+  return (
+    <article
+      data-id={id}
+      className={
+        `TodoInfo ${
+          (completed)
+            ? 'TodoInfo--completed'
+            : ''
+        }`
+      }
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && (<UserInfo user={user} />)}
+    </article>
+  );
+};
