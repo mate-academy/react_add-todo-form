866348280680644e1f2fba999ac4512a5b4ba0db
Mariya Sukhanova <suhanovamasha21@gmail.com>
2022-08-16 13:19:01 +0300

    Added solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,26 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+interface Props {
+  todo: TodoWithUser;
+}
+
+export const TodoInfo = (props: Props) => {
+  const { todo } = props;
+
+  return (
+    <article
+      data-id={todo.id}
+      className={classNames(
+        'TodoInfo', {
+          'TodoInfo--completed': todo.completed,
+        },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+      {todo.user && <UserInfo user={todo.user} />}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,17 @@
-export const TodoList = () => {};
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+interface Props {
+  todos: TodoWithUser[];
+}
+
+export const TodoList = (props: Props) => {
+  const { todos } = props;
+
+  return (
+    <section className="TodoList">
+      {todos.map((todo) => {
+        return <TodoInfo todo={todo} key={todo.id} />;
+      })}
+    </section>
+  );
+};
