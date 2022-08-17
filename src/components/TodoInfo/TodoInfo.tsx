4d98f746bde6e599da8fb292dd9499856be43aec
Zolkiewskyi Oleg <zolkiewskyioleg@gmail.com>
2022-08-17 21:57:53 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,18 @@
-export const TodoInfo = () => {};
+import className from 'classnames';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <article
+    data-id={todo.id}
+    className={className('TodoInfo', { 'TodoInfo--completed': todo.completed })}
+  >
+    <h2 data-cy="titleInput" className="TodoInfo__title">{todo.title}</h2>
+
+    {todo.user && <UserInfo user={todo.user} />}
+  </article>
+);
