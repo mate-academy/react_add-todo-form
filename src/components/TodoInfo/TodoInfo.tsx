1de4961cdea56cdd8e9026871ee0684c6c8ebe40
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-08-24 11:08:17 +0200

    firstCommit

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,19 @@
-export const TodoInfo = () => {};
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <>
+    <h2
+      className="TodoInfo__title"
+      data-id={todo.id}
+    >
+      {todo.title}
+    </h2>
+
+    {todo.user && (<UserInfo {...todo.user} />)}
+  </>
+);
