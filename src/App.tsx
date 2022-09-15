edadb0fa9657ad6ba3bbc954458e6b6034fe2ccf
Jamal Amoian <leonidagutin09@gmail.com>
2022-09-15 16:56:40 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.scss';
 
+import React, { useState } from 'react';
 import usersFromServer from './api/users';
 import todosFromServer from './api/todos';
 
@@ -19,23 +20,94 @@ export const todos: Todo[] = todosFromServer.map(todo => ({
   user: getUser(todo.userId),
 }));
 
-export const App = () => {
+let maxTodosId = [...todos].sort((a, b) => b.id - a.id)[0].id;
+
+export const App: React.FC = () => {
+  const [userId, setUserId] = useState(0);
+  const [title, setTitle] = useState('');
+  const [isValidTitle, setIsValidTitle] = useState(true);
+  const [isValidUser, setIsValidUser] = useState(true);
+
+  const makeNewTodo = () => {
+    const newTodo = {
+      id: maxTodosId + 1,
+      title,
+      completed: false,
+      userId,
+      user: getUser(userId),
+    };
+
+    todos.push(newTodo);
+  };
+
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    setIsValidTitle(true);
+  };
+
+  const handleChangeSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setUserId(+event.target.value);
+    setIsValidUser(true);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!userId) {
+      setIsValidUser(false);
+    }
+
+    if (!title.trim()) {
+      setIsValidTitle(false);
+    }
+
+    if (!userId || !title.trim()) {
+      return;
+    }
+
+    makeNewTodo();
+
+    setUserId(0);
+    setTitle('');
+    setIsValidTitle(true);
+    setIsValidUser(true);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={handleSubmit}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <input
+            type="text"
+            data-cy="titleInput"
+            placeholder="Enter a title"
+            value={title}
+            onChange={handleChangeInput}
+          />
+          {isValidTitle || <span className="error">Please enter a title</span>}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <select
+            data-cy="userSelect"
+            defaultValue={userId}
+            value={userId}
+            onChange={handleChangeSelect}
+          >
             <option value="0" disabled>Choose a user</option>
+
+            {usersFromServer.map(({ name, id }) => (
+              <option value={id} key={id}>{name}</option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {isValidUser || <span className="error">Please choose a user</span>}
         </div>
 
         <button type="submit" data-cy="submitButton">
