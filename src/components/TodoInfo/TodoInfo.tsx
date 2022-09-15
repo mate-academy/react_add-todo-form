269e73af1ec19746263bb41429cc030d2c7082de
Jamal Amoian <leonidagutin09@gmail.com>
2022-09-15 16:19:11 +0300

    added TodoInfo, TodoList and UserInfo

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,34 @@
-export const TodoInfo = () => {};
+import React from 'react';
+
+import classNames from 'classnames';
+
+import { UserInfo } from '../UserInfo/UserInfo';
+
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo,
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    title,
+    user,
+  } = todo;
+
+  return (
+    <article
+      className={
+        classNames(
+          'TodoInfo',
+          {
+            'TodoInfo--completed': todo.completed,
+          },
+        )
+      }
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
