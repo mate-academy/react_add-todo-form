6cea9214561822ddfa8152a8b3dd78682b5afcb6
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-09-19 18:32:42 +0300

    First commit

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,28 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import classNames from 'classnames';
+
+import { Todo } from '../../types/todo';
+
+import { UserInfo } from '../UserInfo/UserInfo';
+
+type Props = {
+  todo: Todo,
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  return (
+    <article className={classNames(
+      'TodoInfo',
+      {
+        'TodoInfo--completed': todo.completed === true,
+      },
+    )}
+    >
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+
+      {todo.user && <UserInfo user={todo.user} />}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,16 @@
-export const TodoList = () => {};
+import React from 'react';
+
+import { Todo } from '../../types/todo';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+type Props = {
+  todosList: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todosList }) => (
+  <section className="TodoList">
+    {todosList.map(todo => (
+      <TodoInfo todo={todo} key={todo.id} />
+    ))}
+  </section>
+);
