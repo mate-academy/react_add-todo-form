a9905062eec4d1eab6335cd0e5c954986a51f93d
Cyril Prue <kir8mir>
2022-09-22 16:47:08 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,32 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+}
+
+interface Props {
+  todo: Todo,
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  return (
+    <article
+      data-id="1"
+      className={classNames(
+        'TodoInfo',
+        { 'TodoInfo--completed': todo.completed },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+
+      <UserInfo todo={todo} key={todo.id} />
+
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,38 @@
-export const UserInfo = () => {};
+import usersFromServer from '../../api/users';
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+}
+
+interface Props {
+  todo: Todo,
+}
+
+export const UserInfo: React.FC<Props> = ({ todo }) => {
+  const users: User[] = [...usersFromServer];
+
+  return (
+    users.map(user => (
+      user.id === todo.userId
+      && (
+        <a
+          className="UserInfo"
+          href={`mailto:${user.email}`}
+          key={user.id}
+        >
+          {user.name}
+        </a>
+      )
+    ))
+  );
+};
