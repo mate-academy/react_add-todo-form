7d652dddddd9789656971f83d5dd20a4c2c6a1f0
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2022-09-23 13:56:11 +0300

    created a form to add new Todos

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,33 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    title,
+    user,
+    completed,
+  } = todo;
+
+  return (
+    <article
+      data-id="1"
+      className={classNames(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': completed,
+        },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user ? <UserInfo user={user} /> : 'There is no user'}
+    </article>
+  );
+};
