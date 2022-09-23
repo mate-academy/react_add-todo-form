0f77425975baaecf1af9abc982c8a0fe361a475d
Ganna Krainiuk <anya.krainiuk@gmail.com>
2022-09-23 14:07:31 +0300

    todo forms solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,15 @@
-export const TodoList = () => {};
+import React from 'react';
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
+
+type TodoType = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<TodoType> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo key={todo.id} todo={todo} />
+    ))}
+  </section>
+);
