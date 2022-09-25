7046b29ed7dc780025b944e60c14d3e56e11bd68
roma-shafihullin <lanister0789@gmail.com>
2022-09-25 19:11:32 +0300

    userInfo, todoInfo, todoList components added

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,20 @@
-export const TodoInfo = () => {};
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/todo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const isCompleted = todo.completed ? 'TodoInfo--completed' : 'TodoInfo';
+
+  return (
+    <article data-id="1" className={isCompleted}>
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+      {todo.user && <UserInfo user={todo.user} />}
+
+    </article>
+  );
+};
