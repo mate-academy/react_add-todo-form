9b6cf26356d7f24c4ebc6db73838c8f03683e6bd
Oleksandr Sobchuk <osobchuk1@gmail.com>
2022-09-26 19:59:45 +0300

    add TODO form

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,27 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type TodoType = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<TodoType> = ({ todo }) => {
+  const { title, completed } = todo;
+
+  return (
+    <>
+      <article
+        data-id={todo.id}
+        className={classNames('TodoInfo', {
+          'TodoInfo--completed': completed === true,
+        })}
+      >
+        <h2 className="TodoInfo__title">{title}</h2>
+        {todo.user
+          ? <UserInfo user={todo.user} />
+          : null}
+      </article>
+    </>
+  );
+};
