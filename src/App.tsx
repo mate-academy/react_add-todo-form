6b818066ce44c9f7a04d0ce97efe50e12dd110ce
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-10-23 13:51:18 +0800

    version1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.scss';
-
-// import usersFromServer from './api/users';
+import usersFromServer from './api/users';
 // import todosFromServer from './api/todos';
+import { TodoList } from './components/TodoList';
 
 export const App = () => {
   return (
@@ -27,35 +27,7 @@ export const App = () => {
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList users={usersFromServer} />
     </div>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,42 @@
-export const TodoList = () => {};
+interface Users {
+  id: number,
+  title: string,
+  completed: boolean,
+  userId: number,
+}
+
+type Props = {
+  users: Users[],
+};
+
+export const TodoList: React.FC<Props> = ({ users }) => {
+  <section className="TodoList">
+    <article data-id="1" className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">
+        delectus aut autem
+      </h2>
+
+      <a className="UserInfo" href="mailto:Sincere@april.biz">
+        Leanne Graham
+      </a>
+    </article>
+
+    <article data-id="15" className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">delectus aut autem</h2>
+
+      <a className="UserInfo" href="mailto:Sincere@april.biz">
+        Leanne Graham
+      </a>
+    </article>
+
+    <article data-id="2" className="TodoInfo">
+      <h2 className="TodoInfo__title">
+        quis ut nam facilis et officia qui
+      </h2>
+
+      <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
+        Patricia Lebsack
+      </a>
+    </article>
+  </section>
+};
