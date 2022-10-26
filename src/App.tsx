6cf1b04438320fa712e39298eb56ad9b79412939
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-10-26 19:16:22 +0300

    Implemented the todo form

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,144 @@
 import './App.scss';
+import React, { useState } from 'react';
+import { TodoList } from './components/TodoList';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { TodoWithUser, User } from './react-app-env';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+const getUser = (userId: number): User | null => {
+  return usersFromServer.find(user => user.id === userId) || null;
+};
+
+const todoWithUser: TodoWithUser[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
 
 export const App = () => {
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [todos, setTodos] = useState<TodoWithUser[]>(todoWithUser);
+  const [selectedUserId, setSelectedUserId] = useState(0);
+  const [title, setTitle] = useState('');
+
+  const addTodo = () => {
+    const maxId = Math.max(...todos.map(todo => todo.id));
+
+    if (title.trim().length) {
+      const newTodo = {
+        id: maxId + 1,
+        title: title.trim(),
+        completed: false,
+        userId: selectedUserId,
+        user: usersFromServer.find(user => user.id === selectedUserId) || null,
+      };
+
+      setTodos(currentTodos => [...currentTodos, newTodo]);
+    }
+  };
+
+  const clearTitle = () => {
+    setTitle('');
+  };
+
+  const clearSelectedUserId = () => {
+    setSelectedUserId(0);
+  };
+
+  const isValidTitle = (text: string) => {
+    const pattern = /^[A-Za-z0 9\s]*$|[бвгґджзклмнпрстфхцчшщйаеєиіїоуюяь]/i;
+
+    return pattern.test(text);
+  };
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedUserId(+event.currentTarget.value);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.currentTarget.value);
+  };
+
+  const onSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsSubmit(true);
+
+    if (title && selectedUserId && isValidTitle(title)) {
+      addTodo();
+      clearTitle();
+      clearSelectedUserId();
+      setIsSubmit(false);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
       <form action="/api/users" method="POST">
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="createTitle">
+            Tilte:&nbsp;
+          </label>
+
+          <input
+            id="createTitle"
+            type="text"
+            value={title}
+            placeholder="Enter a title"
+            data-cy="titleInput"
+            onChange={handleInputChange}
+          />
+
+          {isSubmit && !title && (
+            <span className="error">&nbsp;Please enter a title &#128521;</span>
+          )}
+
+          {isSubmit && !isValidTitle(title) && (
+            <span className="error">
+              &nbsp;The title can contain only letters,
+              numbers and no more than 1 space in a row &#128546;
+            </span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <label htmlFor="selectUser">
+            User:&nbsp;
+          </label>
+
+          <select
+            style={
+              { marginTop: '10px' }
+            }
+            id="selectUser"
+            data-cy="userSelect"
+            value={selectedUserId}
+            onChange={handleSelectChange}
+          >
             <option value="0" disabled>Choose a user</option>
+            {usersFromServer.map(user => (
+              <option value={user.id} key={user.id}>{user.name}</option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {isSubmit && !selectedUserId && (
+            <span className="error">&nbsp;Please choose a user &#128521;</span>
+          )}
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button
+          style={
+            { marginTop: '10px' }
+          }
+          type="submit"
+          data-cy="submitButton"
+          onClick={onSubmit}
+        >
           Add
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={todos} />
     </div>
   );
 };
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,34 @@
-export const TodoInfo = () => {};
+import cn from 'classnames';
+import { TodoWithUser } from '../../react-app-env';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: TodoWithUser;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    title,
+    completed,
+    id,
+    user,
+  } = todo;
+
+  return (
+    <article
+      data-id={id}
+      className={cn(
+        'TodoInfo',
+        { 'TodoInfo--completed': completed },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && (
+        <UserInfo user={user} />
+      )}
+    </article>
+  );
+};
