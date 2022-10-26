f21295ea8a85eea53c8627976af53326611fc15e
Datsenko Mariia <datsenko.mate@gmail.com>
2022-10-27 00:31:49 +0300

    form for adding todo has been created

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,19 @@
-export const UserInfo = () => {};
+import React from 'react';
+import { User } from '../../react-app-env';
+
+type Props = {
+  user: User;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const {
+    name,
+    email,
+  } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
