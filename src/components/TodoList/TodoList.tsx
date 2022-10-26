0d85b30f4469ed524334d8a52c63d60a70696eef
Vadym Mamchur <vmchr344@gmail.com>
2022-10-26 14:34:43 +0300

    todo form has been implemented

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,20 @@
-export const TodoList = () => {};
+import { TodoWithUser } from '../../react-app-env';
+
+import { TodoInfo } from '../TodoInfo';
+
+interface Props {
+  todos: TodoWithUser[];
+}
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo
+          todo={todo}
+          key={todo.id}
+        />
+      ))}
+    </section>
+  );
+};
