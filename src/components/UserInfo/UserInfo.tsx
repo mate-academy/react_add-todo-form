353adf1cdac8486e797ab4e69b696bec697f7da6
Artur Nagaycev <artur.nagaycev@gmail.com>
2022-10-26 21:22:23 +0300

    implemented form to add new todo

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,16 @@
-export const UserInfo = () => {};
+import React from 'react';
+import { User } from '../../react-app-env';
+
+type Props = {
+  user: User,
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const { email, name } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
