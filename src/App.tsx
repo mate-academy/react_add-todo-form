5a3f20c06179df49ac0dda7335376cc40bf0aede
Paul Voronin <voron.paul@gmail.com>
2022-10-28 22:57:33 +0300

    Solved.Ver.1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,129 @@
 import './App.scss';
+import { useState } from 'react';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+
+import { User } from './types/User';
+import { Todo } from './types/Todo';
+import { TodoList } from './components/TodoList';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+export const todos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
+
+function getUserByName(userName: string): User | undefined {
+  return usersFromServer.find(user => user.name === userName);
+}
 
 export const App = () => {
+  const [visibleTodos, setTodos] = useState(todos);
+  const [author, setAuthor] = useState('Choose a user');
+  const [task, setTask] = useState('');
+  const [isActiveTask, setIsActiveTask] = useState(false);
+  const [isActiveAuthor, setIsActiveAuthor] = useState(false);
+  const [isTaskError, setIsTaskError] = useState(false);
+  const [isAuthorError, setIsAuthorError] = useState(false);
+
+  const updateTodos = () => {
+    setIsTaskError(!isActiveTask);
+    setIsAuthorError(!isActiveAuthor);
+
+    if (isActiveTask && isActiveAuthor) {
+      const maxId = Math.max(...todos.map(todo => todo.id));
+      const newTodo: Todo = {
+        id: maxId + 1,
+        userId: getUserByName(author)?.id,
+        title: task,
+        completed: false,
+        user: getUserByName(author),
+      };
+
+      todos.push(newTodo);
+      setTodos(todos);
+      setTask('');
+      setAuthor('Choose a user');
+      setIsActiveTask(false);
+      setIsActiveAuthor(false);
+      setIsTaskError(false);
+      setIsAuthorError(false);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={(e) => {
+          e.preventDefault();
+          updateTodos();
+        }}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="title">Title: </label>
+
+          <input
+            type="text"
+            data-cy="titleInput"
+            id="title"
+            placeholder="Enter a title"
+            value={task}
+            onChange={(e) => {
+              setTask(e.target.value);
+              setIsActiveTask(true);
+              setIsTaskError(false);
+            }}
+          />
+          {isTaskError
+          && <span className="error">Please enter a title</span>}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
+          <label htmlFor="title">User: </label>
+
+          <select
+            data-cy="userSelect"
+            id="title"
+            value={author}
+            onChange={(e) => {
+              setAuthor(e.target.value);
+              setIsActiveAuthor(true);
+              setIsAuthorError(false);
+            }}
+          >
+            <option value="">Choose a user</option>
+            {usersFromServer.map(user => (
+              <option
+                value={user.name}
+                key={user.id}
+              >
+                {user.name}
+              </option>
+            ))}
           </select>
-
-          <span className="error">Please choose a user</span>
+          {isAuthorError
+          && <span className="error">Please choose a user</span>}
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button
+          type="submit"
+          data-cy="submitButton"
+        >
           Add
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={visibleTodos} />
     </div>
   );
 };
