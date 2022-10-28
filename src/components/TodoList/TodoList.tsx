21d235f31a5d327a3229aa292bc15624f7d1e12e
Artem Musii <musiy.artem@gmail.com>
2022-10-28 18:35:24 +0300

    add todo form implemented

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,14 @@
-export const TodoList = () => {};
+import { TodoInfo } from '../TodoInfo';
+import { TodoWithUser } from '../../react-app-env';
+
+type Props = {
+  todos: TodoWithUser[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo key={todo.id} todo={todo} />
+    ))}
+  </section>
+);
