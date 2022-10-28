4f0b621402bf26e73c9cd5afc1cf31c784404d7e
Denys Kleimenov <denyskleimenovit@gmail.com>
2022-10-28 15:06:55 +0300

    UserInfo component was implemented

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,11 @@
-export const UserInfo = () => {};
+import { User } from '../../react-app-env';
+
+type Props = {
+  user: User;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
+);
