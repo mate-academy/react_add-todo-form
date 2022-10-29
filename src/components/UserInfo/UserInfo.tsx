eae77d71172dfe24c7815925d5d32b145db28e7e
Andrii Melnyk <andriimelnyq@gmail.com>
2022-10-29 14:35:06 +0300

    Solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,16 @@
-export const UserInfo = () => {};
+import usersFromServer from '../../api/users';
+
+type Props = {
+  userId: number,
+};
+
+export const UserInfo = ({ userId }: Props) => {
+  const user = usersFromServer
+    .find(curentUser => curentUser.id === userId);
+
+  return (
+    <a className="UserInfo" href={`mailto:${user?.email}`}>
+      {user?.name}
+    </a>
+  );
+};
