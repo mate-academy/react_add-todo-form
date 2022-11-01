fedfb166def43ef2151b9dbf125cedfe7aec89ee
Sasha Shevchenko <sasha.shevchenko763@gmail.com>
2022-11-01 17:18:08 +0200

    chanded types files

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,15 +1,4 @@
 module.exports = {
   extends: "@mate-academy/stylelint-config",
-  rules: {
-    "import/extensions": [
-       "error",
-       "ignorePackages",
-       {
-         "js": "never",
-         "jsx": "never",
-         "ts": "never",
-         "tsx": "never"
-       }
-    ]
- }
+  rules: {}
 };
