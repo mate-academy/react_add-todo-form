dadfa7692e9f3db34aee3af2f1a32e72cba611a5
Denys Topchyi <miliarder97@gmail.com>
2022-11-17 22:49:08 +0200

    react_add-todo-form

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,18 @@
-export const TodoList = () => {};
+import React from 'react';
+import { Todo } from '../../Types/Todo';
+import { TodoInfo } from '../TodoInfo';
+// import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todos: Todo[],
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo key={todo.id} todo={todo} />
+      ))}
+    </section>
+  );
+};
