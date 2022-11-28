4cd10dc88b5a92a96a5a6ec7e3e197bb320a026e
Danyla Krygin <underworldsan@gmail.com>
2022-11-28 19:42:12 +0200

    Add ToDo form completed

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,31 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    userId,
+    title,
+    completed,
+    user,
+  } = todo;
+
+  return (
+    <article className={classNames(
+      'TodoInfo',
+      {
+        'TodoInfo--completed': completed,
+      },
+    )}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && <UserInfo key={userId} user={user} />}
+    </article>
+  );
+};
