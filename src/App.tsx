3e6d130dddf8fe8685a66b70105d5b73e250333b
Svitlana Yudina <osbka31@gmail.com>
2022-11-29 13:25:40 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,121 @@
+import React, { useState } from 'react';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import { User } from './types/User';
+import { Todo } from './types/Todo';
 
-export const App = () => {
+import { TodoList } from './components/TodoList';
+
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+
+function getUser(userId: number): User | null {
+  return usersFromServer.find(user => user.id === userId) || null;
+}
+
+const todos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
+
+export const App: React.FC = () => {
+  const [selectedName, setSelectedName] = useState('');
+  const [title, setTitle] = useState('');
+  const [visibleTodos, setVisibleTodos] = useState(todos);
+  // const [isDataEntred, setIsDataEntred] = useState(false);
+  const [isTitleEntred, setIsTitleEntred] = useState(false);
+  const [isUserChoosed, setIsUserChoosed] = useState(false);
+
+  const handleUpdatingTodos = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    const selectedUser: User | null = usersFromServer
+      .find(user => (user.name === selectedName)) || null;
+
+    const todoID: number = Math
+      .max(...visibleTodos.map(todo => todo.id)) + 1;
+
+    if (selectedUser && title) {
+      setVisibleTodos((prevTodos) => {
+        return [
+          ...prevTodos,
+          {
+            id: todoID,
+            title,
+            completed: false,
+            userId: selectedUser.id,
+            user: selectedUser,
+          },
+        ];
+      });
+      setSelectedName('');
+      setTitle('');
+      setIsTitleEntred(false);
+      setIsUserChoosed(false);
+    } else {
+      setIsTitleEntred(true);
+      setIsUserChoosed(true);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/indent, no-console
+      // console.log(isDataEntred, title);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
       <form action="/api/users" method="POST">
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <input
+            type="text"
+            name="title"
+            data-cy="titleInput"
+            placeholder="Please enter a title"
+            value={title}
+            onChange={(event) => {
+              setTitle(event.target.value);
+              setIsTitleEntred(false);
+            }}
+          />
+          {!title && isTitleEntred
+            && <span className="error">Please enter a title</span>}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
-          </select>
+          <select
+            data-cy="userSelect"
+            value={selectedName}
+            onChange={(event) => {
+              setSelectedName(event.target.value);
+              // setIsUserChoosed(selectedName !== '');
+            }}
+          >
+            <option value="" disabled>
+              Choose a user
+            </option>
 
-          <span className="error">Please choose a user</span>
+            {usersFromServer.map(user => (
+              <option key={user.id} value={user.name}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+          {!selectedName && isUserChoosed
+            && <span className="error">Please choose a user</span>}
+
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button
+          type="submit"
+          data-cy="submitButton"
+          onClick={handleUpdatingTodos}
+        >
           Add
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={visibleTodos} />
     </div>
   );
 };
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,35 @@
-export const TodoInfo = () => {};
+import React from 'react';
+
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+interface Props {
+  todo: Todo,
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    title,
+    completed,
+    user,
+  } = todo;
+
+  return (
+    <article
+      data-id={todo.id}
+      className={classNames(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': completed,
+        },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
