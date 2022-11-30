1c9ce6248c68f98643003dd2701a6f0177ad0d4c
spiritkyiv <baolt.king@gmail.com>
2022-11-30 20:20:19 +0200

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,157 @@
+import { useState } from 'react';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+
+import { User } from './types/User';
+import { Todo } from './types/Todo';
+
+import { TodoList } from './components/TodoList';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+export const todosArr = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
 
 export const App = () => {
+  const [todos, setTodos] = useState<Todo[]>(todosArr);
+  const [userId, setUserId] = useState(0);
+  const [todoTitle, setTodoTitle] = useState('');
+  const [submitAcces, setSubmitAcces] = useState(false);
+  const [visibleAttention, setVisibleAttention] = useState(false);
+
+  const changeSumbitAcces = (todo_title: string, user_id: number) => {
+    if (user_id && todo_title) {
+      setSubmitAcces(true);
+    }
+  };
+
+  const findLastIndex = (prevTodos: Todo[]): number => (
+    prevTodos.reduce((maxID, currentTodo) => (
+      Math.max(maxID, currentTodo.id)), 0)
+  );
+
+  const checkInputString = (str: string): string => {
+    str.replace(/[^a-zA-Z\u0400-\u04FF\0-9 ]/g, '');
+
+    return str;
+  };
+
+  const updateTodos = (todo_title: string, user_id: number) => {
+    setTodos((prevTodos) => (
+      [...prevTodos,
+        {
+          id: findLastIndex(prevTodos) + 1,
+          title: todo_title,
+          userId: user_id,
+          completed: false,
+          user: getUser(user_id),
+        }]
+    ));
+  };
+
+  const resetForm = () => {
+    setUserId(0);
+    setTodoTitle('');
+    setVisibleAttention(false);
+  };
+
+  const handleSubmitData = () => {
+    if (submitAcces) {
+      updateTodos(todoTitle, userId);
+      resetForm();
+    } else {
+      setVisibleAttention(true);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleSubmitData();
+        }}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="title_id">
+            Title:
+          </label>
+          <input
+            type="text"
+            data-cy="titleInput"
+            name="title"
+            value={todoTitle}
+            id="title_id"
+            placeholder="Enter a title"
+            onChange={(event) => {
+              const inputTitle = checkInputString(event.target.value);
+
+              setTodoTitle(inputTitle);
+              changeSumbitAcces(inputTitle, userId);
+            }}
+          />
+          {(!todoTitle && visibleAttention) && (
+            <span className="error">Please enter a title</span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <label htmlFor="user_id">
+            User:
+          </label>
+          <select
+            data-cy="userSelect"
+            name="user"
+            value={userId}
+            id="user_id"
+            onChange={(event) => {
+              const { value } = event.target;
+
+              setUserId(+value);
+              changeSumbitAcces(todoTitle, +value);
+            }}
+          >
             <option value="0" disabled>Choose a user</option>
+            {usersFromServer.map(user => (
+
+              <option
+                key={user.id}
+                value={user.id}
+              >
+                {user.name}
+              </option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {(!userId && visibleAttention) && (
+            <span className="error">Please choose a user</span>
+          )}
+
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button
+          type="submit"
+          data-cy="submitButton"
+
+        >
           Add
         </button>
       </form>
 
       <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
+        <TodoList todos={todos} />
       </section>
     </div>
   );
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,30 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/Todo';
+
+export const TodoInfo = ({ todo }: { todo:Todo }) => {
+  const {
+    completed,
+    id,
+    title,
+    user,
+  } = todo;
+
+  return (
+    <article
+      data-id={id}
+      className={classNames(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': completed,
+        },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
