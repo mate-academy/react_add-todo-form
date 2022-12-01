922b39ce2a08616cd4ca26e26706f6f6c9b096ca
Bohdan Stepanets <number1xy@gmail.com>
2022-12-01 16:29:32 +0200

    Cant choose user in select

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,90 @@
+import React, { useState } from 'react';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
 
-export const App = () => {
+import { User } from './types/User';
+import { Todo } from './types/ToDo';
+import { TodoList } from './components/TodoList';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  // if there is no user with a given userId
+  return foundUser || null;
+}
+
+export const todos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
+
+export const App: React.FC = () => {
+  const [title, setTitle] = useState('');
+  const [userId, setUserId] = useState(0);
+  const [wrongTitleUser, setWrongTitleUser] = useState(false);
+  const [todosToShow, setTodosToShow] = useState(todos);
+
+  function handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const newTodoId = Math.max(...todosToShow.map(todo => todo.id)) + 1;
+
+    if (title.trim() === '' || userId === 0) {
+      setWrongTitleUser(true);
+    } else {
+      const todoToAdd = {
+        id: newTodoId,
+        title,
+        completed: false,
+        userId,
+        user: getUser(userId),
+      };
+
+      setTodosToShow(currentTodos => ([
+        ...currentTodos,
+        todoToAdd,
+      ]));
+    }
+  }
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={handleSubmit}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
+          <input
+            type="text"
+            data-cy="titleInput"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
           <span className="error">Please enter a title</span>
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <select
+            data-cy="userSelect"
+            value={userId}
+            onChange={(event) => setUserId(Number(event.target.value))}
+          >
             <option value="0" disabled>Choose a user</option>
+            {usersFromServer.map(user => (
+              <option value={userId} key={user.id}>
+                {user.name}
+              </option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {userId === 0 && wrongTitleUser === false
+            && <span className="error">Please choose a user</span>}
+          {/* <span className="error">Please choose a user</span> */}
         </div>
 
         <button type="submit" data-cy="submitButton">
@@ -27,35 +92,7 @@ export const App = () => {
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={todosToShow} />
     </div>
   );
 };
