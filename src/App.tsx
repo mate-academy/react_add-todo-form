94e5644f217ebfd8f7f83b20dfb33876c7b6f573
Sasha Yatsykovych <olexandr.y@mate.academy>
2022-12-02 12:46:41 +0200

    add logic with deleting todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,59 @@
-import './App.scss';
+import { useEffect, useState } from 'react';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import './App.scss';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { TodoList } from './components/TodoList';
+import { Todo } from './types/Todo';
+import { TodoForm } from './components/TodoForm';
+
+const todoWithUser: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: usersFromServer.find(user => user.id === todo.userId) || null,
+}));
 
 export const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = (newTodo: Omit<Todo, 'id'>) => {
+    const todoToAdd = {
+      id: Math.max(...todos.map(todo => todo.id)) + 1,
+      ...newTodo,
+    };
+
+    setTodos((currentTodos) => [...currentTodos, todoToAdd]);
+  };
+
+  const deleteTodo = (todoID: number) => {
+    setTodos((currentTodos) => currentTodos.filter(
+      todo => todo.id !== todoID,
+    ));
+  };
+
+  // const updateTodo = (updatedTodo: Todo) => {
+  //   setTodos((currentTodos) => currentTodos.map(
+  //     todo => (todo.id === updatedTodo.id ? updatedTodo : todo),
+  //   ));
+  // };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTodos(todoWithUser);
+    }, 500);
+  }, []);
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
+      <TodoForm
+        onSubmit={addTodo}
+      />
 
-      <form action="/api/users" method="POST">
-        <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
-        </div>
-
-        <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
-          </select>
-
-          <span className="error">Please choose a user</span>
-        </div>
-
-        <button type="submit" data-cy="submitButton">
-          Add
-        </button>
-      </form>
-
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList
+        todos={todos}
+        onTodosDeleted={deleteTodo}
+        // onTodosUpdated={updateTodo}
+      />
     </div>
   );
 };
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,42 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import React, { useState } from 'react';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+// import { TodoForm } from "../TodoForm";
+
+type Props = {
+  todo: Todo;
+  onTodoDeleted: (todoID: number) => void,
+  // onTodoUpdate: (todo: Todo) => void,
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo, onTodoDeleted }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  return (
+    <article
+      data-id={todo.id}
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })}
+    >
+      {isEditing && <p> Form </p> }
+
+      <h2 className="TodoInfo__title">
+        {todo.title}
+
+        <button type="button" onClick={() => onTodoDeleted(todo.id)}>
+          x
+        </button>
+
+        <button type="button" onClick={() => setIsEditing(true)}>
+          edit
+        </button>
+      </h2>
+
+      {todo.user && (
+        <UserInfo user={todo.user}/>
+      )}
+    </article>
+  );
+}
