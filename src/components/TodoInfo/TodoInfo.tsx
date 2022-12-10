adb881578d4a8a1937f605da522c844d4197e2d1
Taras Kozii <taras_kozii@ukr.net>
2022-12-10 11:42:16 +0200

    solution v01

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,26 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+
+import { Todo } from '../../type/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+type Props = {
+  todo: Todo
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const { title, userId, id } = todo;
+
+  return (
+    <article
+      data-id={id}
+      className={
+        classNames('TodoInfo', { 'TodoInfo--completed': false })
+      }
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+      <UserInfo userId={userId} />
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,15 @@
-export const UserInfo = () => {};
+import usersFromServer from '../../api/users';
+
+type Props = {
+  userId: number
+};
+
+export const UserInfo: React.FC<Props> = ({ userId }) => {
+  const user = usersFromServer.find(person => person.id === userId);
+
+  return (
+    <a className="UserInfo" href={`mailto:${user?.email}`}>
+      {user?.name}
+    </a>
+  );
+};
