877c9f7636e813f0d25eaa000b90a8e6cf7237c9
s3rhiienko <r.i.serhiienko@gmail.com>
2022-12-16 09:00:18 +0200

    Add todo form

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,161 @@
 import './App.scss';
+import { useState } from 'react';
+import { TodoList } from './components/TodoList/TodoList';
+import { User } from './types/User';
+import { Todo } from './types/Todo';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+export const todos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
 
 export const App = () => {
+  const [title, setTitle] = useState('');
+  const [visibleTodos, setTodos] = useState(todos);
+  const [username, setUsername] = useState('0');
+  const [isNoTitle, setIsNoTitle] = useState(false);
+  const [isNoUser, setIsNoUser] = useState(false);
+
+  const largestId = Math.max(...todos.map(todo => todo.id));
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>
+  | React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case 'title':
+        if (value.length !== 0) {
+          setIsNoTitle(false);
+        }
+
+        // eslint-disable-next-line no-case-declarations
+        const newValue = value.trimStart().replace(/[^a-zA-Z0-9 ]/g, '');
+
+        return setTitle(newValue);
+
+      case 'username':
+        if (value) {
+          setIsNoUser(false);
+        }
+
+        return setUsername(value);
+
+      default:
+        return 0;
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const selectedUser = usersFromServer.find(user => (
+      user.name === username));
+
+    if (selectedUser && title.length !== 0) {
+      setTodos([
+        ...visibleTodos,
+        {
+          id: largestId + 1,
+          title: `${title}`,
+          completed: false,
+          userId: selectedUser.id,
+          user: selectedUser,
+        },
+      ]);
+      setTitle('');
+      setUsername('0');
+    }
+
+    if (title.length === 0) {
+      setIsNoTitle(true);
+    }
+
+    if (!selectedUser) {
+      setIsNoUser(true);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={handleSubmit}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="title">
+            {'Title: '}
+          </label>
+
+          <input
+            id="title"
+            name="title"
+            type="text"
+            data-cy="titleInput"
+            placeholder="Enter a title"
+            value={title}
+            onChange={handleChange}
+          />
+
+          {isNoTitle && (
+            <span className="error">
+              Please enter a title
+            </span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <label htmlFor="user">
+            {'User: '}
+          </label>
+
+          <select
+            id="user"
+            data-cy="userSelect"
+            name="username"
+            required
+            value={username}
+            onChange={handleChange}
+          >
             <option value="0" disabled>Choose a user</option>
+
+            {usersFromServer.map(currUser => (
+              <option
+                key={currUser.id}
+                value={currUser.name}
+              >
+                {currUser.name}
+              </option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {isNoUser && (
+            <span className="error">
+              Please choose a user
+            </span>
+          )}
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button
+          type="submit"
+          data-cy="submitButton"
+        >
           Add
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={visibleTodos} />
     </div>
   );
 };
