1e40f2be131843f2ab9b0b60b0751679cc918d5c
Oleksandr Filo <filo.sanya@gmail.com>
2022-12-22 22:25:39 +0200

    add todo form was implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,131 @@
+import React, { useState } from 'react';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
 
-export const App = () => {
+import { User } from './types/User';
+import { Todo } from './types/Todo';
+import { TodoList } from './components/TodoList';
+
+function getUserById(id: number, users: User[]) {
+  return users.find(user => id === user.id) as User;
+}
+
+const todoList: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUserById(todo.userId, usersFromServer),
+}));
+
+function getUserByName(name: string, users: User[]) {
+  return users.find(user => name === user.name) as User;
+}
+
+function getLargestTodoId(todos: Todo[]) {
+  return [...todos].sort((todo1, todo2) => todo2.id - todo1.id)[0].id;
+}
+
+export const App: React.FC = () => {
+  const titleDefaultValue = '';
+  const userDefaultValue = 'Choose a user';
+
+  const [title, setTitle] = useState(titleDefaultValue);
+  const [user, setUser] = useState(userDefaultValue);
+  const [todos, setTodos] = useState(todoList);
+  const [isUserSelected, setIsUserSelected] = useState(true);
+  const [isTitleEntered, setIsTitleEntered] = useState(true);
+
+  const handleSubmitForm = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (user !== userDefaultValue && title !== titleDefaultValue) {
+      const newUser = getUserByName(user, usersFromServer);
+      const newTodoId = getLargestTodoId(todos) + 1;
+      const newTodo: Todo = {
+        id: newTodoId,
+        title,
+        completed: false,
+        userId: newUser.id,
+        user: newUser,
+      };
+
+      setTodos(currentTodo => [...currentTodo, newTodo]);
+      setTitle(titleDefaultValue);
+      setUser(userDefaultValue);
+    }
+
+    if (user === userDefaultValue) {
+      setIsUserSelected(false);
+    }
+
+    if (title === titleDefaultValue) {
+      setIsTitleEntered(false);
+    }
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.currentTarget.value);
+    setIsTitleEntered(true);
+  };
+
+  const handleUserChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setUser(event.currentTarget.value);
+    setIsUserSelected(true);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={handleSubmitForm}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="titleInput">
+            {'Title: '}
+          </label>
+
+          <input
+            type="text"
+            id="titleInput"
+            name="titleInput"
+            data-cy="titleInput"
+            placeholder="Enter a title"
+            value={title}
+            onChange={handleTitleChange}
+            autoComplete="off"
+          />
+          {!isTitleEntered && (
+            <span className="error">Please enter a title</span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
+          <label htmlFor="userSelect">
+            {'User: '}
+          </label>
+
+          <select
+            id="userSelect"
+            name="userSelect"
+            data-cy="userSelect"
+            value={user}
+            onChange={handleUserChange}
+          >
+            <option value={userDefaultValue} disabled>
+              {userDefaultValue}
+            </option>
+            {usersFromServer.map(userInfo => (
+              <option value={userInfo.name} key={userInfo.id}>
+                {userInfo.name}
+              </option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {!isUserSelected && (
+            <span className="error">Please choose a user</span>
+          )}
         </div>
 
         <button type="submit" data-cy="submitButton">
@@ -27,35 +133,7 @@ export const App = () => {
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={todos} />
     </div>
   );
 };
