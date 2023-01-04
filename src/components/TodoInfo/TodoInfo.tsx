3c3da853760de4df11b9a20a5f4ea810e3e57e96
Roman Bliashuk <romanblyashuk94@gmail.com>
2023-01-04 23:34:40 +0200

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,30 @@
-export const TodoInfo = () => {};
+import { FC } from 'react';
+import classNames from 'classnames';
+import './TodoInfo.scss';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo
+};
+export const TodoInfo: FC<Props> = ({ todo }) => {
+  const {
+    id, completed, title, user,
+  } = todo;
+
+  return (
+    <article
+      data-id={id}
+      className={classNames(
+        'TodoInfo',
+        { 'TodoInfo--completed': completed },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,13 @@
-export const UserInfo = () => {};
+import { FC } from 'react';
+import './UserInfo.scss';
+import { User } from '../../types/User';
+
+type Props = {
+  user: User
+};
+
+export const UserInfo: FC<Props> = ({ user: { name, email } }) => (
+  <a className="UserInfo" href={`mailto:${email}`}>
+    {name}
+  </a>
+);
