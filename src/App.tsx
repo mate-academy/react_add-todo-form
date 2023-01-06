6899f8caca7d3b0c9c55d74be5ffee5b32a34775
Artem Hrechuk <grechuck.artem.ag@gmail.com>
2023-01-06 12:39:13 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,162 @@
+import React, { FC, useState } from 'react';
 import './App.scss';
+import { TodoList } from './components/TodoList';
+import { Todo } from './types/Todo';
+import { User } from './types/User';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
 
-export const App = () => {
+const getUserById = (array: User[], id: number) => {
+  return array.find(user => user.id === id) || null;
+};
+
+const todosWithUsers: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUserById(usersFromServer, todo.userId),
+}));
+
+export const App: FC = () => {
+  const defaultValueInput = '';
+
+  const [title, setInput] = useState<string>(defaultValueInput);
+  const [selectedUserName, setUser] = useState<string>(defaultValueInput);
+  const [todos, setTasks] = useState<Todo[]>(todosWithUsers);
+  const [errorForTitle, setErrorForTitle] = useState<boolean>(false);
+  const [errorForSelect, setErrorForUserSelect] = useState<boolean>(false);
+
+  const clearForm = (defaultValue: string) => {
+    setInput(defaultValue);
+    setUser(defaultValue);
+  };
+
+  const getLargestUserId = (array: Todo[]) => {
+    return Math.max(...array.map(item => item.id));
+  };
+
+  const getUser = (array: User[]) => {
+    return array.find(person => (
+      person.name === selectedUserName
+    )) || null;
+  };
+
+  const handleSubmitForm = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const fieldsNotEmpty = selectedUserName && title;
+
+    if (fieldsNotEmpty) {
+      const id = getLargestUserId(todos) + 1;
+      const user = getUser(usersFromServer);
+
+      setTasks(currenTodos => [
+        ...currenTodos,
+        {
+          id,
+          title,
+          completed: false,
+          userId: user ? user.id : null,
+          user,
+        }]);
+
+      clearForm(defaultValueInput);
+    }
+
+    if (selectedUserName === defaultValueInput) {
+      setErrorForUserSelect(true);
+    }
+
+    if (title === defaultValueInput) {
+      setErrorForTitle(true);
+    }
+  };
+
+  const handleUserSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.target;
+
+    setErrorForUserSelect(false);
+    setUser(value);
+  };
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setInput(value);
+    setErrorForTitle(false);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={handleSubmitForm}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label
+            htmlFor="titleInput"
+          >
+            {'Title: '}
+          </label>
+
+          <input
+            id="titleInput"
+            name="titleInput"
+            type="text"
+            placeholder="Enter a title"
+            value={title}
+            onChange={handleInput}
+            data-cy="titleInput"
+          />
+          {errorForTitle
+            && (<span className="error">Please enter a title</span>)}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
+          <label
+            htmlFor="userSelect"
+          >
+            {'User: '}
+          </label>
+
+          <select
+            onChange={handleUserSelect}
+            name="userSelect"
+            value={selectedUserName}
+            id="userSelect"
+            data-cy="userSelect"
+          >
+            <option value="" disabled>
+              Choose a user
+            </option>
+
+            {usersFromServer.map((user) => (
+              <option
+                key={user.id}
+                value={user.name}
+              >
+                {user.name}
+              </option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {errorForSelect && (
+            <span className="error">Please choose a user</span>
+          )}
+
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button
+          data-cy="submitButton"
+          type="submit"
+        >
           Add
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={todos} />
     </div>
   );
 };
