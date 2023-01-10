2f924f87128e481f6f0a80038ec76dc946607297
Anastasiia (Nelly) <116572743+anastasiia-roiek@users.noreply.github.com>
2023-01-10 21:59:05 +0200

    created a form to add new TODOs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.scss';
+import { useState } from 'react';
 
 import usersFromServer from './api/users';
 import todosFromServer from './api/todos';
@@ -8,38 +9,115 @@ import { Todo } from './types/Todo';
 
 import { TodoList } from './components/TodoList';
 
-function getUser(userId: number): User | null {
+function getUserByName(userName: string): User | null {
+  return usersFromServer.find(user => (user.name === userName)) || null;
+}
+
+function getUserById(userId: number): User | null {
   const foundUser = usersFromServer.find(user => user.id === userId);
 
   // if there is no user with a given userId
   return foundUser || null;
 }
 
-export const todos: Todo[] = todosFromServer.map(todo => ({
-  ...todo,
-  user: getUser(todo.userId),
-}));
+const prepTodos: Todo[] = todosFromServer.map(todo => {
+  return {
+    ...todo,
+    user: getUserById(todo.userId),
+  };
+});
 
 export const App = () => {
+  const [title, setTitle] = useState('');
+  const [selectedUser, setSelectedUser] = useState('');
+  const [todos, setTodos] = useState(prepTodos);
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.currentTarget.value);
+  };
+
+  const handleUserChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedUser(event.currentTarget.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (title.trim() === '' || !selectedUser) {
+      return;
+    }
+
+    const userToAdd = getUserByName(selectedUser);
+
+    setTodos(current => {
+      const maxTodoId = Math.max(...current.map(todo => todo.id));
+
+      return [
+        ...current,
+        {
+          id: maxTodoId + 1,
+          title,
+          completed: false,
+          userId: userToAdd ? userToAdd.id : null,
+          user: userToAdd,
+        },
+      ];
+    });
+
+    setTitle('');
+    setSelectedUser('');
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={handleSubmit}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
+          <label htmlFor="title"> Write down your task:</label>
+
+          <input
+            type="text"
+            data-cy="titleInput"
+            placeholder="Enter title of task"
+            value={title}
+            onChange={handleTitleChange}
+          />
           <span className="error">Please enter a title</span>
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
+          <label htmlFor="title">Choose a user:</label>
+
+          <select
+            data-cy="userSelect"
+            id="userSelect"
+            name="userSelect"
+            value={selectedUser}
+            onChange={handleUserChange}
+          >
+            <option value="" disabled>Choose a user</option>
+
+            {usersFromServer.map(user => (
+              <option key={user.id} value={user.name}>{user.name}</option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          <span
+            className="error"
+          >
+            Please choose a user
+          </span>
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button
+          type="submit"
+          data-cy="submitButton"
+        >
           Add
         </button>
       </form>
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -10,6 +10,7 @@ type Props = {
 
 export const TodoInfo: React.FC<Props> = ({ todo }) => (
   <article
+    data-id={todo.id}
     className={classNames(
       'TodoInfo',
       { 'TodoInfo--completed bg-success': todo.completed },
