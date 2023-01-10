590108d9dc3a69a6f3858f5be768c34606015b07
Ivan Melnychuk <jacksmilereacher@gmail.com>
2023-01-10 16:14:13 +0200

    created User and Todo types, moved TodoList section to TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
+// import { useState } from 'react';
 import './App.scss';
 
 // import usersFromServer from './api/users';
 // import todosFromServer from './api/todos';
+// import { TodoList } from './components/TodoList';
 
 export const App = () => {
+  // const [todos, setTodos] = useState(todosFromServer);
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
       <form action="/api/users" method="POST">
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label>
+            {'Title: '}
+            <input
+              type="text"
+              data-cy="titleInput"
+              name="title"
+              placeholder="Enter a title"
+            />
+          </label>
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
-          </select>
-
-          <span className="error">Please choose a user</span>
+          <label>
+            {'User: '}
+            <select data-cy="userSelect">
+              <option value="0" disabled>Choose a user</option>
+            </select>
+          </label>
         </div>
 
         <button type="submit" data-cy="submitButton">
@@ -27,35 +39,7 @@ export const App = () => {
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      {/* <TodoList todos={todos} /> */}
     </div>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,33 @@
-export const TodoList = () => {};
+export const TodoList: React.FC = () => {
+  return (
+    <section className="TodoList">
+      <article data-id="1" className="TodoInfo TodoInfo--completed">
+        <h2 className="TodoInfo__title">
+          delectus aut autem
+        </h2>
+
+        <a className="UserInfo" href="mailto:Sincere@april.biz">
+          Leanne Graham
+        </a>
+      </article>
+
+      <article data-id="15" className="TodoInfo TodoInfo--completed">
+        <h2 className="TodoInfo__title">delectus aut autem</h2>
+
+        <a className="UserInfo" href="mailto:Sincere@april.biz">
+          Leanne Graham
+        </a>
+      </article>
+
+      <article data-id="2" className="TodoInfo">
+        <h2 className="TodoInfo__title">
+          quis ut nam facilis et officia qui
+        </h2>
+
+        <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
+          Patricia Lebsack
+        </a>
+      </article>
+    </section>
+  );
+};
