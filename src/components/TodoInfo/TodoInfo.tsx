f4b98ad27b107b0d707550bc7703ac7b59f5ecb0
Anastasiia (Nelly) <116572743+anastasiia-roiek@users.noreply.github.com>
2023-01-10 21:06:09 +0200

    created an `App` component

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,24 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+import './TodoInfo.scss';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <article
+    className={classNames(
+      'TodoInfo',
+      { 'TodoInfo--completed bg-success': todo.completed },
+    )}
+  >
+    <h2 className="TodoInfo__title">{ todo.title }</h2>
+
+    {todo.user && (
+      <UserInfo user={todo.user} />
+    )}
+  </article>
+);
