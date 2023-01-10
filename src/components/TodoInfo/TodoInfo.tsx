080135f5740b46622b9beaface343682076fc0ec
Yana Gonchar <johanna.gonchar@icloud.com>
2023-01-10 13:28:44 +0100

    React - Add ToDo Form

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,26 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import cn from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  return (
+    <article
+      data-id={todo.id}
+      className={cn(
+        'TodoInfo',
+        { 'TodoInfo--completed': todo.completed },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+
+      {todo.user && <UserInfo user={todo.user} />}
+    </article>
+  );
+};
