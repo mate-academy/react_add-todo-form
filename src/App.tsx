4ecb0d45e4c10672671e6234ddf1dc643119a661
Olha Mamai <olgga.ju@gmail.com>
2023-01-11 00:12:17 +0200

    in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,96 @@
 import './App.scss';
+import { useState } from 'react';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import { User } from './types/User';
+import { Todo } from './types/Todo';
+
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+
+import { TodoList } from './components/TodoList';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+const todos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
 
 export const App = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [todoList, setTodoList] = useState(todos);
+  const [title, setTitle] = useState('');
+  const [userId, setUserId] = useState(0);
+
+  function createNewTask(event: React.FormEvent) {
+    event.preventDefault();
+
+    const task = {
+      id: todoList.length,
+      title,
+      userId,
+      completed: false,
+      user: getUser(userId),
+    };
+
+    setTodoList([...todoList, task]);
+  }
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        onSubmit={createNewTask}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
+          <label>
+            Title:
+            <input
+              type="text"
+              data-cy="titleInput"
+              placeholder="Enter a title"
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+            />
+          </label>
           <span className="error">Please enter a title</span>
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
-          </select>
+          <label>
+            User:
+            <select
+              data-cy="userSelect"
+              value={userId}
+              onChange={(event) => setUserId(+event.target.value)}
+            >
+              <option value={0}>Choose a user</option>
+
+              {usersFromServer.map(person => (
+                <option value={person.id} key={person.id}>
+                  {person.name}
+                </option>
+              ))}
+            </select>
+          </label>
 
           <span className="error">Please choose a user</span>
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button
+          type="submit"
+          data-cy="submitButton"
+        >
           Add
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={todoList} />
     </div>
   );
 };
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,18 @@
-export const TodoInfo = () => {};
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/Todo';
+// import { User } from '../../types/User';
+
+// import usersFromServer from '../../api/users';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <article data-id="15" className="TodoInfo TodoInfo--completed">
+    <h2 className="TodoInfo__title">delectus aut autem</h2>
+    {todo.user && (
+      <UserInfo user={todo.user} key={todo.userId} />
+    )}
+  </article>
+);
