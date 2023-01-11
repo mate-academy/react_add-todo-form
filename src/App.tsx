4bcfb0f98f8719aa1deb77464e9ab9f70615d9a2
Yuriy Khoptyanii <hoptyanoyyura@gmail.com>
2023-01-11 09:19:31 +0200

    Solution with styles, but without one test - if the form is not filled out, I get an error for each line separately

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,163 @@
+import React, { useState } from 'react';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import { Button, Paper, FormControl } from '@mui/material';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
 
-export const App = () => {
+import { User } from './types/User';
+import { Todo } from './types/Todo';
+import { TodoList } from './components/TodoList';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  // if there is no user with a given userId
+  return foundUser || null;
+}
+
+function findUserFullname(fullName: string): User | null {
+  return usersFromServer.find(user => user.name === fullName) || null;
+}
+
+export const initialTodosList: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
+
+export const App: React.FC = () => {
+  const [userSelect, setUserSelect] = useState('');
+  const [title, setTitle] = useState('');
+  const [todos, setTodos] = useState(initialTodosList);
+  const [isErrorMessageEmptyTitle, setErrorMessageEmptyTitle] = useState(false);
+  const [isErrorMessageEmptyUser, setErrorMessageEmptyUser] = useState(false);
+
+  const handleTitleEnter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.currentTarget.value);
+    setErrorMessageEmptyTitle(false);
+  };
+
+  const handleChooseUser = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setUserSelect(event.currentTarget.value);
+    setErrorMessageEmptyUser(false);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (title === '') {
+      setErrorMessageEmptyTitle(true);
+
+      return;
+    }
+
+    if (userSelect === '') {
+      setErrorMessageEmptyUser(true);
+
+      return;
+    }
+
+    const addNewTodo = findUserFullname(userSelect);
+
+    setTodos(currentTodos => {
+      const largestTodoId = Math.max(...currentTodos.map(todo => todo.id));
+
+      const choosenTodo = {
+        id: largestTodoId + 1,
+        title,
+        completed: false,
+        userId: addNewTodo ? addNewTodo.id : null,
+        user: addNewTodo,
+      };
+
+      return [...currentTodos, choosenTodo];
+    });
+
+    setUserSelect('');
+    setTitle('');
+  };
+
   return (
-    <div className="App">
-      <h1>Add todo form</h1>
+    <Paper
+      className="Paper"
+      sx={{
+        margin: '0 auto',
+        width: 'max-content',
+      }}
+      elevation={23}
+    >
+      <div className="App">
+        <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
-        <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
-        </div>
+        <form
+          className="Form"
+          action="/api/users"
+          method="POST"
+          onSubmit={handleSubmit}
+        >
+          <div className="field">
+            <label>
+              <FormControl fullWidth>
+                <span>Title: </span>
+                <input
+                  type="text"
+                  data-cy="titleInput"
+                  placeholder="Enter a title"
+                  id="title"
+                  value={title}
+                  onChange={handleTitleEnter}
+                />
+              </FormControl>
+            </label>
 
-        <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
-          </select>
+            {isErrorMessageEmptyTitle
+              && <span className="error">Please enter a title</span>}
+          </div>
 
-          <span className="error">Please choose a user</span>
-        </div>
+          <div className="field">
+            <label>
+              <span>User: </span>
 
-        <button type="submit" data-cy="submitButton">
-          Add
-        </button>
-      </form>
+              <FormControl fullWidth>
+                <select
+                  data-cy="userSelect"
+                  id="userSelect"
+                  value={userSelect}
+                  onChange={handleChooseUser}
+                >
+                  <option
+                    disabled
+                    value=""
+                  >
+                    Choose a user
+                  </option>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
+                  {usersFromServer.map(user => (
+                    <option key={user.id} value={user.name}>
+                      {user.name}
+                    </option>
+                  ))}
+                </select>
+              </FormControl>
+            </label>
 
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
+            {isErrorMessageEmptyUser
+              && <span className="error">Please choose a user</span>}
+          </div>
 
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
+          <Button
+            className="Button"
+            type="submit"
+            data-cy="submitButton"
+            variant="contained"
+            color="success"
+          >
+            Add
+          </Button>
+        </form>
 
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
-    </div>
+        <TodoList todos={todos} />
+      </div>
+    </Paper>
   );
 };
