c60fde4451842db502aa29243f87653cc3643e8b
Daryna Zinchenko <zinchenko.daryna@gmail.com>
2023-01-11 01:04:27 +0100

    in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import React, { useState } from 'react';
 import './App.scss';
-
-// import usersFromServer from './api/users';
+import { TodoList } from './components/TodoList';
+import usersFromServer from './api/users';
 // import todosFromServer from './api/todos';
 
-export const App = () => {
+export const App: React.FC = () => {
+  const [selectedName, setSelectedName] = useState('');
+
+  const handleChange = () => {
+    setSelectedName('name');
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
@@ -15,8 +22,16 @@ export const App = () => {
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <select
+            data-cy="userSelect"
+            name="name"
+            value={selectedName}
+            onChange={handleChange}
+
+          >
             <option value="0" disabled>Choose a user</option>
+            {usersFromServer.map(user => (
+              <option value={user.name}>{user.name}</option>))}
           </select>
 
           <span className="error">Please choose a user</span>
@@ -26,36 +41,7 @@ export const App = () => {
           Add
         </button>
       </form>
-
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList />
     </div>
   );
 };
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,13 @@
-export const TodoInfo = () => {};
+export const TodoInfo = () => {
+  return (
+    <article data-id="2" className="TodoInfo">
+      <h2 className="TodoInfo__title">
+        quis ut nam facilis et officia qui
+      </h2>
+
+      <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
+        Patricia Lebsack
+      </a>
+    </article>
+  );
+};
