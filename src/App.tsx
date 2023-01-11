c95b1ee91285399ac8a4e08d8384f7a6fc3580e0
Diana Demkiv <dianademkiv789@gmail.com>
2023-01-11 02:31:48 +0200

    add todo form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,60 @@
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import { useState } from 'react';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { TodoList } from './components/TodoList';
+import { User } from './types/User';
+import { Todo } from './types/Todo';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+export const todos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
 
 export const App = () => {
+  const [storedTodos, setStoredTodos] = useState(todos);
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        const userId = Number(event.target.elements.user.value);
+        const newTodo = {
+          id: Math.max(...storedTodos.map(todo => todo.id)) + 1,
+          title: event.target.elements.title.value,
+          userId,
+          completed: false,
+          user: getUser(userId),
+        };
+
+        setStoredTodos([...storedTodos, newTodo]);
+        event.target.reset();
+      }}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
+          <input
+            name="title"
+            type="text"
+            data-cy="titleInput"
+          />
           <span className="error">Please enter a title</span>
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <select name="user" data-cy="userSelect" defaultValue="0">
             <option value="0" disabled>Choose a user</option>
+            {usersFromServer.map(user => (
+              <option value={user.id} key={user.id}>{user.name}</option>
+            ))}
           </select>
 
           <span className="error">Please choose a user</span>
@@ -26,36 +64,7 @@ export const App = () => {
           Add
         </button>
       </form>
-
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={storedTodos} />
     </div>
   );
 };
