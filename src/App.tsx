1c0c67627a0155dc0ebdfe8ed72b625c399e4953
Olena Radchuk <allenradchuk@gmail.com>
2023-01-11 11:48:21 +0200

    added types and edited App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,74 @@
+import React, { useState } from 'react';
 import './App.scss';
+import { TodoList } from './components/TodoList';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { Todo } from './types/Todo';
+import { User } from './types/User';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+function findUserById(userId: number): User | null {
+  return usersFromServer.find(user => user.id === userId) || null;
+}
+
+function findUserByName(username: string): User | null {
+  return usersFromServer.find(user => user.name === username) || null;
+}
+
+const preparedTodos: Todo[] = todosFromServer.map(todo => {
+  return {
+    ...todo,
+    user: findUserById(todo.userId),
+  };
+});
 
 export const App = () => {
+  const [title, setTitle] = useState('');
+  const [selectedUser, setSelectedUser] = useState('');
+  const [todos, setTodos] = useState(preparedTodos);
+  const [isUserError, setUserError] = useState(false);
+  const [isTitleEror, setTitleError] = useState(false);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setTitleError(!title.trim());
+    setUserError(!selectedUser);
+
+    if (title.trim() === '' || !selectedUser) {
+      return;
+    }
+
+    const addedUser = findUserByName(selectedUser);
+
+    setTodos(current => {
+      const nextTodo = Math.max(...current.map(todo => todo.id)) + 1;
+
+      return [
+        ...current,
+        {
+          id: nextTodo,
+          title,
+          completed: false,
+          userId: addedUser ? addedUser.id : null,
+          user: addedUser,
+        },
+      ];
+    });
+
+    setTitle('');
+    setSelectedUser('');
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    setTitleError(false);
+  };
+
+  const handleUserChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedUser(event.target.value);
+    setUserError(false);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
