b11da0b132da6a15bc41308d5ca35b7a1e3d42c8
Petryk Dmytro <dimapetr2002@gmail.com>
2023-01-12 01:44:09 +0200

    added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import React, { useState } from 'react';
 import './App.scss';
+import ClearIcon from '@mui/icons-material/Clear';
+import {
+  TextField,
+  IconButton,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormHelperText,
+  Button,
+} from '@mui/material';
 
 import usersFromServer from './api/users';
 import todosFromServer from './api/todos';
@@ -75,50 +86,65 @@ export const App: React.FC = () => {
         }}
       >
         <div className="field">
-          <label>
-            <span>Title: </span>
-            <input
-              type="text"
-              data-cy="titleInput"
-              placeholder="Enter a title"
-              value={newTitle}
-              onChange={event => {
-                setNewTitle(event.target.value);
-                setTitleError(false);
-              }}
-            />
-          </label>
-          {showTitleError && (
-            <span className="error">Please enter a title</span>
-          )}
+          <TextField
+            sx={{ m: 2, width: '250px' }}
+            data-cy="titleInput"
+            label="Title"
+            placeholder="Enter a title"
+            helperText={showTitleError ? 'Please enter a title' : ''}
+            error={showTitleError}
+            value={newTitle}
+            onChange={event => {
+              setNewTitle(event.target.value);
+              setTitleError(false);
+            }}
+            InputProps={{
+              endAdornment: (
+                <IconButton
+                  onClick={() => setNewTitle('')}
+                  sx={{ visibility: newTitle ? 'visible' : 'hidden' }}
+                >
+                  <ClearIcon />
+                </IconButton>
+              ),
+            }}
+          />
         </div>
 
         <div className="field">
-          <label>
-            <span>User: </span>
-            <select
+          <FormControl error={showUserError} sx={{ m: 2, width: '250px' }}>
+            <InputLabel id="select-label">User</InputLabel>
+            <Select
               data-cy="userSelect"
+              id="demo-simple-select"
               value={newUserId}
               onChange={event => {
                 setNewUserId(+event.target.value);
                 setUserError(false);
               }}
+              labelId="select-label"
+              label="User"
             >
-              <option value="0" disabled>Choose a user</option>
+              <MenuItem value="0" disabled>Choose a user</MenuItem>
               {usersFromServer.map(user => (
-                <option value={user.id} key={user.id}>{user.name}</option>
+                <MenuItem value={user.id} key={user.id}>{user.name}</MenuItem>
               ))}
-            </select>
-          </label>
+            </Select>
 
-          {showUserError && (
-            <span className="error">Please choose a user</span>
-          )}
+            {showUserError && (
+              <FormHelperText>Please choose a user</FormHelperText>
+            )}
+          </FormControl>
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <Button
+          type="submit"
+          size="large"
+          data-cy="submitButton"
+          variant="contained"
+        >
           Add
-        </button>
+        </Button>
       </form>
 
       <TodoList todos={todos} />
