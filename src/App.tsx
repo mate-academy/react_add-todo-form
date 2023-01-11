f6890576694873e5ff11220ab41f7b19bb5066e3
Mykhailyk-Vadym <ihappyspam@gmail.com>
2023-01-11 16:27:02 +0200

    react_add_todo_form done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,74 @@
 import './App.scss';
-import { useState } from 'react';
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import cn from 'classnames';
+import { FormEvent, useState } from 'react';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+
+const findUserById = (userId: number) => (
+  usersFromServer.find((user) => user.id === userId)
+);
+
+const GetTodosWithUsers = () => (
+  todosFromServer.map(todo => ({
+    ...todo,
+    user: findUserById(todo.userId),
+  }))
+);
+
+const getNewId = (array: { id: number }[]) => (
+  Math.max(...array.map(el => el.id)) + 1
+);
 
 export const App = () => {
-  const [title, setTitle] = useState('');
-  const [userName] = useState('1');
+  const [todos, setTodos] = useState(GetTodosWithUsers);
+
+  const [newTitle, setNewTitle] = useState('');
+  const [newUserNameId, setNewUserNameId] = useState(0);
+
+  const [newTitleErrorMessage, setNewTitleErrorMessage] = useState('');
+  const [newUserNameIdErrorMessage, setNewUserNameIdErrorMessage]
+   = useState('');
+
+  const heandleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const reset = () => {
+      setNewTitle('');
+      setNewUserNameId(0);
+    };
+
+    if (!newUserNameId || !newTitle) {
+      setNewTitleErrorMessage(!newTitle ? 'Please enter a title' : '');
+      setNewUserNameIdErrorMessage(!newUserNameId
+        ? 'Please choose a user'
+        : '');
+
+      return;
+    }
+
+    setTodos(prev => {
+      const newTodo = {
+        completed: false,
+        id: getNewId(prev),
+        title: newTitle,
+        userId: newUserNameId,
+        user: findUserById(newUserNameId),
+      };
+
+      return [...prev, newTodo];
+    });
+
+    reset();
+  };
+
+  const handleChangeNewTitle = (value: string) => {
+    setNewTitle(value);
+  };
+
+  const handleChangeNewUserNameId = (value: string) => {
+    setNewUserNameId(+value);
+    setNewUserNameIdErrorMessage('');
+  };
 
   return (
     <div className="App">
@@ -14,34 +77,61 @@ export const App = () => {
       <form
         action="/api/users"
         method="POST"
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
+        onSubmit={heandleSubmit}
       >
         <div className="field">
-          <span>Title: </span>
-          <input
-            type="text"
-            data-cy="titleInput"
-            placeholder="Enter a title"
-            value={title}
-            onChange={(event) => {
-              setTitle(event.target.value);
-            }}
-          />
-          <span className="error">Please enter a title</span>
+          <label>
+            Title:
+            <input
+              type="text"
+              data-cy="titleInput"
+              placeholder="Enter a title"
+              value={newTitle}
+              onChange={(event) => handleChangeNewTitle(event.target.value)}
+              className={cn('input', {
+                inputError: Boolean(newTitleErrorMessage),
+              })}
+            />
+          </label>
+          <span className={cn('Please enter a title', 'errorMessage', {
+            errorMessageVisible: Boolean(newTitleErrorMessage),
+          })}
+          >
+            {newTitleErrorMessage}
+          </span>
         </div>
 
         <div className="field">
-          <span>User: </span>
-          <select
-            data-cy="userSelect"
-            value={userName}
-          >
-            <option value="1" disabled>Choose a user</option>
-          </select>
+          <label>
+            User:
+            <select
+              data-cy="userSelect"
+              value={newUserNameId}
+              onChange={(event) => handleChangeNewUserNameId(
+                event.target.value,
+              )}
+              className={cn('input', {
+                inputError: Boolean(newUserNameIdErrorMessage),
+              })}
+            >
+              <option value="0" disabled>Choose a user</option>
+              {usersFromServer.map(user => (
+                <option
+                  key={user.id}
+                  value={user.id}
+                >
+                  {user.name}
+                </option>
+              ))}
+            </select>
+          </label>
 
-          <span className="error">Please choose a user</span>
+          <span className={cn('ErrorMessage', 'errorMessage', {
+            errorMessageVisible: Boolean(newUserNameId),
+          })}
+          >
+            {newUserNameIdErrorMessage}
+          </span>
         </div>
 
         <button type="submit" data-cy="submitButton">
@@ -50,33 +140,26 @@ export const App = () => {
       </form>
 
       <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
+        {todos.map(todo => (
+          <article
+            data-id="1"
+            className={cn(todo.completed
+              ? 'TodoInfo TodoInfo--completed'
+              : 'TodoInfo')}
+            key={todo.id}
+          >
+            <h2 className="TodoInfo__title">
+              {todo.title}
+            </h2>
 
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
+            <a
+              className={todo.user?.username}
+              href={`mailto:${todo.user?.email}`}
+            >
+              {todo.user?.name}
+            </a>
+          </article>
+        ))}
       </section>
     </div>
   );
