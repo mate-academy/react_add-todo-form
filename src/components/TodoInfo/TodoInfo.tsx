6ac321a49d909176c38a832847aa660c071e7a0f
dant0n <dmibuc@gmail.com>
2023-01-17 01:53:36 +0200

    Finish hw.

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,29 @@
-export const TodoInfo = () => {};
+import { FC } from 'react';
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo,
+};
+
+export const TodoInfo: FC<Props> = ({ todo }) => {
+  return (
+    <article
+      data-id={todo.id}
+      key={todo.id}
+      className={classNames(
+        'TodoInfo',
+        { 'TodoInfo--completed': todo.completed },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+
+      {todo.user && (
+        <UserInfo user={todo.user} />
+      )}
+    </article>
+  );
+};
