6b0f82e35e4bdcd5199783b5cb06b44401ee6ca4
Alireza Mohammadhossein <mh.alireza1993@gmail.com>
2023-01-23 22:36:50 +0100

    final

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,40 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import cn from 'classnames';
+
+import './todoinfo.scss';
+
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    completed,
+    title,
+    user,
+  } = todo;
+
+  return (
+    <article
+      data-id={todo.id}
+      className={cn(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': completed,
+        },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && (
+        <a className="UserInfo" href={`mailto:${user.email}`}>
+          {user.name}
+        </a>
+      )}
+    </article>
+  );
+};
