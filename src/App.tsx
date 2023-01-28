c74af37c001658040123a28d7bac4e0f2035488a
Oleh Yablunovskyi <lifserv@gmail.com>
2023-01-28 15:03:07 +0200

    Tests fixed

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ export const App: React.FC = () => {
       user: findUserByID(selectedUserID),
     };
 
-    setTodos((prevTodos => [newTodo, ...prevTodos]));
+    setTodos((prevTodos => [...prevTodos, newTodo]));
 
     clearForm();
   };
