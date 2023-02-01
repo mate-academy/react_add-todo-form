7138a976ea88ae3014f96b2424b978fb7461f450
Maks <comanderius@gmail.com>
2023-02-01 18:49:08 +0100

    React add todo form

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,37 @@
-export const TodoList = () => {};
+import { TodoInfo } from '../TodoInfo';
+
+interface Todo {
+  id: number,
+  title: string,
+  completed: boolean,
+  userId: number,
+}
+
+type Props = {
+  todos: Todo[],
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <>
+      {todos.map(person => {
+        const {
+          id,
+          title,
+          completed,
+          userId,
+        } = person;
+
+        return (
+          <TodoInfo
+            key={id}
+            id={id}
+            title={title}
+            completed={completed}
+            userId={userId}
+          />
+        );
+      })}
+    </>
+  );
+};
