751bb3b9d2b917af7a35e5f05bdbadd6bbc2b053
Sasha-Maniak <116157371+metalikaqq@users.noreply.github.com>
2023-02-06 20:06:45 +0200

    commit message

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,17 @@
-export const TodoList = () => {};
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todoData: Todo[]
+};
+
+export const TodoList: React.FC<Props> = ({ todoData }) => {
+  return (
+    <section className="TodoList">
+      {todoData.map((todo) => (
+        <TodoInfo todo={todo} key={todo.id} />
+      ))}
+    </section>
+  );
+};
