b46c684f6deef89930cc7478ddcdf7fd5408e075
Andrii Andriushchenko <andrewandrew1604@gmail.com>
2023-02-07 19:29:05 +0200

    Function from TodoList  fixed

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -5,12 +5,10 @@ type Props = {
   todos: FullTodo[];
 };
 
-export const TodoList: React.FC<Props> = ({ todos }) => {
-  return (
-    <section className="TodoList">
-      {todos.map((todo) => (
-        <TodoInfo key={todo.id} todo={todo} />
-      ))}
-    </section>
-  );
-};
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map((todo) => (
+      <TodoInfo key={todo.id} todo={todo} />
+    ))}
+  </section>
+);
