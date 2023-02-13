1678845fbe42f6725c5a99c1579a0aec7cea210e
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-02-13 13:23:24 +0200

    Finish basic tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,168 @@
+import { useState } from 'react';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import { TodoList } from './components/TodoList';
+import { User } from './types/User';
+import { Todo } from './types/Todo';
 
-export const App = () => {
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+
+function getUser(userId: number): User | null {
+  return usersFromServer.find((user) => user.id === userId) || null;
+}
+
+export const todos: Todo[] = todosFromServer.map((todo) => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
+
+export const App: React.FC = () => {
+  const [todoList, setTodoList] = useState(todos);
+
+  const [newTodo, setNewTodo] = useState({
+    title: '',
+    userName: '',
+    isTitleValid: true,
+    isUserNameValid: true,
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    const { name, value } = e.target;
+
+    if (name === 'title') {
+      setNewTodo((todo) => ({
+        ...todo,
+        [name]: value,
+        isTitleValid: true,
+      }));
+    }
+
+    if (name === 'userName') {
+      setNewTodo((todo) => ({
+        ...todo,
+        [name]: value,
+        isUserNameValid: true,
+      }));
+    }
+  };
+
+  const handleAddTodo = () => {
+    if (!newTodo.title && !newTodo.userName) {
+      setNewTodo((todo) => ({
+        ...todo,
+        isTitleValid: false,
+        isUserNameValid: false,
+      }));
+
+      return;
+    }
+
+    if (!newTodo.title) {
+      setNewTodo((todo) => ({
+        ...todo,
+        isTitleValid: false,
+      }));
+
+      return;
+    }
+
+    if (!newTodo.userName) {
+      setNewTodo((todo) => ({
+        ...todo,
+        isUserNameValid: false,
+      }));
+
+      return;
+    }
+
+    const lastTodoId = Math.max(...todoList.map((todo) => todo.id));
+
+    const todoUser: User | undefined = usersFromServer.find(
+      (user) => user.name === newTodo.userName,
+    );
+
+    const todoToAdd: Todo = {
+      id: lastTodoId + 1,
+      title: newTodo.title,
+      userId: todoUser?.id,
+      completed: false,
+      user: todoUser,
+    };
+
+    setTodoList((prevTodos) => {
+      prevTodos.push(todoToAdd);
+
+      return [...prevTodos];
+    });
+
+    setNewTodo({
+      title: '',
+      userName: '',
+      isTitleValid: true,
+      isUserNameValid: true,
+    });
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="todoTitle">Title: </label>
+          <input
+            id="todoTitle"
+            type="text"
+            placeholder="Enter a title"
+            data-cy="titleInput"
+            name="title"
+            value={newTodo.title}
+            onChange={handleChange}
+          />
+          {!newTodo.isTitleValid && (
+            <span className="error">Please enter a title</span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
+          <label htmlFor="todoUser">User: </label>
+          <select
+            id="todoUser"
+            data-cy="userSelect"
+            name="userName"
+            value={newTodo.userName}
+            onChange={handleChange}
+          >
+            <option value="" disabled>
+              Choose a user
+            </option>
+            {usersFromServer.map((user) => (
+              <option key={user.id} value={user.name}>
+                {user.name}
+              </option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {!newTodo.isUserNameValid && (
+            <span className="error">Please choose a user</span>
+          )}
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button type="submit" data-cy="submitButton" onClick={handleAddTodo}>
           Add
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      {todos.length && <TodoList todos={todoList} />}
     </div>
   );
 };
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,21 @@
-export const TodoInfo = () => {};
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const { title, completed, user } = todo;
+
+  return (
+    <article
+      data-id={todo.id}
+      className={`TodoInfo ${completed && 'TodoInfo--completed'} `}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
