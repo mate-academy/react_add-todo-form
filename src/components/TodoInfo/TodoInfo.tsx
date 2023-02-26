60c36d5bc9d191ecf3dffe534a88cfe265d93247
Taras Baran <baranprivat22@gmail.com>
2023-02-26 17:59:08 +0200

    finished

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,37 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import classNames from 'classnames';
+
+import './TodoInfo.scss';
+
+import { UserInfo } from '../UserInfo/UserInfo';
+
+import Todo from '../../types/Todo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({
+  todo: {
+    id,
+    title,
+    completed,
+    user,
+  },
+}) => {
+  return (
+    <li
+      data-id={id}
+      className={classNames(
+        'TodoInfo',
+        { 'TodoInfo--completed': completed },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && <UserInfo user={user} />}
+    </li>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,17 @@
-export const UserInfo = () => {};
+import React from 'react';
+
+import './UserInfo.scss';
+
+import User from '../../types/User';
+
+type Props = {
+  user: User;
+};
+
+export const UserInfo: React.FC<Props> = ({ user: { name, email } }) => {
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
