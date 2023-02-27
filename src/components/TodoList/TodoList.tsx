ae48d920b1329b00a23ef47199012a765a021f5e
Skuratovskyi Vladyslav <vladskur0369@gmail.com>
2023-02-27 21:54:43 +0200

    Solution v0.1

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,18 @@
-export const TodoList = () => {};
+import { TodoInfo } from '../TodoInfo';
+
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+};
+
+export const TodoList = ({ todos }: { todos: Todo[] }) => {
+  return (
+    <>
+      {todos.map((titleBlock) => (
+        <TodoInfo todo={titleBlock} key={titleBlock.id} />
+      ))}
+    </>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,16 @@
-export const UserInfo = () => {};
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
+
+export const UserInfo = ({ user }: { user: User }) => {
+  const { email, name } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
