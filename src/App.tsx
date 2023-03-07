c7ce9fc5dfb15014bedac1b7c2c07cd614996d19
Dmitriy Sporysh <dopmoneyd@gmail.com>
2023-03-07 23:42:14 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,147 @@
 import './App.scss';
+import { useState } from 'react';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { Todo } from './types/Todo';
+import { User } from './types/User';
+import { TodoList } from './components/TodoList';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+export const startingTodos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
 
 export const App = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [titleName, setTitleName] = useState('');
+  const [userName, setUserName] = useState('0');
+  const [todos, setTodos] = useState(startingTodos);
+
+  const handleChange = (inputValue: string) => {
+    const reg = new RegExp('[a-zA-Zа-яА-Я\\d ]+');
+    const letter = inputValue[inputValue.length - 1];
+
+    if (letter !== undefined) {
+      if (letter.search(reg) !== -1) {
+        setTitleName(inputValue);
+      }
+    } else {
+      setTitleName('');
+    }
+  };
+
+  const chooseSelect = (selectedValue: string) => {
+    setUserName(selectedValue);
+  };
+
+  const clearingForm = () => {
+    setUserName('0');
+    setTitleName('');
+    setIsClicked(false);
+  };
+
+  const addTodo = () => {
+    const arrayId = (
+      [...todos]
+        .sort((a, b) => (b.id - a.id))[0].id + 1
+    );
+
+    const newTodo = {
+      id: arrayId,
+      title: titleName,
+      completed: false,
+      userId: +userName,
+      user: getUser(+userName),
+    };
+
+    setTodos([...todos, newTodo]);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
       <form action="/api/users" method="POST">
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label>
+            {'Title: '}
+            <input
+              type="text"
+              data-cy="titleInput"
+              placeholder="Enter a title"
+              value={titleName}
+              onChange={(event) => {
+                handleChange(event.target.value);
+              }}
+            />
+          </label>
+
+          {isClicked && (
+            !titleName.trim().length && (
+              <span className="error">Please enter a title</span>
+            )
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
-          </select>
+          <label>
+            {'User: '}
+            <select
+              data-cy="userSelect"
+              value={userName}
+              onChange={(select) => {
+                chooseSelect(select.target.value);
+              }}
+            >
+              <option value="0" disabled>Choose a user</option>
+              {usersFromServer.map((user) => {
+                const {
+                  id,
+                  name,
+                } = user;
 
-          <span className="error">Please choose a user</span>
+                return (
+                  <option
+                    value={id}
+                    key={id}
+                  >
+                    {name}
+                  </option>
+                );
+              })}
+            </select>
+          </label>
+
+          {isClicked && (
+            userName === '0') && (
+            <span className="error">Please choose a user</span>
+          )}
+
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button
+          type="submit"
+          data-cy="submitButton"
+          onClick={(event) => {
+            event.preventDefault();
+            setIsClicked(true);
+            if (userName !== '0' && titleName.trim().length > 0) {
+              addTodo();
+              clearingForm();
+            }
+          }}
+        >
           Add
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={todos} />
     </div>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,19 @@
-export const TodoList = () => {};
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+interface TodosProps {
+  todos: Todo[]
+}
+
+export const TodoList: React.FC<TodosProps> = ({ todos }) => (
+  <>
+    <section className="TodoList">
+      {todos.map((todo) => (
+        <TodoInfo
+          todo={todo}
+          key={todo.id}
+        />
+      ))}
+    </section>
+  </>
+);
