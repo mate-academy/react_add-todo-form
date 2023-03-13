4896fba1f93d04951d6c497caad9e546f6fbf618
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-03-13 12:58:25 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,126 @@
+import React, { useState } from 'react';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+
+import { User } from './types/User';
+import { Todo } from './types/Todo';
+
+import { TodoList } from './components/TodoList';
+
+function getUser(userId: number): User | null {
+  return usersFromServer.find((user) => user.id === userId) || null;
+}
+
+const todosWithUsers: Todo[] = todosFromServer.map((todo) => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
+
+const getLastTodoIndex = (todos: Todo[]) => (
+  Math.max(...todos.map(({ id }) => id))
+);
 
 export const App = () => {
+  const [todos, setTodos] = useState(todosWithUsers);
+  const [title, setTitle] = useState('');
+  const [selectedUserId, setSelectedUserId] = useState(0);
+  const [shouldShowTitleError, setShouldShowTitleError] = useState(false);
+  const [shouldShowUserError, setShouldShowUserError] = useState(false);
+
+  const clearForm = () => {
+    setTitle('');
+    setSelectedUserId(0);
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.nativeEvent.preventDefault();
+
+    const trimmedTitle = title.trim();
+
+    setShouldShowTitleError(!trimmedTitle);
+    setShouldShowUserError(!selectedUserId);
+
+    if (trimmedTitle && selectedUserId) {
+      const newTodo: Todo = {
+        id: getLastTodoIndex(todos) + 1,
+        userId: selectedUserId,
+        title,
+        completed: false,
+        user: getUser(selectedUserId),
+      };
+
+      setTodos((currentTodos) => [...currentTodos, newTodo]);
+      clearForm();
+    }
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setShouldShowTitleError(false);
+
+    const inputValue = event.target.value;
+
+    // removing any special characters from a Title
+    setTitle(inputValue.replace(/[^A-Za-z\s\d\u0400-\u04FF]/g, ''));
+  };
+
+  const handleUserChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedUserId(Number(event.target.value));
+    setShouldShowUserError(false);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={(event) => handleFormSubmit(event)}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="titleInput">
+            Title:
+            <input
+              type="text"
+              id="titleInput"
+              data-cy="titleInput"
+              placeholder="Enter a title"
+              value={title}
+              onChange={(event) => handleTitleChange(event)}
+            />
+          </label>
+          {shouldShowTitleError && (
+            <span className="error">Please enter a title</span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
-          </select>
+          <label htmlFor="userSelect">
+            User:
+            <select
+              data-cy="userSelect"
+              id="userSelect"
+              placeholder="Choose a user"
+              value={selectedUserId}
+              onChange={(event) => handleUserChange(event)}
+            >
+              <option value="0" disabled>
+                Choose a user
+              </option>
 
-          <span className="error">Please choose a user</span>
+              {usersFromServer.map(({ name, id }) => (
+                <option value={id} key={id}>
+                  {name}
+                </option>
+              ))}
+            </select>
+          </label>
+
+          {shouldShowUserError && (
+            <span className="error">Please choose a user</span>
+          )}
         </div>
 
         <button type="submit" data-cy="submitButton">
@@ -27,35 +128,7 @@ export const App = () => {
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={todos} />
     </div>
   );
 };
