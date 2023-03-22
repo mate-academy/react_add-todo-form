35386644103524af33decf0758bcde7fd966682a
Sergey Mironenko <serg1prime@gmail.com>
2023-03-23 00:04:29 +0200

    Todo form first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,112 @@
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import React, { useState } from 'react';
 
-export const App = () => {
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+
+import { TodoList } from './components/TodoList/TodoList';
+
+import { User } from './types/User';
+import { Todo } from './types/Todo';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  // if there is no user with a given userId
+  return foundUser || null;
+}
+
+export const todos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
+
+export const App: React.FC = () => {
+  const [inputTitle, setInputTitle] = useState('');
+  const [inputUser, setInputUser] = useState('');
+  const [visibleTodos, setVisibleTodos] = useState(todos);
+  const [enteredTitle, setEnteredTitle] = useState(true);
+  const [enteredUser, setEnteredUser] = useState(true);
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={(event) => {
+          event.preventDefault();
+
+          const newUser = usersFromServer.find(
+            user => user.name === inputUser,
+          ) || null;
+
+          const newTodo: Todo = {
+            id: (visibleTodos.reduce(
+              (acc, curr) => (acc.id > curr.id ? acc : curr),
+            )).id + 1,
+            userId: newUser !== null ? newUser.id : null,
+            title: inputTitle,
+            completed: false,
+            user: newUser,
+          };
+
+          // eslint-disable-next-line
+          inputTitle !== '' ? setEnteredTitle(true) : setEnteredTitle(false);
+          // eslint-disable-next-line
+          inputUser !== '' ? setEnteredUser(true) : setEnteredUser(false);
+
+          if (inputTitle !== '' && inputUser !== '') {
+            setVisibleTodos(current => [
+              ...current,
+              newTodo,
+            ]);
+            setInputTitle('');
+            setInputUser('');
+          }
+        }}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label>
+            Title:&nbsp;
+            <input
+              type="text"
+              data-cy="titleInput"
+              className="input"
+              placeholder="Enter a title"
+              value={inputTitle}
+              onChange={(event) => {
+                setInputTitle(event.target.value);
+              }}
+            />
+          </label>
+          {(enteredTitle === false && inputTitle === '') && (
+            <span className="error">Please enter a title</span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
-          </select>
+          <label>
+            User:&nbsp;
+            <select
+              data-cy="userSelect"
+              value={inputUser}
+              onChange={(event) => {
+                setInputUser(event.target.value);
+              }}
+            >
+              <option value="" disabled>Choose a user</option>
+              {usersFromServer.map(user => (
+                <option value={user.name}>{user.name}</option>
+              ))}
+            </select>
+          </label>
 
-          <span className="error">Please choose a user</span>
+          {(enteredUser === false && inputUser === '') && (
+            <span className="error">Please choose a user</span>
+          )}
         </div>
 
         <button type="submit" data-cy="submitButton">
@@ -28,33 +115,7 @@ export const App = () => {
       </form>
 
       <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
+        <TodoList todos={visibleTodos} />
       </section>
     </div>
   );
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,33 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    id,
+    title,
+    completed,
+    user,
+  } = todo;
+
+  return (
+    <>
+      {user && (
+        <article
+          data-id={id}
+          className={
+            completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'
+          }
+        >
+          <h2 className="TodoInfo__title">{title}</h2>
+
+          <UserInfo user={user} />
+        </article>
+      )}
+    </>
+  );
+};
