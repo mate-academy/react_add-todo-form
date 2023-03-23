30cd6b9ec95513f2503df3c3a801e58528327641
Sergey Mironenko <serg1prime@gmail.com>
2023-03-23 14:09:44 +0200

    Todo form second commit

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,35 @@ export const App: React.FC = () => {
   const [inputTitle, setInputTitle] = useState('');
   const [inputUser, setInputUser] = useState('');
   const [visibleTodos, setVisibleTodos] = useState(todos);
-  const [enteredTitle, setEnteredTitle] = useState(true);
-  const [enteredUser, setEnteredUser] = useState(true);
+  const [hasTitle, setHasTitle] = useState(true);
+  const [hasUser, setHasUser] = useState(true);
+  const addTodo = () => {
+    const newUser = usersFromServer.find(
+      user => user.name === inputUser,
+    ) || null;
+
+    const newTodo: Todo = {
+      id: (visibleTodos.reduce(
+        (acc, curr) => (acc.id > curr.id ? acc : curr),
+      )).id + 1,
+      userId: newUser !== null ? newUser.id : null,
+      title: inputTitle,
+      completed: false,
+      user: newUser,
+    };
+
+    setHasTitle(inputTitle !== '');
+    setHasUser(inputUser !== '');
+
+    if (inputTitle !== '' && inputUser !== '') {
+      setVisibleTodos(current => [
+        ...current,
+        newTodo,
+      ]);
+      setInputTitle('');
+      setInputUser('');
+    }
+  };
 
   return (
     <div className="App">
@@ -39,33 +66,7 @@ export const App: React.FC = () => {
         onSubmit={(event) => {
           event.preventDefault();
 
-          const newUser = usersFromServer.find(
-            user => user.name === inputUser,
-          ) || null;
-
-          const newTodo: Todo = {
-            id: (visibleTodos.reduce(
-              (acc, curr) => (acc.id > curr.id ? acc : curr),
-            )).id + 1,
-            userId: newUser !== null ? newUser.id : null,
-            title: inputTitle,
-            completed: false,
-            user: newUser,
-          };
-
-          // eslint-disable-next-line
-          inputTitle !== '' ? setEnteredTitle(true) : setEnteredTitle(false);
-          // eslint-disable-next-line
-          inputUser !== '' ? setEnteredUser(true) : setEnteredUser(false);
-
-          if (inputTitle !== '' && inputUser !== '') {
-            setVisibleTodos(current => [
-              ...current,
-              newTodo,
-            ]);
-            setInputTitle('');
-            setInputUser('');
-          }
+          addTodo();
         }}
       >
         <div className="field">
@@ -82,7 +83,7 @@ export const App: React.FC = () => {
               }}
             />
           </label>
-          {(enteredTitle === false && inputTitle === '') && (
+          {(hasTitle === false && inputTitle === '') && (
             <span className="error">Please enter a title</span>
           )}
         </div>
@@ -104,7 +105,7 @@ export const App: React.FC = () => {
             </select>
           </label>
 
-          {(enteredUser === false && inputUser === '') && (
+          {(hasUser === false && inputUser === '') && (
             <span className="error">Please choose a user</span>
           )}
         </div>
