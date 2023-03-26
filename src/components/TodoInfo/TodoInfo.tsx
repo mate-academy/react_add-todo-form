1e4a0f449bfc297106da24cddd1dadcc1902e9c6
Olena Hapon <olenagapon90gmail.com>
2023-03-26 11:16:14 +0200

    Added changes

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -16,18 +16,16 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => {
   } = todo;
 
   return (
-    <>
-      <article
-        data-id={id}
-        className={classNames(
-          'TodoInfo',
-          { 'TodoInfo--completed': completed },
-        )}
-      >
-        <h2 className="TodoInfo__title">{title}</h2>
+    <article
+      data-id={id}
+      className={classNames(
+        'TodoInfo',
+        { 'TodoInfo--completed': completed },
+      )}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
 
-        {user && <UserInfo user={user} />}
-      </article>
-    </>
+      {user && <UserInfo user={user} />}
+    </article>
   );
 };
