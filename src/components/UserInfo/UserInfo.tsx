7a66cb1ff6f3471797e5986c036c0fba31eb9bc0
Kateryna Savelieva <ksavelyevaaa@gmail.com>
2023-03-27 00:23:38 +0300

    react add todo form done

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,23 @@
-export const UserInfo = () => {};
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+
+export type Props = {
+  user: User;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const {
+    name,
+    email,
+  } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
