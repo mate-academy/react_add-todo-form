d572ae34cfa98606f8740cf3e7d40708789cc42c
Bogdan <bogdan_mykhailov@icloud.com>
2023-03-27 12:51:52 +0300

    :white_check_mark: fix style

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -17,4 +17,41 @@ export default [
     completed: false,
     userId: 4,
   },
+
+  {
+    id: 1,
+    title: 'delectus aut autem',
+    completed: true,
+    userId: 1,
+  },
+  {
+    id: 15,
+    title: 'some other todo',
+    completed: false,
+    userId: 1,
+  },
+  {
+    id: 2,
+    title: 'quis ut nam facilis et officia qui',
+    completed: false,
+    userId: 4,
+  },
+  {
+    id: 1,
+    title: 'delectus aut autem',
+    completed: true,
+    userId: 1,
+  },
+  {
+    id: 15,
+    title: 'some other todo',
+    completed: false,
+    userId: 1,
+  },
+  {
+    id: 2,
+    title: 'quis ut nam facilis et officia qui',
+    completed: false,
+    userId: 4,
+  },
 ];
