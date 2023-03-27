04cb818245650dd0510a4c76e99d238628d19e7f
Yaroslav Vasilyev <yaroslavvas49@gmail.com>
2023-03-28 02:13:30 +0300

    Solution in progress v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { ThemeProvider } from 'styled-components';
 import original from 'react95/dist/themes/original';
-
 import { useState } from 'react';
 import './App.scss';
+
+import {
+  styleReset,
+  Button,
+  Window,
+  WindowHeader,
+  WindowContent,
+} from 'react95';
+
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+/* Pick a theme of your choice */
+
 import { TodoList } from './components/TodoList';
 
 // api
@@ -13,8 +24,26 @@ import todosFromServer from './api/todos';
 import { Todo } from './types/Todo';
 import { User } from './types/User';
 
-function getUserById(userId: number): User | undefined {
-  return usersFromServer.find((user) => user.id === userId);
+const GlobalStyles = createGlobalStyle`
+  ${styleReset}
+  @font-face {
+    font-family: 'ms_sans_serif';
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    font-weight: bold;
+    font-style: normal
+  }
+  body, input, select, textarea {
+    font-family: 'ms_sans_serif';
+  }
+`;
+
+function getUserById(userId: number): User | null {
+  return usersFromServer.find((user) => user.id === userId)
+  || null;
 }
 
 const todos: Todo[] = todosFromServer.map((todo) => ({
@@ -59,55 +88,65 @@ export const App = () => {
   };
 
   return (
-    <div className="App">
-      <h1>Add todo form</h1>
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={original}>
+        <Window resizable className="window">
+          <WindowHeader className="window-title">
+            <span>Add todo form</span>
+          </WindowHeader>
+          <WindowContent>
+            <div className="App">
+              <form onSubmit={handleFormSubmit}>
+                <div className="field">
+                  <input
+                    type="text"
+                    data-cy="titleInput"
+                    placeholder="Enter a title"
+                    value={newTodoName}
+                    onChange={(event) => {
+                      setNewTodoName(event.target.value);
+                    }}
+                  />
 
-      <form onSubmit={handleFormSubmit}>
-        <div className="field">
-          <input
-            type="text"
-            data-cy="titleInput"
-            placeholder="Enter a title"
-            value={newTodoName}
-            onChange={(event) => {
-              setNewTodoName(event.target.value);
-            }}
-          />
+                  {hasTodoNameError
+                    && <span className="error">Please enter a title</span>}
+                </div>
 
-          {hasTodoNameError
-            && <span className="error">Please enter a title</span>}
-        </div>
+                <div className="field">
+                  <select
+                    data-cy="userSelect"
+                    value={newUserId}
+                    onChange={(event) => setNewUserId(+event.target.value)}
+                  >
+                    <option key={0} value={0} disabled>
+                      Choose a user
+                    </option>
+                    {usersFromServer.map((currentUser) => (
+                      <option key={currentUser.id} value={currentUser.id}>
+                        {currentUser.name}
+                      </option>
+                    ))}
+                  </select>
 
-        <div className="field">
-          <select
-            data-cy="userSelect"
-            value={newUserId}
-            onChange={(event) => setNewUserId(+event.target.value)}
-          >
-            <option value="0" disabled>Choose a user</option>
-            {usersFromServer.map(currentUser => (
-              <option
-                key={currentUser.id}
-                value={currentUser.id}
-              >
-                {currentUser.name}
-              </option>
-            ))}
-          </select>
+                  {hasUserError
+                    && <span className="error">Please choose a user</span>}
+                </div>
 
-          {hasUserError
-            && <span className="error">Please choose a user</span>}
-        </div>
+                <Button
+                  fullWidth
+                  type="submit"
+                  data-cy="submitButton"
+                >
+                  Add
+                </Button>
+              </form>
 
-        <button
-          type="submit"
-          data-cy="submitButton"
-        >
-          Add
-        </button>
-      </form>
-
-      <TodoList todos={newTodos} />
-    </div>
+              <TodoList todos={newTodos} />
+            </div>
+          </WindowContent>
+        </Window>
+      </ThemeProvider>
+    </>
   );
 };
