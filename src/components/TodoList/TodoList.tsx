2c864b471f064e7cbb6dfa740653fd2fedecec96
Sergey Kovyazin <mr.roland1989@gmail.com>
2023-04-04 03:34:30 +0300

    React todo form is ready

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,25 @@
-export const TodoList = () => {};
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todos: Todo[]
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  const myStyle = {
+    listStyleType: 'none',
+  };
+
+  return (
+    <section className="TodoList">
+      <ul style={myStyle}>
+        {todos && todos.map((todo) => (
+          <li key={todo.id}>
+            <TodoInfo todo={todo} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
