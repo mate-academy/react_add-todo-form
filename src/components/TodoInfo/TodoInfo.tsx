e5177465943a3207a82b4826d8b97b41156d07ee
diamat1997 <diamat1997@gmail.com>
2023-04-05 14:24:03 +0300

    commit message

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,37 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import classname from 'classnames';
+import './TodoInfo.scss';
+import { Todo } from '../../Types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo,
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    title,
+    completed,
+    user,
+    id,
+  } = todo;
+
+  return (
+    <article
+      data-id={id}
+      className={
+        classname('TodoInfo', {
+          'TodoInfo--completed': completed,
+        })
+      }
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && (
+        <UserInfo user={user} />
+      )}
+    </article>
+  );
+};
