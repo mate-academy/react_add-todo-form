00331ea0d91083cb3a781f6ec1dce65e913b41e9
Roman Kvas <fallenmadara@192.168.1.6>
2023-04-05 10:20:57 +0200

    Add new todo

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,34 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { Todos } from '../../types/Todos';
+import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
+
+type Props = {
+  todo: Todos
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    title,
+    completed,
+    user,
+    id,
+  } = todo;
+
+  return (
+    <article
+      data-id={id}
+      className={classNames(
+        'TodoInfo',
+        { 'TodoInfo--completed': completed },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+      {user && (
+        <UserInfo user={user} />
+      )}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,17 @@
-export const TodoList = () => {};
+import { Todos } from '../../types/Todos';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todos: Todos[],
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo key={todo.id} todo={todo} />
+      ))}
+
+    </section>
+  );
+};
