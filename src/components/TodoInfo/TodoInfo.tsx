5a2405fd41b47306f0c489c6a38862377af66fba
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-04-18 07:54:45 +0200

    fix task

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cn from 'classnames';
 import { Todo } from '../../types/Todo';
 import { UserInfo } from '../UserInfo';
 
@@ -12,7 +13,7 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => {
   } = todo;
 
   return (
-    <article data-id={`${id}`} className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}>
+    <article data-id={`${id}`} className={cn('TodoInfo', { 'TodoInfo--completed': completed })}>
       <h2 className="TodoInfo__title">{title}</h2>
 
       {user && (
