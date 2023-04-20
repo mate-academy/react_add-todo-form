778fc669b8dde6ae88e2162c0c23fda5e40d1f76
Kate Matviiv <katyaya56@gmail.com>
2023-04-20 21:35:48 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -10,23 +10,26 @@ type Props = {
 
 export const TodoInfo: React.FC<Props> = ({ todo }) => {
   const {
+    id,
     title,
     completed,
     user,
   } = todo;
 
   return (
-    <article className={classNames(
-      'TodoInfo',
-      {
-        'TodoInfo--completed': completed,
-      },
-    )}
+    <li
+      data-id={id}
+      className={classNames(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': completed,
+        },
+      )}
     >
       <h2 className="TodoInfo__title">{title}</h2>
       {user && (
         <UserInfo user={user} />
       )}
-    </article>
+    </li>
   );
 };
