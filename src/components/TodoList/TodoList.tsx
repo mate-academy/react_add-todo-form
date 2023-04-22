ff2735c9f2c1661c9f355ae8c57cffe31e1bfad1
Vlad <bojko.vladislav16@gmail.com>
2023-04-22 10:07:48 +0300

    The task has been completed

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,13 @@
-export const TodoList = () => {};
+import React from 'react';
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  todos: Todo[]
+}
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <>
+    {todos.map(todo => <TodoInfo key={todo.id} todo={todo} />)}
+  </>
+);
