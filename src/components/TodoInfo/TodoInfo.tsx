4cc1e8d53a1714e632f5507c5064f9be392df44a
Denys Krysanov <denis.krisanov321@gmail.com>
2023-04-25 15:23:12 +0300

    todo app added

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,34 @@
-export const TodoInfo = () => {};
+import { FC } from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/types';
+import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
+
+interface Props {
+  todo: Todo,
+}
+
+export const TodoInfo: FC<Props> = ({ todo }) => {
+  const {
+    id,
+    title,
+    user,
+    completed,
+  } = todo;
+
+  return (
+    <li
+      data-id={id}
+      className={classNames(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': completed,
+        },
+      )}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && <UserInfo user={user} />}
+    </li>
+  );
+};
