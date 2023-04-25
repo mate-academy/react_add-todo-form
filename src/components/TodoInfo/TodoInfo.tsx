8995d81630c3f5eda0dfbd9f1cdb1b3a4cfc8894
Oleksandr Bavdyk <sbavdik@gmail.com>
2023-04-25 18:53:39 +0300

    created todo form

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,33 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { Todo } from '../../Types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    completed,
+    title,
+    user,
+    id,
+  } = todo;
+
+  return (
+    <section className="TodoList">
+      <article
+        data-id={id}
+        className={classNames('TodoInfo', {
+          'TodoInfo TodoInfo--completed': completed,
+        })}
+      >
+        <h2 className="TodoInfo__title">
+          {title}
+        </h2>
+
+        {user && <UserInfo user={user} />}
+      </article>
+    </section>
+  );
+};
