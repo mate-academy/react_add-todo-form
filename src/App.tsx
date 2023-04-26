c35ae50d404900ac65ac35185b0a5458b8b1a38d
Nazarii Chepil <nazzikk1998@gmail.com>
2023-04-26 11:55:24 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,136 @@
 import './App.scss';
+import { useState } from 'react';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+
+import { Todo } from './types/Todo';
+import { User } from './types/User';
+import { TodoList } from './components/TodoList';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+export const todos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
 
 export const App = () => {
+  const [titleError, setTitleError] = useState<boolean>(false);
+  const [userError, setUserError] = useState<boolean>(false);
+  const [addUser, setAddUser] = useState<string>('');
+  const [addTitle, setAddTitle] = useState<string>('');
+  const [addTodos, setAddTodos] = useState<Todo[]>(todos);
+
+  const handleAddTodo = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    event.preventDefault();
+
+    const title = addTitle.trim();
+    const newIndex = Math.max(...addTodos.map(todo => todo.id)) + 1;
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    !title
+      ? setTitleError(true)
+      : setTitleError(false);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    !addUser
+      ? setUserError(true)
+      : setUserError(false);
+
+    if (title && addUser) {
+      const newTodo: Todo = {
+        id: newIndex,
+        title,
+        completed: false,
+        userId: 1,
+        user: getUser(+addUser),
+      };
+
+      setAddTodos([
+        ...addTodos,
+        newTodo,
+      ]);
+
+      setAddTitle('');
+      setAddUser('');
+    }
+  };
+
+  const handleSetAddUser = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setAddUser(event.target.value);
+    setUserError(false);
+  };
+
+  const handleSetAddTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAddTitle(event.target.value);
+    setTitleError(false);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
       <form action="/api/users" method="POST">
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <span className="label">Title: </span>
+          <input
+            type="text"
+            data-cy="titleInput"
+            placeholder="Enter a title"
+            value={addTitle}
+            onChange={handleSetAddTitle}
+          />
+          <span className="error">
+            {titleError && ('Please enter a title')}
+          </span>
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
+          <span className="label">User: </span>
+          <select
+            name="User"
+            data-cy="userSelect"
+            value={addUser}
+            onChange={handleSetAddUser}
+          >
+            <option disabled value="">Choose a user</option>
+            {
+              usersFromServer.map(user => (
+                <option
+                  key={user.id}
+                  value={user.id}
+                >
+                  {`${user.name}`}
+                </option>
+              ))
+            }
           </select>
 
-          <span className="error">Please choose a user</span>
+          <span className="error">
+            {userError && ('Please choose a user')}
+          </span>
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button
+          type="submit"
+          data-cy="submitButton"
+          onClick={(event) => handleAddTodo(event)}
+        >
           Add
         </button>
       </form>
 
       <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
+        <div className="App">
+          <TodoList todos={addTodos} />
+        </div>
       </section>
     </div>
   );
