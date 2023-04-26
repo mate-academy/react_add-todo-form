a32f7478296a2f8ea729197af949b8b8c7595f92
Anastasiia Yuzyfyshyn <nastyajuzufichn@gmail.com>
2023-04-26 17:09:46 +0300

    created todo form

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,137 @@
 import './App.scss';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
 
-export const App = () => {
+import { User } from './types/User';
+import { Todo } from './types/Todo';
+import { TodoList } from './components/TodoList';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+export const todos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
+
+export const App: React.FC = () => {
+  const [visibleTodos, setVisibleTodos] = useState(todos);
+  const [title, setTitle] = useState('');
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [titleError, setTitleError] = useState(false);
+  const [userError, setUserError] = useState(false);
+
+  const handleChange = (
+    event: ChangeEvent<HTMLSelectElement | HTMLInputElement>,
+  ) => {
+    switch (event.target.id) {
+      case 'titleInput':
+        setTitleError(false);
+        setTitle(event.target.value);
+        break;
+
+      case 'userSelect':
+        setUserError(false);
+        setSelectedUser(getUser(+(event.target.value)));
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!title) {
+      setTitleError(true);
+    }
+
+    if (!selectedUser) {
+      setUserError(true);
+    }
+
+    if (!title || !selectedUser) {
+      return;
+    }
+
+    const newTodo: Todo = {
+      id: Math.max(...todos.map(todo => todo.id)) + 1,
+      title,
+      userId: selectedUser ? selectedUser.id : -1,
+      completed: false,
+      user: selectedUser,
+    };
+
+    setVisibleTodos([...visibleTodos, newTodo]);
+    setTitle('');
+    setSelectedUser(null);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={handleSubmit}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="titleInput">Title: </label>
+          <input
+            type="text"
+            id="titleInput"
+            placeholder="Enter a title"
+            data-cy="titleInput"
+            pattern="[a-zA-Zа-яА-Я0-9\s]*"
+            title="Only letters (ua and en), digits, and spaces are allowed."
+            value={title}
+            onChange={handleChange}
+          />
+          {titleError && (
+            <span className="error">Please enter a title</span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
+          <label htmlFor="userSelect">User: </label>
+          <select
+            id="userSelect"
+            placeholder="Choose a user"
+            data-cy="userSelect"
+            value={selectedUser ? selectedUser.id : 0}
+            onChange={handleChange}
+          >
+            <option value="">Choose a user</option>
+
+            {usersFromServer.map(user => (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            ))}
           </select>
+          {userError && (
+            <span className="error">Please choose a user</span>
+          )}
 
-          <span className="error">Please choose a user</span>
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button
+          type="submit"
+          data-cy="submitButton"
+        >
           Add
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
+      <TodoList todos={visibleTodos} />
 
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
     </div>
   );
 };
