7ff5490f663ae235edbed44c5e9d234f92048868
Zibi95 <sylwester.sob95@gmail.com>
2023-05-11 11:15:47 +0200

    UserInfo component

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,15 @@
-export const UserInfo = () => {};
+export type User = {
+  id: number,
+  name: string,
+  email: string,
+};
+
+type UserProps = {
+  user: User,
+};
+
+export const UserInfo = ({ user }: UserProps) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
+);
