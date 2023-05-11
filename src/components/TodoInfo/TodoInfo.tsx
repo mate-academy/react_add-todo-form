ec7865767bdafa98328e40565100eb34914a6954
Zibi95 <sylwester.sob95@gmail.com>
2023-05-11 11:16:20 +0200

    TodoInfo Component

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,27 @@
-export const TodoInfo = () => {};
+import { UserInfo, User } from '../UserInfo';
+
+export type Todo = {
+  id: number,
+  userId: number,
+  title: string,
+  completed: boolean,
+  user: User | null,
+};
+
+type TodoProps = {
+  todo: Todo,
+};
+
+export const TodoInfo = ({ todo }: TodoProps) => (
+  <article
+    className={
+      todo.completed
+        ? 'TodoInfo TodoInfo--completed'
+        : 'TodoInfo'
+    }
+    data-id={todo.id}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    {todo.user && <UserInfo user={todo.user} />}
+  </article>
+);
