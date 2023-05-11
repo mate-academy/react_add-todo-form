f7aa8d1b7bff754966f3c9a7b5821070469fb360
Ilia <koleshnik160799@gmail.com>
2023-05-11 22:07:58 +0300

    Done

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,22 @@
-export const TodoList = () => {};
+import { TodoInfo } from '../TodoInfo';
+import { User } from '../../types/users';
+import { Todo } from '../../types/todo';
+
+type TodoListType = {
+  users:User[];
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<TodoListType> = ({ todos, users }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => {
+        const userTodos = users.find(user => user.id === todo.userId);
+
+        return (
+          <TodoInfo todo={todo} user={userTodos} />
+        );
+      })}
+    </section>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,13 @@
-export const UserInfo = () => {};
+import { User } from '../../types/users';
+
+type UserInfoProps = {
+  user:User;
+};
+
+export const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
+};
