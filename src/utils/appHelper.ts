c656117e3df3dc9ed9208c1746f3e1c49296d056
Zibi95 <sylwester.sob95@gmail.com>
2023-05-12 12:00:23 +0200

    Move App helper functions to seperate file

---
+++ src/utils/appHelper.ts
@@ -0,0 +1,18 @@
+/* eslint-disable max-len */
+import usersFromServer from '../api/users';
+import todosFromServer from '../api/todos';
+
+export const getUser = (userId: number) => usersFromServer.find((user) => user.id === userId) ?? null;
+
+export const getNewId = () => todosFromServer.reduce((acc, curr) => Math.max(acc, curr.id), 0) + 1;
+
+export const getTodos = () => todosFromServer.map((todo) => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
+
+export const validateTitleInput = (string: string) => {
+  const regexForOnlyEnRuWords = /^[a-zA-Zа-яА-ЯёЁ\d\s]*$/;
+
+  return regexForOnlyEnRuWords.test(string);
+};
