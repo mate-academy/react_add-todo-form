7572ff34df5f3ab24df66e25814ba9af62bedd49
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-05-15 15:28:14 +0200

    solution v1.1

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -11,24 +11,6 @@ export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
       {todos.map(todo => (
         <TodoInfo key={todo.id} todo={todo} />
       ))}
-
-      {/* <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article> */}
     </section>
   );
 };
