896da835d6c69e0b6dd03a3c1f853daad1009ac2
Samir Mamedov <samirmamedoviphone@gmail.com>
2023-05-16 20:13:10 +0300

    React add todo task was done

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,38 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { UserInfo, User } from '../UserInfo';
+
+export type Todo = {
+  id: number,
+  userId: number,
+  title: string,
+  completed: boolean,
+  user: User | null,
+};
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo = ({ todo }: Props) => {
+  const {
+    id,
+    title,
+    completed,
+    user,
+  } = todo;
+
+  return (
+    <article
+      data-id={id}
+      className={classNames(
+        'TodoInfo',
+        { 'TodoInfo--completed': completed },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,11 @@
-export const TodoList = () => {};
+import { TodoInfo, Todo } from '../TodoInfo';
+
+type TodoProps = {
+  todos: Todo[],
+};
+
+export const TodoList = ({ todos }: TodoProps) => (
+  <section className="TodoList">
+    {todos.map(todo => <TodoInfo todo={todo} key={todo.id} />)}
+  </section>
+);
