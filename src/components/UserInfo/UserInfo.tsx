7484cfc9ecbb3fd06a908207c56e4188b0271604
marshalek37 <patrykmarp@gmai.com>
2023-05-17 20:39:54 +0200

    Solution V1

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,19 @@
-export const UserInfo = () => {};
+import { User } from '../../types/User';
+import './UserInfo.scss';
+
+interface UserProps {
+  user: User;
+}
+
+export const UserInfo: React.FC<UserProps> = ({ user }) => {
+  const {
+    name,
+    email,
+  } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
