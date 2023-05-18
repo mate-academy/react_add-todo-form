265264063e5649250caac429c59ecad082ae5e05
Katarzyna Bogiel <katarzyna.bogiel@poczta.onet.pl>
2023-05-18 13:15:57 +0200

    initial version

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,27 @@
-export const TodoInfo = () => {};
+import { UserInfo, User } from '../UserInfo';
+
+export interface Todo {
+  id: number,
+  userId: number,
+  title: string,
+  completed: boolean,
+  user: User | null,
+}
+
+interface TodoInfoProps {
+  todo: Todo;
+}
+
+export const TodoInfo: React.FC<TodoInfoProps> = ({ todo }) => {
+  const { completed, title, user } = todo;
+
+  return (
+    <article
+      data-id={todo.id}
+      className={`TodoInfo ${completed && 'TodoInfo--completed'}`}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
