51f8d4f1b4ab44faff7e5de4e7e013f8921eec72
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2023-05-19 16:32:47 +0300

    Refimenets

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import './App.scss';
+import "./App.scss";
 
-import { useEffect, useState } from 'react';
-import usersFromServer from './api/users';
-import todosFromServer from './api/todos';
-import { TodoList } from './components/TodoList';
-import { User } from './types/user';
-import { Todo } from './types/todo';
-import { findUserByTodoUserId } from './helpers/findUserByTodoUserId';
+import { useEffect, useState } from "react";
+import usersFromServer from "./api/users";
+import todosFromServer from "./api/todos";
+import { TodoList } from "./components/TodoList";
+import { User } from "./types/user";
+import { Todo } from "./types/todo";
+import { findUserByTodoUserId } from "./helpers/findUserByTodoUserId";
 
 interface FormValue {
-  userId: string;
+  userId: number;
   title: string;
   emptyTitleError: boolean;
   emptyUserError: boolean;
 }
 
+const defaultFormValue: FormValue = {
+  userId: 0,
+  title: "",
+  emptyTitleError: false,
+  emptyUserError: false,
+};
+
 export const App = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [formValue, setFormValue] = useState<FormValue>({
-    userId: '0',
-    title: '',
-    emptyTitleError: false,
-    emptyUserError: false,
-  });
+  const [formValue, setFormValue] = useState<FormValue>(defaultFormValue);
 
   useEffect(() => {
     setUsers(usersFromServer);
@@ -36,12 +38,12 @@ export const App = () => {
     const { title, userId } = formValue;
     let error = false;
 
-    if (userId === '0') {
+    if (!userId) {
       setFormValue((prevState) => ({ ...prevState, emptyUserError: true }));
       error = true;
     }
 
-    if (title === '') {
+    if (!title) {
       setFormValue((prevState) => ({ ...prevState, emptyTitleError: true }));
       error = true;
     }
@@ -50,8 +52,7 @@ export const App = () => {
       return;
     }
 
-    const userIdNumber = +userId;
-    const user = findUserByTodoUserId(users, userIdNumber);
+    const user = findUserByTodoUserId(users, userId);
 
     if (user) {
       const newTodo = {
@@ -60,18 +61,13 @@ export const App = () => {
             .id + 1,
         title: formValue.title,
         completed: false,
-        userId: userIdNumber,
+        userId: userId,
       };
 
       setTodos([...todos, newTodo]);
     }
 
-    setFormValue({
-      userId: '0',
-      title: '',
-      emptyTitleError: false,
-      emptyUserError: false,
-    });
+    setFormValue(defaultFormValue);
   };
 
   const { title, userId } = formValue;
@@ -88,7 +84,7 @@ export const App = () => {
             value={title}
             placeholder="Enter a title"
             onChange={(event) => {
-              if (formValue.emptyTitleError && event.target.value !== '') {
+              if (formValue.emptyTitleError && event.target.value) {
                 setFormValue((prevState) => ({
                   ...prevState,
                   emptyTitleError: false,
@@ -110,9 +106,9 @@ export const App = () => {
         <div className="field">
           <select
             data-cy="userSelect"
-            value={userId}
+            value={userId.toString()}
             onChange={(event) => {
-              if (formValue.emptyUserError && event.target.value !== '0') {
+              if (formValue.emptyUserError && +event.target.value) {
                 setFormValue((prevState) => ({
                   ...prevState,
                   emptyUserError: false,
@@ -121,7 +117,7 @@ export const App = () => {
 
               setFormValue((prevState) => ({
                 ...prevState,
-                userId: event.target.value,
+                userId: +event.target.value,
               }));
             }}
           >
