41ce2eff5d5754bc10c9dd68910223c698ee2b55
Nikitin Evgeny <ngenya81@gmail.com>
2023-05-19 12:52:44 +0300

    commit message

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,27 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import ClassNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo ;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const { completed, title, user } = todo;
+
+  return (
+    <>
+      <article className={ClassNames(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': completed,
+        },
+      )}
+      >
+        <h2 className="TodoInfo__title">{title}</h2>
+        {user && <UserInfo user={user} />}
+      </article>
+    </>
+  );
+};
