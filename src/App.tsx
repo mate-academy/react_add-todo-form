b18f39d1e1edf297c5b079a0afa7ec659daada20
Kacper Łyczba <suaby1725@gmail.com>
2023-05-21 22:40:11 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,131 @@
+import { useState } from 'react';
 import './App.scss';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { TodoList } from './components/TodoList/TodoList';
+import { User } from './types/User';
+import { Todo } from './types/Todo';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+export function getUserFromId(userId: number): User | null {
+  return usersFromServer.find((person) => person.id === userId) || null;
+}
+
+export const todosWithUser: Todo[] = todosFromServer.map((todo) => ({
+  ...todo,
+  user: getUserFromId(todo.userId),
+}));
 
 export const App = () => {
+  const [todos, setTodos] = useState(todosWithUser);
+  const [title, setTitle] = useState('');
+  const [userId, setUserId] = useState(0);
+  const [titleError, setTitleError] = useState(false);
+  const [userIdError, setUserIdError] = useState(false);
+  const userList = usersFromServer;
+  let highestTodoId = Math.max(...todos.map(todo => (todo.id)));
+
+  function changeTitle(event: React.ChangeEvent<HTMLInputElement>) {
+    // I opted to do the optional instruction
+    const legalCharacters = 'abcdefghijklmnopqrstuvwxyz1234567890 ' /* 'en' */
+      + 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'; /* 'ru' */
+    const croppedInput = event.target.value.split('').filter((char) => (
+      legalCharacters.includes(char.toLowerCase()) || char === ' '
+    )).join('');
+
+    setTitle(croppedInput);
+
+    if (titleError && croppedInput.length > 0) {
+      setTitleError(false);
+    }
+  }
+
+  function changeUser(event: React.ChangeEvent<HTMLSelectElement>) {
+    setUserId(Number(event.target.value));
+
+    if (userIdError && Number(event.target.value) > 0) {
+      setUserIdError(false);
+    }
+  }
+
+  function submitForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (title.length > 0 && userId > 0) {
+      highestTodoId += 1;
+
+      const newTodo = {
+        id: highestTodoId + 0, // This is to ensure it isn't a reference
+        title,
+        completed: false,
+        userId,
+        user: getUserFromId(userId),
+      };
+
+      setTodos([...todos, newTodo]);
+      setUserId(0);
+      setTitle('');
+    } else {
+      if (title.length === 0) {
+        setTitleError(true);
+      }
+
+      if (userId <= 0) {
+        setUserIdError(true);
+      }
+    }
+  }
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={submitForm}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="title">
+            {'Title: '}
+          </label>
+          <input
+            id="title"
+            type="text"
+            data-cy="titleInput"
+            placeholder="Title"
+            value={title}
+            onChange={changeTitle}
+          />
+          {titleError && (
+            <span className="error">Please enter a title</span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <label htmlFor="user">
+            {'User: '}
+          </label>
+          <select
+            id="user"
+            data-cy="userSelect"
+            value={userId}
+            onChange={changeUser}
+          >
             <option value="0" disabled>Choose a user</option>
+            {userList.map((person) => {
+              const { id, name } = person;
+
+              return (
+                <option key={id} value={id}>
+                  {name}
+                </option>
+              );
+            })}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {userIdError && (
+            <span className="error">Please choose a user</span>
+          )}
         </div>
 
         <button type="submit" data-cy="submitButton">
@@ -27,35 +133,7 @@ export const App = () => {
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      {todos && <TodoList todos={todos} />}
     </div>
   );
 };
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,28 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+interface TodoInfoProps {
+  todo: Todo,
+}
+
+export const TodoInfo: React.FC<TodoInfoProps> = ({
+  todo: {
+    title,
+    completed,
+    id,
+    user,
+  },
+}) => {
+  const className = completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo';
+
+  return (
+    <article data-id={id} className={className}>
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
