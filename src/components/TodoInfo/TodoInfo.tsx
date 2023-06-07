8d609f61374d2ea829fece6b7609eba97a039607
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2023-06-07 19:34:13 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,23 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+interface TodoInfoProps {
+  todo: Todo;
+}
+
+export const TodoInfo: React.FC<TodoInfoProps> = ({ todo }) => {
+  const { completed, title, user } = todo;
+  const todoInfoClasses = classNames('TodoInfo', {
+    'TodoInfo--completed': completed,
+  });
+
+  return (
+    <article className={todoInfoClasses}>
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      { user && <UserInfo user={user} />}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,15 @@
-export const TodoList = () => {};
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+interface ToDoListProps {
+  todos: Todo[];
+}
+
+export const TodoList: React.FC <ToDoListProps> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map((todo) => (
+      <TodoInfo todo={todo} key={todo.id} />
+    ))}
+  </section>
+);
