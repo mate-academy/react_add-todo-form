43d4f7d0086eb9f814f37b02fba96b47c10ea10b
Volodymyr Sherstov <sherstov93@gmail.com>
2023-06-09 19:07:04 +0300

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,30 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { User, UserInfo } from '../UserInfo';
+
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+  user: User | null;
+}
+
+type TodoProps = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<TodoProps> = ({ todo }) => (
+  <article
+    data-id={todo.id}
+    className={classNames(
+      'TodoInfo',
+      { 'TodoInfo--completed': todo.completed },
+    )}
+  >
+    <h2 className="TodoInfo__title">
+      {todo.title}
+    </h2>
+
+    {todo.user && <UserInfo user={todo.user} />}
+  </article>
+);
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,18 @@
-export const TodoList = () => {};
+import { TodoInfo, Todo } from '../TodoInfo';
+
+type TodoListProps = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo
+          todo={todo}
+          key={todo.id}
+        />
+      ))}
+    </section>
+  );
+};
