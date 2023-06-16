bac6706b2ca441cfe6ab162fc56907db3123f346
Stepan Haida <stepanhaida@gmail.com>
2023-06-16 16:01:27 +0300

    move some components to a separate files

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,13 @@
-export const TodoInfo = () => {};
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = () => {
+  return (
+    <article data-id="1" className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">
+        delectus aut autem
+      </h2>
+
+      <UserInfo />
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,15 +1,9 @@
+import { TodoInfo } from '../TodoInfo';
+
 export const TodoList = () => {
   return (
     <section className="TodoList">
-      <article data-id="1" className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">
-          delectus aut autem
-        </h2>
-
-        <a className="UserInfo" href="mailto:Sincere@april.biz">
-          Leanne Graham
-        </a>
-      </article>
+      <TodoInfo />
 
       <article data-id="15" className="TodoInfo TodoInfo--completed">
         <h2 className="TodoInfo__title">delectus aut autem</h2>
