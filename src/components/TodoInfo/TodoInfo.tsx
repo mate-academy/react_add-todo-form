3b76d47017bbf87d45236e70a2b7a1956fa02717
Stepan Haida <stepanhaida@gmail.com>
2023-06-18 12:08:35 +0300

    maintaned components rendering

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import { FC } from 'react';
 import { TodoWithUser } from '../../Types';
 import { UserInfo } from '../UserInfo';
@@ -7,15 +8,25 @@ interface Props {
 }
 
 export const TodoInfo:FC<Props> = ({ todoItem }) => {
-  const { id, title } = todoItem;
+  const {
+    id,
+    title,
+    completed,
+    user,
+  } = todoItem;
 
   return (
-    <article data-id={id} className="TodoInfo TodoInfo--completed">
+    <article
+      data-id={id}
+      className={cn('TodoInfo', {
+        'TodoInfo--completed': completed,
+      })}
+    >
       <h2 className="TodoInfo__title">
         {title}
       </h2>
 
-      <UserInfo />
+      {user && <UserInfo user={user} />}
     </article>
   );
 };
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,7 +1,16 @@
-export const UserInfo = () => {
+import { FC } from 'react';
+import { User } from '../../Types';
+
+interface Props {
+  user: User;
+}
+
+export const UserInfo:FC<Props> = ({ user }) => {
+  const { email, name } = user;
+
   return (
-    <a className="UserInfo" href="mailto:Sincere@april.biz">
-      Leanne Graham
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
     </a>
   );
 };
