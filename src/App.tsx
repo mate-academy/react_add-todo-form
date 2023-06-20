151b3f2eed227919367e9e3c997a065525010631
Kiril Shorin <kirillshorin8838@gmail.com>
2023-06-20 11:52:30 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
+import React, { useState } from 'react';
 import './App.scss';
-import { useState } from 'react';
 import { TodoList } from './components/TodoList';
 
 import usersFromServer from './api/users';
