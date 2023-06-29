0f9f154d85d07adc23838d6ce697811a27304601
Pavlo-Petrashevskyi <119119324+Pavlo-Petrashevskyi@users.noreply.github.com>
2023-06-29 06:39:16 +0300

    Update src/components/TodoInfo/TodoInfo.tsx
    Co-authored-by: Serhii Kirichenko <91411692+SerhiiKirik@users.noreply.github.com>

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -10,8 +10,9 @@ export const TodoInfo: React.FC<TodoInfoProps> = ({ todo }) => {
   return (
     <article
       data-id={todo.id}
-      className={classNames('TodoInfo',
-        { 'TodoInfo--completed': todo.completed })}
+      className={classNames('TodoInfo', {
+         'TodoInfo--completed': todo.completed 
+       })}
     >
       <h2 className="TodoInfo__title">
         {todo.title}
