b849791fb8e5a3ec787a2d0f611ec3283618b56f
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-07-04 23:20:39 +0300

    made major logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,43 @@
+import { useState } from 'react';
+
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+
+import { NewTodo } from './components/NewTodo';
+import { TodoList } from './components/TodoList';
+
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import { Todo } from './types/Todo';
 
 export const App = () => {
+  const [listTodo, setListTodo] = useState([...todosFromServer]);
+
+  const addTodo = (todo: Omit<Todo, 'id'>) => {
+    const lastTodoIndex = listTodo.reduce((maxI, curI) => {
+      return curI.id > maxI ? curI.id : maxI;
+    }, 0);
+
+    setListTodo(currrentList => [...currrentList, {
+      ...todo,
+      id: lastTodoIndex + 1,
+    }]);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/todos" method="POST">
-        <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
-        </div>
+      <NewTodo
+        users={usersFromServer}
+        onAdd={(todo) => {
+          addTodo((todo));
+        }}
+      />
 
-        <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
-          </select>
-
-          <span className="error">Please choose a user</span>
-        </div>
-
-        <button type="submit" data-cy="submitButton">
-          Add
-        </button>
-      </form>
-
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList
+        todos={listTodo}
+      />
     </div>
   );
 };
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,48 @@
-export const TodoInfo = () => {};
+import cn from 'classnames';
+import usersFromServer from '../../api/users';
+
+import { UserInfo } from '../UserInfo';
+
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+
+function findUser(userId: number, users: User[]): User {
+  const findedUser = users.find(user => user.id === userId);
+
+  return !findedUser
+    ? {
+      id: 0,
+      name: 'Anonymous',
+      username: 'Anonymous',
+      email: '',
+    }
+    : findedUser;
+}
+
+type Props = {
+  todo: Todo
+};
+
+export const TodoInfo: React.FC<Props> = ({
+  todo: {
+    id,
+    completed,
+    title,
+    userId,
+  },
+}) => {
+  // const user = usersFromServer.find(usr => usr.id === userId);
+  const user = findUser(userId, usersFromServer);
+
+  return (
+    <article
+      data-id={id}
+      className={cn('TodoInfo', { 'TodoInfo--completed': completed })}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+      <UserInfo user={user} />
+    </article>
+  );
+};
