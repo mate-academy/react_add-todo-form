02f49a8be1015b1eda8da49f77d1c7589a83eda6
Skrudj <ssinilov@gmail.com>
2023-07-07 19:55:25 +0300

    implement TodoInfo and user type

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,36 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import cn from 'classnames';
+import { User } from '../../types/user';
+
+type Props = {
+  id: number,
+  title: string,
+  completed: boolean,
+  user: User,
+};
+
+export const TodoInfo: React.FC<Props> = ({
+  id,
+  title,
+  completed,
+  user,
+}) => {
+  const { name, email } = user;
+
+  return (
+    <article
+      data-id={id}
+      className={cn('TodoInfo', {
+        'TodoInfo--completed': completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      <a className="UserInfo" href={`mailto:${email}`}>
+        {name}
+      </a>
+    </article>
+  );
+};
