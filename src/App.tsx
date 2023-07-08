092f234d5126404aff74ca8f639659114ae7700c
Edgar Kocuba <edgarkocuba@gmail.com>
2023-07-08 11:55:35 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,133 @@
+import React, { useState } from 'react';
+import { TodoList } from './components/TodoList/TodoList';
+import { Todo } from './Types/Todo';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { User } from './Types/User';
 
-export const App = () => {
+function getUserById(userId: number, users: User[]): User | null {
+  const foundUser = users.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+function getNewTodoId(currentTodos: Todo[]) {
+  return Math.max(...currentTodos.map((todo) => todo.id)) + 1;
+}
+
+const todosWithUsers: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUserById(todo.userId, usersFromServer),
+}));
+
+export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>(todosWithUsers);
+  const [title, setTitle] = useState<string>('');
+  const [userId, setUserId] = useState<number | string>('');
+  const [isTitleValid, setIsTitleValid] = useState<boolean>(true);
+  const [isUserSelected, setIsUserSelected] = useState<boolean>(true);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!title) {
+      setIsTitleValid(false);
+    }
+
+    if (!userId) {
+      setIsUserSelected(false);
+    }
+
+    if (!title || !userId) {
+      return;
+    }
+
+    const newTodo = {
+      id: getNewTodoId(todos),
+      userId: +userId,
+      title,
+      completed: false,
+      user: getUserById(+userId, usersFromServer),
+    };
+
+    setTodos((prevTodos => [...prevTodos, newTodo]));
+    setTitle('');
+    setUserId('');
+  };
+
+  const handleUserChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setUserId(Number(event.target.value));
+    setIsUserSelected(true);
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    setIsTitleValid(true);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/users" method="POST">
+      <form
+        action="/api/users"
+        method="POST"
+        onSubmit={handleSubmit}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label>
+            {'Title: '}
+            <input
+              type="text"
+              placeholder="Enter a title"
+              data-cy="titleInput"
+              value={title}
+              name="todoTitle"
+              onChange={handleTitleChange}
+            />
+          </label>
+          {!isTitleValid && <span className="error">Please enter a title</span>}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
-          </select>
-
-          <span className="error">Please choose a user</span>
+          <label>
+            {'User: '}
+            <select
+              name="userSelect"
+              onChange={handleUserChange}
+              data-cy="userSelect"
+              value={userId}
+            >
+              <option
+                value=""
+                disabled
+              >
+                Choose a user
+              </option>
+              {usersFromServer.map((user: User) => (
+                <option
+                  key={user.id}
+                  value={user.id}
+                >
+                  {user.name}
+                </option>
+              ))}
+            </select>
+          </label>
+          {!isUserSelected
+            && <span className="error">Please choose a user</span>}
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button
+          type="submit"
+          data-cy="submitButton"
+        >
           Add
         </button>
       </form>
-
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={todos} />
     </div>
   );
 };
