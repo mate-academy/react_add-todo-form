ac2a46a88dc3090f491a259b49c44e50b1d0a9c3
Diana <diana.gs202@gmail.com>
2023-07-11 19:17:28 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,133 @@
+import { useState } from 'react';
+import classNames from 'classnames';
+
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { TodoList } from './components/TodoList';
+import { Todos } from './types/Todos';
+import { User } from './types/User';
+
+const todosWithUser = todosFromServer.map(todo => {
+  const userForTodo = usersFromServer.find(user => todo.userId === user.id);
+
+  return {
+    ...todo,
+    user: userForTodo as User,
+  };
+});
 
 export const App = () => {
+  const [allTodos, setAllTodos] = useState(todosWithUser);
+  const [title, setTitle] = useState('');
+  const [hasTitleError, setHasTitleError] = useState(false);
+  const [userId, setUserId] = useState(0);
+  const [hasUserIdError, setHasUserIdError] = useState(false);
+
+  const findUser = (id: number) => {
+    return usersFromServer.find(user => user.id === id) as User;
+  };
+
+  const addNewTodo = (newTodo: Todos) => {
+    setAllTodos(todos => [...todos, newTodo]);
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    setHasTitleError(false);
+  };
+
+  const handleUserIdChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setUserId(+event.target.value);
+    setHasUserIdError(false);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event?.preventDefault();
+
+    setHasTitleError(!title);
+    setHasUserIdError(!userId);
+
+    if (!title || !userId) {
+      return;
+    }
+
+    addNewTodo({
+      id: 0,
+      title,
+      completed: false,
+      userId,
+      user: findUser(userId),
+    });
+  };
+
   return (
-    <div className="App">
-      <h1>Add todo form</h1>
+    <div className="container is-max-desktop">
+      <h1 className="title">Add todo form</h1>
 
-      <form action="/api/todos" method="POST">
+      <form
+        action="/api/todos"
+        method="POST"
+        onSubmit={handleSubmit}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="title" className="label">Title: </label>
+          <input
+            type="text"
+            className={classNames(
+              'input is-success', { 'is-danger': hasTitleError },
+            )}
+            data-cy="titleInput"
+            id="title"
+            placeholder="Enter a title"
+            value={title}
+            onChange={handleTitleChange}
+          />
+
+          {hasTitleError && (
+            <span className="help is-danger">Please enter a title</span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
-          </select>
+          <label htmlFor="user-id" className="label">User: </label>
 
-          <span className="error">Please choose a user</span>
+          <div className={classNames(
+            'select is-success', { 'is-danger': hasUserIdError },
+          )}
+          >
+            <select
+              data-cy="userSelect"
+              id="user-id"
+              value={userId}
+              onChange={handleUserIdChange}
+            >
+              <option value="0" disabled>Choose a user</option>
+
+              {usersFromServer.map(user => (
+                <option value={user.id} key={user.id}>{user.name}</option>
+              ))}
+            </select>
+          </div>
+
+          {hasUserIdError && (
+            <span className="help is-danger">Please choose a user</span>
+          )}
         </div>
 
-        <button type="submit" data-cy="submitButton">
+        <button
+          type="submit"
+          data-cy="submitButton"
+          className="button is-success"
+        >
           Add
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList
+        todos={allTodos}
+      />
     </div>
   );
 };
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,13 @@
-export const UserInfo = () => {};
+import React from 'react';
+import { User } from '../../types/User';
+
+interface Props{
+  user: User | undefined,
+}
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  return (
+    <a className="UserInfo" href="mailto:Sincere@april.biz">
+      {user?.name}
+    </a>
+  );
+};
