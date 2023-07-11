a75763f008bb126f4c0e647cc19cbe4b4a05a082
yurii-krymskiy <yurii.krymskyi@gmail.com>
2023-07-11 16:05:07 +0200

    s

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,18 @@
-export const TodoInfo = () => {};
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <article
+    className={(todo.completed
+      ? 'TodoInfo TodoInfo--completed'
+      : 'TodoInfo')}
+    data-id={todo.id}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    <UserInfo user={(!todo.user) ? null : todo.user} />
+  </article>
+);
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,14 @@
-export const UserInfo = () => {};
+import { User } from '../../types/User';
+
+type Props = {
+  user: User | null;
+};
+
+export const UserInfo:React.FC<Props> = ({ user }) => (
+  <a
+    className="UserInfo"
+    href={`mailto:${user?.email}`}
+  >
+    {user?.name}
+  </a>
+);
