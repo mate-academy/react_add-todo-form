d4877b6a583ef30662ebbaaf065528c85e44165e
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-07-18 15:05:34 +0300

    first solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,163 @@
+import { useState } from 'react';
+import { TodoList } from './components/TodoList';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { Todo } from './types/Todo';
+
+function getUser(id: number) {
+  return usersFromServer.find(person => id === person.id);
+}
+
+const todosWithUser = todosFromServer.map(todo => {
+  const user = getUser(todo.userId);
+
+  return {
+    ...todo,
+    user,
+  };
+});
+
+function getId(todos: Todo[]) {
+  const ids = todos.map(todo => todo.id);
+  const newId = Math.max(...ids) + 1;
+
+  return newId;
+}
 
 export const App = () => {
+  const [todos, setTodos] = useState(todosWithUser);
+  const [formValues, setFormValues] = useState({
+    todoTitle: '',
+    todoUser: '0',
+  });
+  const [formErrors, setFormErrors] = useState({
+    titleError: false,
+    userError: false,
+  });
+
+  function formReset() {
+    setFormValues({
+      todoTitle: '',
+      todoUser: '0',
+    });
+  }
+
+  function handleSubmitErrors() {
+    if (!formValues.todoTitle.trim()) {
+      setFormErrors(prevErrors => ({
+        ...prevErrors,
+        titleError: true,
+      }));
+    }
+
+    if (!+formValues.todoUser) {
+      setFormErrors(prevErrors => ({
+        ...prevErrors,
+        userError: true,
+      }));
+    }
+  }
+
+  function handleChanges(event: React.ChangeEvent<HTMLInputElement>
+  | React.ChangeEvent<HTMLSelectElement>) {
+    setFormValues(prevFormValues => ({
+      ...prevFormValues,
+      [event.target.name]: event.target.value,
+    }));
+    if (formErrors.titleError && event.target.name === 'todoTitle') {
+      setFormErrors(prevErrors => ({
+        ...prevErrors,
+        titleError: false,
+      }));
+    }
+
+    if (formErrors.userError && event.target.name === 'todoUser') {
+      setFormErrors(prevErrors => ({
+        ...prevErrors,
+        userError: false,
+      }));
+    }
+  }
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    const user = getUser(+formValues.todoUser);
+
+    const newTodo = {
+      id: getId(todos),
+      title: formValues.todoTitle,
+      completed: false,
+      userId: +formValues.todoUser,
+      user,
+    };
+
+    handleSubmitErrors();
+
+    if (!formValues.todoTitle.trim() || !+formValues.todoUser) {
+      return;
+    }
+
+    setTodos(prevTodos => [...prevTodos, newTodo]);
+
+    formReset();
+  }
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/todos" method="POST">
+      <form
+        action="/api/todos"
+        method="POST"
+        onSubmit={handleSubmit}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="todoTitle">Title: </label>
+          <input
+            type="text"
+            id="todoTitle"
+            data-cy="titleInput"
+            name="todoTitle"
+            placeholder="Enter a title"
+            value={formValues.todoTitle}
+            onChange={handleChanges}
+          />
+          {formErrors.titleError
+            && <span className="error">Please enter a title</span>}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <label htmlFor="todoUser">User: </label>
+          <select
+            data-cy="userSelect"
+            id="todoUser"
+            name="todoUser"
+            value={formValues.todoUser}
+            onChange={handleChanges}
+          >
             <option value="0" disabled>Choose a user</option>
+            {usersFromServer.map(user => (
+              <option
+                value={user.id}
+                key={user.id}
+              >
+                {user.name}
+              </option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {formErrors.userError
+            && <span className="error">Please choose a user</span>}
         </div>
 
         <button type="submit" data-cy="submitButton">
           Add
         </button>
       </form>
-
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={todos} />
     </div>
   );
 };
