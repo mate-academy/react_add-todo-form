4ecb0cef246c8c9d4511e70348b5a8624cd94c7a
Alexey Taranenko <alloha.t.99@gmail.com>
2023-07-18 23:30:36 +0300

    chore/add-todo-form

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,11 @@
-export const UserInfo = () => {};
+type Props = {
+  user: UserType | null
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  return (
+    <a className="UserInfo" href={`mailto:${user?.email}`}>
+      {user?.name}
+    </a>
+  );
+};
