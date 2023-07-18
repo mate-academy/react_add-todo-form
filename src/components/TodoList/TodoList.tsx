59c0f3ff733f5eaf121d9cf201e5152d822ba269
Diana Pinchuk <diana9360@gmail.com>
2023-07-18 18:29:59 +0300

    add task solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,12 @@
-export const TodoList = () => {};
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+import { Todo } from '../../types/Todo';
+
+export const TodoList = ({ todos }: { todos: Todo[] }) => (
+  <section className="TodoList">
+    {
+      todos.map(todo => (
+        <TodoInfo todo={todo} data-id={todo.id} />
+      ))
+    }
+  </section>
+);
