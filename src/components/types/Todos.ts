2a2976f133a44d27e767234e95844260c12025d5
fawnique <stolpnik.s.i@gmail.com>
2023-07-19 04:48:19 +0300

    Solution

---
+++ src/components/types/Todos.ts
@@ -0,0 +1,11 @@
+import User from './User';
+
+export interface Todos {
+  id: number,
+  title: string,
+  completed: boolean,
+  userId: number,
+  user?: User | null
+}
+
+export type ArrayOfTodos = Todos[];
