84c58f1f89947d25237c370937db0d89e500ff8a
Nadiia Kovalchuk <nadiaaaa1256@gmail.com>
2023-07-19 09:12:50 +0300

    add task solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,15 @@
-export const UserInfo = () => {};
+/* eslint-disable import/no-cycle */
+import { User } from '../../App';
+
+export interface UserProps {
+  user: User;
+}
+
+export const UserInfo = ({ user }: UserProps) => (
+  <a
+    className="UserInfo"
+    href={`mailto:${user.email}`}
+  >
+    {user.name}
+  </a>
+);
