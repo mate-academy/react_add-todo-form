962483facbd3bb04f11f9f0de8a38b6884918c84
Misha Hrynko <misha@mate.academy>
2023-07-20 10:48:29 +0300

    add React.memo

--- src/GoodList.tsx
+++ src/GoodList.tsx
@@ -1,19 +1,27 @@
+/* eslint-disable no-console */
+import React from 'react';
 import { Good } from './types';
 
 type Porps = {
   goods: Good[];
 };
 
-export const GoodList: React.FC<Porps> = ({ goods }) => (
-  <ul>
-    {goods.map(good => (
-      <li
-        key={good.id}
-        style={{ color: good.color?.name || 'black' }}
-      >
-        {good.name}
-        {good.id}
-      </li>
-    ))}
-  </ul>
-);
+const List: React.FC<Porps> = ({ goods }) => {
+  console.log('Render GoodList');
+
+  return (
+    <ul>
+      {goods.map(good => (
+        <li
+          key={good.id}
+          style={{ color: good.color?.name || 'black' }}
+        >
+          {good.name}
+          {good.id}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export const GoodList = React.memo(List);
