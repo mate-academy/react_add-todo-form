3332f13d7c01b0f68fda70d4b39b93bc333e751d
david.shamiiev <david.shamiiev@rad4m.com>
2023-07-21 17:44:27 +0300

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,27 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types';
+import './TodoInfo.scss';
+
+export const TodoInfo: React.FC<Todo> = ({
+  title,
+  id,
+  completed,
+  user,
+}) => (
+  <article
+    data-id={id}
+    className={completed
+      ? 'TodoInfo TodoInfo--completed'
+      : 'TodoInfo'}
+  >
+    <h2 className="TodoInfo__title">
+      {title}
+    </h2>
+
+    <UserInfo
+      name={user.name}
+      email={user.email}
+    />
+  </article>
+);
