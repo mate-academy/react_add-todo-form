82f22517a55039313b1a82016dcc2044e0e29327
Hishchak Tetiana <tanaiosered@ukr.net>
2023-07-29 11:07:34 +0300

    add-todo-form

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,17 @@
-export const TodoList = () => {};
+import { Todo } from '../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList : React.FC <Props> = ({ todos }) => (
+  <section>
+    {todos.map(todo => (
+      <TodoInfo
+        key={todo.id}
+        todo={todo}
+      />
+    ))}
+  </section>
+);
