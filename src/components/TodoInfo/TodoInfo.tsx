2d994885637aeca2c9f52fc6121061e5fe231d08
Denys <denchik9026@gmail.com>
2023-08-16 20:05:10 +0300

    implement todo form

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,35 @@
-export const TodoInfo = () => {};
+import './TodoInfo.scss';
+
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+import usersFromServer from '../../api/users';
+import { User } from '../../types/User';
+
+type Props = {
+  todo: Todo,
+};
+
+function getUserById(userId: number): User | undefined {
+  return usersFromServer
+    .find(user => user.id === userId);
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const user = getUserById(todo.userId);
+
+  return (
+    <article
+      data-id={todo.id}
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
