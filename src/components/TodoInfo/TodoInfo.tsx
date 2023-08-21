fde5e225fddddffcd429459dd8c4a7836c668b85
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-08-21 19:03:55 +0300

    trim variable added insead of trim function

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -6,19 +6,17 @@ type Props = {
   todo: Todo
 };
 
-export const TodoInfo: React.FC<Props> = ({ todo }) => {
-  return (
-    <article
-      data-id={todo.id}
-      className={classNames('TodoInfo', {
-        'TodoInfo--completed': todo.completed,
-      })}
-    >
-      <h2 className="TodoInfo__title">
-        {todo.title}
-      </h2>
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <article
+    data-id={todo.id}
+    className={classNames('TodoInfo', {
+      'TodoInfo--completed': todo.completed,
+    })}
+  >
+    <h2 className="TodoInfo__title">
+      {todo.title}
+    </h2>
 
-      {todo.user ? <UserInfo user={todo.user} /> : null}
-    </article>
-  );
-};
+    {todo.user ? <UserInfo user={todo.user} /> : null}
+  </article>
+);
