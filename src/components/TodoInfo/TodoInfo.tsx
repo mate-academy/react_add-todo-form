30f964c75987cb1ca2d49d5010a05217f7ab4f3d
Oleksandr Voronov <alexdroid33@gmail.com>
2023-08-23 19:27:01 +0300

    React Add TODO Form

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,34 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { Todo } from '../../type/todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo
+};
+
+export const TodoInfo:React.FC<Props> = ({ todo }) => {
+  const {
+    title,
+    user,
+    completed,
+    id,
+  } = todo;
+
+  return (
+    <article
+      data-id={id}
+      className={
+        classNames(
+          'TodoInfo',
+          { 'TodoInfo--completed': completed },
+        )
+      }
+    >
+
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+      { user ? <UserInfo user={user} /> : null}
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,14 @@
-export const UserInfo = () => {};
+import { User } from '../../type/user';
+
+type Props = {
+  user: User
+};
+export const UserInfo:React.FC<Props> = ({ user }) => {
+  const { email, name } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
