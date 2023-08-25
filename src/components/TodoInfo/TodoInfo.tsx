59bbaaea67d25e7dfd05067d0378fb103da70e88
Maksym Shevelin <shevmax01@gmail.com>
2023-08-25 15:16:29 +0300

    done but users is separate props

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,38 @@
-export const TodoInfo = () => {};
+import cn from 'classnames';
+import React from 'react';
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+
+type Props = {
+  todo: Todo,
+  users: User[]
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo, users }) => {
+  if (!users) {
+    return null;
+  }
+
+  const getUserById = (id: number) => (users
+    ? users.find(user => user.id === id)
+    : null
+  );
+
+  const user = getUserById(todo.userId);
+
+  return (
+    <article
+      data-id={todo.id}
+      className={cn('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,17 @@
-export const TodoList = () => {};
+import React from 'react';
+import { User } from '../../types/User';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todos: Todo[],
+  users: User[]
+};
+
+export const TodoList: React.FC<Props> = ({ todos, users }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo todo={todo} users={users} />
+    ))}
+  </section>
+);
