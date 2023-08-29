a681450fe88fcce77eb792bfaa4e0bae0d76fb02
Hania Nassalska <hanna.nassalska@gmail.com>
2023-08-29 21:35:41 +0200

    header removed

--- src/App.tsx
+++ src/App.tsx
@@ -116,7 +116,6 @@ export const App = () => {
         </button>
       </form>
       <div className="App">
-        <h1 className="App__title">Static list of todos</h1>
         <TodoList todos={todos()} />
       </div>
     </div>
