69ec17b04dd982ab6bd7999397cbabc1bd2ddd8b
Valentyn Bilyk <valentynbilyk22@gmail.com>
2023-09-09 21:19:28 +0300

    solution

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,111 @@
+import { useState } from 'react';
+import usersFromServer from '../../api/users';
+import { Todo } from '../../types/todo';
+import { getUserById } from '../../services/userId';
+
+type Props = {
+  onSubmit: (todo: Todo) => void;
+  todos: Todo[]
+};
+
+export const Form: React.FC<Props> = ({ onSubmit, todos }) => {
+  const [title, setTitle] = useState('');
+  const [userId, setUserId] = useState(0);
+  const [hasTitleError, setHasTitleError] = useState(false);
+  const [hasUserIdError, setHasUserIdError] = useState(false);
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    setHasTitleError(false);
+  };
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setUserId(+event.target.value);
+    setHasUserIdError(false);
+  };
+
+  const reset = () => {
+    setTitle('');
+    setUserId(0);
+  };
+
+  const newId = Math.max(...todos.map(todo => todo.id)) + 1;
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    setHasTitleError(!title);
+    setHasUserIdError(!userId);
+
+    if (!title || !userId) {
+      return;
+    }
+
+    onSubmit({
+      id: newId,
+      userId,
+      title,
+      completed: false,
+      user: getUserById(userId),
+    });
+
+    reset();
+  };
+
+  return (
+    <form
+      action="/api/todos"
+      method="POST"
+      onSubmit={handleSubmit}
+    >
+
+      <div className="field">
+        <label htmlFor="title">Title: </label>
+
+        <input
+          id="title"
+          type="text"
+          data-cy="titleInput"
+          placeholder="Enter a title"
+          value={title}
+          onChange={handleTitleChange}
+        />
+        {hasTitleError
+          && (
+            <span className="error">Please enter a title</span>
+          )}
+
+      </div>
+
+      <div className="field">
+        <label htmlFor="select-user">User: </label>
+
+        <select
+          id="select-user"
+          data-cy="userSelect"
+          value={userId}
+          onChange={handleSelectChange}
+        >
+          <option value="0" disabled>Choose a user</option>
+
+          {usersFromServer.map(user => (
+            <option
+              value={user.id}
+              key={user.id}
+            >
+              {user.name}
+            </option>
+          ))}
+        </select>
+
+        {hasUserIdError && (
+          <span className="error">Please choose a user</span>
+        )}
+      </div>
+
+      <button type="submit" data-cy="submitButton">
+        Add
+      </button>
+    </form>
+  );
+};
