6bdcbda1c40cd2b020e2255979810f65d21417b0
vanom <envinenvin@gmail.com>
2023-09-09 22:16:12 +0300

    implemented form

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,38 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { TodoWithUser } from '../../types';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: TodoWithUser,
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    id,
+    completed,
+    title,
+    user,
+  } = todo;
+
+  return (
+    <article
+      data-id={id}
+      className={classNames('TodoInfo',
+        {
+          'TodoInfo--completed': completed,
+        })}
+      key={id}
+    >
+      <h2
+        className="TodoInfo__title"
+        style={{
+          color: completed ? 'green' : 'red',
+        }}
+      >
+        {title}
+      </h2>
+
+      <UserInfo user={user} />
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,14 @@
-export const TodoList = () => {};
+import { TodoWithUser } from '../../types';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todos: TodoWithUser[],
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo todo={todo} />
+    ))}
+  </section>
+);
