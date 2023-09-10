f336a712bec896bda2c8203753dbf47fbd110dd2
Oleg <gtheplayer2@gmail.com>
2023-09-10 23:24:48 +0300

    React Add ToDo Form

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,17 @@
-export const TodoList = () => {};
+import React from 'react';
+import { TodoInfo } from '../TodoInfo';
+import { ToDo } from '../TSTypes/ToDo';
+
+type Props = {
+  todos: ToDo[]
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo todo={todo} key={todo.id} />
+      ))}
+    </section>
+  );
+};
