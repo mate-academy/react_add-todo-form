afda9f5a92d2e7d1573fe7031501562c7ac429a0
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2023-09-11 16:49:25 +0200

    task solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,23 @@
-export const TodoList = () => {};
+import React from 'react';
+import { TodoInfo } from '../TodoInfo';
+import { TodoWithUser } from '../Types/TodoWithUser';
+
+type TodosListProps = {
+  todos: TodoWithUser[],
+};
+
+export const TodoList : React.FC<TodosListProps> = (
+  { todos },
+) => {
+  return (
+    <section className="TodoList">
+      { todos.map(todo => (
+        <TodoInfo
+          key={todo.id}
+          todo={todo}
+        />
+      ))}
+
+    </section>
+  );
+};
