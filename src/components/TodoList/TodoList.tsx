1fe9a00d2eb1d8fdb866c3af7c0df0e21f783809
Dmytro Fefelov <reseneweb@gmail.com>
2023-09-12 18:31:43 +0300

    added TodoInfo component

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,5 @@
-import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
-import { UserInfo } from '../UserInfo';
+import { TodoInfo } from '../TodoInfo';
 
 type Props = {
   todos: Todo[];
@@ -10,17 +9,7 @@ export const TodoList: React.FC<Props> = ({ todos }) => {
   return (
     <section className="TodoList">
       {todos.map(todo => (
-        <article
-          key={todo.id}
-          data-id={todo.id}
-          className={classNames('TodoInfo', {
-            'TodoInfo--completed': todo.completed,
-          })}
-        >
-          <h2 className="TodoInfo__title">{todo.title}</h2>
-
-          <UserInfo user={todo.user} />
-        </article>
+        <TodoInfo todo={todo} />
       ))}
     </section>
   );
