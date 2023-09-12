1fb7c19dc3444c85ffe3ec4882d5add17b10cb70
Dmytro Fefelov <reseneweb@gmail.com>
2023-09-12 17:54:12 +0300

    Solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,27 @@
-export const TodoList = () => {};
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <article
+          key={todo.id}
+          data-id={todo.id}
+          className={classNames('TodoInfo', {
+            'TodoInfo--completed': todo.completed,
+          })}
+        >
+          <h2 className="TodoInfo__title">{todo.title}</h2>
+
+          <UserInfo user={todo.user} />
+        </article>
+      ))}
+    </section>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,23 @@
-export const UserInfo = () => {};
+import { User } from '../../types/User';
+
+type Props = {
+  user: User | null;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const name = user?.name;
+  const email = user?.email;
+
+  if (!name || !email) {
+    return null;
+  }
+
+  return (
+    <a
+      className="UserInfo"
+      href={`mailto:${email}`}
+    >
+      {name}
+    </a>
+  );
+};
