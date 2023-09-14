e67a2d22b280d72ca98fbb9048df2ccd9373e8a9
Nikita Orliak <nikitonorl@gmail.com>
2023-09-14 22:37:35 +0200

    Add task solution (all tests, but without ua + eng regex)

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,37 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../helpers/Todo';
+import { UserInfo } from '../UserInfo';
+
+interface Props {
+  todo: Todo,
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    id,
+    title,
+    user,
+    userId,
+    completed = false,
+  } = todo;
+
+  return (
+    <article
+      data-id={id}
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {
+        user && (
+          <UserInfo user={user} key={userId} />
+        )
+      }
+    </article>
+  );
+};
