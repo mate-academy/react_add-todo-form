49ef04f7ec656dfc21f52742d58ae9feeef84114
Anna Shevtsova <aniashevtsova@gmail.com>
2023-09-25 11:15:27 +0200

    all tests pass

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,35 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import usersFromServer from '../../api/users';
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
+
+export type TodoItemType = {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
+};
+
+type TodoInfoProps = {
+  todo: TodoItemType;
+};
+
+export const TodoInfo: React.FC<TodoInfoProps> = ({ todo }) => {
+  const user = usersFromServer.find((userItem) => userItem.id === todo.userId);
+
+  return (
+    <article data-id={todo.id} className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {user && (
+        <a className="UserInfo" href={`mailto:${user.email}`}>
+          {user.name}
+        </a>
+      )}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,16 @@
-export const TodoList = () => {};
+import React from 'react';
+import { TodoInfo, TodoItemType } from '../TodoInfo';
+
+type TodoListProps = {
+  todos: TodoItemType[];
+};
+
+export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map((todo) => (
+        <TodoInfo key={todo.id} todo={todo} />
+      ))}
+    </section>
+  );
+};
