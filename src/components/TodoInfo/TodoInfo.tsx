612cb567d46993730dad14d414c6ea571a597ac9
Leonid Krainik <rialleons@gmail.com>
2023-09-26 13:17:16 +0300

    Implement the ability to add TODOs

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,26 @@
-export const TodoInfo = () => {};
+import React from 'react';
+
+import './TodoInfo.scss';
+import { UserInfo } from '../UserInfo';
+import { TodoWithUser } from '../../types/TodoWithUser';
+
+type Props = {
+  todo: TodoWithUser,
+};
+
+export const TodoInfo: React.FC<Props> = ({
+  todo: {
+    id, completed, title, user,
+  },
+}) => (
+  <article
+    data-id={id}
+    className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}
+  >
+    <h2 className="TodoInfo__title">{title}</h2>
+
+    {!!user && (
+      <UserInfo user={user} />
+    )}
+  </article>
+);
