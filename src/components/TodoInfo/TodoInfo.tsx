4ce27b0045cd88c8ef4867cfd9219e00e79bca5f
Anton Bilous <toxanteloxran@gmail.com>
2023-09-30 15:03:55 +0300

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -6,16 +6,18 @@ export const TodoInfo: React.FC<{ todo: Todo }> = (
   { todo: { title, completed, user } },
 ) => {
   return (
-    <article className={classNames(
-      'TodoInfo',
-      { 'TodoInfo--completed': completed },
-    )}
-    >
-      <h2 className="TodoInfo__title">
-        {title}
-      </h2>
+    <>
+      <article className={classNames(
+        'TodoInfo',
+        { 'TodoInfo--completed': completed },
+      )}
+      >
+        <h2 className="TodoInfo__title">
+          {title}
+        </h2>
 
-      {user !== null ? <UserInfo user={user as User} /> : null}
-    </article>
+        {user !== null ? <UserInfo user={user as User} /> : null}
+      </article>
+    </>
   );
 };
