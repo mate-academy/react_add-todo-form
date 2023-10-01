32cd221222738b94508bfbaf6f614ee885058dc5
OlegRak86 <rakolegit@gmail.com>
2023-10-01 15:02:09 +0300

    react_add-todo-form

---
+++ src/HelpFunction/getUserById.tsx
@@ -0,0 +1,6 @@
+import usersFromServer from '../api/users';
+
+export const getUserById = (userId:number) => {
+  return usersFromServer.find(user => user.id === userId)
+      || null;
+};
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,19 @@
-export const TodoInfo = () => {};
+import { Todo } from '../../Types/Todo';
+
+type Props = {
+  todo: Todo;
+};
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  return (
+    <article data-id={todo.id} className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+
+      <a className="UserInfo" href="mailto:Sincere@april.biz">
+        {todo.user
+        && todo.user.name}
+      </a>
+    </article>
+  );
+};
