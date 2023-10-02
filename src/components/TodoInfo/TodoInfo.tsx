9c58e0ab89e3d7f0896032e77c31cc059b1823c8
Yuliia Dub <yuliia.kasaraba@gmail.com>
2023-10-02 12:15:25 +0300

    add todo form

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,26 @@
-export const TodoInfo = () => {};
+import React from 'react';
+// import { Todo } from '../types/todo';
+import { UserInfo } from '../UserInfo';
+import { User } from '../types/user';
+
+type Props = {
+  title: string;
+  completed: boolean;
+  user: User | null;
+};
+
+export const TodoInfo: React.FC<Props> = ({
+  title,
+  completed = false,
+  user,
+}) => {
+  return (
+    <article data-id="1" className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}>
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,20 @@
-export const TodoList = () => {};
+import React from 'react';
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../types/todo';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo
+        key={todo.id}
+        user={todo.user}
+        completed={todo.completed}
+        title={todo.title}
+      />
+    ))}
+  </section>
+);
