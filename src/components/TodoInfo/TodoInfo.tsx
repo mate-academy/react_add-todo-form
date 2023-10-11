4e8c57c9a0e2516f15dea6498484b5554f14edb4
Oleg Tarasov <tarasov.oleg.9779@gmail.com>
2023-10-11 16:20:18 +0300

    add todo form v1

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,18 @@
-export const TodoInfo = () => {};
+interface TodoInfoProps {
+  post: Post;
+}
+
+interface Post {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+}
+
+export const TodoInfo: React.FC<TodoInfoProps> = ({ post }) => {
+  return (
+    <h2 className="TodoInfo__title">
+      {post.title}
+    </h2>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,17 @@
-export const UserInfo = () => {};
+import React from 'react';
+
+interface UserInfoProps {
+  userName: string;
+  // userEmail: string;
+  userLink: string;
+}
+
+export const UserInfo: React.FC<UserInfoProps> = (
+  { userName, userLink },
+) => {
+  return (
+    <a className="UserInfo" href={userLink}>
+      {userName}
+    </a>
+  );
+};
