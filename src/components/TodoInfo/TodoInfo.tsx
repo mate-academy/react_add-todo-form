15f02fcf13c08d6cfb4eebbeb0f39632c80ef570
Ivanna Zaiets <iva.zaiets@gmail.com>
2023-10-23 17:04:39 +0300

    solution react_add_todo_form task

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,26 @@
-export const TodoInfo = () => {};
+import cn from 'classnames';
+import { Todos } from '../../types/todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todos,
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  return (
+    <>
+      <article
+        data-id={todo.id}
+        className={cn('TodoInfo', {
+          'TodoInfo--completed': todo.completed,
+        })}
+      >
+        <h2 className="TodoInfo__title">
+          {todo.title}
+        </h2>
+
+        <UserInfo user={todo.user} />
+      </article>
+    </>
+  );
+};
