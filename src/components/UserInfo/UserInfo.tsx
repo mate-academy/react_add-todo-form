088cdc494ed78a32275ef3db50ad7ebed2eae101
Leonid <aleksandrovi4.l.p@gmail.com>
2023-10-24 03:13:09 +0300

    To do form

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,12 @@
-export const UserInfo = () => {};
+import { User } from '../../Types/User';
+
+interface Props {
+  user: User
+}
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
+};
