151da352a874565624be82659b81711118a6e055
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-10-25 13:02:15 +0300

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
 import './App.scss';
 import { useState } from 'react';
-import todosFromServer from './api/todos';
+import todos from './api/todos';
 import { TodoList } from './components/TodoList';
 import { Todo } from './interface/Todo';
 import { Form } from './components/Form/Form';
 import { getUserById } from './utils/usersId';
 
-export const initialTodos: Todo[] = todosFromServer.map((todo) => ({
+export const initialTodos: Todo[] = todos.map((todo) => ({
   ...todo,
   user: getUserById(todo.userId),
 }));
 
-function getNewTodoId(todos: Todo[]) {
-  const maxId = Math.max(...todos.map((todo) => todo.id));
+function getNewTodoId(newTodos: Todo[]) {
+  const maxId = Math.max(...newTodos.map((todo) => todo.id));
 
   return maxId + 1;
 }
 
 export const App = () => {
-  const [todos, setTodos] = useState<Todo[]>(initialTodos);
+  const [newTodos, setTodos] = useState<Todo[]>(initialTodos);
 
   const addTodo = (todo: Todo) => {
     const newTodo = {
       ...todo,
-      id: getNewTodoId(todos),
+      id: getNewTodoId(newTodos),
     };
 
     setTodos((currentTodos) => [...currentTodos, newTodo]);
@@ -34,7 +34,7 @@ export const App = () => {
       <h1>Add todo form</h1>
       <Form onSubmit={addTodo} />
 
-      <TodoList todos={todos} />
+      <TodoList todos={newTodos} />
     </div>
   );
 };
