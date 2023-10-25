344c97138d7901ee794decc7abf36f4611be9fb9
Nazar <bidenkonazar@gmail.com>
2023-10-26 02:00:36 +0300

    after check without separate file for form

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -8,18 +8,20 @@ interface Props {
   todo: Todo,
 }
 
-export const TodoInfo: React.FC<Props> = ({ todo }) => (
-  <article
-    data-id={todo.id}
-    className={cn('TodoInfo', {
-      'TodoInfo--completed': todo.completed,
-    })}
-  >
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const isCompleted = todo.completed && 'TodoInfo--completed';
 
-    <h2 className="TodoInfo__title">
-      {todo.title}
-    </h2>
+  return (
+    <article
+      data-id={todo.id}
+      className={cn('TodoInfo', isCompleted)}
+    >
 
-    {todo.user && <UserInfo user={todo.user} />}
-  </article>
-);
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+
+      {todo.user && <UserInfo user={todo.user} />}
+    </article>
+  );
+};
