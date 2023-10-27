add2dc3779cfad414c1a5a6bc93bafb6a4530c6d
Hryhorii Starykov <hr.starykov@gmail.com>
2023-10-27 03:51:58 +0300

    Fixed data-id={todo.id} attribute

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -8,7 +8,10 @@ type Props = {
 };
 // Add the required props
 export const TodoInfo:React.FC<Props> = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
+  <article
+    className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    data-id={todo.id}
+  >
     <h2 className="TodoInfo__title">{todo.title}</h2>
     <UserInfo user={todo.user} />
   </article>
