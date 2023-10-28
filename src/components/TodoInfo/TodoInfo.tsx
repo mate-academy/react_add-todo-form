952f64b589910cc7144fcb6f6fb06ec78113329c
Hryhorii Starykov <hr.starykov@gmail.com>
2023-10-28 09:28:24 +0300

    Fixed after mentor comments

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -7,12 +7,14 @@ type Props = {
   todo: Todo;
 };
 // Add the required props
-export const TodoInfo:React.FC<Props> = ({ todo }) => (
-  <article
-    className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
-    data-id={todo.id}
-  >
-    <h2 className="TodoInfo__title">{todo.title}</h2>
-    <UserInfo user={todo.user} />
-  </article>
-);
+export const TodoInfo:React.FC<Props> = ({ todo }) => {
+  return (
+    <article
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+      data-id={todo.id}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      <UserInfo user={todo.user} />
+    </article>
+  );
+};
