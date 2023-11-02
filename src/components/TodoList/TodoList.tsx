0472175c903285c63858b24200c4a80691bd22c6
andrew gidrovich <mr.androvich55555@gmail.com>
2023-11-02 19:35:51 +0200

    add task solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,17 @@
-export const TodoList = () => {};
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types';
+
+type TodoListProps = {
+  todos: Todo[];
+};
+
+export const TodoList = ({ todos }: TodoListProps) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo
+        key={todo.id}
+        todo={todo}
+      />
+    ))}
+  </section>
+);
