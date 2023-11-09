30332108020c1f47c6e0da18dfc7f84e6270754b
Yaroslav Liutyi <slavko5481@gmail.com>
2023-11-09 19:35:02 +0200

    error handlers were updated

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,8 @@ export const App = () => {
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
 
-    setHasTitleError(true);
-    setHasUserIdError(true);
+    setHasTitleError(!title);
+    setHasUserIdError(!userId);
 
     if (!title || !userId) {
       // eslint-disable-next-line no-useless-return
