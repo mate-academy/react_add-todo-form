94e1c274568483a1f2c26520037bfcbcd05798a8
Wita-Shchurko <vitaliya.mememe@gmail.com>
2023-11-09 18:05:15 +0200

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,37 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo';
+import usersFromServer from '../../api/users';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo,
+};
+
+export const TodoInfo: React.FC<Props> = ({
+  todo,
+}) => {
+  const currentUser
+  = usersFromServer.find(user => user.id === todo.userId);
+
+  return (
+    <article
+      key={todo.id}
+      data-id={String(todo.id)}
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+
+      {currentUser && (
+        <UserInfo
+          name={currentUser.name}
+          email={currentUser.email}
+        />
+      )}
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,15 @@
-export const UserInfo = () => {};
+import React from 'react';
+
+type Props = {
+  name: string,
+  email: string,
+};
+
+export const UserInfo: React.FC<Props> = ({
+  name,
+  email,
+}) => (
+  <a className="UserInfo" href={`mailto:${email}`}>
+    {name}
+  </a>
+);
