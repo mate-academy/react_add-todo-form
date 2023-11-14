5a5c1736208349520d90fe7d02b9c2828cbd668a
Yulia <lutsenkoyulia2004@gmail.com>
2023-11-14 10:31:50 +0200

    fixed userInfo

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { User } from '../../types/User';
 
 type Props = {
-  user:User | null;
+  user: User | null;
 };
 
-export const UserInfo:React.FC<Props> = ({ user }) => {
+export const UserInfo: React.FC<Props> = ({ user }) => {
   return (
-    <a className="UserInfo" href={`mailto:${user?.email}`}>
-      {user?.name}
-    </a>
+    user && (
+      <a className="UserInfo" href={`mailto:${user.email}`}>
+        {user.name}
+      </a>
+    )
   );
 };
