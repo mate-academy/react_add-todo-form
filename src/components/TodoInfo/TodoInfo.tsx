f8e18d844b62a8951d95f9f3afc5795936ad8a9d
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-11-17 01:51:59 +0200

    react todo adder was realised

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,36 @@
-export const TodoInfo = () => {};
+import cn from 'classnames';
+import User from '../../types/User';
+import { UserInfo } from '../UserInfo';
+import Todo from '../../types/Todo';
+import usersFromServer from '../../api/users';
+
+interface Props {
+  todo: Todo
+}
+
+export const TodoInfo: React.FC<Props> = ({
+  todo,
+}) => {
+  const {
+    id, title, completed, userId,
+  } = todo;
+
+  const user: User
+    = usersFromServer.find((us: User) => us.id === userId)
+    ?? usersFromServer[0];
+
+  return (
+    <article
+      data-id={id}
+      className={cn('TodoInfo', {
+        'TodoInfo--completed': completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      <UserInfo user={user} />
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,22 @@
-export const TodoList = () => {};
+import Todo from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+interface Props {
+  todos: Todo[],
+}
+
+export const TodoList: React.FC<Props> = ({
+  todos,
+}) => {
+  return (
+    <section className="TodoList">
+      {todos.map((todo: Todo) => {
+        return (
+          <TodoInfo
+            todo={todo}
+          />
+        );
+      })}
+    </section>
+  );
+};
