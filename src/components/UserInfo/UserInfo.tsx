07e6d4bcfdcefa752b3a9a378df073d31c062dfb
Nazar Kolomiets <lexnazar91@gmail.com>
2023-11-22 13:32:52 +0300

    todo-form

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,21 @@
-export const UserInfo = () => {};
+import React from 'react';
+import { User } from '../../type/user';
+
+interface Props {
+  user: User,
+}
+export const UserInfo: React.FC<Props> = ({
+  user: {
+    email,
+    name,
+  },
+}) => {
+  return (
+    <a
+      className="UserInfo"
+      href={`mailto:${email}`}
+    >
+      {name}
+    </a>
+  );
+};
