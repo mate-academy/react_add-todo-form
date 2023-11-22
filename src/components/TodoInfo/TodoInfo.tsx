a65f17c4fe70e89f4bcddccdb088444dc542813b
Vitaliy <110530047+Vitalii-Maistrenko@users.noreply.github.com>
2023-11-22 16:58:03 +0200

    todoForm

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,33 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../type/todo';
+
+interface Props {
+  todo: Todo,
+}
+
+export const TodoInfo: React.FC<Props> = ({
+  todo: {
+    id,
+    title,
+    completed = false,
+    user,
+  },
+}) => {
+  return (
+    <article
+      data-id={id}
+      className={classNames(
+        'TodoInfo', { 'TodoInfo--completed': completed },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && <UserInfo user={user} />}
+
+    </article>
+  );
+};
