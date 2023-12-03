71c627a127d3697be3c6f1f9e743649c1341afdf
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-12-03 14:20:52 +0100

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,32 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { Todo } from '../../types/todo';
+import { UserInfo } from '../UserInfo';
+
+export type TodoInfoProps = {
+  todo: Todo
+};
+
+export const TodoInfo = ({ todo }: TodoInfoProps) => {
+  const {
+    id, title, completed, user,
+  } = todo;
+
+  return (
+    <article
+      data-id={id}
+      className={classNames(
+        'TodoInfo', completed && 'TodoInfo--completed',
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && (
+        <UserInfo user={user} />
+      )}
+    </article>
+  );
+};
+
+// "TodoInfo TodoInfo--completed"
