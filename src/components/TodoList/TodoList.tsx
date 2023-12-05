b02b97a8b3a54594b849ae3c2947d06985689406
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-05 16:34:34 +0200

    Revert "Todo form"
    This reverts commit d8963d09e3366cdc9d848f649db1c0ce2018ad6d.

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,16 +1,1 @@
-import { Todo } from '../types/Todo';
-import { TodoInfo } from '../TodoInfo';
-
-type Props = {
-  todos: Todo[];
-};
-
-export const TodoList: React.FC<Props> = ({ todos }) => {
-  return (
-    <section className="TodoList">
-      {todos.map(todo => (
-        <TodoInfo key={todo.id} todo={todo} />
-      ))}
-    </section>
-  );
-};
+export const TodoList = () => {};
