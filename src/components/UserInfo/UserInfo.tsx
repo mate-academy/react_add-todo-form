41f01dabb89bdce9d9ecabdde1133c526ffc11a3
Yuliia Babych <jl.babych29@gmail.com>
2023-12-06 19:27:30 +0200

    add task solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,22 @@
-export const UserInfo = () => {};
+import React from 'react';
+
+type User = {
+  email: string,
+  name: string;
+};
+
+type Props = {
+  user: User | null;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
+};
