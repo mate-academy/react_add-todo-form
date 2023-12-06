d33b9850bf37314213bd36d5b1bae28cea69d42b
Fluebubble <Trots.anatoly@gmail.com>
2023-12-06 14:13:04 +0200

    added fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,17 @@ import { generateTodoId, getUserById } from './helpers/helpers';
 
 const todosWithUsers = todosFromServer.map(todo => ({
   ...todo,
-  user: getUserById(usersFromServer, todo.id),
+  user: getUserById(usersFromServer, todo.userId),
 }));
 
 export const App = () => {
   const [todos, setTodos] = useState(todosWithUsers);
-  const [users] = useState(usersFromServer);
   const [todoTitle, setTodoTitle] = useState('');
   const [selectedUserId, setSelectedUserId] = useState<number | null>(null);
   const [isErrorShown, setisErrorShown] = useState({
     select: false,
     title: false,
   });
-  // const [isTitleEnteredError, setIsTitleEnteredError] = useState(false);
 
   const onTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const titleValue = event.target.value;
@@ -62,7 +60,7 @@ export const App = () => {
     setisErrorShown(prevState => ({ ...prevState, select: false }));
   };
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     if (!selectedUserId && !todoTitle.trim()) {
@@ -97,7 +95,7 @@ export const App = () => {
       title: todoTitle.trim(),
       completed: false,
       userId: selectedUserId,
-      user: getUserById(users, selectedUserId),
+      user: getUserById(usersFromServer, selectedUserId),
     };
 
     setTodos(prevTodos => [
@@ -116,7 +114,7 @@ export const App = () => {
       <form
         action="/api/todos"
         method="POST"
-        onSubmit={onSubmit}
+        onSubmit={handleFormSubmit}
       >
         <div className="field">
           <label>
@@ -142,13 +140,18 @@ export const App = () => {
             User:
             <select
               data-cy="userSelect"
-              onChange={e => onUserSelect(e)}
+              onChange={onUserSelect}
               value={selectedUserId ?? '0'}
               onBlur={onBlurUserSelect}
             >
               <option value="0" disabled>Choose a user</option>
-              {users.map(user => (
-                <option value={user.id}>{user.name}</option>
+              {usersFromServer.map(user => (
+                <option
+                  value={user.id}
+                  key={user.id}
+                >
+                  {user.name}
+                </option>
               ))}
             </select>
           </label>
--- src/api/todos.ts
+++ src/api/todos.ts
@@ -17,22 +17,4 @@ export default [
     completed: false,
     userId: 4,
   },
-  // {
-  //   id: 8,
-  //   title: 'extrawaganza',
-  //   completed: true,
-  //   userId: 4,
-  // },
-  // {
-  //   id: 11,
-  //   title: 'Headshotnul babool`koo',
-  //   completed: true,
-  //   userId: 6,
-  // },
-  // {
-  //   id: 18,
-  //   title: 'loremIpsumDolorSitAmet',
-  //   completed: true,
-  //   userId: 122,
-  // },
 ];
