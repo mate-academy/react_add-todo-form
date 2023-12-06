6f904e971b4ecb8e755bf1f666febfbebe6d233a
Andrii Rykal <andreuka79@gmail.com>
2023-12-06 17:17:17 +0200

    Solution fail tests

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,116 @@
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import { User } from '../../types/User';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  users: User[];
+  todos: Todo[];
+  setTodos: Dispatch<SetStateAction<Todo[]>>;
+};
+
+export const Form: React.FC<Props> = ({
+  users,
+  todos,
+  setTodos,
+}) => {
+  const reg = /^[a-zа-яіїґє'0-9,.\s]+$/i;
+  const [title, setTitle] = useState('');
+  const [userId, setUserId] = useState(0);
+  const [titleError, setTitleError] = useState(false);
+  const [userError, setUserError] = useState(false);
+
+  const todoId = [...todos].sort((a, b) => b.id - a.id)[0].id + 1;
+  const newTodo = {
+    id: todoId,
+    title: title.trim(),
+    completed: false,
+    userId,
+  };
+
+  const hendleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (reg.test(e.target.value) || !e.target.value) {
+      reg.lastIndex = 0;
+      setTitle(e.target.value);
+    }
+
+    setTitleError(false);
+  };
+
+  const hendleUserIdChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setUserId(+e.target.value);
+    setUserError(false);
+  };
+
+  const hendleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!title.trim()) {
+      setTitleError(true);
+    }
+
+    if (!userId) {
+      setUserError(true);
+    }
+
+    if (title.trim() && userId) {
+      setTodos(currentTodos => ([
+        ...currentTodos,
+        newTodo,
+      ]));
+
+      setTitle('');
+      setUserId(0);
+    }
+  };
+
+  return (
+    <form
+      action="/api/todos"
+      method="POST"
+      onSubmit={hendleSubmit}
+    >
+      <div className="field">
+        <label>
+          Title:
+          <input
+            type="text"
+            data-cy="titleInput"
+            placeholder="Enter a title"
+            value={title}
+            onChange={hendleTitleChange}
+          />
+        </label>
+
+        {titleError && <span className="error">Please enter a title</span>}
+      </div>
+
+      <div className="field">
+        <label>
+          User:
+          <select
+            data-cy="userSelect"
+            value={userId}
+            onChange={hendleUserIdChange}
+
+          >
+            <option value="0" disabled>Choose a user</option>
+            {users.map((user) => (
+              <option
+                value={user.id}
+                key={user.id}
+              >
+                {user.name}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        {userError && <span className="error">Please choose a user</span>}
+      </div>
+
+      <button type="submit" data-cy="submitButton">
+        Add
+      </button>
+    </form>
+  );
+};
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,30 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import cn from 'classnames';
+import './TodoInfo.scss';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo;
+  users: User[];
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo, users }) => {
+  const currentUser = users.find(user => user.id === todo.userId) as User;
+
+  return (
+    <article
+      data-id={todo.id}
+      className={cn('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+
+      <UserInfo user={currentUser} />
+    </article>
+  );
+};
