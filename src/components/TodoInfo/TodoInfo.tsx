8b1357663b0ea81a82d8e867f9ac3848c925624d
Mirosław Wandyk <barosz1111@gmail.com>
2023-12-06 10:45:22 +0100

    imported my todos implementation

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,22 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  let className = 'TodoInfo';
+
+  if (todo.completed) {
+    className += ' TodoInfo--completed';
+  }
+
+  return (
+    <article className={className}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {todo.user && <UserInfo user={todo.user} />}
+    </article>
+  );
+};
