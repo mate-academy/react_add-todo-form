51b0b7098693629a536754e956c9ac5bda9dd05d
Oleksandr Kosenko <d1izel@ukr.net>
2023-12-09 19:56:20 +0200

    addreact_add-todo-form

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,41 @@
-export const TodoInfo = () => {};
+import cn from 'classnames';
+import { Todo } from '../../types/Todo';
+import usersFromServer from '../../api/users';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({
+  todo,
+}) => {
+  const {
+    id,
+    title,
+    completed,
+    userId,
+  } = todo;
+
+  const findUser = usersFromServer
+    .find(user => user.id === userId) || null;
+
+  return (
+    <article
+      data-id={id}
+      className={cn('TodoInfo', {
+        'TodoInfo--completed': completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {findUser && (
+        <UserInfo
+          user={findUser}
+        />
+      )}
+    </article>
+  );
+};
