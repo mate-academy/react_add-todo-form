a1aa7c10108262b5f0d6012e4d8506b3733c3e73
Natalia Netzel <140259327+netzelnatalia@users.noreply.github.com>
2023-12-14 16:09:15 +0100

    Update types.tsx

--- src/types.tsx
+++ src/types.tsx
@@ -1,17 +1,26 @@
-import { User } from './user';
-
-export interface Todo {
-  id: number,
-  title: string,
-  completed: boolean,
-  userId: number,
-  user? : User,
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
 }
 
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+  user?: User;
+}
 
-export interface User {
-  id: number,
-  name: string,
-  username: string,
-  email: string,
+export interface TodoListProps {
+  todos: Todo[];
+}
+
+export interface TodoInfoProps {
+  todo: Todo;
+}
+
+export interface UserInfoProps {
+  user: User;
 }
