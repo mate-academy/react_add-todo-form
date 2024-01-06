06d20ad481a375b4e1a5e6e53bf1a9564cfc3832
boytrend0108 <boytrend@gmail.com>
2024-01-06 19:12:11 +0200

    fix errors

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -28,7 +28,10 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => {
         {title}
       </h2>
 
-      <UserInfo user={user} />
+      {user && (
+        <UserInfo user={user} />
+      )}
+
     </article>
   );
 };
