cc6b1438717dc11309edb501ce134162d37e00fd
Serhii Gramma <grammas56@gmail.com>
2024-01-07 13:44:00 +0200

    Solution_1.0

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,13 @@
-export const UserInfo = () => {};
+import usersFromServer from '../../api/users';
+
+interface Props {
+  userId: number
+}
+
+export const UserInfo:React.FC<Props> = ({ userId }) => {
+  return (
+    <a className="UserInfo" href="mailto:Sincere@april.biz">
+      {usersFromServer.find(user => user.id === userId)?.name}
+    </a>
+  );
+};
