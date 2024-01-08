8cbf894b10f8167c63c2ee657ddf2fd967673be9
Heorhii Churhuliia <gio4929189@gmail.com>
2024-01-08 19:36:19 +0000

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Render } from './rendering/Render';
 import { Todo } from './types/Todo';
 import todosFromServer from './api/todos';
 import { getUserById } from './services/user';
+import { TodoList } from './components/TodoList';
 
 const todos = todosFromServer.map(el => {
   return {
@@ -27,35 +28,8 @@ export const App = () => {
           todoList={list}
           onSubmit={addList}
         />
-        <section className="TodoList">
-          <article data-id="1" className="TodoInfo TodoInfo--completed">
-            <h2 className="TodoInfo__title">
-              delectus aut autem
-            </h2>
 
-            <a className="UserInfo" href="mailto:Sincere@april.biz">
-              Leanne Graham
-            </a>
-          </article>
-
-          <article data-id="15" className="TodoInfo TodoInfo--completed">
-            <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-            <a className="UserInfo" href="mailto:Sincere@april.biz">
-              Leanne Graham
-            </a>
-          </article>
-
-          <article data-id="2" className="TodoInfo">
-            <h2 className="TodoInfo__title">
-              quis ut nam facilis et officia qui
-            </h2>
-
-            <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-              Patricia Lebsack
-            </a>
-          </article>
-        </section>
+        <TodoList todos={list} />
       </div>
     </div>
   );
