cd05d024882081d96cb2f80a19c76551c363c359
foxstudent103376 <yashopua@gmail.com>
2024-01-08 19:47:48 +0200

    first beta for tests

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,28 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import getUserById from '../../services/userServeces';
+
+type Props = {
+  todo: Todo,
+};
+
+export const TodoInfo:React.FC<Props> = ({ todo }) => {
+  const user = getUserById(todo.userId);
+
+  return (
+    <article
+      data-id={todo.id}
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+      <a className="UserInfo" href={`mailto:${user?.email}`}>
+        {user?.name}
+      </a>
+    </article>
+  );
+};
---
+++ src/services/todosServices.ts
@@ -0,0 +1,5 @@
+import { Todo } from '../types/Todo';
+
+export default function getMaxTodosId(todos:Todo[]): number {
+  return Math.max(...todos.map(todo => todo.id));
+}
