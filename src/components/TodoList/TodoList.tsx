f71d9876bfe25a1bbea70109e95d0ef7d620d8b4
Volodymyr Konoplianyi <volodymyr.konoplianyi@gmail.com>
2024-01-13 20:20:17 +0200

    TodoInfo key added

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -9,7 +9,7 @@ export const TodoList: React.FC<Props> = ({ todos }) => {
   return (
     <section className="TodoList">
       {todos.map(todo => (
-        <TodoInfo todo={todo} />
+        <TodoInfo todo={todo} key={todo.id} />
       ))}
     </section>
   );
