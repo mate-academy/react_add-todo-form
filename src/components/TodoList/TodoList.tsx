7e07786b108803d376072eb0bdcb7bbf9f032639
Ruslan Voshchylo <ruslan.voshchylo2@gmail.com>
2024-01-16 15:12:34 +0200

    task solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,19 @@
-export const TodoList = () => {};
+import { TodoWithUser } from '../../types/TodoWithUser';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todos: TodoWithUser[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => {
+      return (
+        <TodoInfo
+          key={todo.id}
+          todo={todo}
+        />
+      );
+    })}
+  </section>
+);
