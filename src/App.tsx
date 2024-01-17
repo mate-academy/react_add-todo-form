224efd4531fd5e22df18c167727dc2a7a9c554fe
Dmytro Manankin <dmitry.manankin@gmail.com>
2024-01-17 17:24:45 +0200

    task done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,145 @@
+// #region IMPORTS
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import './App.scss';
+import { TodoList } from './components/TodoList';
+import { User } from './types/User';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+// #endregion
+
+function getUserById(id: number): User {
+  return usersFromServer.find(user => user.id === id) as User;
+}
+
+const prepareList = todosFromServer.map(todo => {
+  return {
+    ...todo,
+    user: getUserById(todo.userId),
+  };
+});
 
 export const App = () => {
+  const [preparedList, setPreparedList] = useState(prepareList);
+  const [title, setTitle] = useState('');
+  const [userId, setUserId] = useState(0);
+  const [touched, setTouched] = useState(false);
+  const [isUserSelected, setIsUserSelected] = useState(false);
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  const titleError = !title && touched;
+
+  const userError = userId === 0 && isUserSelected;
+
+  function getNewTodoId() {
+    const currentMax = Math.max(...preparedList.map(todo => todo.id));
+
+    return currentMax + 1;
+  }
+
+  function handleSubmitNewTodo(event: React.FormEvent) {
+    event.preventDefault();
+
+    if (!title.trim()) {
+      setTitle('');
+      setTouched(true);
+    }
+
+    if (userId === 0) {
+      setIsUserSelected(true);
+    }
+
+    if (!title.trim() || userId === 0) {
+      return;
+    }
+
+    if (isUserSelected && title.trim()) {
+      setPreparedList([
+        ...preparedList,
+        {
+          id: getNewTodoId(),
+          title,
+          completed: isCompleted,
+          userId,
+          user: getUserById(userId),
+        }]);
+      setTitle('');
+      setUserId(0);
+      setIsCompleted(false);
+      setTouched(false);
+      setIsUserSelected(false);
+    }
+  }
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/todos" method="POST">
+      <form
+        action="/api/todos"
+        method="POST"
+        onSubmit={handleSubmitNewTodo}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="">
+            Write your Todo task in the field
+            <input
+              type="text"
+              data-cy="titleInput"
+              className={classNames('input', {
+                'is-danger': titleError,
+              })}
+              placeholder="type here"
+              value={title}
+              onChange={event => setTitle(event.target.value)}
+              onBlur={() => setTouched(true)}
+            // required
+            />
+            {titleError && (
+              <span className="error">
+                {/* eslint-disable-next-line @typescript-eslint/quotes */}
+                {` - type your title, please`}
+              </span>
+            )}
+          </label>
         </div>
 
+        <br />
+
         <div className="field">
-          <select data-cy="userSelect">
+          <label htmlFor="user-id"> Select user: </label>
+          <select
+            id="user-id"
+            data-cy="userSelect"
+            defaultValue={userId}
+            required
+            onChange={(event) => {
+              setUserId(+event.target.value);
+              setIsUserSelected(true);
+            }}
+          >
             <option value="0" disabled>Choose a user</option>
+            {usersFromServer.map(user => (
+              <option value={user.id} key={user.id}>{user.name}</option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {userError && (
+            <span className="error">Please choose a user</span>
+          )}
+        </div>
+
+        <br />
+
+        <div className="field">
+          <label htmlFor="">
+            Is your task completed?
+            <input
+              type="checkbox"
+              onChange={() => setIsCompleted(!isCompleted)}
+            />
+          </label>
         </div>
 
         <button type="submit" data-cy="submitButton">
@@ -27,35 +147,10 @@ export const App = () => {
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
+      <hr />
 
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
+      <TodoList todos={preparedList} />
 
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
     </div>
   );
 };
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,24 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+interface Props {
+  todo: Todo;
+  key: number;
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <article
+    data-id="1"
+    className={classNames(
+      'TodoInfo',
+      { 'TodoInfo--completed': todo.completed },
+    )}
+  >
+    <h2 className="TodoInfo__title">
+      {todo.title}
+    </h2>
+
+    <UserInfo user={todo.user} />
+  </article>
+);
