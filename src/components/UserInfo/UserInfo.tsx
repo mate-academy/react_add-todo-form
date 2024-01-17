4109b5d514ef23f9d986b0b7085b8d23870d66a8
Oleksandr Boiko <iccc97@gmail.com>
2024-01-17 23:25:40 +0200

    add todo-form solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,17 @@
-export const UserInfo = () => {};
+import React from 'react';
+import { User } from '../../types/User';
+
+interface Props {
+  user: User,
+}
+
+export const UserInfo: React.FC<Props> = ({
+  user: {
+    email,
+    name,
+  },
+}) => (
+  <a className="UserInfo" href={`mailto:${email}`}>
+    {name}
+  </a>
+);
