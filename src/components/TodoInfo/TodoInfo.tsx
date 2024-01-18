cdcbb12688fe59dfb617b998fba8e06609df5f46
Yura Bublyk <yurabublik703@gmail.com>
2024-01-18 14:22:39 +0200

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,31 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+// import { UserInfo } from '../UserInfo';
+import React from 'react';
+import { TodowithUser } from '../types';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: TodowithUser;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    id,
+    completed = false,
+    title,
+    user,
+  } = todo;
+
+  return (
+    <article
+      data-id={id}
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      <UserInfo user={user} />
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,23 @@
-export const TodoList = () => {};
+import React from 'react';
+import { TodowithUser } from '../types';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todos: TodowithUser[];
+  // users: User[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map((todo) => (
+        <TodoInfo
+          key={todo.id}
+          todo={todo}
+          // user={todo}
+        />
+      ))}
+
+    </section>
+  );
+};
