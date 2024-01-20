8294238a6bd77b72fdadcb73bedfff98e1924949
Inna Sheremet <innsheremet88@gmail.com>
2024-01-20 18:46:15 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,102 @@
+import React, { useState } from 'react';
 import './App.scss';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { TodoInfo } from './components/TodoInfo';
+import { UserInfo } from './components/UserInfo';
+import { TodoList } from './components/TodoList';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+export const App: React.FC = () => {
+  const [title, setTitle] = useState('');
+  const [hasTitleError, setHasTitleError] = useState(false);
 
-export const App = () => {
+  const [userId, setUserId] = useState(0);
+  const [hasUserIdError, setHasUserIdError] = useState(false);
+
+  const [addTodos, setAddTodos] = useState(todosFromServer);
+
+  const handleTitleChange = (
+    setter: React.Dispatch<React.SetStateAction<string>>,
+    value: string,
+  ) => {
+    setter(value);
+    setHasTitleError(false);
+  };
+
+  const handleUserIdChange = (
+    setter: React.Dispatch<React.SetStateAction<number>>,
+    value: number,
+  ) => {
+    setter(value);
+    setHasUserIdError(false);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!title) {
+      setHasTitleError(true);
+    } else {
+      setHasTitleError(false);
+    }
+
+    if (!userId) {
+      setHasUserIdError(true);
+    } else {
+      setHasUserIdError(false);
+    }
+
+    if (!title || !userId) {
+      return;
+    }
+
+    const newId = Math.max(...addTodos.map(todo => todo.id), 0) + 1;
+
+    const newTodo = {
+      id: newId,
+      title,
+      completed: false,
+      userId,
+    };
+
+    setAddTodos((prevTodos) => [...prevTodos, newTodo]);
+    setTitle('');
+    setUserId(0);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/todos" method="POST">
-        <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
-        </div>
+      <form
+        action="/api/todos"
+        method="POST"
+        onSubmit={handleSubmit}
+      >
+        <TodoInfo
+          value={title}
+          label="Title: "
+          onChange={(value) => handleTitleChange(setTitle, value)}
+          hasError={hasTitleError}
+        />
 
-        <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>Choose a user</option>
-          </select>
-
-          <span className="error">Please choose a user</span>
-        </div>
+        <UserInfo
+          value={userId}
+          label="User: "
+          users={usersFromServer}
+          onChange={(value) => handleUserIdChange(setUserId, value)}
+          hasError={hasUserIdError}
+        />
 
         <button type="submit" data-cy="submitButton">
           Add
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">
-            delectus aut autem
-          </h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList
+        todos={addTodos}
+        users={usersFromServer}
+      />
     </div>
   );
 };
