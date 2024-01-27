480c566ed99cdef630c59030ed3fccbe988d7145
mykyta01 <mykyta.romansihyn@gmail.com>
2024-01-27 16:38:47 +0100

    task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,36 @@
-export const TodoInfo = () => {};
+import cn from 'classnames';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import { UserInfo } from '../UserInfo';
+import usersFromServer from '../../api/users';
+import './TodoInfo.scss';
+
+type Props = {
+  todo: Todo;
+};
+
+function getUserById(userId: number): User | null {
+  return usersFromServer.find(user => user.id === userId) || null;
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const user = getUserById(todo.userId);
+
+  return (
+    <article
+      data-id={todo.id}
+      className={cn('TodoInfo', {
+        'TodoInfo--completed': todo.completed === true,
+      })}
+      key={todo.id}
+    >
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+
+      {user && (
+        <UserInfo user={user} />
+      )}
+    </article>
+  );
+};
