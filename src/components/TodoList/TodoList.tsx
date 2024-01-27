6bce53f8b816a42072c9faa9c21a0a5db3ec8384
Misha Martynov <martunmtm@gmail.com>
2024-01-27 19:59:49 +0200

    add_todo_form

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,20 @@
-export const TodoList = () => {};
+import React from 'react';
+import { Todo } from '../../Type/todos';
+import { TodoInfo } from '../TodoInfo';
+
+type TodosListProps = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<TodosListProps> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo
+          key={todo.id}
+          todo={todo}
+        />
+      ))}
+    </section>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,22 @@
-export const UserInfo = () => {};
+import React from 'react';
+
+type UserProps = {
+  user: {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+  } | null;
+};
+
+export const UserInfo: React.FC<UserProps> = ({ user }) => {
+  return (
+    <>
+      {user && (
+        <a className="UserInfo" href={`mailto:${user.email}`}>
+          {user.name}
+        </a>
+      )}
+    </>
+  );
+};
