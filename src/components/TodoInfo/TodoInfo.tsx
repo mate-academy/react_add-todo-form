443caf3b0e49780f0bb1ed180bbbf1acb35998b0
Danylo-Chumakov <danil26032003@gmail.com>
2024-02-06 15:43:23 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,32 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import cn from 'classnames';
+import { User, UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo,
+};
+
+export interface Todo {
+  id: number,
+  title: string,
+  completed: boolean,
+  userId: number,
+  user?: User,
+}
+
+export const TodoInfo:React.FC<Props> = ({ todo }) => {
+  const { title, completed, user } = todo;
+
+  return (
+    <article
+      data-id={todo.id}
+      className={cn('TodoInfo', { 'Todo-Info': completed })}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,20 @@
-export const UserInfo = () => {};
+export interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+}
+
+type Props = {
+  user: User,
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const { name, email } = user;
+
+  return (
+    <a className="User-Info" href={`email: ${email}`}>
+      {name}
+    </a>
+  );
+};
