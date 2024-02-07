41e427fe125ec800369d25537073a41f0d228c1f
Ruslan Hrynchuk <r.momos@gmail.com>
2024-02-07 16:19:42 +0200

    Todo Form. Solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,19 @@
-export const TodoList = () => {};
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+interface Props {
+  visibleTodos: Todo[],
+}
+
+export const TodoList: React.FC<Props> = ({ visibleTodos }) => {
+  return (
+    <section className="TodoList">
+      {visibleTodos.map((todo) => (
+        <TodoInfo
+          key={todo.id}
+          todoInfo={todo}
+        />
+      ))}
+    </section>
+  );
+};
