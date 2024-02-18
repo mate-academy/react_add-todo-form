ea0f99cb20050b270961349da110eec3ab29f7f1
alona <alionazhv@gmail.com>
2024-02-18 11:26:02 +0100

    The content from App was moved to corresponding components

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,17 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { ToDo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo: React.FC<{ todo: ToDo }> = ({ todo }) => {
+  return (
+    <article
+      data-id="16"
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {todo.user && <UserInfo user={todo.user} />}
+    </article>
+  );
+};
