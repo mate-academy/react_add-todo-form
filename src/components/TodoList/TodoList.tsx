c82c273ecaa89a0200006c9e0abeb025e47100ae
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-02-22 20:20:14 +0200

    done

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,17 @@
-export const TodoList = () => {};
+import React from 'react';
+import { TodoInfo } from '../TodoInfo';
+import { UserTodo } from '../../Types/UserTodo';
+
+interface Props {
+  todos: UserTodo[];
+}
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo key={todo.id} todo={todo} />
+      ))}
+    </section>
+  );
+};
