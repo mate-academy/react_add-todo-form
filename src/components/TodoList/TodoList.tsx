d41e95e68e4a16b8d5b681372fffacc76a4759da
Siarhei Tsishkavets <kenny.sergey@gmail.com>
2024-03-10 13:34:45 +0100

    Add task solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,29 @@
-export const TodoList = () => {};
+import { TodoInfo } from '../TodoInfo';
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
+
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+  user: User;
+};
+
+type Props = {
+  todos: Todo[];
+};
+export const TodoList = ({ todos }: Props) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => {
+        return <TodoInfo todo={todo} key={todo.id} />;
+      })}
+    </section>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,18 @@
-export const UserInfo = () => {};
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
+
+type Props = {
+  user: User;
+};
+
+export const UserInfo = ({ user }: Props) => {
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
+};
