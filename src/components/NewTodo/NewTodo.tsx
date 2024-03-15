37186917f565f85377502086e7bfcb13704fabc2
Nora Audonnet <noraaudonnet@gmail.com>
2024-03-15 18:10:55 +0200

    solution

---
+++ src/components/NewTodo/NewTodo.tsx
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+import users from '../../api/users';
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  onAdd: (todo: Todo) => void;
+}
+
+export const NewTodo: React.FC<Props> = ({ onAdd }) => {
+  const [title, setTitle] = useState('');
+  const [hasTitleError, setHasTitleError] = useState(false);
+
+  const [userId, setUserId] = useState(0);
+  const [hasUserIdError, setHasUserIdError] = useState(false);
+
+  const [currentId, setCurrentId] = useState(16);
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    setHasTitleError(false);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!title) {
+      setHasTitleError(true);
+    }
+
+    if (userId === 0) {
+      setHasUserIdError(true);
+    }
+
+    if (!title || userId === 0) {
+      return;
+    }
+
+    const id = currentId;
+
+    setCurrentId(currentId + 1);
+
+    onAdd({
+      title,
+      userId,
+      id,
+      completed: false,
+      user: null,
+    });
+
+    setTitle('');
+    setUserId(0);
+  };
+
+  return (
+    <>
+      <h1>Add todo form</h1>
+      <form action="/api/todos" method="POST" onSubmit={handleSubmit}>
+        <div className="field">
+          <input
+            type="text"
+            data-cy="titleInput"
+            value={title}
+            placeholder="Enter a title"
+            onChange={handleTitleChange}
+          />
+          {hasTitleError && <span className="error">Please enter a title</span>}
+        </div>
+
+        <div className="field">
+          <select
+            data-cy="userSelect"
+            value={userId}
+            onChange={event => setUserId(+event.target.value)}
+          >
+            <option value="0" disabled>
+              Choose a user
+            </option>
+            {users.map(user => (
+              <option value={user.id}>{user.name}</option>
+            ))}
+          </select>
+
+          {hasUserIdError && (
+            <span className="error">Please choose a user</span>
+          )}
+        </div>
+
+        <button type="submit" data-cy="submitButton">
+          Add
+        </button>
+      </form>
+    </>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,11 @@
-export const UserInfo = () => {};
+import { User } from '../../types/User';
+
+interface Props {
+  user: User;
+}
+
+export const UserInfo: React.FC<Props> = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`} key={user.id}>
+    {user.name}
+  </a>
+);
