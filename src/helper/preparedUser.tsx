0937132ffd83d4f68724d9618d0190f03b6f4dfb
Kateryna Shepetska <shepetskayaa@gmail.com>
2024-03-15 19:12:12 +0200

    add task solution

---
+++ src/helper/preparedUser.tsx
@@ -0,0 +1,6 @@
+import { User } from '../types/User';
+import usersFromServer from '../api/users';
+
+export function getPreparedUser(userId: number): User | null {
+  return usersFromServer.find(user => user.id === userId) || null;
+}
