f52344570ab8c63280fe1f7ef5b49e9ad7f40bf5
SerjDSA <serjdsa7@gmail.com>
2024-03-21 07:23:53 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,24 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo';
+import { TodoItem } from '../../types/TodoItem';
+
+interface TodoInfoProps {
+  todo: TodoItem;
+}
+
+export const TodoInfo: React.FC<TodoInfoProps> = ({ todo }) => {
+  const { id, completed, title, user } = todo;
+
+  return (
+    <article
+      key={id}
+      data-id={id}
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
