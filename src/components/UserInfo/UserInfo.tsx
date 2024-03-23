2190059e8ddc98bd285a4efed102c6bf39aab223
winniepuhh <mihajlevskijbogdan@gmail.com>
2024-03-23 16:11:07 +0200

    fixed

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -4,8 +4,16 @@ interface Props {
   user: User | null;
 }
 
-export const UserInfo: React.FC<Props> = ({ user }) => (
-  <a className="UserInfo" href={`mailto:${user?.email}`}>
-    {user?.name}
-  </a>
-);
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  if (!user) {
+    return null; // Render nothing if user is null
+  }
+
+  const { email, name } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
