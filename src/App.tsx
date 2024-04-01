05101279f362bc37cc068672d74e83b33da9cf54
ihor_romaniuk <iromaniuk@mycubes.nl>
2024-04-01 22:18:06 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,152 @@
+import { useState } from 'react';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import { TodoList } from './components/TodoList';
+import { SaveTodoDTO } from './interfaces/saveTodoDto';
+import { SaveTodoError } from './interfaces/saveTodoError';
+import { Todo } from './interfaces/todo';
+import { TodoUser } from './interfaces/todoUser';
+import { User } from './interfaces/user';
+
+const getUserById = (userArr: User[], userId: number): User => {
+  return userArr.find(user => user.id === userId) as User;
+};
+
+const getVisibleTodos = (todosArr: Todo[]): TodoUser[] => {
+  return [...todosArr].map(todo => ({
+    ...todo,
+    user: getUserById(usersFromServer, todo.userId),
+  }));
+};
+
+const defaultSaveTodo: SaveTodoDTO = {
+  title: '',
+  userId: 0,
+};
+
+const defaultSaveTodoError: SaveTodoError = {
+  title: '',
+  user: '',
+};
+
+const getNextId = (todoArr: Todo[]): number => {
+  return Math.max(...todoArr.map(todo => todo.id)) + 1;
+};
+
+const validateTitle = (title: string): string => {
+  if (!title || !title.trim()) {
+    return 'Please enter a title';
+  } else {
+    if (!/^[a-zA-Z0-9А-ЩЬЮЯҐЄІЇа-щьюяґєії ]*$/g.test(title)) {
+      return 'Only EN, UA letters, numbers and spaces allowed';
+    }
+  }
+
+  return '';
+};
+
+const validateUser = (userId: number): string => {
+  return userId === 0 ? 'Please choose a user' : '';
+};
 
 export const App = () => {
+  const [todos, setTodos] = useState(todosFromServer);
+  const [saveTodo, setSaveTodo] = useState(defaultSaveTodo);
+
+  const [saveTodoError, setSaveTodoError] = useState(defaultSaveTodoError);
+
+  const visibleTodos = getVisibleTodos(todos);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!validateTitle(saveTodo.title) && !validateUser(saveTodo.userId)) {
+      setTodos([
+        ...todos,
+        { id: getNextId(todos), completed: false, ...saveTodo },
+      ]);
+      setSaveTodo(defaultSaveTodo);
+    } else {
+      setSaveTodoError({
+        title: validateTitle(saveTodo.title),
+        user: validateUser(saveTodo.userId),
+      });
+    }
+  };
+
+  const setSaveTodoErrorFn = (
+    field: keyof SaveTodoError,
+    value: string,
+  ): void => {
+    setSaveTodoError(prev => {
+      const newErr = { ...prev };
+
+      newErr[field] = value;
+
+      return newErr;
+    });
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/todos" method="POST">
+      <form action="/api/todos" method="POST" onSubmit={handleSubmit}>
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="title">Title:</label>
+          <input
+            id="title"
+            type="text"
+            data-cy="titleInput"
+            value={saveTodo.title}
+            placeholder="Kangaroo"
+            onChange={event => {
+              if (saveTodoError.title) {
+                setSaveTodoErrorFn('title', '');
+              }
+
+              return setSaveTodo(prev => {
+                return { ...prev, title: event.target.value };
+              });
+            }}
+          />
+          {saveTodoError.title && (
+            <span className="error">{saveTodoError.title}</span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <label htmlFor="userId">User:</label>
+          <select
+            id="userId"
+            data-cy="userSelect"
+            value={saveTodo.userId}
+            onChange={event => {
+              {
+                if (saveTodoError.user) {
+                  setSaveTodoErrorFn('user', '');
+                }
+
+                setSaveTodo(prev => {
+                  return { ...prev, userId: +event.target.value };
+                });
+              }
+            }}
+          >
             <option value="0" disabled>
               Choose a user
             </option>
+            {usersFromServer.map(user => (
+              <option value={user.id} key={user.id}>
+                {user.name}
+              </option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {saveTodoError.user && (
+            <span className="error">{saveTodoError.user}</span>
+          )}
         </div>
 
         <button type="submit" data-cy="submitButton">
@@ -29,33 +154,7 @@ export const App = () => {
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={visibleTodos} />
     </div>
   );
 };
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,21 @@
-export const TodoInfo = () => {};
+import cn from 'classnames';
+import { TodoUser } from '../../interfaces/todoUser';
+import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
+
+type Props = {
+  todo: TodoUser;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  return (
+    <article
+      data-id={todo.id}
+      className={cn('TodoInfo', { 'TodoInfo--completed': todo.completed })}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <UserInfo user={todo.user} />
+    </article>
+  );
+};
