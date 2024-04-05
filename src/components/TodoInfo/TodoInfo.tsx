cdf7c46696731af38e74d508d2c5e40aecbf85d5
Bohdan Shyrii <shyrii11bohdan@gmail.com>
2024-04-05 16:57:35 +0200

    cope

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,25 @@
-export const TodoInfo = () => {};
+import { UserInfo } from '../UserInfo';
+import { Todo, User } from '../../types/types';
+
+type Props = {
+  todos: Todo[];
+  users: User[];
+};
+
+export const TodoInfo = ({ todos, users }: Props) => {
+  return (
+    <>
+      {todos.map(todo => {
+        const USER = users.find(user => user.id === todo.userId);
+
+        return (
+          <article key={todo.id} className="TodoInfo TodoInfo--completed">
+            <h2 className="TodoInfo__title">{todo.title}</h2>
+
+            {USER && <UserInfo user={USER} />}
+          </article>
+        );
+      })}
+    </>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,15 @@
-export const TodoList = () => {};
+import { TodoInfo } from '../TodoInfo';
+import { Todo, User } from '../../types/types';
+
+type Props = {
+  todos: Todo[];
+  users: User[];
+};
+
+export const TodoList = ({ todos, users }: Props) => {
+  return (
+    <section className="TodoList">
+      <TodoInfo todos={todos} users={users} />
+    </section>
+  );
+};
