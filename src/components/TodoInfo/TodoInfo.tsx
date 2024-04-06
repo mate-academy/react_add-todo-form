bef815438bd7c394772bf9dd9e44302c6e411636
Robert Szałapak <robsza1212@gmail.com>
2024-04-06 20:04:53 +0200

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,20 @@
-export const TodoInfo = () => {};
+import { UserInfo } from '../UserInfo/UserInfo';
+import { Todo } from '../../types/Todo';
+import React from 'react';
+
+export const TodoInfo = ({ todo }: Props) => {
+  const todoClasses = todo.completed
+    ? 'TodoInfo TodoInfo--completed'
+    : 'TodoInfo';
+
+  return (
+    <article className={todoClasses} key={todo.id} data-id={todo.id}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {todo.user && <UserInfo user={todo.user} />}
+    </article>
+  );
+};
+
+type Props = {
+  todo: Todo;
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,14 @@
-export const UserInfo = () => {};
+import React from 'react';
+import { User } from '../../types/User';
+
+export const UserInfo = ({ user }: Props) => (
+  <>
+    <a className="UserInfo" href={`mailto:${user.email}`} key={user.id}>
+      {user.name}
+    </a>
+  </>
+);
+
+type Props = {
+  user: User;
+};
