ab27030730e389d7396810fb8affe7de2045ca97
Богдан <BoGoBooM@MacBook-Pro---Bohdan.local>
2024-04-06 18:48:36 +0300

    errors was fixed 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -103,8 +103,9 @@ export const App: React.FC = () => {
             ))}
           </select>
 
-          {hasUserIdError 
-            && <span className="error">Please choose a user</span>}
+          {hasUserIdError && (
+            <span className="error">Please choose a user</span>)
+          }
         </div>
 
         <button type="submit" data-cy="submitButton">
