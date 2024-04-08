1577acaa584bc681c4404cd0ebaf4cac8cb78f69
Olaf <olaf.chuszno@gmail.com>
2024-04-08 16:57:55 +0200

    Find user by id, not arr index and allow to be undefined

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,9 +1,9 @@
 import { type User } from '../../App';
 
-export const UserInfo: React.FC<{ user: User }> = ({ user }) => {
+export const UserInfo: React.FC<{ user: User | undefined }> = ({ user }) => {
   return (
-    <a className="UserInfo" href={`mailto:${user.email}`}>
-      {user.name}
+    <a className="UserInfo" href={`mailto:${user?.email}`}>
+      {user?.name}
     </a>
   );
 };
