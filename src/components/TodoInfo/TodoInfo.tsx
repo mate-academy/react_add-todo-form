eddd57dc944780981a0cbb0ac791ffef92dfe4b0
Medvid Oleksandr <samaster010604@gmail.com>
2024-04-14 20:33:00 +0300

    addTodoForm

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,24 @@
-export const TodoInfo = () => {};
+import cn from 'classnames';
+import './TodoInfo.scss';
+import { UserInfo } from '../UserInfo';
+import { Task } from '../../types/Task';
+import React from 'react';
+
+type Props = {
+  todo: Task;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  return (
+    <article
+      data-id={todo.id}
+      className={cn('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <UserInfo user={todo.user} />
+    </article>
+  );
+};
