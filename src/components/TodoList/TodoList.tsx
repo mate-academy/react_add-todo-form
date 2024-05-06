19135bb67178c5d1a892e5004c96900d8687b608
Grygoryy <kachurovskyy123@gmail.com>
2024-05-06 12:56:46 +0300

    solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,15 @@
-export const TodoList = () => {};
+import TodoWithUser from '../../types/TodoWithUser';
+import { TodoInfo } from '../TodoInfo';
+import { findMaxTodoId } from '../../services';
+
+interface Props {
+  todos: TodoWithUser[];
+}
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map((todo, _index, arr) => (
+      <TodoInfo todo={{ ...todo, id: findMaxTodoId(arr) + 1 }} key={todo.id} />
+    ))}
+  </section>
+);
