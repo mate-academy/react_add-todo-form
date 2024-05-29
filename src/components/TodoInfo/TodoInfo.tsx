0b2d4884a5289f71c0c4151d06894dfd70739100
Oleksandr <alex.volkovskyi@gmail.com>
2024-05-29 11:18:00 +0300

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -12,7 +12,7 @@ export const TodoInfo: React.FC<Props> = ({
   <article
     data-id={id}
     className={classNames('TodoInfo', {
-      'TodoInfo--completed': completed === true,
+      'TodoInfo--completed': completed,
     })}
   >
     <h2 className="TodoInfo__title">{title}</h2>
