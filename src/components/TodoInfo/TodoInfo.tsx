a667eb1bf3975af92743f754de1c9675fa944068
Volodymyr Vynnyk <vinnikvvv@gmail.com>
2024-06-15 12:26:56 +0300

    add App PostForm

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,23 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import cn from 'classnames';
+import { TodoWithUsers } from '../../type/TodoWithUsers';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: TodoWithUsers;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const { title, user, id, completed } = todo;
+
+  return (
+    <article
+      data-id={id}
+      className={cn('TodoInfo', { 'TodoInfo--completed': completed })}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
