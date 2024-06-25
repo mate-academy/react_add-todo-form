0cffa82c0b519e9bd327e61700ebd030f1e54e11
Vitalii <shut.vitaliy.ol@gmail.com>
2024-06-25 23:01:57 +0300

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,17 @@
-export const TodoInfo = () => {};
+import { FC } from 'react';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { ToDo } from '../../App';
+
+interface Props {
+  todo: ToDo;
+}
+
+export const TodoInfo: FC<Props> = ({ todo }) => (
+  <article
+    className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    data-id={todo.id}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    {todo.user && <UserInfo user={todo.user} />}
+  </article>
+);
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,99 @@
-export const TodoList = () => {};
+import { FC } from 'react';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+import { ToDo } from '../../App';
+import users from '../../api/users';
+import { useState } from 'react';
+
+interface Props {
+  todos: ToDo[] | null;
+  addNewTodo: (todo: ToDo) => void;
+}
+
+export const TodoList: FC<Props> = ({ todos, addNewTodo }) => {
+  const [newTodoTitle, setNewTodoTitle] = useState('');
+  const [selectedUserId, setSelectedUserId] = useState<number>(0);
+  const [titleError, setTitleError] = useState<string>('');
+  const [userError, setUserError] = useState<string>('');
+
+  const getLargestId = Math.max(...(todos?.map(el => el.id) || [])) + 1;
+  const errorsDisable = () => {
+    setUserError('');
+    setTitleError('');
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!newTodoTitle.trim()) {
+      setTitleError('Please enter a title');
+    }
+
+    if (!selectedUserId) {
+      setUserError('Please choose a user');
+    }
+
+    if (!selectedUserId || !newTodoTitle.trim()) {
+      return;
+    }
+
+    const newTodo: ToDo = {
+      id: getLargestId,
+      title: newTodoTitle,
+      completed: false,
+      userId: selectedUserId,
+      user: users.find(user => user.id === selectedUserId) || null,
+    };
+
+    addNewTodo(newTodo);
+    setNewTodoTitle('');
+    setSelectedUserId(0);
+  };
+
+  return (
+    <section className="TodoList">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            data-cy="titleInput"
+            value={newTodoTitle}
+            onChange={event => setNewTodoTitle(event.target.value)}
+            placeholder="Enter todo title"
+          />
+          {titleError && !newTodoTitle && (
+            <span className="error" style={{ color: 'red' }}>
+              {titleError}
+            </span>
+          )}
+        </div>
+        <br />
+        <div>
+          <select
+            data-cy="userSelect"
+            value={selectedUserId}
+            onChange={event => setSelectedUserId(Number(event.target.value))}
+          >
+            <option value="0">Choose a user</option>
+            {users.map(user => (
+              <option value={user.id} key={user.id}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+          {userError && !selectedUserId && (
+            <span className="error" style={{ color: 'red' }}>
+              {userError}
+            </span>
+          )}
+        </div>
+        <br />
+        <div>
+          <button type="submit" onClick={errorsDisable} data-cy="submitButton">
+            Add
+          </button>
+        </div>
+      </form>
+      {todos?.map(todo => <TodoInfo key={todo.id} todo={todo} />)}
+    </section>
+  );
+};
