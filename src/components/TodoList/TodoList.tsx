52cecad0b5b31c4bc06a7c4e7916396aaa81810a
Stanislav <stanislavandr@gmail.com>
2024-06-26 22:04:04 +0300

    solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,27 @@
-export const TodoList = () => {};
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import usersFromServer from '../../api/users';
+import { TodoInfo } from '../TodoInfo';
+
+interface Props {
+  todos: Todo[];
+}
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  const preparedTodos = todos.map(todo => {
+    const user = usersFromServer.find(user => user.id === todo.userId) || null;
+
+    return {
+      ...todo,
+      user,
+    };
+  });
+
+  return (
+    <section className="TodoList">
+      {preparedTodos.map(todo => (
+        <TodoInfo key={todo.id} todo={todo} />
+      ))}
+    </section>
+  );
+};
