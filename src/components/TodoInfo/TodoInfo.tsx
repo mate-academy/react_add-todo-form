336c5954290c43f70fef4f95925ce9420a6cc90c
Serdiuk Sergii <sergejserduk096@gmail.com>
2024-06-26 14:54:42 +0300

    add solution task react_add-todo-form

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,27 @@
-export const TodoInfo = () => {};
+import cn from 'classnames';
+
+import usersFromServer from '../../api/users';
+import React from 'react';
+import { ToDo } from '../../components/Types/ToDo';
+
+type Props = {
+  todo: ToDo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const users = usersFromServer.find(user => user.id === todo.userId);
+
+  return (
+    <article
+      data-id={todo.id}
+      className={cn('TodoInfo', { 'TodoInfo--completed': todo.completed })}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {users && (
+        <a className="UserInfo" href={`mailto:${users.email}`}>
+          {users.name}
+        </a>
+      )}
+    </article>
+  );
+};
