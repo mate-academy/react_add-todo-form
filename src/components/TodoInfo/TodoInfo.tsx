9d3f473cfc196ba073a2b546c67c185cb2dea6f7
Yaroslav Leshchenko <yaroslavleshenkoforwork@gmail.com>
2024-07-01 15:18:41 +0300

    split files

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,21 @@
-export const TodoInfo = () => {};
+import { UserInfo } from '../UserInfo';
+import React from 'react';
+import cn from 'classnames';
+import { Todo } from '../types/Todos';
+
+interface Props {
+  todo: Todo;
+}
+
+export const TodoInfo: React.FC<Props> = ({
+  todo: { title, id, user, completed },
+}) => (
+  <article
+    data-id={id}
+    className={cn('TodoInfo', { 'TodoInfo--completed': completed })}
+  >
+    <h2 className="TodoInfo__title">{title}</h2>
+
+    {user && <UserInfo user={user} />}
+  </article>
+);
