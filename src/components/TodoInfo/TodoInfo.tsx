c4c668f9cc8642b714de6aa02108ec6629ba43bf
Sophia <sophia13gal@gmail.com>
2024-07-03 22:16:05 +0300

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,25 @@
-export const TodoInfo = () => {};
+import { Todo } from '../types/Todos';
+import usersFromServer from '../../api/users';
+import React from 'react';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const users = usersFromServer.find(user => user.id === todo.userId);
+
+  return (
+    <article
+      data-id={todo.id}
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {users && (
+        <a className="UserInfo" href={`mailto:${users.email}`}>
+          {users.name}
+        </a>
+      )}
+    </article>
+  );
+};
