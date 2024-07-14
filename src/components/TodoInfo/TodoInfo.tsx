4435dcc62e879cf7e331276916ccc6fde236abed
Rusłan Karaniewski <karane88vka@gmail.com>
2024-07-14 19:25:44 +0200

    My solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,23 @@
-export const TodoInfo = () => {};
+import { Todo } from '../../types/todo';
+import { UserInfo } from '../UserInfo';
+import usersFromServer from '../../api/users';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo = ({ todo }: Props) => {
+  const { id, title, userId, completed } = todo;
+  const user = usersFromServer.find(el => el.id === userId)!;
+
+  return (
+    <article
+      data-id={id}
+      className={`TodoInfo ${completed && 'TodoInfo--completed'}`}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      <UserInfo user={user} />
+    </article>
+  );
+};
