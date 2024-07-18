c9bb87bdc841e801095d10d50b4dc9e877dd99d7
Mariia <mulitskaya@gmail.com>
2024-07-17 22:12:57 -0400

    develop

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,48 @@
-export const TodoList = () => {};
+import React from 'react';
+import { TodoInfo } from '../TodoInfo';
+import users from '../../api/users';
+
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+};
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <div>
+      {todos.map(todo => {
+        const user = users.find(
+          (tempUser: { id: number }) => tempUser.id === todo.userId,
+        );
+
+        if (!user) {
+          return null;
+        }
+
+        return (
+          <TodoInfo
+            key={todo.id}
+            todo={{
+              userId: todo.userId,
+              id: todo.id,
+              title: todo.title,
+              completed: todo.completed,
+              user: {
+                id: user.id,
+                name: user.name,
+                username: user.username,
+                email: user.email,
+              },
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,14 @@
-export const UserInfo = () => {};
+import React from 'react';
+
+type Props = {
+  name: string;
+  email: string;
+};
+
+export const UserInfo: React.FC<Props> = ({ name, email }) => {
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
