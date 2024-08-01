171fc4fa5f708599a84cc0484119dc7b8e4c6a6a
Vadym Babisov <vadim.babisov@gmail.com>
2024-08-01 15:55:08 +0300

    add solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,22 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/types';
+
+interface TodoInfoProps {
+  todo: Todo;
+}
+
+export const TodoInfo: React.FC<TodoInfoProps> = ({ todo }) => {
+  const user = todo.user;
+
+  return (
+    <article
+      key={todo.id}
+      data-id={todo.id}
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
