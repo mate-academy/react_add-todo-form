407b3767bfa8056ca015db38c3e533ec3ee46d78
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2024-08-04 19:44:02 +0200

    fix bugs 1.1

--- src/App.tsx
+++ src/App.tsx
@@ -108,13 +108,11 @@ export const App = () => {
             <option value="0" disabled>
               Choose a user
             </option>
-            {usersFromServer.map(({ id, name }) => {
-              return (
-                <option key={id} value={id}>
-                  {name}
-                </option>
-              );
-            })}
+            {usersFromServer.map(({ id, name }) => (
+              <option key={id} value={id}>
+                {name}
+              </option>
+            ))}
           </select>
           {hasUserError && <span className="error">Please choose a user</span>}
         </div>
