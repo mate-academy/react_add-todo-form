3fd4d9f4a8102f1b42dde16b0075d50c799d57cf
Vladislav <njiuyfqqq123@gmail.com>
2024-08-05 16:33:10 +0300

    why i dont see new posts?

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,38 @@
-export const TodoList = () => {};
+import { Post } from '../../types/Post';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todos: Post[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <>
+      <article data-id="1" className="TodoInfo TodoInfo--completed">
+        <h2 className="TodoInfo__title">delectus aut autem</h2>
+
+        <a className="UserInfo" href="mailto:Sincere@april.biz">
+          Leanne Graham
+        </a>
+      </article>
+      <article data-id="1" className="TodoInfo TodoInfo--completed">
+        <h2 className="TodoInfo__title">delectus aut autem</h2>
+
+        <a className="UserInfo" href="mailto:Sincere@april.biz">
+          Leanne Graham
+        </a>
+      </article>
+
+      <article data-id="2" className="TodoInfo">
+        <h2 className="TodoInfo__title">quis ut nam facilis et officia qui</h2>
+
+        <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
+          Patricia Lebsack
+        </a>
+      </article>
+      {todos.map(todo => {
+        <UserInfo key={todo.id} currentTodo={todo} />;
+      })}
+    </>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,17 @@
-export const UserInfo = () => {};
+import { Post } from '../../types/Post';
+
+type Props = {
+  currentTodo: Post;
+};
+
+export const UserInfo: React.FC<Props> = ({ currentTodo }) => {
+  return (
+    <article data-id="2" className="TodoInfo">
+      <h2 className="TodoInfo__title">{currentTodo.user?.name}</h2>
+
+      <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
+        Patricia Lebsack
+      </a>
+    </article>
+  );
+};
