eb861672bd6a8cd5ece6f95c055e21f2233286ce
Oleksandr Cherpak <cherpak.oleksandr.wsei@gmail.com>
2024-08-17 20:37:24 +0200

    add cond rendering

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -2,17 +2,13 @@ import { Todo } from '../services';
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }: { todo: Todo }) => {
-  if (todo.user === undefined) {
-    return;
-  }
-
   return (
     <article
       data-id={todo.id}
       className={todo.completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'}
     >
       <h2 className="TodoInfo__title">{todo.title}</h2>
-      <UserInfo user={todo.user} />
+      {todo.user && <UserInfo user={todo.user} />}
     </article>
   );
 };
