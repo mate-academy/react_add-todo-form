7b8bbf94657c1d9799bfd9bc23351fb68d2c4b19
Marian <getmoodyup@gmail.com>
2024-08-21 20:06:29 +0300

    merge

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import { User } from '../../types/User';
-
-interface UserInfoProps {
-  user: User;
-}
-
-export const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
-  return (
-    <a className="UserInfo" href={`mailto:${user.email}`}>
-      {user.name}
-    </a>
-  );
-};
-=======
 import React from 'react';
 import { User } from '../../types/User';
 
@@ -25,4 +10,3 @@ export const UserInfo: React.FC<Props> = ({ user }) => (
     {user.name}
   </a>
 );
->>>>>>> 94e5644f217ebfd8f7f83b20dfb33876c7b6f573
