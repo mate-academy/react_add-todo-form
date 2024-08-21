f25e37362bb350a44532ccaa2d613b7db31fdb95
AmateZz <yarike451@gmail.com>
2024-08-21 14:40:14 +0300

    problem

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -7,6 +7,19 @@ type Todo = {
   userId: number;
 };
 
+type NewTodo = {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+  user: {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+  };
+};
+
 type User = {
   id: number;
   name: string;
@@ -23,9 +36,18 @@ export const TodoList: React.FC<Props> = ({ todos, users }) => {
   return (
     <section className="TodoList">
       {todos.map((todo: Todo) => {
-        const user = users.find(u => u.id === todo.userId);
+        const fUser = users.find(u => u.id === todo.userId);
 
-        return user && <TodoInfo todo={todo} key={todo.id} user={user} />;
+        if (!fUser) {
+          return null;
+        }
+
+        const newTodo: NewTodo = {
+          ...todo,
+          user: fUser,
+        };
+
+        return newTodo && <TodoInfo todo={newTodo} key={newTodo.id} />;
       })}
     </section>
   );
