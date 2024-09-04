cb15e09056beae2d79d93200932dde3a4d06e726
Oleksandr Sushko <alexandrsushko25@gmail.com>
2024-09-04 21:57:14 +0200

    commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,106 @@
 import './App.scss';
-
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import React, { useState } from 'react';
+import users from './api/users';
 
 export const App = () => {
+  const [count, setCount] = useState(0);
+
+  const [userId, setUserId] = useState(0);
+  const [hasUserIdErrorMessage, setHasUserIdErrorMessage] = useState('');
+
+  const [title, setTitle] = useState('');
+  const [hasTitleErrorMessage, setHasTitleErrorMessage] = useState('');
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    if (hasTitleErrorMessage) {
+      setHasTitleErrorMessage(''); // Убираем сообщение об ошибке, если пользователь начал вводить текст
+    }
+  };
+
+  const handleUserChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setUserId(+event.target.value);
+    if (hasUserIdErrorMessage) {
+      setHasUserIdErrorMessage(''); // Убираем сообщение об ошибке, если пользователь выбрал пользователя
+    }
+  };
+
+  const reset = () => {
+    setTitle('');
+    setUserId(0);
+    setHasTitleErrorMessage('');
+    setHasUserIdErrorMessage('');
+    setCount(count + 1);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    let hasError = false;
+
+    if (!title) {
+      setHasTitleErrorMessage('Please enter a title');
+      hasError = true;
+    }
+
+    if (!userId) {
+      setHasUserIdErrorMessage('Please choose a user');
+      hasError = true;
+    }
+
+    if (!title || !userId) {
+      return;
+    }
+
+    if (!hasError) {
+      reset();
+    }
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/todos" method="POST">
+      <form action="/api/todos" method="POST" onSubmit={handleSubmit}>
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            placeholder="Enter a title"
+            data-cy="titleInput"
+            value={title}
+            onChange={handleTitleChange}
+            onBlur={() => setHasTitleErrorMessage(' ')}
+          />
+          {hasTitleErrorMessage && (
+            <span className="error">{hasTitleErrorMessage}</span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <label htmlFor="user">User: </label>
+          <select
+            id="user"
+            required
+            value={userId}
+            onChange={handleUserChange}
+            onBlur={() => setHasUserIdErrorMessage(' ')}
+            data-cy="userSelect"
+          >
             <option value="0" disabled>
               Choose a user
             </option>
+
+            {users.map(user => (
+              <option value={user.id} key={user.id}>
+                {user.name}
+              </option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {hasUserIdErrorMessage && (
+            <span className="error">{hasUserIdErrorMessage}</span>
+          )}
         </div>
 
         <button type="submit" data-cy="submitButton">
@@ -32,7 +111,6 @@ export const App = () => {
       <section className="TodoList">
         <article data-id="1" className="TodoInfo TodoInfo--completed">
           <h2 className="TodoInfo__title">delectus aut autem</h2>
-
           <a className="UserInfo" href="mailto:Sincere@april.biz">
             Leanne Graham
           </a>
@@ -40,7 +118,6 @@ export const App = () => {
 
         <article data-id="15" className="TodoInfo TodoInfo--completed">
           <h2 className="TodoInfo__title">delectus aut autem</h2>
-
           <a className="UserInfo" href="mailto:Sincere@april.biz">
             Leanne Graham
           </a>
@@ -50,7 +127,6 @@ export const App = () => {
           <h2 className="TodoInfo__title">
             quis ut nam facilis et officia qui
           </h2>
-
           <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
             Patricia Lebsack
           </a>
