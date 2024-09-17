030cf738e0672aed90f9c37c9042b2d5c7ec1bdd
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-09-17 14:42:43 +0200

    task solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,19 @@
-export const UserInfo = () => {};
+import React from 'react';
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+} | null;
+
+type Props = {
+  user: User;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) =>
+  user ? (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  ) : null;
