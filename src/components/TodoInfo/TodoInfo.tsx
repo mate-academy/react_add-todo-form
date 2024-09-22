37ad7a958cf68e135ea6babb17b71d1f008d37ee
Dmytro Ivasenko <dmtivasenko@gmail.com>
2024-09-22 15:44:50 +0700

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,26 @@
-export const TodoInfo = () => {};
+import { UserInfo } from '../UserInfo';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import React from 'react';
+import users from '../../api/users';
+import { User } from '../../types/User';
+
+interface Props {
+  todo: Todo;
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const user = users.find((u: User) => u.id === todo.userId);
+
+  return (
+    <article
+      data-id={todo.id}
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
