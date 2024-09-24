b52bcd5bf96c5670c24e5d2494eb945f9963c541
Vlad Tkachuk <vlad.tkachuk.04@gmail.com>
2024-09-24 15:04:33 +0300

    add solution

---
+++ src/utils/getUserById.ts
@@ -0,0 +1,13 @@
+import { User } from '../types/User';
+import usersFromServer from '../api/users';
+
+const anonymousUser: User = {
+  id: 0,
+  name: 'Anonymous',
+  username: 'anon',
+  email: 'anonymous@unknown.com',
+};
+
+export function getUserById(id: number): User {
+  return usersFromServer.find(user => user.id === id) || anonymousUser;
+}
