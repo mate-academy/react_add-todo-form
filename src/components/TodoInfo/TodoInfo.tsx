c9eaf57fe7d574d569ffcd8139561757ee8b49e0
Daryna Kukharets <darinakuharec61@gmail.com>
2024-10-03 00:31:14 +0300

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,26 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { getUserById } from '../../services/userService';
+import { Todo } from '../../types/todo';
+import { User } from '../../types/user';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const user: User | null = getUserById(todo.userId);
+
+  return (
+    <article
+      data-id="1"
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <UserInfo user={user} />
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,17 @@
-export const UserInfo = () => {};
+import { User } from '../../types/user';
+
+type Props = {
+  user: User | null;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  if (!user) {
+    return <span>User not found</span>;
+  }
+
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
+};
