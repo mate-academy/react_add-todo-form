35875c06232f12d898bf292f48f897cfced97233
Anna Maksai <annamaksai99@gmail.com>
2024-10-04 13:01:56 +0300

    fix: extension

--- src/components/AddTodoForm/constants.ts
+++ src/components/AddTodoForm/constants.ts
@@ -5,5 +5,3 @@ export const DEFAULT_VALUES = {
 
 export const MISSING_TITLE_ERROR = 'Please enter a title';
 export const MISSING_USER_ERROR = 'Please choose a user';
-export const WRONG_TITLE_ERROR =
-  'Only letters (ua and en), digits, and spaces are allowed';
