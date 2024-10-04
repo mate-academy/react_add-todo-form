986b1ee1769778bbcfb372a8c62278ed5ec7a98e
Anna Maksai <annamaksai99@gmail.com>
2024-10-04 13:01:18 +0300

    fix: extension

--- src/components/AddTodoForm/constants.js
+++
@@ -1,7 +0,0 @@
-export const DEFAULT_VALUES = {
-  title: '',
-  userId: 0,
-};
-
-export const MISSING_TITLE_ERROR = 'Please enter a title';
-export const MISSING_USER_ERROR = 'Please choose a user';
