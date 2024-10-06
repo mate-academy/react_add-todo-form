cd570337674adfab36ae2d67b230611c02f9b057
Ihor Barban <ihorbarban@Ihors-MacBook-Air.local>
2024-10-06 21:16:30 +0200

    Some tests are not passed, but looking good

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,22 @@
-export const TodoList = () => {};
+import React from 'react';
+
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import { TodoInfo } from '../TodoInfo';
+
+interface Props {
+  users: User[];
+  todos: Todo[];
+}
+
+export const TodoList: React.FC<Props> = ({ users, todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => {
+        const userAtTodo = users.find(user => todo.userId === user.id) as User;
+
+        return <TodoInfo todo={todo} user={userAtTodo} key={todo.id} />;
+      })}
+    </section>
+  );
+};
