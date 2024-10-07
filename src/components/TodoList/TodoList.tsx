78346868e2d677318b6f1c0f6e18c709cbb64eb8
Daria Nastas <bycinska@gmail.com>
2024-10-07 16:14:43 +0300

    add task solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,20 @@
-export const TodoList = () => {};
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import { TodoInfo } from '../TodoInfo';
+
+interface TodoListProps {
+  todos: Todo[];
+  users: User[];
+}
+
+export const TodoList = ({ todos, users }: TodoListProps) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => {
+        const user = users.find(u => u.id === todo.userId) ?? null;
+
+        return <TodoInfo key={todo.id} todo={{ ...todo, user }} />;
+      })}
+    </section>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,18 @@
-export const UserInfo = () => {};
+import { User } from '../../types/User';
+
+interface UserInfoProps {
+  user: User | null;
+}
+
+export const UserInfo = ({ user }: UserInfoProps) => {
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <div>
+      <span>{user.name}</span>
+      <a href={`mailto:${user.email}`}>{user.email}</a>
+    </div>
+  );
+};
