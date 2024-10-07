d8ffcdc2d3d68c0d335532edadf2c413608fe27a
Yevhenii Melnyk <rpm.steel@gmail.com>
2024-10-07 16:25:43 +0300

    solution except of ids

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,14 @@
-export const UserInfo = () => {};
+import { FC } from 'react';
+import { User } from '../../types/User';
+
+type Props = {
+  user: User;
+};
+
+export const UserInfo: FC<Props> = ({ user }) => {
+  return (
+    <a className="UserInfo" href={user.email}>
+      {user.name}
+    </a>
+  );
+};
