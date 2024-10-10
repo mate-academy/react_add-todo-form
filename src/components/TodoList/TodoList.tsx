0aeb0d8451965d09b96234db1da4720d3a45a22a
Daria Nastas <bycinska@gmail.com>
2024-10-10 09:51:19 +0300

    add task solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,19 +1,13 @@
 import { Todo } from '../../types/Todo';
-import { User } from '../../types/User';
 import { TodoInfo } from '../TodoInfo';
-
 interface TodoListProps {
   todos: Todo[];
-  users: User[];
 }
-
-export const TodoList = ({ todos, users }: TodoListProps) => {
+export const TodoList = ({ todos }: TodoListProps) => {
   return (
     <section className="TodoList">
       {todos.map(todo => {
-        const user = users.find(u => u.id === todo.userId) ?? null;
-
-        return <TodoInfo key={todo.id} todo={{ ...todo, user }} />;
+        return <TodoInfo key={todo.id} todo={todo} />;
       })}
     </section>
   );
