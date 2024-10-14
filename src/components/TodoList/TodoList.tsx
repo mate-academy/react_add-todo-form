60d67ec653bcb6b2d26b25c4b0e8de995fed2a16
Vasyliazhenko Dmytro Vitaliyovych <demon4225@gmail.com>
2024-10-14 11:17:34 +0300

    fixes

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,17 +1,15 @@
 import { FC } from 'react';
 import { Todo } from '../../types/types';
-import { TodoInfo } from '../TodoInfo';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
 
 interface Props {
   todos: Todo[];
 }
 
-export const TodoList: FC<Props> = ({ todos }) => {
-  return (
-    <section className="TodoList">
-      {todos.map(todo => {
-        return <TodoInfo todo={todo} key={todo.id} />;
-      })}
-    </section>
-  );
-};
+export const TodoList: FC<Props> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo todo={todo} key={todo.id} />
+    ))}
+  </section>
+);
--- src/utils/getUserByID.tsx
+++ src/utils/getUserByID.tsx
@@ -1,6 +1,6 @@
 import { User } from '../types/types';
 import usersFromServer from '../api/users';
 
-export function userById(id: number): User {
+export function getUserById(id: number): User {
   return usersFromServer.find(user => user.id === id)!;
 }
