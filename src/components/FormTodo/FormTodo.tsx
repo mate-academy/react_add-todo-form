42623313d6ea68217902639574cdc7738e3d4fd1
Pavlo Svinarchuk <shokkontent2004@gmail.com>
2024-11-01 17:43:44 +0200

    add task solution

---
+++ src/components/FormTodo/FormTodo.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { Todo, User } from '../../types';
+
+interface Props {
+  onAdd: (todo: Todo) => void;
+  user: User[];
+}
+
+export const FormTodo: React.FC<Props> = ({ onAdd, user }) => {
+  const [title, setTitleValue] = useState('');
+  const [name, setUserName] = useState('');
+  const [nameError, setNameError] = useState(false);
+  const [titleError, setTitleError] = useState(false);
+
+  const maxId = Math.max(...user.map(todo => todo.id), 0);
+
+  const titleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitleValue(event.target.value);
+    if (titleError && event.target.value) {
+      setTitleError(false);
+    }
+  };
+
+  const nameChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setUserName(event.target.value);
+    if (nameError && event.target.value) {
+      setNameError(false);
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    let hasError = false;
+
+    if (!title.trim()) {
+      setTitleError(true);
+
+      hasError = true;
+    } else {
+      setTitleError(false);
+    }
+
+    if (name === '') {
+      setNameError(true);
+      hasError = true;
+    } else {
+      setNameError(false);
+    }
+
+    if (hasError) {
+      return;
+    }
+
+    const newTodo: Todo = {
+      id: maxId,
+      title: title,
+      completed: false,
+      userId: parseInt(name),
+    };
+
+    onAdd(newTodo);
+    setTitleValue('');
+    setUserName('');
+  };
+
+  return (
+    <form action="/api/todos" method="POST" onSubmit={handleSubmit} noValidate>
+      <div className="field">
+        <input
+          type="text"
+          data-cy="titleInput"
+          value={title}
+          onChange={titleChange}
+          required
+          placeholder="Enter a title"
+        />
+        {titleError && <span className="error">Please enter a title</span>}
+      </div>
+
+      <div className="field">
+        <select
+          required
+          data-cy="userSelect"
+          value={name}
+          onChange={nameChange}
+        >
+          <option value="" disabled>
+            Choose a user
+          </option>
+          {user.map(users => (
+            <option value={users.id} key={users.id}>
+              {users.name}
+            </option>
+          ))}
+        </select>
+        {nameError && <span className="error">Please choose a user</span>}
+      </div>
+
+      <button type="submit" data-cy="submitButton">
+        Add
+      </button>
+    </form>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,19 @@
-export const TodoList = () => {};
+import React from 'react';
+import { Todo } from '../../types';
+import { TodoInfo } from '../TodoInfo';
+
+interface Props {
+  todos: Todo[];
+}
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  const maxId = Math.max(...todos.map(todo => todo.id), 0);
+
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo key={todo.id} todo={{ ...todo, id: maxId + 1 }} />
+      ))}
+    </section>
+  );
+};
