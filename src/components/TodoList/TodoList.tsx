8ce50ed5c419891d9df08eb8fba6b16954801427
Svitlana Klymuk <svitlanaklymuk31@gmail.com>
2024-11-05 16:14:31 +0200

    solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,21 @@
-export const TodoList = () => {};
+import React from "react";
+import { Todo } from "../../types";
+import { TodoInfo } from "../TodoInfo/TodoInfo";
+
+interface TodoListProps {
+  todos: Todo[];
+}
+
+export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+  return (
+    <>
+      {todos.length > 0 ? (
+        todos.map(todo => (
+          <TodoInfo key={todo.id} todo={todo} />
+        ))
+      ) : (
+        <p>No todos available</p>
+      )}
+    </>
+  );
+};
