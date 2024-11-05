eae3e04c3af613c56662db039da3691b45b0a623
Oleksii Kudriavtsev <Lesha61945@gmail.com>
2024-11-05 18:10:04 +0200

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,31 @@
-export const TodoInfo = () => {};
+import cn from 'classnames';
+import { UserInfo } from '../UserInfo';
+
+interface User {
+  name: string;
+  email: string;
+}
+
+interface Todo {
+  title: string;
+  completed: boolean;
+  userId: number;
+  user?: User;
+}
+
+export const TodoInfo = ({ todo }: { todo: Todo }) => {
+  const { title, completed, userId } = todo;
+
+  return (
+    <article
+      data-id={userId}
+      className={cn('TodoInfo', {
+        'TodoInfo--completed': completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {todo.user && <UserInfo user={todo.user} />}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,22 @@
-export const TodoList = () => {};
+import { TodoInfo } from '../TodoInfo';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+}
+
+interface TodoListProps {
+  todos: Todo[];
+}
+
+export const TodoList = ({ todos }: TodoListProps) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo todo={todo} key={todo.id} />
+      ))}
+    </section>
+  );
+};
