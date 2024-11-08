a53f34e4a2237aa40acc55cb9661bbdc76ccb6ba
Stas <staskapica93@gmail.com>
2024-11-09 00:30:12 +0100

    solution 0.1

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,32 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import React from 'react';
+import usersFromServer from '../../api/users';
+import { UserInfo } from '../UserInfo';
+
+type Togo = {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+};
+
+interface Props {
+  todo: Togo;
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const userFind = usersFromServer.find(person => person.id === todo.userId);
+
+  return (
+    <article
+      data-id={todo.id}
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      {userFind && <UserInfo user={userFind} />}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,23 @@
-export const TodoList = () => {};
+import React from 'react';
+import { TodoInfo } from '../TodoInfo';
+
+type Togo = {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+};
+
+interface Props {
+  todos: Togo[];
+}
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo todo={todo} key={todo.id} />
+      ))}
+    </section>
+  );
+};
