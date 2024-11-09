4907f2c98b587eb695ffdedf1c4007c58732b8d6
NazarHaida <nazar.haida.knm.2020@lpnu.ua>
2024-11-09 22:58:13 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,15 @@
-export const TodoInfo = () => {};
+import { UserInfo } from '../UserInfo';
+// import usersFromServer from './api/users';
+
+export const TodoInfo = ({ todo }) => {
+  return (
+    <article
+      data-id={todo.id}
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <UserInfo user={todo.user} key={todo.userId} />
+    </article>
+  );
+};
