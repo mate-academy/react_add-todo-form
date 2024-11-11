74e554e91b3a80df8d88b4e9c80f1e7bf6c8d829
Italo Magno <italomagno10@gmail.com>
2024-11-11 17:17:04 -0300

    finishing

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,16 @@
-export const TodoList = () => {};
+import { Todo, User } from '../../App';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+type TodoListProps = {
+  todos: (Todo & { user: User })[];
+};
+
+export const TodoList = ({ todos }: TodoListProps) => (
+  <>
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo key={todo.id} todo={todo} />
+      ))}
+    </section>
+  </>
+);
