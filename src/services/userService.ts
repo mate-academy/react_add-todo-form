0e574317ccc0bdf93cc51ee2dcc650043ba63b2e
Nadinnnka <tymoshyk02@gmail.com>
2024-11-22 14:24:50 +0200

    add solution

---
+++ src/services/userService.ts
@@ -0,0 +1,6 @@
+import users from '../api/users';
+import { User } from '../types/User';
+
+export function getUserById(userId: number): User | null {
+  return users.find(user => user.id === userId) || null;
+}
