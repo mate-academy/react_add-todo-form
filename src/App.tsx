3e96aad96ae13cefb04990be345ec5013d73206c
Nataliia Fedorchuk <natali.fedorchuk2211@gmail.com>
2024-12-05 17:06:03 +0200

    add some changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import todosFromServer from './api/todos';
 import { getUserById } from './services/user';
 import { getNewTodoId } from './services/id';
 
-export const initialTodos = todosFromServer.map(todo => ({
+export const initialTodos: Todo[] = todosFromServer.map(todo => ({
   ...todo,
   user: getUserById(todo.userId),
 }));
@@ -33,46 +33,3 @@ export const App: React.FC = () => {
     </div>
   );
 };
-
-// import './App.scss';
-// import { TodoList } from './components/TodoList';
-
-// import todosFromServer from './api/todos';
-// import React, { useState } from 'react';
-// import { getUserById } from './services/user';
-// import { TodoForm } from './components/TodoForm/TodoForm';
-// import { Todo } from './Types/Todo';
-
-// export const todos = todosFromServer.map(todo => ({
-//   ...todo,
-//   user: getUserById(todo.userId),
-// }));
-
-// function getNewTodoId(todos: Todo[]) {
-//   const maxId = Math.max(...todos.map(todo => todo.id));
-
-//   return maxId + 1;
-// }
-
-// export const App: React.FC<Todo> = () => {
-//   const [selectedTodo, setTodos] = useState<Todo[]>(todos);
-
-//   const addTodo = (todo: Todo) => {
-//     const newTodo = {
-//       ...todo,
-//       id: getNewTodoId(selectedTodo),
-//     };
-
-//     setTodos(currentTodos => [...currentTodos, newTodo]);
-//   };
-
-//   return (
-//     <div className="App">
-//       <h1>Add todo form</h1>
-
-//       <TodoForm onAdd={addTodo} />
-
-//       <TodoList todos={todos} />
-//     </div>
-//   );
-// };
