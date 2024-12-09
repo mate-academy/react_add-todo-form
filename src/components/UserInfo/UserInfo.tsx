213fe314d6e492f68fe3d05f99ddbb78515cbf80
RuslanMudruj <13072003.rm@gmail.com>
2024-12-10 00:32:06 +0200

    solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,19 @@
-export const UserInfo = () => {};
+import { IUser } from '../../interface/IUser';
+
+type Props = {
+  user: IUser | null;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  if (!user) {
+    return <p>No user information available.</p>;
+  }
+
+  const { name, email } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
