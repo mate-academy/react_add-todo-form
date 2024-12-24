eaaec8bed1fa87c3ce506e0b0516e4aee11ee8e4
sinner1993 <sergio1993goga@gmail.com>
2024-12-24 13:05:20 +0100

    asdasdada

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,17 @@
-export const TodoList = () => {};
+import { ToDo } from '../../types/types';
+import { TodoInfo } from '../TodoInfo';
+import React from 'react';
+
+type Props = {
+  todos: ToDo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map((todo, indx) => {
+        return <TodoInfo key={indx} todo={todo} />;
+      })}
+    </section>
+  );
+};
