ba34bcaafe0991e067eb83d9deba1459950c6dd1
dima <shymdima74@gmail.com>
2024-12-30 14:53:28 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,27 @@
-export const TodoInfo = () => {};
+import React from "react";
+import users from "../../api/users";
+
+import { todo, user } from "../types";
+import classNames from "classnames";
+import { UserInfo } from "../UserInfo";
+
+type Props = {
+  todo: todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+ 
+  return (
+    <article
+      data-id={todo.id}
+      className={classNames("TodoInfo", { "TodoInfo--completed": todo.completed })}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {todo.user ? (
+        <UserInfo user={todo.user} />
+      ) : (
+        <p className="TodoInfo__no-user">User not found</p>
+      )}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,14 @@
-export const TodoList = () => {};
+import { todo } from "../types";
+import { TodoInfo } from "../TodoInfo";
+type Props = {
+    todos: todo[]
+}
+
+export const TodoList: React.FC<Props> = ({todos}) => {
+    return (<section className="TodoList">
+        {todos.map(todo => [
+            <TodoInfo todo={todo}/>
+        ])}
+        
+      </section>)
+};
