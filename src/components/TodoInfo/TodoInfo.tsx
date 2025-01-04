2b3c7bdf46576e66db2c2e796be05d4cde5f1d6e
Artem Puzenko <a.puzenko2004@gmail.com>
2025-01-04 17:04:18 +0200

    add solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,32 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import classNames from 'classnames';
+
+type TodoProps = {
+  todo: {
+    id: number;
+    title: string;
+    completed: boolean;
+  };
+  user: {
+    id: number;
+    name: string;
+    email: string;
+  };
+};
+
+export const TodoInfo: React.FC<TodoProps> = ({ todo, user }) => {
+  return (
+    <article
+      data-id={todo.id}
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+        'TodoInfo--active': !todo.completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      <a className="UserInfo" href={`mailto:${user.email}`}>
+        {user.name}
+      </a>
+    </article>
+  );
+};
