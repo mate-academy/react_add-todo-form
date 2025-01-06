7e49e451cb09a5e80ca8c40fe5de3eecdac26555
Denys Kovalchuk <denkov414@gmail.com>
2025-01-06 22:51:13 +0200

    add task solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,37 @@
-export const TodoList = () => {};
+import { TodoInfo } from '../TodoInfo';
+
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+  user: {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+  } | null;
+};
+
+export const TodoList = ({ todos }: { todos: Todo[] }) => {
+  return (
+    <section className="TodoList">
+      {todos.map((todo: Todo) => {
+        if (!todo.user) {
+          return null;
+        }
+
+        return (
+          <TodoInfo
+            id={todo.id}
+            title={todo.title}
+            email={todo.user.email}
+            name={todo.user.name}
+            completed={todo.completed}
+            key={todo.id}
+          />
+        );
+      })}
+    </section>
+  );
+};
