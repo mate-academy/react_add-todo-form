8d7c712af641832f44b8a7001378e0a6ed9c4706
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2025-01-17 14:48:01 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,27 @@
-export const TodoInfo = () => {};
+import { UserInfo } from '../UserInfo';
+
+type User = {
+  name: string;
+  email: string;
+};
+
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+  user?: User;
+};
+
+export const TodoInfo = ({ todo }: { todo: Todo }) => {
+  return (
+    <section className="TodoList">
+      <article
+        data-id={todo.id}
+        className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+      >
+        <h2 className="TodoInfo__title">{todo.title}</h2>
+        {todo.user && <UserInfo user={todo.user} />}
+      </article>
+    </section>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,25 @@
-export const TodoList = () => {};
+import { TodoInfo } from '../TodoInfo';
+
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+  user?: {
+    name: string;
+    email: string;
+  };
+};
+
+type TodoListProps = {
+  todos: Todo[];
+};
+
+export const TodoList = ({ todos }: TodoListProps) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo todo={todo} key={todo.id} />
+    ))}
+  </section>
+);
+
+export default TodoList;
