eff0f07969d559676057b208fdb6b694d0e6d5fe
Vitalii Venher <vengervn@gmail.com>
2025-01-19 23:47:25 +0200

    add form validaton

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,38 @@ import './App.scss';
 import { TodoList } from './components/TodoList';
 import { Todos } from './Types/Todos';
 import { User } from './Types/User';
-import { AppForm } from './components/AppForm/AppForm';
+import { AppForm } from './components/AppForm';
 
 import usersFromServer from './api/users';
 import todosFromServer from './api/todos';
+import { useState } from 'react';
 
-const getUserById = (userId: number): User | null => {
+export const getUserById = (userId: number): User | null => {
   return usersFromServer.find(user => user.id === userId) || null;
 };
 
-const todos: Todos[] = todosFromServer.map(todo => ({
+export const maxId = (todos: Todos[]): number => {
+  const id = Math.max(...todos.map(todo => todo.id));
+
+  return id + 1;
+};
+
+const initialTodos: Todos[] = todosFromServer.map(todo => ({
   ...todo,
   user: getUserById(todo.userId),
 }));
 
 export const App = () => {
+  const [todos, setTodos] = useState(initialTodos);
+  const addTodo = (newTodo: Todos) => {
+    setTodos(prewTodo => [...prewTodo, newTodo]);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <AppForm />
+      <AppForm users={usersFromServer} onAdd={addTodo} />
       <TodoList todos={todos} />
     </div>
   );
--- src/components/AppForm/AppForm.tsx
+++ src/components/AppForm/AppForm.tsx
@@ -1,19 +1,76 @@
-export const AppForm = () => {
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { User } from '../../Types/User';
+import { maxId } from '../../App';
+import { getUserById } from '../../App';
+
+type Props = {
+  users: User[];
+  onAdd: (newTodo: any) => void;
+};
+
+type FormValue = {
+  title: string;
+  userId: string;
+};
+
+export const AppForm: React.FC<Props> = ({ users, onAdd }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormValue>();
+
+  const onSubmit: SubmitHandler<FormValue> = data => {
+    const newTodo = {
+      id: maxId,
+      title: data.title,
+      userId: Number(data.userId),
+      completed: false,
+      user: getUserById(+data.userId),
+    };
+
+    onAdd(newTodo);
+    reset();
+  };
+
   return (
-    <form action="/api/todos" method="POST">
+    <form action="/api/todos" method="POST" onSubmit={handleSubmit(onSubmit)}>
       <div className="field">
-        <input type="text" data-cy="titleInput" />
-        <span className="error">Please enter a title</span>
+        <label>
+          Title:
+          <input
+            type="text"
+            placeholder="Please enter a title"
+            data-cy="titleInput"
+            {...register('title', { required: 'Please enter a title' })}
+          />
+        </label>
+        {errors.title?.message && (
+          <span className="error">Please enter a title</span>
+        )}
       </div>
 
       <div className="field">
-        <select data-cy="userSelect">
-          <option value="0" disabled>
-            Choose a user
-          </option>
-        </select>
+        <label>
+          User:
+          <select
+            data-cy="userSelect"
+            {...register('userId', { required: 'Please choose a user' })}
+          >
+            <option value="0">Choose a user</option>
 
-        <span className="error">Please choose a user</span>
+            {users.map(user => (
+              <option value={user.id} key={user.id}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        {errors.userId?.message && (
+          <span className="error">Please choose a user</span>
+        )}
       </div>
 
       <button type="submit" data-cy="submitButton">
