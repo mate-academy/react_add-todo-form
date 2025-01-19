d3701944d019c6473d85f9a69f0a97c530790272
Vitalii Venher <vengervn@gmail.com>
2025-01-19 17:48:15 +0200

    add components

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,18 @@
-export const TodoInfo = () => {};
+import { UserInfo } from '../UserInfo';
+import { Todos } from '../../Types/Todos';
+
+type Props = {
+  todo: Todos;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const { title, id, user } = todo;
+
+  return (
+    <article data-id={id} className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
