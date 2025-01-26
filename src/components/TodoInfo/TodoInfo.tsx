6ea770cea0bc35db4464dc2348ec287f6b886561
Sasha <sashatsapko91@gmail.com>
2025-01-26 16:05:42 +0200

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,37 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import { UserInfo } from '../UserInfo';
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId?: number;
+  user: User | null;
+};
+
+type Props = {
+  todo: Todo;
+  users: User[];
+};
+
+export const TodoInfo = ({ todo }: Props) => {
+  const user = todo.user;
+
+  return (
+    <section className="TodoList">
+      <article
+        data-id={todo.id}
+        className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+      >
+        <h2 className="TodoInfo__title">{todo.title}</h2>
+        {user && <UserInfo user={user} />}
+      </article>
+    </section>
+  );
+};
