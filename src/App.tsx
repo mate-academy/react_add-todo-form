58b63514e3ad577a5d5679a1ee3fc02a59f2bc8b
Oleg <scor1@ukr.net>
2025-01-29 10:52:31 +0100

    solve

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,104 @@
-import './App.scss';
+/* eslint-disable max-len */
+/* eslint-disable padding-line-between-statements */
+/* eslint-disable prefer-const */
+/* eslint-disable import/extensions */
+/* eslint-disable prettier/prettier */
+// import './App.scss';
 
 // import usersFromServer from './api/users';
 // import todosFromServer from './api/todos';
 
-export const App = () => {
+// export const App = () => {
+//   return (
+//     <div className="App">
+//       <h1>Add todo form</h1>
+
+//       <form action="/api/todos" method="POST">
+//         <div className="field">
+//           <input type="text" data-cy="titleInput" />
+//           <span className="error">Please enter a title</span>
+//         </div>
+
+//         <div className="field">
+//           <select data-cy="userSelect">
+//             <option value="0" disabled>
+//               Choose a user
+//             </option>
+//           </select>
+
+//           <span className="error">Please choose a user</span>
+//         </div>
+
+//         <button type="submit" data-cy="submitButton">
+//           Add
+//         </button>
+//       </form>
+
+//       <section className="TodoList">
+//         <article data-id="1" className="TodoInfo TodoInfo--completed">
+//           <h2 className="TodoInfo__title">delectus aut autem</h2>
+
+//           <a className="UserInfo" href="mailto:Sincere@april.biz">
+//             Leanne Graham
+//           </a>
+//         </article>
+
+//         <article data-id="15" className="TodoInfo TodoInfo--completed">
+//           <h2 className="TodoInfo__title">delectus aut autem</h2>
+
+//           <a className="UserInfo" href="mailto:Sincere@april.biz">
+//             Leanne Graham
+//           </a>
+//         </article>
+
+//         <article data-id="2" className="TodoInfo">
+//           <h2 className="TodoInfo__title">
+//             quis ut nam facilis et officia qui
+//           </h2>
+
+//           <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
+//             Patricia Lebsack
+//           </a>
+//         </article>
+//       </section>
+//     </div>
+//   );
+// };
+import React, { useState } from 'react';
+import './App.scss';
+import { TodoList } from './components/TodoList';
+import { TodoForm } from './components/TodoForm';
+import { Todo, User } from './components/types';
+
+const usersFromServer: User[] = [
+  { id: 1, name: 'Leanne Graham', username: 'Bret', email: 'Sincere@april.biz' },
+  { id: 2, name: 'Ervin Howell', username: 'Antonette', email: 'Shanna@melissa.tv' },
+  // ... add more users as needed
+];
+
+const todosFromServer: Todo[] = [
+  { id: 1, title: 'delectus aut autem', completed: true, userId: 1 },
+  { id: 15, title: 'some other todo', completed: false, userId: 1 },
+  { id: 2, title: 'quis ut nam facilis et officia qui', completed: false, userId: 4 },
+];
+
+export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>(todosFromServer);
+  const [users] = useState<User[]>(usersFromServer);
+
+  const addTodo = (newTodo: Todo) => {
+    setTodos(prevTodos => [...prevTodos, newTodo]);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
-
-      <form action="/api/todos" method="POST">
-        <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
-        </div>
-
-        <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>
-              Choose a user
-            </option>
-          </select>
-
-          <span className="error">Please choose a user</span>
-        </div>
-
-        <button type="submit" data-cy="submitButton">
-          Add
-        </button>
-      </form>
-
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoForm users={users} onAddTodo={addTodo} todos={todos} />
+      <TodoList todos={todos} users={users} />
     </div>
   );
 };
+
+export default App;
+
+
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,33 @@
-export const TodoList = () => {};
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Todo, User } from '../types';
+
+interface TodoListProps {
+  todos: Todo[];
+  users: User[];
+}
+
+export const TodoList: React.FC<TodoListProps> = ({ todos, users }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => {
+        const user = users.find(u => u.id === todo.userId);
+        
+        return (
+          <article
+            key={todo.id}
+            data-id={todo.id}
+            className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+          >
+            <h2 className="TodoInfo__title">{todo.title}</h2>
+            {user && (
+              <a className="UserInfo" href={`mailto:${user.email}`}>
+                {user.name}
+              </a>
+            )}
+          </article>
+        );
+      })}
+    </section>
+  );
+};
