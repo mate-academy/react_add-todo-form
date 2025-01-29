21ee95e129be6d31220d40d5fc564117f60a2a2b
Mykola Melnichuk <nikolfutdeveloper@gmail.com>
2025-01-29 02:53:07 +0100

    made solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,22 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { TodoType, UserType } from '../TodoList';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: TodoType;
+  user?: UserType;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <article
+    data-id={todo.id}
+    key={todo.id}
+    className={classNames('TodoInfo', {
+      'TodoInfo--completed': todo.completed,
+    })}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    <UserInfo user={todo.user} />
+  </article>
+);
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,33 @@
-export const TodoList = () => {};
+import { TodoInfo } from '../TodoInfo';
+
+export type TodoType = {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+  user?: UserType;
+};
+
+export type UserType = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
+
+type Props = {
+  todos: TodoType[];
+  users: UserType[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos, users }) => (
+  <section className="TodoList">
+    {todos.map((todo: TodoType) => {
+      const foundedUser = users?.find((user: UserType) => user.id === todo.userId);
+
+      const newTodo = { ...todo, user: foundedUser };
+
+      return <TodoInfo todo={newTodo} key={todo.id} />;
+    })}
+  </section>
+);
