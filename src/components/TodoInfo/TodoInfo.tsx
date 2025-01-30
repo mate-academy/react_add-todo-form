799961507138554bda32aaf1727fd035a01b96e2
Dominik Dudek <dom-dud774@o2.pl>
2025-01-30 17:42:37 +0100

    Task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,22 @@
-export const TodoInfo = () => {};
+import classNames from 'classnames';
+import { Todo } from '../../App';
+
+interface TodoInfoProps {
+  todo: Todo;
+}
+export const TodoInfo: React.FC<TodoInfoProps> = ({ todo }) => {
+  const todoClass = classNames({
+    TodoInfo: true,
+    'TodoInfo--completed': todo.completed,
+  });
+
+  return (
+    <article data-id={todo.id} className={todoClass}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <a className="UserInfo" href={`mailto:${todo.user?.email}`}>
+        {todo.user?.name}
+      </a>
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,17 @@
-export const UserInfo = () => {};
+import { User } from '../../App';
+
+interface UserInfoProps {
+  user: User | null;
+}
+
+export const UserInfo: React.FC<UserInfoProps> = user => {
+  if (!user) {
+    return <span>No user available</span>;
+  }
+
+  return (
+    <a className="UserInfo" href={`mailto:${user.user?.email}`}>
+      {user.user?.name}
+    </a>
+  );
+};
