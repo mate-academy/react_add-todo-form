f531715a2b8b35e4b92a702bf15d2ee1ed106299
DavidKuznets <davidkuznets7@gmail.com>
2025-02-03 10:51:07 +0200

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -25,9 +25,7 @@ export const TodoInfo: React.FC<TodoInfoProps> = ({ todo }) => {
       data-id={todo.id}
       className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
     >
-      <h2 className="TodoInfo__title" data-cy="titleInput">
-        {todo.title}
-      </h2>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
       {todo.user && <UserInfo user={todo.user} />}
       {}
     </article>
