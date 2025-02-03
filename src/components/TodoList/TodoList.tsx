38cbf3b79eb4235cae4b9383606c2b65d8e91653
Dionimar Discion <discion.dionimar@gmail.com>
2025-02-03 13:18:05 +0800

    develop

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -12,28 +12,5 @@ export const TodoList: React.FC<Props> = ({ todos }) => (
     {todos.map(todo => (
       <TodoInfo key={todo.id} todo={todo} />
     ))}
-    {/* <article data-id="1" className="TodoInfo TodoInfo--completed">
-      <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-      <a className="UserInfo" href="mailto:Sincere@april.biz">
-        Leanne Graham
-      </a>
-    </article>
-
-    <article data-id="15" className="TodoInfo TodoInfo--completed">
-      <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-      <a className="UserInfo" href="mailto:Sincere@april.biz">
-        Leanne Graham
-      </a>
-    </article>
-
-    <article data-id="2" className="TodoInfo">
-      <h2 className="TodoInfo__title">quis ut nam facilis et officia qui</h2>
-
-      <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-        Patricia Lebsack
-      </a>
-    </article> */}
   </section>
 );
