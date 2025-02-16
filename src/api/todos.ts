434ba1a0402644630a678035541f81d54effab9c
tiblazy <hauntedapply@gmail.com>
2025-02-16 02:05:12 -0300

    solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,16 +1,6 @@
 export default [
-  {
-    id: 1,
-    title: 'delectus aut autem',
-    completed: true,
-    userId: 1,
-  },
-  {
-    id: 15,
-    title: 'some other todo',
-    completed: false,
-    userId: 1,
-  },
+  { id: 1, title: 'delectus aut autem', completed: true, userId: 1 },
+  { id: 15, title: 'some other todo', completed: false, userId: 1 },
   {
     id: 2,
     title: 'quis ut nam facilis et officia qui',
