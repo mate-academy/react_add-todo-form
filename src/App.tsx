2e7e5a68028ed05cf13b9c2a7cdbdfc958f9ce40
Yehor <y.krapivka18@gmail.com>
2025-02-21 10:47:44 -0500

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,84 @@
+import { useState } from 'react';
 import './App.scss';
-
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import todosFromServer from './api/todos';
+import users from './api/users';
+import { TodoList } from './components/TodoList';
 
 export const App = () => {
+  const [userChosen, setUserChosen] = useState(0);
+  const [title, setTitle] = useState('');
+  const [todos, setTodos] = useState(todosFromServer);
+  const [errorTitle, setErrorTitle] = useState(false);
+  const [errorSelect, setErrorSelect] = useState(false);
+  const id = Math.max(...todos.map(item => item.id)) + 1;
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setErrorSelect(userChosen === 0);
+    setErrorTitle(!title);
+
+    if (!title || userChosen === 0) {
+      return;
+    }
+
+    setTodos(initialTodos => [
+      ...initialTodos,
+      { id: id, title: title, completed: false, userId: userChosen },
+    ]);
+    setTitle('');
+    setUserChosen(0);
+  }
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/todos" method="POST">
+      <form action="/api/todos" method="POST" onSubmit={handleSubmit}>
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="titleInput">Title: </label>
+          <input
+            type="text"
+            id="titleInput"
+            data-cy="titleInput"
+            placeholder="Enter a title"
+            value={title}
+            onChange={ev => {
+              setTitle(ev.target.value);
+              setErrorTitle(false);
+            }}
+          />
+          {errorTitle && <span className="error">Please enter a title</span>}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
-            <option value="0" disabled>
+          <label htmlFor="userSelect">User: </label>
+          <select
+            data-cy="userSelect"
+            value={userChosen}
+            id="userSelect"
+            onChange={ev => {
+              setUserChosen(+ev.target.value);
+              setErrorSelect(false);
+            }}
+          >
+            <option value={0} disabled={userChosen !== 0}>
               Choose a user
             </option>
+            {users.map(user => (
+              <option value={user.id} key={user.id}>
+                {user.name}
+              </option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {errorSelect && <span className="error">Please choose a user</span>}
         </div>
 
         <button type="submit" data-cy="submitButton">
           Add
         </button>
       </form>
-
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={todos} />
     </div>
   );
 };
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,25 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import { Todo } from '../TodoList';
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo';
+import users from '../../api/users';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const user = users.find(us => us.id === todo.userId);
+
+  return (
+    <article
+      data-id={todo.id}
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      <UserInfo user={user} />
+    </article>
+  );
+};
